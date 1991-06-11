83a57513860d30d5a9c8e0078a1b7ecf447a3c88
Roland McGrath <roland@gnu.org>
1991-06-11 01:20:23 +0000

    entered into RCS

--- sysdeps/unix/bsd/tahoe/sysdep.h
+++ sysdeps/unix/bsd/tahoe/sysdep.h
@@ -1,22 +1,5 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
-This file is part of the GNU C Library.
-
-The GNU C Library is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 1, or (at your option)
-any later version.
-
-The GNU C Library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with the GNU C Library; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
-
 /* The Tahoe is just like the Vax, except the
    `chmk' instruction is called `kcall'.  */
 
-#define	__MAGIC_INSN	"kcall"
-#include "../sysdeps/unix/bsd/vax/sysdep.h"
+#define	chmk	kcall
+#include <sysdeps/unix/bsd/vax/sysdep.h>
