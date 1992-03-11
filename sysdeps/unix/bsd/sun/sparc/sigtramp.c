1ab822933f98a2ba77a3c439382768585a54c4d6
Roland McGrath <roland@gnu.org>
1992-03-11 23:09:31 +0000

    Formerly unix/bsd/sun/sparc/sigtramp.c.~2~

--- sysdeps/unix/bsd/sun/sparc/sigtramp.c
+++ sysdeps/unix/bsd/sun/sparc/sigtramp.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -17,6 +17,10 @@ not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
 #include <ansidecl.h>
+
+#ifndef	__GNUC__
+  #error This file uses GNU C extensions; you must compile with GCC.
+#endif
 
 /* Get the definition of `struct sigcontext'.  */
 #define	KERNEL
