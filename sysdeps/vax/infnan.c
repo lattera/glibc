e419b460b0668d8076e83faca36d1a8a1663dcac
Roland McGrath <roland@gnu.org>
1992-03-15 22:51:19 +0000

    entered into RCS

--- sysdeps/vax/infnan.c
+++ sysdeps/vax/infnan.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -17,7 +17,7 @@ not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
 #ifndef	__GNUC__
-#include <../sysdeps/generic/infnan.c>
+  #error This file uses GNU C extensions; you must compile with GCC.
 #else
 
 #include <ansidecl.h>
