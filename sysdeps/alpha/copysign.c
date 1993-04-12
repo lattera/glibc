f63f39a68625449e60dd59c0ba9a123cb474b4a8
Roland McGrath <roland@gnu.org>
1993-04-12 00:49:08 +0000

    Formerly alpha/__copysign.c.~2~

--- sysdeps/alpha/copysign.c
+++ sysdeps/alpha/copysign.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -18,7 +18,7 @@ Cambridge, MA 02139, USA.  */
 
 #include <math.h>
 
-/* Return X with its signed changed to Y's.  */
+/* Return X with its sign changed to Y's.  */
 double
 __copysign (double x, double y)
 {
