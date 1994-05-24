5d3fde1a0211e08ba21f929e067514529ae3b1b4
Roland McGrath <roland@gnu.org>
1994-05-24 05:56:11 +0000

    Formerly unix/sysv/sysv4/i386/sysdep.h.~2~

--- sysdeps/unix/sysv/sysv4/i386/sysdep.h
+++ sysdeps/unix/sysv/sysv4/i386/sysdep.h
@@ -16,7 +16,7 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <sysdeps/unix/i386/sysdep.h>
+#include <sysdeps/unix/sysv/i386/sysdep.h>
 
 /* In SVR4 some system calls can fail with the error ERESTART,
    and this means the call should be retried.  */
