eacddc0a20f6fa71fff1a4bba4bf91497193680b
Roland McGrath <roland@gnu.org>
1994-11-08 23:34:47 +0000

    entered into RCS

--- sysdeps/mach/hurd/alpha/longjmp-ts.c
+++ sysdeps/mach/hurd/alpha/longjmp-ts.c
@@ -1,5 +1,5 @@
-/* Perform a `longjmp' on a `struct sigcontext'.  Alpha version.
-Copyright (C) 1994 Free Software Foundation, Inc.
+/* Perform a `longjmp' on a Mach thread_state.  Alpha version.
+Copyright (C) 1991, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -17,22 +17,25 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <setjmp.h>
 #include <hurd/signal.h>
-#include <string.h>
+#include <setjmp.h>
+#include <mach/thread_status.h>
+
+
+/* Set up STATE to do the equivalent of `longjmp (ENV, VAL);'.  */
 
 void
-_hurd_longjmp_sigcontext (struct sigcontext *scp, jmp_buf env, int retval)
+_hurd_longjmp_thread_state (void *state, jmp_buf env, int val)
 {
-  memset (scp, 0, sizeof (*scp));
-  scp->sc_regs[9] = env[0].__9;
-  scp->sc_regs[11] = env[0].__11;
-  scp->sc_regs[12] = env[0].__12;
-  scp->sc_regs[13] = env[0].__13;
-  scp->sc_regs[14] = env[0].__14;
-  scp->sc_regs[15] = (long int) env[0].__fp;
-  scp->sc_regs[30] = (long int) env[0].__sp;
-  scp->sc_pc = (long int) env[0].__pc;
+  struct alpha_thread_state *ts = state;
 
-  memcpy (&scp->sc_fpregs[2], &env[0].__f2, sizeof (double));
+  ts->r9 = env[0].__9;
+  ts->r11 = env[0].__11;
+  ts->r12 = env[0].__12;
+  ts->r13 = env[0].__13;
+  ts->r14 = env[0].__14;
+  ts->r15 = (long int) env[0].__fp;
+  ts->r30 = (long int) env[0].__sp;
+  ts->pc = (long int) env[0].__pc;
+  ts->r0 = val ?: 1;
 }
