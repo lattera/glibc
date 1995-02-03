37da91a00d7295b56dc889dc2a0bc72be97b413a
Roland McGrath <roland@gnu.org>
1995-02-03 23:15:48 +0000

    Use `long int' for sigcode.

--- sysdeps/mach/hurd/mips/exc2signal.c
+++ sysdeps/mach/hurd/mips/exc2signal.c
@@ -1,5 +1,5 @@
 /* Translate Mach exception codes into signal numbers.  MIPS version.
-Copyright (C) 1991, 1992, 1994 Free Software Foundation, Inc.
+Copyright (C) 1991, 1992, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -26,7 +26,7 @@ Cambridge, MA 02139, USA.  */
 
 void
 _hurd_exception2signal (int exception, int code, int subcode,
-			int *signo, int *sigcode, int *error)
+			int *signo, long int *sigcode, int *error)
 {
   *error = 0;
 
