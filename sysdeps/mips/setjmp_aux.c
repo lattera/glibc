a4b6477451f4147299aa953c9f9d5917ff94e00c
Roland McGrath <roland@gnu.org>
1995-02-03 23:29:20 +0000

    Use ENV[0].__jmpbuf[0].

--- sysdeps/mips/setjmp_aux.c
+++ sysdeps/mips/setjmp_aux.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1994, 1995 Free Software Foundation, Inc.
    Contributed by Brendan Kehoe (brendan@zen.org).
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -27,40 +27,38 @@ int
 __sigsetjmp_aux (jmp_buf env, int savemask, int sp, int fp)
 {
   /* Store the floating point callee-saved registers...  */
-  asm volatile ("s.d $f20, %0" : : "m" (env[0].__fpregs[0]));
-  asm volatile ("s.d $f22, %0" : : "m" (env[0].__fpregs[1]));
-  asm volatile ("s.d $f24, %0" : : "m" (env[0].__fpregs[2]));
-  asm volatile ("s.d $f26, %0" : : "m" (env[0].__fpregs[3]));
-  asm volatile ("s.d $f28, %0" : : "m" (env[0].__fpregs[4]));
-  asm volatile ("s.d $f30, %0" : : "m" (env[0].__fpregs[5]));
+  asm volatile ("s.d $f20, %0" : : "m" (env[0].__jmpbuf[0].__fpregs[0]));
+  asm volatile ("s.d $f22, %0" : : "m" (env[0].__jmpbuf[0].__fpregs[1]));
+  asm volatile ("s.d $f24, %0" : : "m" (env[0].__jmpbuf[0].__fpregs[2]));
+  asm volatile ("s.d $f26, %0" : : "m" (env[0].__jmpbuf[0].__fpregs[3]));
+  asm volatile ("s.d $f28, %0" : : "m" (env[0].__jmpbuf[0].__fpregs[4]));
+  asm volatile ("s.d $f30, %0" : : "m" (env[0].__jmpbuf[0].__fpregs[5]));
 
   /* .. and the PC;  */
-  asm volatile ("sw $31, %0" : : "m" (env[0].__pc));
+  asm volatile ("sw $31, %0" : : "m" (env[0].__jmpbuf[0].__pc));
 
   /* .. and the stack pointer;  */
-  asm volatile ("sw %1, %0" : : "m" (env[0].__sp), "r" (sp));
+  env[0].__jmpbuf[0].__sp = sp;
 
   /* .. and the FP; it'll be in s8. */
-  asm volatile ("sw %1, %0" : : "m" (env[0].__fp), "r" (fp));
+  env[0].__jmpbuf[0].__fp = fp;
 
   /* .. and the GP; */
-  asm volatile ("sw $gp, %0" : : "m" (env[0].__gp));
+  asm volatile ("sw $gp, %0" : : "m" (env[0].__jmpbuf[0].__gp));
 
   /* .. and the callee-saved registers; */
-  asm volatile ("sw $16, %0" : : "m" (env[0].__regs[0]));
-  asm volatile ("sw $17, %0" : : "m" (env[0].__regs[1]));
-  asm volatile ("sw $18, %0" : : "m" (env[0].__regs[2]));
-  asm volatile ("sw $19, %0" : : "m" (env[0].__regs[3]));
-  asm volatile ("sw $20, %0" : : "m" (env[0].__regs[4]));
-  asm volatile ("sw $21, %0" : : "m" (env[0].__regs[5]));
-  asm volatile ("sw $22, %0" : : "m" (env[0].__regs[6]));
-  asm volatile ("sw $23, %0" : : "m" (env[0].__regs[7]));
+  asm volatile ("sw $16, %0" : : "m" (env[0].__jmpbuf[0].__regs[0]));
+  asm volatile ("sw $17, %0" : : "m" (env[0].__jmpbuf[0].__regs[1]));
+  asm volatile ("sw $18, %0" : : "m" (env[0].__jmpbuf[0].__regs[2]));
+  asm volatile ("sw $19, %0" : : "m" (env[0].__jmpbuf[0].__regs[3]));
+  asm volatile ("sw $20, %0" : : "m" (env[0].__jmpbuf[0].__regs[4]));
+  asm volatile ("sw $21, %0" : : "m" (env[0].__jmpbuf[0].__regs[5]));
+  asm volatile ("sw $22, %0" : : "m" (env[0].__jmpbuf[0].__regs[6]));
+  asm volatile ("sw $23, %0" : : "m" (env[0].__jmpbuf[0].__regs[7]));
 
   /* .. and finally get and reconstruct the floating point csr.  */
-  asm ("cfc1 %0, $31" : "=r" (env[0].__fpc_csr));
+  asm ("cfc1 %0, $31" : "=r" (env[0].__jmpbuf[0].__fpc_csr));
 
   /* Save the signal mask if requested.  */
-  __sigjmp_save (env, savemask);
-
-  return 0;
+  return __sigjmp_save (env, savemask);
 }
