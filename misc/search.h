30e77772456f3d31373bf1bd002af5b498496e08
Roland McGrath <roland@gnu.org>
1995-03-01 06:28:14 +0000

    Wed Mar  1 00:57:47 1995  Roland McGrath  <roland@churchy.gnu.ai.mit.edu>
    	* misc/search.h: New file.
    	* misc/Makefile (headers): Add search.h.
    	* misc/insremque.c: Include search.h.
    	(struct qelem): Type removed.

--- misc/search.h
+++ misc/search.h
@@ -1,4 +1,5 @@
-/* Copyright (C) 1992, 1995 Free Software Foundation, Inc.
+/* search.h -- declarations for `insque' and `remque'
+Copyright (C) 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -16,34 +17,33 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <ansidecl.h>
-#include <stddef.h>
-#include <search.h>
+/* These functions are provided for compatibility with BSD.  */
+
+#ifndef _SEARCH_H
+#define	_SEARCH_H 1
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+/* Prototype structure for a linked-list data structure.
+   This is the type used by the `insque' and `remque' functions.  */
+
+struct qelem
+  {
+    struct qelem *q_forw;
+    struct qelem *q_back;
+    char q_data[1];
+  };
+
 
 /* Insert ELEM into a doubly-linked list, after PREV.  */
-
-void
-DEFUN(insque, (elem, prev) ,
-      struct qelem *elem AND struct qelem *prev)
-{
-  struct qelem *next = prev->q_forw;
-  prev->q_forw = elem;
-  if (next != NULL)
-    next->q_back = elem;
-  elem->q_forw = next;
-  elem->q_back = prev;
-}
+extern void insque __P ((struct qelem *__elem, struct qelem *__prev));
 
 /* Unlink ELEM from the doubly-linked list that it is in.  */
+extern void remque __P ((struct qelem *elem))
 
-void
-DEFUN(remque, (elem),
-      struct qelem *elem)
-{
-  struct qelem *next = elem->q_forw;
-  struct qelem *prev = elem->q_back;
-  if (next != NULL)
-    next->q_back = prev;
-  if (prev != NULL)
-    prev->q_forw = next;
-}
+
+__END_DECLS
+
+#endif	/* search.h */
