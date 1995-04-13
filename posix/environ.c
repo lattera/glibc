6432a7793f6be8d3b6840e47e752103beaca70c9
Roland McGrath <roland@gnu.org>
1995-04-13 09:00:11 +0000

    Wed Apr 12 23:27:22 1995  Roland McGrath  <roland@churchy.gnu.ai.mit.edu>
    	* posix/environ.c: Add weak alias `_environ'.

--- posix/environ.c
+++ posix/environ.c
@@ -6,3 +6,7 @@
 /* This must be initialized; we cannot have a weak alias into bss.  */
 char **__environ = NULL;
 weak_alias (__environ, environ)
+
+/* The SVR4 ABI says `_environ' will be the name to use
+   in case the user overrides the weak alias `environ'.  */
+weak_alias (__environ, _environ)
