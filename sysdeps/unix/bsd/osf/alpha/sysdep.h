b2531dea3aa1021f9f4cefee3ba4a3b60307af26
Brendan Kehoe <brendan@zen.org>
1995-04-15 01:56:26 +0000

    * sysdeps/unix/bsd/osf/alpha/sysdep.h (NO_UNDERSCORES): Define.

--- sysdeps/unix/bsd/osf/alpha/sysdep.h
+++ sysdeps/unix/bsd/osf/alpha/sysdep.h
@@ -16,6 +16,9 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
+/* OSF/1 does not precede the asm names of C symbols with a `_'. */
+#define	NO_UNDERSCORES
+
 #include <sysdeps/unix/sysdep.h>
 
 #ifdef	ASSEMBLER
