0fb807c1dbb08de4408c440c2db490b52bd16f57
Roland McGrath <roland@gnu.org>
1995-05-05 14:36:22 +0000

    Add weak alias to non-__ name.

--- mach/mig-alloc.c
+++ mach/mig-alloc.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -26,3 +26,5 @@ __mig_allocate (vm_address_t *addr,
   if (__vm_allocate (__mach_task_self (), addr, size, 1) != KERN_SUCCESS)
     *addr = 0;
 }
+
+weak_alias (__mig_allocate, mig_allocate)
