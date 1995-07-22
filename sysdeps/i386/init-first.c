0bf9d0eb2bc40ee304e8ff0315112cde711fa1b2
Roland McGrath <roland@gnu.org>
1995-07-22 05:16:58 +0000

    Removed gratuitous #include <hurd.h>.

--- sysdeps/i386/init-first.c
+++ sysdeps/i386/init-first.c
@@ -17,7 +17,6 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <hurd.h>
 #include <unistd.h>
 
 extern void __libc_init (int, char **, char **);
