24e0f77bc6dd89d5f6be70444f16f029f28f2661
Roland McGrath <roland@gnu.org>
1995-07-22 11:17:58 +0000

    Removed stub warning.

--- sysdeps/stub/init-posix.c
+++ sysdeps/stub/init-posix.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -16,23 +16,12 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <ansidecl.h>
-
 #ifndef	HAVE_GNU_LD
 
 void
-DEFUN_VOID(__init_posix)
+__init_posix ()
 {
   return;
 }
 
 #endif
-
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__init_posix);
-
-#endif	/* GNU stabs.  */
