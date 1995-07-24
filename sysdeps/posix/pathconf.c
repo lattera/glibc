51813e8065f8b03c09ceca85eaada73645096d26
Roland McGrath <roland@gnu.org>
1995-07-24 07:14:51 +0000

    Mon Jul 24 03:13:16 1995  Roland McGrath  <roland@churchy.gnu.ai.mit.edu>
    	* sysdeps/posix/pathconf.c: New file.

---
+++ sysdeps/posix/pathconf.c
@@ -0,0 +1,40 @@
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <errno.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/* Get file-specific information about PATH.  */
+long int
+__pathconf (const char *path, int name)
+{
+  int fd = open (path, 0);
+  if (fd >= 0)
+    {
+      long int value = __fpathconf (0, name);
+      int save = errno;
+      (void) close (fd);
+      errno = save;
+      return value;
+    }
+  return -1L;
+}
+
+weak_alias (__pathconf, pathconf)
