f0b11018358086848fe3b141a9520e1c6128211b
Roland McGrath <roland@gnu.org>
1996-01-30 10:00:24 +0000

    Mon Jan 29 21:53:40 1996  Roland McGrath  <roland@churchy.gnu.ai.mit.edu>
    	* sysdeps/stub/msync.c (msync): Declare third arg FLAGS.
    
    	* resolv/Makefile (+cflags): Append to this instead of CFLAGS.
    
    Mon Jan 29 16:29:16 1996  Miles Bader  <miles@gnu.ai.mit.edu>
    
    	* sysdeps/mach/hurd/select.c (__select): Recalculate GOT to
     	include an increment for each operation allowed on each fd.
    
    Mon Jan 29 11:44:38 1996  Roland McGrath  <roland@churchy.gnu.ai.mit.edu>
    
    	* sysdeps/unix/sysv/linux/configure: Set $inhibit_glue.
    
    	* Makefile: Use -include for sysd-dirs.
    
    	* configure.in (uname): Check for uname in syscalls.list files.
    	* sysdeps/unix/sysv/syscalls.list: Remove uname.
    	* sysdeps/unix/syscalls.list: Add uname.
    
    	* stdlib/strtol.c (weak1): New macro, to get the right name declared
    	weak.
    
    	* sysdeps/posix/getcwd.c (size_t): Don't define #ifdef __GNU_LIBRARY__.
    
    	* db/hash/ndbm.c: Change all uses of `errno' struct member to `errnum'.
    	(dbm_open): Use dynamic allocation for file name buffer, instead of
    	limitting its size to MAXPATHLEN.
    
    	* db/btree/bt_open.c (tmp): Use dynamic allocation for file name
    	buffer, instead of limitting its size to MAXPATHLEN.
    
    	* db/hash/hash.h (HTAB): Rename member `errno' to `errnum'.
    	* db/hash/hash.c: Change all uses of `errno' struct member to `errnum'.

--- sysdeps/stub/msync.c
+++ sysdeps/stub/msync.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1994, 1995 Free Software Foundation, Inc.
+/* Copyright (C) 1994, 1995, 1996 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -25,7 +25,7 @@ Cambridge, MA 02139, USA.  */
    unpredictable before this is done.  */
 
 int
-msync (caddr_t addr, size_t len)
+msync (caddr_t addr, size_t len, int flags)
 {
   errno = ENOSYS;
   return -1;
