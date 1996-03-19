4e4e7fca8e8b2c68665b6864d1757fca20fbacf3
Roland McGrath <roland@gnu.org>
1996-03-19 19:52:39 +0000

    Wed Feb 14 00:21:17 1996  David Mosberger-Tang  <davidm@azstarnet.com>
    	* sysdeps/unix/bsd/osf/alpha/sysdep.h: include
     	sysdeps/unix/alpha/sysdep.h and removed definitions now in that file.

--- sysdeps/unix/bsd/osf/alpha/sysdep.h
+++ sysdeps/unix/bsd/osf/alpha/sysdep.h
@@ -19,58 +19,11 @@ Cambridge, MA 02139, USA.  */
 /* OSF/1 does not precede the asm names of C symbols with a `_'. */
 #define	NO_UNDERSCORES
 
-#include <sysdeps/unix/sysdep.h>
+#include <sysdeps/unix/alpha/sysdep.h>
 
-#ifdef	ASSEMBLER
+#ifdef ASSEMBLER
 
 #include <machine/pal.h>		/* get PAL_callsys */
 #include <regdef.h>
 
-#ifdef __STDC__
-#define ENTRY(name) \
-  .globl name;								      \
-  .ent name,0;								      \
-  name##:;								      \
-  .frame sp,0,ra
-#else
-#define ENTRY(name) \
-  .globl name;								      \
-  .ent name,0;								      \
-  name/**/:;								      \
-  .frame sp,0,ra
 #endif
-
-#ifdef __STDC__
-#define PSEUDO(name, syscall_name, args) \
-  ENTRY(name);								      \
-  ldiq v0, SYS_##syscall_name;						      \
-  .set noat;							    	      \
-  call_pal PAL_callsys;							      \
-  .set at;							    	      \
-  beq a3, 10f;								      \
-  br gp, 20f;								      \
-20:;									      \
-  ldgp gp, 0(gp);							      \
-  jmp zero, syscall_error;						      \
-10:
-#else
-#define PSEUDO(name, syscall_name, args) \
-  ENTRY(name);								      \
-  ldiq v0, SYS_/**/syscall_name;					      \
-  .set noat;							    	      \
-  call_pal PAL_callsys;							      \
-  .set at;							    	      \
-  beq a3, 10f;								      \
-  br gp, 20f;								      \
-20:;									      \
-  ldgp gp, 0(gp);							      \
-  jmp zero, syscall_error;						      \
-10:
-#endif
-
-#define ret		ret zero,(ra),1
-#define r0		v0
-#define r1		a4
-#define MOVE(x,y)	mov x, y
-
-#endif	/* ASSEMBLER */
