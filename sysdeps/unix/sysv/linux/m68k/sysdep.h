1bf8715604009306146921de4c53cb1dcde2a1c5
Roland McGrath <roland@gnu.org>
1996-03-25 09:56:36 +0000

    Wed Mar 20 20:08:46 1996  Andreas Schwab  <schwab@issan.informatik.uni-dortmund.de>
    	* sysdeps/unix/sysv/linux/m68k/sysdep.h: Don't define
    	NO_UNDERSCORES.

--- sysdeps/unix/sysv/linux/m68k/sysdep.h
+++ sysdeps/unix/sysv/linux/m68k/sysdep.h
@@ -18,9 +18,6 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-/* In the Linux/ELF world, C symbols are asm symbols.  */
-#define NO_UNDERSCORES
-
 #include <sysdeps/unix/sysdep.h>
 
 /* For Linux we can use the system call table in the header file
