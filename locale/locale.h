a141dd342d3fc7590a5cc55cdeab6d702ed31790
Roland McGrath <roland@gnu.org>
1996-03-28 19:34:47 +0000

    * locale/locale.h (LC_*): Values reordered to match Linux libc.

--- locale/locale.h
+++ locale/locale.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1995 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1995, 1996 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -29,12 +29,12 @@ __BEGIN_DECLS
 
 /* These are the possibilities for the first argument to setlocale.
    The code assumes that LC_ALL is the highest value, and zero the lowest.  */
-#define	LC_COLLATE	0
-#define	LC_CTYPE	1
-#define	LC_MONETARY	2
-#define	LC_NUMERIC	3
-#define	LC_TIME		4
-#define	LC_MESSAGES	5
+#define LC_CTYPE        0
+#define LC_NUMERIC      1
+#define LC_TIME         2
+#define LC_COLLATE      3
+#define LC_MONETARY     4
+#define LC_MESSAGES     5
 #define	LC_ALL		6
 
 
