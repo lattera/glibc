a1a5fa67c6df00e4e7b230a6dae301b6bbf89c78
Miles Bader <miles@gnu.org>
1996-04-08 23:35:57 +0000

    (__gettimeofday):   Don't fail if the user passes in TZ, just zero it (emacs passes in a dummy     variable, and rms says the hurd should be the one to change).

--- sysdeps/mach/gettimeofday.c
+++ sysdeps/mach/gettimeofday.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1995 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1995, 1996 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -31,10 +31,7 @@ DEFUN(__gettimeofday, (tv, tz),
   kern_return_t err;
 
   if (tz != NULL)
-    {
-      errno = ENOSYS;
-      return -1;
-    }
+    *tz = (struct timezone){0, 0}; /* XXX */
 
   if (err = __host_get_time (__mach_host_self (), (time_value_t *) tv))
     {
