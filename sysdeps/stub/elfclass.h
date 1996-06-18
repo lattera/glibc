8882a2dad5060a5c950ae31486863496a2ff05b1
Roland McGrath <roland@gnu.org>
1996-06-18 02:41:29 +0000

    Sun Jun 16 03:22:49 1996  Roland McGrath  <roland@delasyd.gnu.ai.mit.edu>
    	* Make-dist (subdir): Make empty value really empty.

---
+++ sysdeps/stub/elfclass.h
@@ -0,0 +1,8 @@
+/* This file specifies the native word size of the machine, which indicates
+   the ELF file class used for executables and shared objects on this
+   machine.  */
+
+#define __ELF_NATIVE_CLASS ??
+
+/* This file goes in sysdeps/wordsize-?? and sysdeps/MACHINE/Implies lists
+   wordsize-?? for MACHINE's wordsize.  */
