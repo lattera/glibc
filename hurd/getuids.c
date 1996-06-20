ca591d426e04512bdd14de32dddf577c029af6e4
Miles Bader <miles@gnu.org>
1996-06-20 16:18:45 +0000

    (getuids): New weak link to __getuids.

--- hurd/getuids.c
+++ hurd/getuids.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1994, 1996 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -61,3 +61,5 @@ __getuids (int n, uid_t *uidset)
 
   return nuids;
 }
+
+weak_alias (__getuids, getuids)
