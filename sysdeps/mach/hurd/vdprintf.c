b51c27c0328382d5e0909f6c595ed381392b9200
Thomas Bushnell, BSG <thomas@gnu.org>
1997-01-17 19:29:07 +0000

    Fri Jan 17 14:16:28 1997  Thomas Bushnell, n/BSG  <thomas@gnu.ai.mit.edu>
    	* stdio/vdprintf.c: Moved file to ...
    	* sysdeps/posix/vdprintf.c: Here.  De-ansideclificate.
    	* sysdeps/stub/vdprintf.c: New file.
    	* sysdeps/mach/hurd/vdprintf.c: New file.

--- sysdeps/mach/hurd/vdprintf.c
+++ sysdeps/mach/hurd/vdprintf.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1997 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -16,28 +16,34 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <ansidecl.h>
 #include <errno.h>
 #include <limits.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <hurd/fd.h>
 
 /* Write formatted output to file descriptor D according to the format string
    FORMAT, using the argument list in ARG.  */
 int
-DEFUN(vdprintf, (d, format, arg),
-      int d AND CONST char *format AND va_list arg)
+vdprintf (int d, const char *format, va_list arg)
 {
   int done;
   FILE f;
+  struct hurd_fd *fd;
+
+  HURD_CRITICAL_BEGIN;
+  fd = _hurd_fd_get (fd);
+  HURD_CRITICAL_END;
+
+  if (!fd)
+    return 0;
 
   /* Create an unbuffered stream talking to D on the stack.  */
   memset ((PTR) &f, 0, sizeof(f));
   f.__magic = _IOMAGIC;
   f.__mode.__write = 1;
-  f.__cookie = (PTR) (long int) d; /* Casting to long quiets GCC on Alpha.  */
+  f.__cookie = fd;
   f.__room_funcs = __default_room_functions;
   f.__io_funcs = __default_io_functions;
   f.__seen = 1;
