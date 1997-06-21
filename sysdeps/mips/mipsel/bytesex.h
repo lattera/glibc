18e5142049b78c5890777b55a57c98daf524c28a
Ulrich Drepper <drepper@redhat.com>
1997-06-21 02:06:57 +0000

    moved to bits/.

--- sysdeps/mips/mipsel/bytesex.h
+++
@@ -1,4 +0,0 @@
-/* The MIPS architecture has selectable endianness.
-   This file is for a machine using little-endian mode.  */
-
-#define __BYTE_ORDER __LITTLE_ENDIAN
--- sysdeps/mips/p40/bytesex.h
+++
@@ -1,4 +0,0 @@
-/* The MIPS has selectable endianness.
-   The Japanese homebrew P40 architecture uses big-endian mode.  */
-
-#define __BYTE_ORDER __BIG_ENDIAN
