fc1eaef3e8eae583bf161663bce5fc80db4226ba
Ulrich Drepper <drepper@redhat.com>
1997-12-03 23:31:37 +0000

    Don't define ___LIBC_INTERNAL_MATH_INLINES here.

--- sysdeps/m68k/fpu/s_llrint.c
+++ sysdeps/m68k/fpu/s_llrint.c
@@ -19,7 +19,6 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
-#define __LIBC_INTERNAL_MATH_INLINES
 #include <math.h>
 #include "math_private.h"
 
