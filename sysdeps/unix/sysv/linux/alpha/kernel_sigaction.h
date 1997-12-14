94b8f616432fd1f9658f7801e1a5ec653784d2fa
Ulrich Drepper <drepper@redhat.com>
1997-12-14 22:19:05 +0000

    Define as old_kernel_sigaction.

--- sysdeps/unix/sysv/linux/alpha/kernel_sigaction.h
+++ sysdeps/unix/sysv/linux/alpha/kernel_sigaction.h
@@ -1,6 +1,6 @@
 /* This is the sigaction struction from the Linux 2.1.20 kernel.  */
 
-struct kernel_sigaction {
+struct old_kernel_sigaction {
 	__sighandler_t sa_handler;
 	unsigned long sa_mask;
 	unsigned int sa_flags;
