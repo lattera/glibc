71a14d64831fed0b9fd6a0488aff6d6eab89b448
Ulrich Drepper <drepper@redhat.com>
1998-02-17 16:14:44 +0000

    Update.
    1998-02-17  Ulrich Drepper  <drepper@cygnus.com>
    
    	* sysdeps/generic/bits/select.h (__FD_ISSET): Compare result with
    	0 to get always an int.
    
    	Make compatible with older kernels.  Patch by Richard Henderson.

--- sysdeps/generic/bits/select.h
+++ sysdeps/generic/bits/select.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1997, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -30,6 +30,6 @@
     for (__i = 0; __i < sizeof (__fd_set) / sizeof (__fd_mask); ++__i)	      \
       __arr->fds_bits[__i] = '\0';					      \
   } while (0)
-#define __FD_SET(d, set)	((set)->fds_bits[__FDELT(d)] |= __FDMASK(d))
-#define __FD_CLR(d, set)	((set)->fds_bits[__FDELT(d)] &= ~__FDMASK(d))
-#define __FD_ISSET(d, set)	((set)->fds_bits[__FDELT(d)] & __FDMASK(d))
+#define __FD_SET(d, set)     ((set)->fds_bits[__FDELT(d)] |= __FDMASK(d))
+#define __FD_CLR(d, set)     ((set)->fds_bits[__FDELT(d)] &= ~__FDMASK(d))
+#define __FD_ISSET(d, set)   (((set)->fds_bits[__FDELT(d)] & __FDMASK(d)) != 0)
