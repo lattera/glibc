d424955575ad2bd2a70c2b85fbee6c4103b802d1
Ulrich Drepper <drepper@redhat.com>
1998-02-26 08:38:37 +0000

    (FE_TOWARDZERO): Correct typo.

--- sysdeps/m68k/fpu/bits/fenv.h
+++ sysdeps/m68k/fpu/bits/fenv.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1997, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -47,8 +47,8 @@ enum
   {
     FE_TONEAREST = 0,
 #define FE_TONEAREST	FE_TONEAREST
-    FE_TOWARDSZERO = 1 << 4,
-#define FE_TOWARDSZERO	FE_TOWARDSZERO
+    FE_TOWARDZERO = 1 << 4,
+#define FE_TOWARDZERO	FE_TOWARDZERO
     FE_DOWNWARD = 2 << 4,
 #define FE_DOWNWARD	FE_DOWNWARD
     FE_UPWARD = 3 << 4
