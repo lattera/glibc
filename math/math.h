9a24f9061c4e5e567350734bfb6a2aaa4e2106dc
Ulrich Drepper <drepper@redhat.com>
1998-03-23 09:37:02 +0000

    (M_*l): Extend long double constants to be usable for 128 bit floats.

--- math/math.h
+++ math/math.h
@@ -281,21 +281,21 @@ extern int matherr __P ((struct exception *__exc));
 
 /* The above constants are not adequate for computation using `long double's.
    Therefore we provide as an extension constants with similar names as a
-   GNU extension.  */
+   GNU extension.  Provide enough digits for the 128-bit IEEE quad.  */
 #ifdef __USE_GNU
-# define M_El		2.7182818284590452354L	/* e */
-# define M_LOG2El	1.4426950408889634074L	/* log_2 e */
-# define M_LOG10El	0.43429448190325182765L	/* log_10 e */
-# define M_LN2l		0.69314718055994530942L	/* log_e 2 */
-# define M_LN10l	2.30258509299404568402L	/* log_e 10 */
-# define M_PIl		3.14159265358979323846L	/* pi */
-# define M_PI_2l	1.57079632679489661923L	/* pi/2 */
-# define M_PI_4l	0.78539816339744830962L	/* pi/4 */
-# define M_1_PIl	0.31830988618379067154L	/* 1/pi */
-# define M_2_PIl	0.63661977236758134308L	/* 2/pi */
-# define M_2_SQRTPIl	1.12837916709551257390L	/* 2/sqrt(pi) */
-# define M_SQRT2l	1.41421356237309504880L	/* sqrt(2) */
-# define M_SQRT1_2l	0.70710678118654752440L	/* 1/sqrt(2) */
+# define M_El		2.7182818284590452353602874713526625L  /* e */
+# define M_LOG2El	1.4426950408889634073599246810018922L  /* log_2 e */
+# define M_LOG10El	0.4342944819032518276511289189166051L  /* log_10 e */
+# define M_LN2l		0.6931471805599453094172321214581766L  /* log_e 2 */
+# define M_LN10l	2.3025850929940456840179914546843642L  /* log_e 10 */
+# define M_PIl		3.1415926535897932384626433832795029L  /* pi */
+# define M_PI_2l	1.5707963267948966192313216916397514L  /* pi/2 */
+# define M_PI_4l	0.7853981633974483096156608458198757L  /* pi/4 */
+# define M_1_PIl	0.3183098861837906715377675267450287L  /* 1/pi */
+# define M_2_PIl	0.6366197723675813430755350534900574L  /* 2/pi */
+# define M_2_SQRTPIl	1.1283791670955125738961589031215452L  /* 2/sqrt(pi) */
+# define M_SQRT2l	1.4142135623730950488016887242096981L  /* sqrt(2) */
+# define M_SQRT1_2l	0.7071067811865475244008443621048490L  /* 1/sqrt(2) */
 #endif
 
 
