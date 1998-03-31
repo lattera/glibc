71bedb76ac4e71537d980705ed8d390950570071
Ulrich Drepper <drepper@redhat.com>
1998-03-31 23:45:03 +0000

    Update.
    	* iconvdata/Makefile: Use libKSC shared object in JOHAB and
    	EUC-KR.
    
    	* sysdeps/unix/sysv/linux/bits/socket.h (PF_FILE): Correct
    	comment.
    
    1998-03-30 08:43  H.J. Lu  <hjl@gnu.org>
    
    	* sysdeps/alpha/elf/start.S: Let __libc_start_main do most of
    	the init stuff.
    
    1998-03-31  Ulrich Drepper  <drepper@cygnus.com>
    
    	* sysdeps/unix/sysv/linux/alpha/syscalls.list: Wrapper for
    	adjtimex syscall.
    	* sysdeps/unix/sysv/linux/alpha/Makefile (sysdep_routines): Add
    	adjtimex.
    	Patches by Dominik Kubla <kubla@sundiver.zdv.uni-mainz.de>.
    
    1998-03-31  Andreas Jaeger  <aj@arthur.rhein-neckar.de>
    
    	* Makerules (install): Change comment for libc.so following the
    	libc_nonshared changes.
    
    1998-03-31  Ulrich Drepper  <drepper@cygnus.com>

--- sysdeps/unix/sysv/linux/bits/socket.h
+++ sysdeps/unix/sysv/linux/bits/socket.h
@@ -56,7 +56,7 @@ enum __socket_type
 #define	PF_UNSPEC	0	/* Unspecified.  */
 #define	PF_LOCAL	1	/* Local to host (pipes and file-domain).  */
 #define	PF_UNIX		PF_LOCAL /* Old BSD name for PF_LOCAL.  */
-#define	PF_FILE		PF_LOCAL /* POSIX name for PF_LOCAL.  */
+#define	PF_FILE		PF_LOCAL /* Another non-standard name for PF_LOCAL.  */
 #define	PF_INET		2	/* IP protocol family.  */
 #define	PF_AX25		3	/* Amateur Radio AX.25.  */
 #define	PF_IPX		4	/* Novell Internet Protocol.  */
