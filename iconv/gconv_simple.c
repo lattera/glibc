f9f219d99a5b3b29af184bff9afe70aa9ed427a1
Ulrich Drepper <drepper@redhat.com>
1998-04-01 12:27:00 +0000

    Update.
    1998-04-01  Ulrich Drepper  <drepper@cygnus.com>
    
    	* iconv/gconv_simple.c (__gconv_transform_ucs4_utf8): Compute
    	number of converted bytes correctly.

--- iconv/gconv_simple.c
+++ iconv/gconv_simple.c
@@ -1,5 +1,5 @@
 /* Simple transformations functions.
-   Copyright (C) 1997 Free Software Foundation, Inc.
+   Copyright (C) 1997, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1997.
 
@@ -130,7 +130,7 @@ __gconv_transform_ucs4_utf8 (struct gconv_step *step,
 
 	  /* Remember how much we converted.  */
 	  do_write += newinbuf - inbuf;
-	  *inlen -= (newinbuf - inbuf) * sizeof (wchar_t);
+	  *inlen -= newinbuf - inbuf;
 
 	  data->outbufavail += actually;
 
