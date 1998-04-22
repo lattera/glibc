dc9335c14f91623093bf0c7db855e9d1fbc67071
Ulrich Drepper <drepper@redhat.com>
1998-04-22 09:27:22 +0000

    (_JMPBUF_UNWINDS): Compare addresses as pointers, not signed integers.

--- sysdeps/i386/bits/setjmp.h
+++ sysdeps/i386/bits/setjmp.h
@@ -38,4 +38,4 @@ typedef int __jmp_buf[6];
 /* Test if longjmp to JMPBUF would unwind the frame
    containing a local variable at ADDRESS.  */
 #define _JMPBUF_UNWINDS(jmpbuf, address) \
-  ((int) (address) < (jmpbuf)[JB_SP])
+  ((void *) (address) < (void *) (jmpbuf)[JB_SP])
