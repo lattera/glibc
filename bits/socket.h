0777024193516ccc4dbee53f6a2fb281fee5fb10
Ulrich Drepper <drepper@redhat.com>
1998-04-24 14:03:49 +0000

    Update.
    1998-04-24 13:56  Ulrich Drepper  <drepper@cygnus.com>
    
    	* sysdeps/generic/bits/socket.h: Add #defines for enum values.
    	Patch by Thomas Bushnell, n/BSG.

--- bits/socket.h
+++ bits/socket.h
@@ -131,13 +131,21 @@ struct sockaddr
 enum
   {
     MSG_OOB		= 0x01,	/* Process out-of-band data.  */
+#define MSG_OOB MSG_OOB
     MSG_PEEK		= 0x02,	/* Peek at incoming messages.  */
+#define MSG_PEEK MSG_PEEK
     MSG_DONTROUTE	= 0x04,	/* Don't use local routing.  */
+#define MSG_DONTROUTE MSG_DONTROUTE
     MSG_EOR		= 0x08,	/* Data completes record.  */
+#define MSG_EOR MSG_EOR
     MSG_TRUNC		= 0x10,	/* Data discarded before delivery.  */
+#define MSG_TRUNC MSG_TRUNC
     MSG_CTRUNC		= 0x20,	/* Control data lost before delivery.  */
+#define MSG_CTRUNC MSG_CTRUNC
     MSG_WAITALL		= 0x40,	/* Wait for full request or error.  */
+#define MSG_WAITALL MSG_WAITALL
     MSG_DONTWAIT	= 0x80,	/* This message should be nonblocking.  */
+#define MSG_DONTWAIT MSG_DONTWAIT
   };
 
 
@@ -166,31 +174,47 @@ struct msghdr
 enum
   {
     SO_DEBUG = 0x0001,		/* Record debugging information.  */
+#define SO_DEBUG SO_DEBUG
     SO_ACCEPTCONN = 0x0002,	/* Accept connections on socket.  */
+#define SO_ACCEPTCONN SO_ACCEPTCONN
     SO_REUSEADDR = 0x0004,	/* Allow reuse of local addresses.  */
+#define SO_REUSEADDR SO_REUSEADDR
     SO_KEEPALIVE = 0x0008,	/* Keep connections alive and send
 				   SIGPIPE when they die.  */
+#define SO_KEEPALIVE SO_KEEPALIVE
     SO_DONTROUTE = 0x0010,	/* Don't do local routing.  */
+#define SO_DONTROUTE SO_DONTROUTE
     SO_BROADCAST = 0x0020,	/* Allow transmission of
 				   broadcast messages.  */
+#define SO_BROADCAST SO_BROADCAST
     SO_USELOOPBACK = 0x0040,	/* Use the software loopback to avoid
 				   hardware use when possible.  */
+#define SO_USELOOPBACK SO_USELOOPBACK
     SO_LINGER = 0x0080,		/* Block on close of a reliable
 				   socket to transmit pending data.  */
+#define SO_LINGER SO_LINGER
     SO_OOBINLINE = 0x0100,	/* Receive out-of-band data in-band.  */
-
+#define SO_OOBINLINE SO_OOBINLINE
     SO_REUSEPORT = 0x0200,	/* Allow local address and port reuse.  */
-
+#define SO_REUSEPORT SO_REUSEPORT
     SO_SNDBUF = 0x1001,		/* Send buffer size.  */
+#define SO_SNDBUF SO_SNDBUF
     SO_RCVBUF = 0x1002,		/* Receive buffer.  */
+#define SO_RCVBUF SO_RCVBUF
     SO_SNDLOWAT = 0x1003,	/* Send low-water mark.  */
+#define SO_SNDLOWAT SO_SNDLOWAT
     SO_RCVLOWAT = 0x1004,	/* Receive low-water mark.  */
+#define SO_RCVLOWAT SO_RCVLOWAT
     SO_SNDTIMEO = 0x1005,	/* Send timeout.  */
+#define SO_SNDTIMEO SO_SNDTIMEO
     SO_RCVTIMEO = 0x1006,	/* Receive timeout.  */
-
+#define SO_RCVTIMEO SO_RCVTIMEO
     SO_ERROR = 0x1007,		/* Get and clear error status.  */
+#define SO_ERROR SO_ERROR
     SO_STYLE = 0x1008,		/* Get socket connection style.  */
+#define SO_STYLE SO_STYLE
     SO_TYPE = SO_STYLE,		/* Compatible name for SO_STYLE.  */
+#define SO_TYPE SO_TYPE
   };
 
 /* Structure used to manipulate the SO_LINGER option.  */
