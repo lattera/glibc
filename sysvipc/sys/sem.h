936e9dafaa3e3c58e18c9a8a6e5dd606be47910f
Ulrich Drepper <drepper@redhat.com>
1998-04-28 22:41:57 +0000

    Update.
    	* sysvipc/sys/sem.h: Don't use union semun in semctl prototype.

--- sysvipc/sys/sem.h
+++ sysvipc/sys/sem.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1995, 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1995, 1996, 1997, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -44,8 +44,7 @@ struct sembuf
 __BEGIN_DECLS
 
 /* Semaphore control operation.  */
-extern int semctl __P ((int __semid, int __semnum, int __cmd,
-			union semun __arg));
+extern int semctl __P ((int __semid, int __semnum, int __cmd, ...));
 
 /* Get semaphore.  */
 extern int semget __P ((key_t __key, int __nsems, int __semflg));
