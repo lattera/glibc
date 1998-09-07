3121cc0437913ae426bcb18d99ca304e2903d173
Ulrich Drepper <drepper@redhat.com>
1998-09-07 15:06:36 +0000

    Update.
    	* resolv/res_debug.c: Likewise.

--- resolv/res_debug.c
+++ resolv/res_debug.c
@@ -452,11 +452,12 @@ __p_fqnname(cp, msg, msglen, name, namelen)
 	if ((n = dn_expand(msg, cp + msglen, cp, name, namelen)) < 0)
 		return (NULL);
 	newlen = strlen (name);
-	if (newlen == 0 || name[newlen - 1] != '.')
+	if (newlen == 0 || name[newlen - 1] != '.') {
 		if (newlen+1 >= namelen)        /* Lack space for final dot */
 			return (NULL);
 		else
 			strcpy(name + newlen, ".");
+	}
 	return (cp + n);
 }
 
