fa03e7a0ebc77c0bf5a306aee626cb8cd4046179
Ulrich Drepper <drepper@redhat.com>
1998-10-14 12:59:04 +0000

    Define __kernel_termios here instead of including kernel_termios.h.

--- sysdeps/unix/sysv/linux/alpha/bits/ioctls.h
+++ sysdeps/unix/sysv/linux/alpha/bits/ioctls.h
@@ -22,7 +22,20 @@
 
 /* Use the definitions from the kernel header files.  */
 #include <asm/ioctls.h>
-#include <kernel_termios.h>
+
+/* We need the kernel definition of the `termios' struct.  */
+#define __KERNEL_NCCS 19
+struct __kernel_termios
+  {
+    tcflag_t c_iflag;		/* input mode flags */
+    tcflag_t c_oflag;		/* output mode flags */
+    tcflag_t c_cflag;		/* control mode flags */
+    tcflag_t c_lflag;		/* local mode flags */
+    cc_t c_cc[__KERNEL_NCCS];	/* control characters */
+    cc_t c_line;		/* line discipline */
+    speed_t c_ispeed;		/* input speed */
+    speed_t c_ospeed;		/* output speed */
+  };
 
 /* Oh well, this is necessary since the kernel data structure is
    different from the user-level version.  */
