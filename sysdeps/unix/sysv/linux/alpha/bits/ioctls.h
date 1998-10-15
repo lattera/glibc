edb87b12b3127a9bd1e60fed1f25cd73df618f9d
Ulrich Drepper <drepper@redhat.com>
1998-10-15 09:21:29 +0000

    Remove __kernel_termios again.  Use char[44] to define size of the struct.

--- sysdeps/unix/sysv/linux/alpha/bits/ioctls.h
+++ sysdeps/unix/sysv/linux/alpha/bits/ioctls.h
@@ -20,25 +20,8 @@
 # error "Never use <bits/ioctls.h> directly; include <sys/ioctl.h> instead."
 #endif
 
-/* Get the necessary definitions.  */
-#include <termios.h>
-
 /* Use the definitions from the kernel header files.  */
 #include <asm/ioctls.h>
-
-/* We need the kernel definition of the `termios' struct.  */
-#define __KERNEL_NCCS 19
-struct __kernel_termios
-  {
-    tcflag_t c_iflag;		/* input mode flags */
-    tcflag_t c_oflag;		/* output mode flags */
-    tcflag_t c_cflag;		/* control mode flags */
-    tcflag_t c_lflag;		/* local mode flags */
-    cc_t c_cc[__KERNEL_NCCS];	/* control characters */
-    cc_t c_line;		/* line discipline */
-    speed_t c_ispeed;		/* input speed */
-    speed_t c_ospeed;		/* output speed */
-  };
 
 /* Oh well, this is necessary since the kernel data structure is
    different from the user-level version.  */
@@ -46,9 +29,9 @@ struct __kernel_termios
 #undef  TCSETS
 #undef  TCSETSW
 #undef  TCSETSF
-#define TCGETS	_IOR ('t', 19, struct __kernel_termios)
-#define TCSETS	_IOW ('t', 20, struct __kernel_termios)
-#define TCSETSW	_IOW ('t', 21, struct __kernel_termios)
-#define TCSETSF	_IOW ('t', 22, struct __kernel_termios)
+#define TCGETS	_IOR ('t', 19, char[44])
+#define TCSETS	_IOW ('t', 20, char[44])
+#define TCSETSW	_IOW ('t', 21, char[44])
+#define TCSETSF	_IOW ('t', 22, char[44])
 
 #include <linux/sockios.h>
