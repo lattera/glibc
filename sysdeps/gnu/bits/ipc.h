88fd9830347fab598a85c7edbc19ef646d87b3a8
Ulrich Drepper <drepper@redhat.com>
1998-12-16 08:28:16 +0000

    Update.
    1998-12-16  Ulrich Drepper  <drepper@cygnus.com>
    
    	* iconvdata/ibm866.c: New file.
    	* iconvdata/cp1258.c: New file.
    	* iconvdata/cp1258.h: New file.
    	* iconvdata/mac-uk.c: New file.
    	* iconvdata/gconv-modules: Add rules for new modules.
    	* iconvdata/Makefile: Likewise.
    
    	* sysdeps/gnu/bits/ipc.h: Move libc internal bits to...
    	* include/bits/ipc.h: ..here.  New file.
    
    1998-12-16  Andreas Jaeger  <aj@arthur.rhein-neckar.de> 
     
    	* sysdeps/unix/sysv/linux/tcsetattr.c (tcsetattr): Don't change 
    	read-only struct termios_p* but change k_termios member directly.

--- sysdeps/gnu/bits/ipc.h
+++ sysdeps/gnu/bits/ipc.h
@@ -48,25 +48,3 @@ struct ipc_perm
     unsigned short int mode;		/* Read/write permission.  */
     unsigned short int __seq;		/* Sequence number.  */
   };
-
-
-__BEGIN_DECLS
-
-/* The actual system call: all functions are multiplexed by this.  */
-extern int __ipc __P ((int __call, int __first, int __second, int __third,
-		       void *__ptr));
-
-__END_DECLS
-
-/* The codes for the functions to use the multiplexer `__ipc'.  */
-#define IPCOP_semop	 1
-#define IPCOP_semget	 2
-#define IPCOP_semctl	 3
-#define IPCOP_msgsnd	11
-#define IPCOP_msgrcv	12
-#define IPCOP_msgget	13
-#define IPCOP_msgctl	14
-#define IPCOP_shmat	21
-#define IPCOP_shmdt	22
-#define IPCOP_shmget	23
-#define IPCOP_shmctl	24
