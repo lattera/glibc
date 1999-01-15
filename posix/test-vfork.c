d0ab77802ac748c07d7302aa728ad063ce98309f
Ulrich Drepper <drepper@redhat.com>
1999-01-15 13:35:35 +0000

    Update.
    	* posix/Makefile (tests): Add test-vfork.
    	* posix/test-vfork.c: New file by Andreas Schwab.

---
+++ posix/test-vfork.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <error.h>
+#include <errno.h>
+
+void noop (void);
+
+int
+main (void)
+{
+  int pid;
+
+  printf ("Before vfork\n");
+  fflush (stdout);
+  pid = vfork ();
+  if (pid == 0)
+    {
+      /* This will clobber the return pc from vfork in the parent on
+	 machines where it is stored on the stack, if vfork wasn't
+	 implemented correctly, */
+      noop ();
+      _exit (2);
+    }
+  else if (pid < 0)
+    error (1, errno, "vfork");
+  printf ("After vfork (parent)\n");
+  wait (0);
+  exit (0);
+}
+
+void
+noop ()
+{
+}
