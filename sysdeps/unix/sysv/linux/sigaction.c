7804eb37e8db3dad34f1bc422ec033209de137ce
Ulrich Drepper <drepper@redhat.com>
1999-01-26 10:08:17 +0000

    Update.
    1999-01-26  Ulrich Drepper  <drepper@cygnus.com>
    
    	* sysdeps/unix/sysv/linux/sigaction.c: Update comment to say the
    	__libc_missing_rt_sigs is defined here.
    	* sysdeps/unix/sysv/linux/sigpending.c: Only declare
    	__libc_missing_rt_sigs.
    	* sysdeps/unix/sysv/linux/sigprocmask.c: Likewise.
    	* sysdeps/unix/sysv/linux/sigsuspend.c: Likewise.

--- sysdeps/unix/sysv/linux/sigaction.c
+++ sysdeps/unix/sysv/linux/sigaction.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1997, 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1997, 1998, 1999 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -34,7 +34,7 @@ extern int __syscall_rt_sigaction (int, const struct kernel_sigaction *,
 				   struct kernel_sigaction *, size_t);
 
 /* The variable is shared between all wrappers around signal handling
-   functions which have RT equivalents.  */
+   functions which have RT equivalents.  This is the definition.  */
 int __libc_missing_rt_sigs;
 
 
--- sysdeps/unix/sysv/linux/sigpending.c
+++ sysdeps/unix/sysv/linux/sigpending.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1997, 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1997, 1998, 1999 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -28,8 +28,8 @@ extern int __syscall_rt_sigpending (sigset_t *, size_t);
 
 
 /* The variable is shared between all wrappers around signal handling
-   functions which have RT equivalents.  */
-int __libc_missing_rt_sigs;
+   functions which have RT equivalents.  The definition is in sigaction.c.  */
+extern int __libc_missing_rt_sigs;
 
 
 /* Change the set of blocked signals to SET,
