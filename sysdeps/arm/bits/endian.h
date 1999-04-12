3b40f7d3d4c2691737e17df6729eda6e46a59a1f
Ulrich Drepper <drepper@redhat.com>
1999-04-12 09:04:34 +0000

    Support big endian operation.

--- sysdeps/arm/bits/endian.h
+++ sysdeps/arm/bits/endian.h
@@ -4,5 +4,9 @@
 # error "Never use <bits/endian.h> directly; include <endian.h> instead."
 #endif
 
+#ifdef __ARMEB__
+#define __BYTE_ORDER __BIG_ENDIAN
+#else
 #define __BYTE_ORDER __LITTLE_ENDIAN
+#endif
 #define __FLOAT_WORD_ORDER __BIG_ENDIAN
