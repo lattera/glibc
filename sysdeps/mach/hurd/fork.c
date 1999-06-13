a1c93156c80012b1d54acd87e450841e57478bcb
Roland McGrath <roland@gnu.org>
1999-06-13 09:53:59 +0000

    1999-06-12  Mark Kettenis  <kettenis@gnu.org>
    * sysdeps/mach/hurd/fork.c (__fork): Make use of `insert_type' when 
    inserting a send right into the child instead of unconditionally 
    copying the send right.

--- sysdeps/mach/hurd/fork.c
+++ sysdeps/mach/hurd/fork.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1994, 1995, 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1994,95,96,97,99 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -371,8 +371,7 @@ __fork (void)
 		/* Insert the chosen send right into the child.  */
 		err = __mach_port_insert_right (newtask,
 						portnames[i],
-						insert,
-						MACH_MSG_TYPE_COPY_SEND);
+						insert, insert_type);
 	      switch (err)
 		{
 		case KERN_NAME_EXISTS:
