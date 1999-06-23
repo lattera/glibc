50fc48c54691e9119bfdc083ff8a7b9c3247c00e
Ulrich Drepper <drepper@redhat.com>
1999-06-23 10:45:47 +0000

    Don't define alias if fputc is defined as a macro.

--- stdio/fputc.c
+++ stdio/fputc.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1996, 1997, 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1996, 1997, 1998, 1999 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -35,4 +35,6 @@ fputc (c, stream)
   return __putc (c, stream);
 }
 
+#ifndef fputc
 weak_alias (fputc, fputc_unlocked)
+#endif
