c11f120913b8c6a6be67ddf3dc36a3d239f2f322
Ulrich Drepper <drepper@redhat.com>
1999-06-30 17:17:53 +0000

    Update.
    1999-06-29  Andreas Jaeger  <aj@arthur.rhein-neckar.de>
    
    	* pwd/Makefile (tests): Add rules for tst-getpw.
    
    	* pwd/tst-getpw.c: New program to test getpw.

---
+++ pwd/tst-getpw.c
@@ -0,0 +1,41 @@
+/* Copyright (C) 1999 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <pwd.h>
+
+static void
+check (uid_t uid)
+{
+  char buf[1024];
+
+  (void) getpw (uid, buf);
+}
+
+int
+main (void)
+{
+  uid_t uid;
+
+  /* Just call it a different number of times the range should be
+     large enough to find some existing and some non existing uids.  */
+
+  for (uid = 0; uid < 2000; ++uid)
+    check (uid);
+
+  return 0;
+}
