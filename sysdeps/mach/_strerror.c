6818bcaef77f8c1082a18bdb9e0626cd188ae87b
Roland McGrath <roland@gnu.org>
1999-07-21 16:46:32 +0000

    1999-07-20  Mark Kettenis  <kettenis@gnu.org>
    	* sysdeps/mach/_strerror.c: Include <libintl.h>.

--- sysdeps/mach/_strerror.c
+++ sysdeps/mach/_strerror.c
@@ -16,6 +16,7 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
+#include <libintl.h>
 #include <stdio.h>
 #include <string.h>
 #include <mach/error.h>
