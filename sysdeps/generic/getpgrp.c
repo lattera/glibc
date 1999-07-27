650364fa29479ad5926870f03083854cdcad3810
Ulrich Drepper <drepper@redhat.com>
1999-07-27 05:10:43 +0000

    Update.
    	* sysdeps/unix/sysv/sysv4/setsid.c: Correct types.
    	* sysdeps/generic/getpgrp.c: Likewise.
    	* sysdeps/unix/sysv/sysv4/setpgid.c: Likewise.
    	* sysdeps/unix/sysv/sysv4/getpgid.c: Likewise.
    	Patch by John Tobey <jtobey@banta-im.com>.

--- sysdeps/generic/getpgrp.c
+++ sysdeps/generic/getpgrp.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1995, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995, 1997, 1999 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -20,7 +20,7 @@
 #include <unistd.h>
 
 /* Get the process group ID of the calling process.  */
-int
+pid_t
 getpgrp (void)
 {
   return __getpgid (0);
