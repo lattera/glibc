e69dbc25e7182f5eea750db40a555015326043d4
Ulrich Drepper <drepper@redhat.com>
1999-07-27 07:14:15 +0000

    Update.
    1999-07-26  Andreas Jaeger  <aj@arthur.rhein-neckar.de>
    
    	* time/Versions: Add POSIX.1b timer and clock functions to GLIBC_2.2.

--- localedata/tst-rpmatch.sh
+++ localedata/tst-rpmatch.sh
@@ -1,6 +1,6 @@
 #! /bin/sh -f
 #
-# Copyright (C) 1998 Free Software Foundation, Inc.
+# Copyright (C) 1998, 1999 Free Software Foundation, Inc.
 # This file is part of the GNU C Library and contains tests for
 # the rpmatch(3)-implementation.
 # contributed by Jochen Hein <jochen.hein@delphi.central.de>
@@ -20,7 +20,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 common_objpfx=$1
-IFS="§"
+IFS="&"
 rc=0
 while read locale string result dummy; do
     if [ "$locale" != "#" ]; then
@@ -30,31 +30,31 @@ while read locale string result dummy; do
 	|| exit 1
     fi
 done <<EOF
-#§ These are the tests for rpmatch in glibc.  Each line contains one test,
-#§ comments start with #§ in the first column.  The fields are seperated
-#§ by paragraph signs and contain: the locale, the string, the expected
-#§ return value of rpmatch(3).  If the test fails, test-rpmatch prints
-#§ all these informations
-C§Yes§1
-C§yes§1
-C§YES§1
-C§YeS§1
-C§YEs§1
-C§yEs§1
-C§yES§1
-C§yeS§1
-C§No§0
-C§no§0
-#§ Uh, that's nonsense
-C§nonsens§0
-C§Error§-1
-de_DE§Yes§1
-de_DE§Ja§1
-de_DE§Jammerschade§1
-de_DE§dejavu§-1
-de_DE§Nein§0
-de_DE§Fehler§-1
-de_DE§jein§1
+#& These are the tests for rpmatch in glibc.  Each line contains one test,
+#& comments start with #& in the first column.  The fields are seperated
+#& by paragraph signs and contain: the locale, the string, the expected
+#& return value of rpmatch(3).  If the test fails, test-rpmatch prints
+#& all these informations
+C&Yes&1
+C&yes&1
+C&YES&1
+C&YeS&1
+C&YEs&1
+C&yEs&1
+C&yES&1
+C&yeS&1
+C&No&0
+C&no&0
+#& Uh, that's nonsense
+C&nonsens&0
+C&Error&-1
+de_DE&Yes&1
+de_DE&Ja&1
+de_DE&Jammerschade&1
+de_DE&dejavu&-1
+de_DE&Nein&0
+de_DE&Fehler&-1
+de_DE&jein&1
 EOF
 
 rc=$?
