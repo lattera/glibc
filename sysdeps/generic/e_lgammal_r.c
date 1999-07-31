899a282782e09ff205d7fce117d0fec5a3277e15
Ulrich Drepper <drepper@redhat.com>
1999-07-31 07:16:41 +0000

    Update.
    1999-07-31  Ulrich Drepper  <drepper@cygnus.com>
    
    	* sysdeps/i386/fpu/e_pow.S: Use addl %esp instead of pop.
    	* sysdeps/i386/fpu/e_powf.S: Likewise.
    	* sysdeps/i386/fpu/e_powl.S: Likewise.  [PR libc/1213].
    
    	* sysdeps/generic/e_lgammal_r.c: Set *signgam to zero.
    	* sysdeps/generic/e_gammal_r.c: Likewise.  [PR libc/1212].

--- sysdeps/generic/e_lgammal_r.c
+++ sysdeps/generic/e_lgammal_r.c
@@ -5,6 +5,7 @@
 long double
 __ieee754_lgammal_r (long double x, int *signgamp)
 {
+  *signgamp = 0;
   fputs ("__ieee754_lgammal_r not implemented\n", stderr);
   __set_errno (ENOSYS);
   return 0.0;
