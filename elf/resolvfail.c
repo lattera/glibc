ff5fad1641bbd3b76501e01467b179c6aa6421b8
Ulrich Drepper <drepper@redhat.com>
1999-08-01 21:43:03 +0000

    Update.
    	* elf/Makefile (tests): Add resolvfail.  Add rules to build the
    	program.
    	* elf/resolvfail.c: New file.

---
+++ elf/resolvfail.c
@@ -0,0 +1,25 @@
+#include <dlfcn.h>
+
+static const char obj[] = "testobj1.so";
+
+int
+main (void)
+{
+  void *d = dlopen (obj, RTLD_LAZY);
+  int n;
+
+  if (d == NULL)
+    {
+      printf ("cannot load %s: %s\n", obj, dlerror ());
+      return 1;
+    }
+
+  for (n = 0; n < 10000; ++n)
+    if (dlsym (d, "does not exist") != NULL)
+      {
+	puts ("dlsym() did not fail");
+	return 1;
+      }
+
+  return 0;
+}
