f79973fab37970cadd2994e497a475b1b27400a7
Ulrich Drepper <drepper@redhat.com>
1999-08-06 23:23:02 +0000

    Update.
    	* inet/netinet/in.h (IN6ADDR_ANY_INIT): Add correct number of
    	braces to avoid warnings.
    	(IN6ADDR_LOOPBACK_INIT): Likewise.

--- inet/netinet/in.h
+++ inet/netinet/in.h
@@ -177,8 +177,8 @@ struct in6_addr
 
 extern const struct in6_addr in6addr_any;        /* :: */
 extern const struct in6_addr in6addr_loopback;   /* ::1 */
-#define IN6ADDR_ANY_INIT { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
-#define IN6ADDR_LOOPBACK_INIT { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 }
+#define IN6ADDR_ANY_INIT { { { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 } } }
+#define IN6ADDR_LOOPBACK_INIT { { { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 } } }
 
 #define INET_ADDRSTRLEN 16
 #define INET6_ADDRSTRLEN 46
