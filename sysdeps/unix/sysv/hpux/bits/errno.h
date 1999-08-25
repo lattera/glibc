7dab346125c2a6060a895636418dc26dc60c4c8b
Ulrich Drepper <drepper@redhat.com>
1999-08-25 20:26:03 +0000

    HPUX error number definitions.

---
+++ sysdeps/unix/sysv/hpux/bits/errno.h
@@ -0,0 +1,36 @@
+/* Copyright (C) 1991, 1994, 1996, 1997 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+/* This file defines the `errno' constants.  */
+
+#if !defined __Emath_defined && (defined _ERRNO_H || defined __need_Emath)
+#undef	__need_Emath
+#define	__Emath_defined	1
+
+#endif
+
+#ifdef	_ERRNO_H
+#define EBADF		9
+#define ENOMEM		12
+#define EINVAL          22
+#define ERANGE		34
+#define ENOMSG		35
+#define ENOSYS		251
+#endif
+
+#define __set_errno(val) errno = (val)
