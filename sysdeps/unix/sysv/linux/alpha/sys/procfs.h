876914479ac53fd426d2557c4861494b2fda79e7
Ulrich Drepper <drepper@redhat.com>
1999-09-03 07:05:16 +0000

    Include <sys/user.h> instead of <asm/user.h>.

--- sysdeps/unix/sysv/linux/alpha/sys/procfs.h
+++ sysdeps/unix/sysv/linux/alpha/sys/procfs.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 1997, 1999 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -28,7 +28,7 @@
 #include <signal.h>
 #include <sys/time.h>
 #include <sys/types.h>
-#include <asm/user.h>
+#include <sys/user.h>
 #include <asm/elf.h>
 
 __BEGIN_DECLS
