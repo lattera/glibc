f2fe71afa2e364a3df6a92d8bbb6ed0036e78c13
Ulrich Drepper <drepper@redhat.com>
1999-11-15 08:00:07 +0000

    Remove duplicate definitions.

--- sysdeps/unix/sysv/linux/arm/sys/procfs.h
+++ sysdeps/unix/sysv/linux/arm/sys/procfs.h
@@ -101,17 +101,6 @@ typedef fpregset_t prfpregset_t;
    therefore habe only ine PID type.  */
 typedef __pid_t lwpid_t;
 
-/* Addresses.  */
-typedef void *psaddr_t;
-
-/* Register sets.  Linux has different names.  */
-typedef gregset_t prgregset_t;
-typedef fpregset_t prfpregset_t;
-
-/* We don't have any differences between processes and threads,
-   therefore habe only ine PID type.  */
-typedef __pid_t lwpid_t;
-
 
 typedef struct elf_prstatus prstatus_t;
 typedef struct elf_prpsinfo prpsinfo_t;
