8d2d51e373dd7c6c4a9b417a5bb7bdda3fe09b5f
Ulrich Drepper <drepper@redhat.com>
1999-11-24 20:48:27 +0000

    Update.
    1999-11-24  Ulrich Drepper  <drepper@cygnus.com>
    
    	* include/string.h: Remove K&R compatibility.

--- include/string.h
+++ include/string.h
@@ -1,25 +1,28 @@
 #ifndef _STRING_H
+
+#include <sys/types.h>
+
+extern void *__memccpy (void *__dest, __const void *__src,
+			  int __c, size_t __n);
+
+extern size_t __strnlen (__const char *__string, size_t __maxlen);
+
+extern char *__strsep (char **__stringp, __const char *__delim);
+
+extern int __strverscmp (__const char *__s1, __const char *__s2);
+
+extern int __strncasecmp (__const char *__s1, __const char *__s2,
+			  size_t __n);
+
+extern char *__strndup (__const char *__string, size_t __n);
+
+extern void *__rawmemchr (__const void *__s, int __c);
+
+extern char *__strchrnul (__const char *__s, int __c);
+
+extern void *__memrchr (__const void *__s, int __c, size_t __n);
+
+/* Now the real definitions.  We do this here since some of the functions
+   above are defined as macros in the headers.  */
 #include <string/string.h>
-
-/* Now define the internal interfaces.  */
-extern __ptr_t __memccpy __P ((__ptr_t __dest, __const __ptr_t __src,
-			       int __c, size_t __n));
-
-extern size_t __strnlen __P ((__const char *__string, size_t __maxlen));
-
-extern char *__strsep __P ((char **__stringp, __const char *__delim));
-
-extern int __strverscmp __P ((__const char *__s1, __const char *__s2));
-
-extern int __strncasecmp __P ((__const char *__s1, __const char *__s2,
-			       size_t __n));
-
-extern char *__strndup __P ((__const char *__string, size_t __n));
-
-extern __ptr_t __rawmemchr __P ((__const __ptr_t __s, int __c));
-
-extern char *__strchrnul __P ((__const char *__s, int __c));
-
-extern __ptr_t __memrchr __P ((__const __ptr_t __s, int __c, size_t __n));
-
 #endif
