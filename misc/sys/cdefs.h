ae177b9c8a14da52538db5749eee90f08f5aedea
Ulrich Drepper <drepper@redhat.com>
1999-12-19 08:35:44 +0000

    Update.
    	* locale/langinfo.h: Always define CODESET.
    	(CODESET): Define also as macro.

--- misc/sys/cdefs.h
+++ misc/sys/cdefs.h
@@ -138,15 +138,15 @@
 
 # define __attribute__(xyz)	/* Ignore */
 
+#endif
+
 /* At some point during the gcc 2.96 development the `malloc' attribute
    for functions was introduced.  We don't want to use it unconditionally
    (although this would be possible) since it generates warnings.  */
-# if __GNUC_PREREQ (2,96)
-#  define __attribute_malloc__ __attribute__ ((__malloc__))
-# else
-#  define __attribute_malloc__ /* Ignore */
-# endif
-
+#if __GNUC_PREREQ (2,96)
+# define __attribute_malloc__ __attribute__ ((__malloc__))
+#else
+# define __attribute_malloc__ /* Ignore */
 #endif
 
 /* It is possible to compile containing GCC extensions even if GCC is
