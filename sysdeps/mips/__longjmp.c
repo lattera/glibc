f81825597154caf652683efff52ae2bc3eb71b20
Andreas Jaeger <aj@suse.de>
2000-01-11 17:12:43 +0000

    2000-01-11  Andreas Schwab  <schwab@suse.de>
    	* sysdeps/mips/__longjmp.c (__longjmp): Change call to abort into
    	infinite loop to avoid pulling in stdio in the dynamic linker.

--- sysdeps/mips/__longjmp.c
+++ sysdeps/mips/__longjmp.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992, 1995, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1995, 1997, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Brendan Kehoe (brendan@zen.org).
 
@@ -79,5 +79,6 @@ __longjmp (env, val_arg)
 
   asm volatile ("j $31");
 
-  abort ();
+  /* Avoid `volatile function does return' warnings.  */
+  for (;;);
 }
