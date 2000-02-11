f9a7302fd1f88e311dd89e1c9d8b4ad572af6cfa
Ulrich Drepper <drepper@redhat.com>
2000-02-11 21:04:20 +0000

    Update.
    2000-02-09  Andreas Jaeger  <aj@suse.de>
    
    	* time/strptime.c (strptime_internal): Fix pointer dereference.
    	Patch by Juergen Heinzl <juergen@monocerus.demon.co.uk>, closes PR
    	libc/1591.

--- time/strptime.c
+++ time/strptime.c
@@ -1,5 +1,5 @@
 /* Convert a string representation of time to a time value.
-   Copyright (C) 1996, 1997, 1998, 1999 Free Software Foundation, Inc.
+   Copyright (C) 1996, 1997, 1998, 1999, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1996.
 
@@ -459,7 +459,7 @@ strptime_internal (rp, fmt, tm, decided, era_cnt)
 		}
 	      else
 		{
-		  if (decided == not
+		  if (*decided == not
 		      && strcmp (_NL_CURRENT (LC_TIME, D_FMT), HERE_D_FMT))
 		    *decided = loc;
 		  want_xday = 1;
