37b22a8dd3975eb3ab47a93bf5237bf802e7bf55
Ulrich Drepper <drepper@redhat.com>
2000-02-28 04:59:24 +0000

    Fix type of second parameter.

--- sysdeps/unix/sysv/irix4/setpriority.c
+++ sysdeps/unix/sysv/irix4/setpriority.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1994, 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1994, 1996, 1997, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -23,7 +23,7 @@
 int
 setpriority (which, who, prio)
      enum __priority_which which;
-     int who;
+     id_t who;
      int prio;
 {
   switch (which)
