6213a05545cd8052135271c25b73af2c5490e291
Ulrich Drepper <drepper@redhat.com>
2000-03-14 10:39:15 +0000

    Update.
    	* sysdeps/unix/sysv/linux/i386/sys/ucontext.h: Prepend REG_ to enum
    	values and macros for register names.

--- sysdeps/unix/sysv/linux/i386/sys/ucontext.h
+++ sysdeps/unix/sysv/linux/i386/sys/ucontext.h
@@ -44,44 +44,44 @@ typedef greg_t gregset_t[NGREG];
 /* Number of each register is the `gregset_t' array.  */
 enum
 {
-  GS = 0,
-# define GS	GS
-  FS,
-# define FS	FS
-  ES,
-# define ES	ES
-  DS,
-# define DS	DS
-  EDI,
-# define EDI	EDI
-  ESI,
-# define ESI	ESI
-  EBP,
-# define EBP	EBP
-  ESP,
-# define ESP	ESP
-  EBX,
-# define EBX	EBX
-  EDX,
-# define EDX	EDX
-  ECX,
-# define ECX	ECX
-  EAX,
-# define EAX	EAX
-  TRAPNO,
-# define TRAPNO	TRAPNO
-  ERR,
-# define ERR	ERR
-  EIP,
-# define EIP	EIP
-  CS,
-# define CS	CS
-  EFL,
-# define EFL	EFL
-  UESP,
-# define UESP	UESP
-  SS
-# define SS	SS
+  REG_GS = 0,
+# define REG_GS		REG_GS
+  REG_FS,
+# define REG_FS		REG_FS
+  REG_ES,
+# define REG_ES		REG_ES
+  REG_DS,
+# define REG_DS		REG_DS
+  REG_EDI,
+# define REG_EDI	REG_EDI
+  REG_ESI,
+# define REG_ESI	REG_ESI
+  REG_EBP,
+# define REG_EBP	REG_EBP
+  REG_ESP,
+# define REG_ESP	REG_ESP
+  REG_EBX,
+# define REG_EBX	REG_EBX
+  REG_EDX,
+# define REG_EDX	REG_EDX
+  REG_ECX,
+# define REG_ECX	REG_ECX
+  REG_EAX,
+# define REG_EAX	REG_EAX
+  REG_TRAPNO,
+# define REG_TRAPNO	REG_TRAPNO
+  REG_ERR,
+# define REG_ERR	REG_ERR
+  REG_EIP,
+# define REG_EIP	REG_EIP
+  REG_CS,
+# define REG_CS		REG_CS
+  REG_EFL,
+# define REG_EFL	REG_EFL
+  REG_UESP,
+# define REG_UESP	REG_UESP
+  REG_SS
+# define REG_SS	REG_SS
 };
 #endif
 
