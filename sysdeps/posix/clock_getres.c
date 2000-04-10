13fa3676961e9dd1a8a26eabec8aa9b2b4247899
Ulrich Drepper <drepper@redhat.com>
2000-04-10 06:59:44 +0000

    Update.
    	* sysdeps/unix/clock_gettime.c: New file.
    	* sysdeps/unix/clock_settime.c: New file.
    	* sysdeps/posix/clock_getres.c: New file.

---
+++ sysdeps/posix/clock_getres.c
@@ -0,0 +1,63 @@
+/* Copyright (C) 1999, 2000 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <errno.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+
+#ifndef EXTRA_CLOCK_CASES
+# define EXTRA_CLOCK_CASES
+#endif
+
+/* Get resolution of clock.  */
+int
+clock_getres (clockid_t clock_id, struct timespec *res)
+{
+  int retval;
+
+  switch (clock_id)
+    {
+    case CLOCK_REALTIME:
+      {
+	long int clk_tck = sysconf (_SC_CLK_TCK);
+
+	retval = -1;
+	if (__builtin_expect (clk_tck != -1, 1))
+	  {
+	    /* This implementation assumes that the realtime clock has a
+	       resolution higher than 1 second.  This is the case for any
+	       reasonable implementation.  */
+	    res->tv_sec = 0;
+	    res->tv_nsec = 1000000000 / clk_tck;
+
+	    retval = 0;
+	  }
+      }
+      break;
+
+      EXTRA_CLOCK_CASES
+
+    default:
+      __set_errno (EINVAL);
+      retval = -1;
+      break;
+    }
+
+  return retval;
+}
---
+++ sysdeps/unix/clock_settime.c
@@ -0,0 +1,51 @@
+/* Copyright (C) 1999, 2000 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <errno.h>
+#include <sys/time.h>
+
+
+#ifndef EXTRA_CLOCK_CASES
+# define EXTRA_CLOCK_CASES
+#endif
+
+/* Set CLOCK to value TP.  */
+int
+clock_settime (clockid_t clock_id, const struct timespec *tp)
+{
+  struct timeval tv;
+  int retval;
+
+  switch (clock_id)
+    {
+    case CLOCK_REALTIME:
+      TIMESPEC_TO_TIMEVAL (&tv, tp);
+
+      retval = settimeofday (&tv, NULL);
+      break;
+
+      EXTRA_CLOCK_CASES
+
+    default:
+      __set_errno (EINVAL);
+      retval = -1;
+      break;
+    }
+
+  return retval;
+}
