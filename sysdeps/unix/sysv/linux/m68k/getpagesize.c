bb0607a2aaa43dd9a8e081eec48be923323cef25
Andreas Schwab <schwab@suse.de>
2000-05-08 15:01:07 +0000

    New file.  Use getpagesize syscall if available.

---
+++ sysdeps/unix/sysv/linux/m68k/getpagesize.c
@@ -0,0 +1,43 @@
+/* Copyright (C) 2000 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Contributed by Andreas Schwab <schwab@suse.de>.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <unistd.h>
+#include <sys/param.h>
+#include <errno.h>
+
+#include <sysdep.h>
+#include <sys/syscall.h>
+
+/* Return the system page size.  */
+int
+__getpagesize ()
+{
+#ifdef __NR_getpagesize
+  int result;
+
+  result = INLINE_SYSCALL (getpagesize, 0);
+  /* The only possible error is ENOSYS.  */
+  if (result != -1)
+    return result;
+#endif
+
+  return EXEC_PAGESIZE;
+}
+
+weak_alias (__getpagesize, getpagesize)
