4a624ae307f083a759f10224428440585a7f615e
Ulrich Drepper <drepper@redhat.com>
2000-05-22 01:03:59 +0000

    Update.
    2000-05-19  Andreas Jaeger  <aj@suse.de>
    
    	* sysdeps/unix/sysv/linux/i386/i686/sysdep.h
    	(SYSCALL_ERROR_HANDLER): Remove unneeded syscall_error which
    	breaks compilation without linuxthreads.

--- sysdeps/unix/sysv/linux/i386/i686/sysdep.h
+++ sysdeps/unix/sysv/linux/i386/i686/sysdep.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1998, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper, <drepper@cygnus.com>, 1998.
 
@@ -49,7 +49,6 @@
    not modify the stack!  */
 # else
 #  define SYSCALL_ERROR_HANDLER						      \
-  .type syscall_error,@function;					      \
 1:movl (%esp),%ecx;							      \
   ret;									      \
 0:call 1b;								      \
