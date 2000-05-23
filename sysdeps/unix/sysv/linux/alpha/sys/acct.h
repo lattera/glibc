2d2d8237e0595f1c20d8d9c142845d46cba36e1b
Andreas Jaeger <aj@suse.de>
2000-05-23 09:07:17 +0000

    Remove K&R support.

--- sysdeps/unix/sysv/linux/alpha/sys/acct.h
+++ sysdeps/unix/sysv/linux/alpha/sys/acct.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 1997, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -59,7 +59,7 @@ enum
 
 
 /* Switch process accounting on and off.  */
-extern int acct __P ((__const char *__filename));
+extern int acct (__const char *__filename) __THROW;
 
 __END_DECLS
 
