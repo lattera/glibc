cddf254d1739c51406a13047916a634e4f7f7f66
Ulrich Drepper <drepper@redhat.com>
2000-05-26 20:30:20 +0000

    Test for truncate and ftruncate.

---
+++ posix/tst-truncate.c
@@ -0,0 +1,123 @@
+/* Tests for ftruncate and truncate.
+   Copyright (C) 2000 Free Software Foundation, Inc.
+   Contributed by Ulrich Drepper <drepper@cygnus.com>, 2000.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <errno.h>
+#include <error.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+
+/* Allow testing of the 64-bit versions as well.  */
+#ifndef TRUNCATE
+# define TRUNCATE truncate
+# define FTRUNCATE ftruncate
+#endif
+
+#define STRINGIFY(s) STRINGIFY2 (s)
+#define STRINGIFY2(s) #s
+
+/* Prototype for our test function.  */
+extern void do_prepare (int argc, char *argv[]);
+extern int do_test (int argc, char *argv[]);
+
+/* We have a preparation function.  */
+#define PREPARE do_prepare
+
+/* We might need a bit longer timeout.  */
+#define TIMEOUT 20 /* sec */
+
+/* This defines the `main' function and some more.  */
+#include <test-skeleton.c>
+
+/* These are for the temporary file we generate.  */
+char *name;
+int fd;
+
+void
+do_prepare (int argc, char *argv[])
+{
+   char name_len;
+
+     name_len = strlen (test_dir);
+     name = malloc (name_len + sizeof ("/truncateXXXXXX"));
+     mempcpy (mempcpy (name, test_dir, name_len),
+	      "/truncateXXXXXX", sizeof ("/truncateXXXXXX"));
+     add_temp_file (name);
+
+     /* Open our test file.   */
+     fd = mkstemp (name);
+     if (fd == -1)
+       error (EXIT_FAILURE, errno, "cannot open test file `%s'", name);
+}
+
+
+int
+do_test (int argc, char *argv[])
+{
+  struct stat st;
+  char buf[1000];
+  int i;
+
+  memset (buf, '\0', sizeof (buf));
+
+  if (write (fd, buf, sizeof (buf)) != sizeof (buf))
+    error (EXIT_FAILURE, errno, "during write");
+
+  if (fstat (fd, &st) < 0 || st.st_size != sizeof (buf))
+    error (EXIT_FAILURE, 0, "initial size wrong");
+
+
+  if (FTRUNCATE (fd, 800) < 0)
+    error (EXIT_FAILURE, errno, "size reduction with ftruncate failed");
+
+  if (fstat (fd, &st) < 0 || st.st_size != 800)
+    error (EXIT_FAILURE, 0, "size after reduction with ftruncate incorrect");
+
+  /* The following test covers more than POSIX.  POSIX does not require
+     that ftruncate() can increase the file size.  But we are testing
+     Unix systems.  */
+  if (FTRUNCATE (fd, 1200) < 0)
+    error (EXIT_FAILURE, errno, "size increase with ftruncate failed");
+
+  if (fstat (fd, &st) < 0 || st.st_size != 1200)
+    error (EXIT_FAILURE, 0, "size after increase with ftruncate incorrect");
+
+
+  if (TRUNCATE (name, 800) < 0)
+    error (EXIT_FAILURE, errno, "size reduction with truncate failed");
+
+  if (fstat (fd, &st) < 0 || st.st_size != 800)
+    error (EXIT_FAILURE, 0, "size after reduction with truncate incorrect");
+
+  /* The following test covers more than POSIX.  POSIX does not require
+     that truncate() can increase the file size.  But we are testing
+     Unix systems.  */
+  if (TRUNCATE (name, 1200) < 0)
+    error (EXIT_FAILURE, errno, "size increase with truncate failed");
+
+  if (fstat (fd, &st) < 0 || st.st_size != 1200)
+    error (EXIT_FAILURE, 0, "size after increase with truncate incorrect");
+
+
+  close (fd);
+  unlink (name);
+
+  return 0;
+}
