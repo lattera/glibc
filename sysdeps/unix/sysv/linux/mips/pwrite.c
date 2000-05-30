245061db80961b1b9bf0a8cf059d63399e2983ca
Andreas Jaeger <aj@suse.de>
2000-05-30 12:50:17 +0000

    * sysdeps/unix/sysv/linux/mips/truncate64.c: New file. 	* sysdeps/unix/sysv/linux/mips/ftruncate64.c: New file. 	* sysdeps/unix/sysv/linux/mips/pread64.c: Readded file. 	* sysdeps/unix/sysv/linux/mips/pwrite64.c: Readded file. 	* sysdeps/unix/sysv/linux/mips/pread.c: Readded file. 	* sysdeps/unix/sysv/linux/mips/pwrite.c: Readded file.

---
+++ sysdeps/unix/sysv/linux/mips/pwrite.c
@@ -0,0 +1,71 @@
+/* Copyright (C) 1997, 1998, 2000 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Contributed by Ulrich Drepper <drepper@cygnus.com>, 1997.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <errno.h>
+#include <unistd.h>
+
+#include <sysdep.h>
+#include <sys/syscall.h>
+
+#include <kernel-features.h>
+
+#if defined __NR_pwrite || __ASSUME_PWRITE_SYSCALL > 0
+
+extern ssize_t __syscall_pwrite (int fd, const void *buf, size_t count,
+				 int dummy, off_t offset_hi, off_t offset_lo);
+
+# if __ASSUME_PWRITE_SYSCALL == 0
+static ssize_t __emulate_pwrite (int fd, const void *buf, size_t count,
+				 off_t offset) internal_function;
+# endif
+
+ssize_t
+__libc_pwrite (fd, buf, count, offset)
+     int fd;
+     const void *buf;
+     size_t count;
+     off_t offset;
+{
+  ssize_t result;
+
+  /* First try the syscall.  */
+# if defined(__MIPSEB__)
+  result = INLINE_SYSCALL (pwrite, 6, fd, buf, count, 0, 0, offset);
+# elif defined(__MIPSEL__)
+  result = INLINE_SYSCALL (pwrite, 6, fd, buf, count, 0, offset, 0);
+# endif
+# if __ASSUME_PWRITE_SYSCALL == 0
+  if (result == -1 && errno == ENOSYS)
+    /* No system call available.  Use the emulation.  */
+    result = __emulate_pwrite (fd, buf, count, offset);
+# endif
+
+  return result;
+}
+
+strong_alias (__libc_pwrite, __pwrite)
+weak_alias (__libc_pwrite, pwrite)
+
+# define __libc_pwrite(fd, buf, count, offset) \
+     static internal_function __emulate_pwrite (fd, buf, count, offset)
+#endif
+
+#if __ASSUME_PWRITE_SYSCALL == 0
+# include <sysdeps/posix/pwrite.c>
+#endif
