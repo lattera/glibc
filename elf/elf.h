68979757bc2813c52f751a1e7fbc784567798b31
Ulrich Drepper <drepper@redhat.com>
2000-06-02 00:27:08 +0000

    Update.
    	* elf/elf.h (ELF64_R_INFO): Cast sym value to Elf64_Xword before
    	shifting.

--- elf/elf.h
+++ elf/elf.h
@@ -476,7 +476,7 @@ typedef struct
 
 #define ELF64_R_SYM(i)			((i) >> 32)
 #define ELF64_R_TYPE(i)			((i) & 0xffffffff)
-#define ELF64_R_INFO(sym,type)		(((sym) << 32) + (type))
+#define ELF64_R_INFO(sym,type)		((((Elf64_Xword) (sym)) << 32) + (type))
 
 /* Program segment header.  */
 
