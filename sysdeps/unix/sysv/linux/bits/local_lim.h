104d06900c76cb7211d1b5556fb20292098335a8
Ulrich Drepper <drepper@redhat.com>
2000-06-08 06:32:43 +0000

    Update.
    	* sysdeps/unix/sysv/linux/bits/local_lim.h: Also undefine LINK_MAX
    	if not defined by the user.

--- sysdeps/unix/sysv/linux/bits/local_lim.h
+++ sysdeps/unix/sysv/linux/bits/local_lim.h
@@ -1,5 +1,5 @@
 /* Minimum guaranteed maximum values for system limits.  Linux version.
-   Copyright (C) 1993, 94, 95, 96, 97, 98 Free Software Foundation, Inc.
+   Copyright (C) 1993, 94, 95, 96, 97, 98, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -17,10 +17,14 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
-/* The kernel header pollutes the namespace with the NR_OPEN symbol.
+/* The kernel header pollutes the namespace with the NR_OPEN symbol
+   and defines LINK_MAX although filesystems have different maxima.
    Remove this after including the header if necessary.  */
 #ifndef NR_OPEN
 # define __undef_NR_OPEN
+#endif
+#ifndef LINK_MAX
+# define __undef_LINK_MAX
 #endif
 
 /* The kernel sources contain a file with all the needed information.  */
@@ -30,6 +34,11 @@
 #ifdef __undef_NR_OPEN
 # undef NR_OPEN
 # undef __undef_NR_OPEN
+#endif
+/* Have to remove LINK_MAX?  */
+#ifdef __undef_LINK_MAX
+# undef LINK_MAX
+# undef __undef_LINK_MAX
 #endif
 
 /* Maximum amount by which a process can descrease its asynchronous I/O
