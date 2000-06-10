c79c9039b3fe3904c994afcfbae1e8c620043329
Ulrich Drepper <drepper@redhat.com>
2000-06-10 00:05:44 +0000

    Update.
    2000-06-09  Jes Sorensen  <jes@linuxcare.com>
    	    Ulrich Drepper  <drepper@redhat.com>
    
    	* sysdeps/unix/sysv/linux/ia64/sigaction.c: New file.
    	* sysdeps/unix/sysv/linux/ia64/sigpending.c: New file.
    	* sysdeps/unix/sysv/linux/ia64/sigprocmask.c: New file.
    	* sysdeps/unix/sysv/linux/ia64/sigsuspend.c: New file.
    	* sysdeps/unix/sysv/linux/ia64/bits/sigaction.h: New file.
    	* sysdeps/unix/sysv/linux/ia64/bits/sigstack.h: New file.

---
+++ sysdeps/unix/sysv/linux/ia64/sigprocmask.c
@@ -0,0 +1,46 @@
+/* Copyright (C) 1997, 1998, 1999, 2000 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Linux/IA64 specific sigprocmask
+   Written by Jes Sorensen, <Jes.Sorensen@cern.ch>, April 1999.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+/* Linux/ia64 only has rt signals, thus we do not even want to try falling
+   back to the old style signals as the default Linux handler does. */
+
+#include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include <sysdep.h>
+#include <sys/syscall.h>
+
+extern int __syscall_rt_sigprocmask (int, const sigset_t *, sigset_t *,
+				     size_t);
+
+/* Get and/or change the set of blocked signals.  */
+int
+__sigprocmask (how, set, oset)
+     int how;
+     const sigset_t *set;
+     sigset_t *oset;
+{
+
+  /* XXX The size argument hopefully will have to be changed to the
+     real size of the user-level sigset_t.  */
+  return INLINE_SYSCALL (rt_sigprocmask, 4, how, set, oset, _NSIG / 8);
+}
+weak_alias (__sigprocmask, sigprocmask)
