ce723583fc8c5241879dfb5c1d7f940ded72f283
Ulrich Drepper <drepper@redhat.com>
2000-06-17 19:22:43 +0000

    Update.
    	* stdio-common/tiformat.c (sprint_ints): Add tests for I flag.
    	* libio/tst_wprintf.c (main): Add test for I flag.

--- libio/tst_wprintf.c
+++ libio/tst_wprintf.c
@@ -6,5 +6,6 @@ main (int argc, char *argv[])
 {
   fputws (L"Hello world!\n", stdout);
   wprintf (L"This %s a %ls string: %d\n", "is", L"mixed", 42);
+  wprintf (L"%Iu\n", 0xfeedbeef);
   return 0;
 }
