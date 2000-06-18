a9c464ab1b027f8b4308e3b4a03d44fdaabf9609
Ulrich Drepper <drepper@redhat.com>
2000-06-18 05:20:07 +0000

    Update.
    	* sysdeps/unix/sysv/linux/alpha/pathconf.c: Define LINK_MAX.

--- sysdeps/unix/sysv/linux/alpha/pathconf.c
+++ sysdeps/unix/sysv/linux/alpha/pathconf.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1995, 1996, 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995, 1996, 1998, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -24,6 +24,10 @@
 #include <sys/statfs.h>
 
 #include <linux_fsinfo.h>
+
+
+/* The Linux kernel header mentioned this as a kind of generic value.  */
+#define LINUX_LINK_MAX	127
 
 static long int default_pathconf (const char *path, int name);
 
