43c81906dbf4c2bf9f89a53a08f0e05c7adf4113
Greg McGary <greg@mcgary.org>
2000-06-26 16:48:10 +0000

    * sysdeps/i386/asm-syntax.h (R, MEM, MEM_DISP, MEM_INDEX,
    INSN1, INSN2, TEXT, GLOBL): Remove macros. 
    (ALIGN): Cleanup surrounding conditionals.
    	* sysdeps/i386/addmul_1.S: Replace asm-syntax macros with gas syntax.
    	Call ENTER/LEAVE macros.
    	* sysdeps/i386/mul_1.S: Replace asm-syntax macros with gas syntax.
    	* sysdeps/i386/submul_1.S: Likewise.
    
    	* sysdeps/i386/i586/addmul_1.S: Replace asm-syntax macros with gas
    	syntax.  Define & use symbolic argument stack offsets.  Call
    	ENTER/LEAVE macros.
    	* sysdeps/i386/i586/mul_1.S: Likewise.
    	* sysdeps/i386/i586/submul_1.S: Likewise.
    2000-06-26  Greg McGary  <greg@mcgary.org>
    
    	* sysdeps/i386/asm-syntax.h (R, MEM, MEM_DISP, MEM_INDEX,
    	INSN1, INSN2, TEXT, GLOBL): Remove macros.
    	(ALIGN): Cleanup surrounding conditionals.
    
    	* sysdeps/i386/addmul_1.S: Replace asm-syntax macros with gas syntax.
    	Call ENTER/LEAVE macros.
    	* sysdeps/i386/mul_1.S: Replace asm-syntax macros with gas syntax.
    	* sysdeps/i386/submul_1.S: Likewise.
    
    	* sysdeps/i386/i586/addmul_1.S: Replace asm-syntax macros with gas
    	syntax.  Define & use symbolic argument stack offsets.  Call
    	ENTER/LEAVE macros.
    	* sysdeps/i386/i586/mul_1.S: Likewise.
    	* sysdeps/i386/i586/submul_1.S: Likewise.

--- sysdeps/i386/asm-syntax.h
+++ sysdeps/i386/asm-syntax.h
@@ -1,5 +1,5 @@
 /* Definitions for x86 syntax variations.
-   Copyright (C) 1992, 1994, 1995, 1997 Free Software Foundation, Inc.
+   Copyright (C) 1992, 1994, 1995, 1997, 2000 Free Software Foundation, Inc.
 
    This file is part of the GNU C Library.  Its master source is NOT part of
    the C library, however.  The master source lives in the GNU MP Library.
@@ -20,61 +20,23 @@
    Boston, MA 02111-1307, USA.  */
 
 #undef ALIGN
-
-#ifdef GAS_SYNTAX
-#define R(r) %r
-#define MEM(base)(base)
-#define MEM_DISP(base,displacement)displacement(R(base))
-#define MEM_INDEX(base,index,size)(R(base),R(index),size)
-#ifdef __STDC__
-#define INSN1(mnemonic,size_suffix,dst)mnemonic##size_suffix dst
-#define INSN2(mnemonic,size_suffix,dst,src)mnemonic##size_suffix src,dst
+#if defined NOLOG_ALIGN || defined HAVE_ELF
+# define ALIGN(log) .align 1<<log
 #else
-#define INSN1(mnemonic,size_suffix,dst)mnemonic/**/size_suffix dst
-#define INSN2(mnemonic,size_suffix,dst,src)mnemonic/**/size_suffix src,dst
-#endif
-#define TEXT .text
-#define ALIGN(log) .align log
-#define GLOBL .globl
-#endif
-
-#ifdef INTEL_SYNTAX
-#define R(r) r
-#define MEM(base)[base]
-#define MEM_DISP(base,displacement)[base+(displacement)]
-#define MEM_INDEX(base,index,size)[base+index*size]
-#define INSN1(mnemonic,size_suffix,dst)mnemonic dst
-#define INSN2(mnemonic,size_suffix,dst,src)mnemonic dst,src
-#define TEXT .text
-#define ALIGN(log) .align log
-#define GLOBL .globl
-#endif
-
-#ifdef BROKEN_ALIGN
-#undef ALIGN
-#define ALIGN(log) .align log,0x90
-#endif
-
-#if !defined (NOLOG_ALIGN) && defined (HAVE_ELF)
-#define NOLOG_ALIGN
-#endif
-
-#ifdef NOLOG_ALIGN
-#undef ALIGN
-#define ALIGN(log) .align 1<<log
+# define ALIGN(log) .align log
 #endif
 
 #undef L
 #ifdef __ELF__
-#ifdef __STDC__
-#define L(body)	.L##body
+# ifdef __STDC__
+#  define L(body) .L##body
+# else
+#  define L(body) .L/**/body
+# endif
 #else
-#define L(body)	.L/**/body
-#endif
-#else
-#ifdef __STDC__
-#define L(body) L##body
-#else
-#define L(body) L/**/body
-#endif
+# ifdef __STDC__
+#  define L(body) L##body
+# else
+#  define L(body) L/**/body
+# endif
 #endif
