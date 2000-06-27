5b905722f66719de502ecd6129ef9a1bda4f9f47
Andreas Jaeger <aj@suse.de>
2000-06-27 12:14:09 +0000

    * Makefile (tests): Add tests from tests-mbwc subdirectory,
    comment them out for now. 
    (subdir-dirs): New for tests-mbwc, add also vpaths.
    	* Makefile (tests): Add tests from tests-mbwc subdirectory,
    	comment them out for now.
    	(subdir-dirs): New for tests-mbwc, add also vpaths.

---
+++ localedata/tests-mbwc/tgn_locdef.h
@@ -0,0 +1,16 @@
+#ifndef TGN_LOCDEF_H
+#define TGN_LOCDEF_H
+
+/* Defines for all locales used in the suite.  */
+
+#define TST_LOC_de	"de_DE.ISO-8859-1"
+
+/* For US we use ANSI_X3.4-1968 (ASCII).  */
+#define TST_LOC_enUS	"en_US.ANSI_X3.4-1968"
+
+#define TST_LOC_eucJP	"ja_JP.EUC-JP"
+
+/* End marker - must appear in each table.  */
+#define	TST_LOC_end	"lastEntry"
+
+#endif /* TGN_LOCDEF_H */
