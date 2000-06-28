93fc3eb7b49fdbb8917491499e39576f2f35bf12
Andreas Jaeger <aj@suse.de>
2000-06-28 08:24:36 +0000

    (TST_LOC_de_UTF8): Added.
    2000-06-28  Andreas Jaeger  <aj@suse.de>
    
    	* tests-mbwc/tgn_locdef.h (TST_LOC_de_UTF8): Added.

--- localedata/tests-mbwc/tgn_locdef.h
+++ localedata/tests-mbwc/tgn_locdef.h
@@ -3,14 +3,19 @@
 
 /* Defines for all locales used in the suite.  */
 
+/* German locale with ISO-8859-1.  */
 #define TST_LOC_de	"de_DE.ISO-8859-1"
 
 /* For US we use ANSI_X3.4-1968 (ASCII).  */
 #define TST_LOC_enUS	"en_US.ANSI_X3.4-1968"
 
+/* Japanese locale with EUC-JP.  */
 #define TST_LOC_eucJP	"ja_JP.EUC-JP"
 
-/* End marker - must appear in each table.  */
+/* German locale with UTF-8.  */
+#define TST_LOC_de_UTF8	"de_DE.UTF-8"
+
+/* End marker - must appear in each table as last entry.  */
 #define	TST_LOC_end	"lastEntry"
 
 #endif /* TGN_LOCDEF_H */
