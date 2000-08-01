9bd0bec2c9f4e21332f37618faff5a01c7d876de
Andreas Jaeger <aj@suse.de>
2000-08-01 16:12:06 +0000

    Update.
    	* nss/nss_files/files-ethers.c: Use case-insensitive lookup.
    	(LINE_PARSER): Allow any number of spaces between ip address and number.
    	Fixes PR libc/1833, reported by rp010gf@voruta.vu.lt.

--- nss/nss_files/files-ethers.c
+++ nss/nss_files/files-ethers.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 1997, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -45,7 +45,7 @@ LINE_PARSER
        if (cnt < 5)
 	 INT_FIELD (number, ISCOLON , 0, 16, (unsigned int))
        else
-	 INT_FIELD (number, isspace, 0, 16, (unsigned int))
+	 INT_FIELD (number, isspace, 1, 16, (unsigned int))
 
        if (number > 0xff)
 	 return 0;
@@ -60,7 +60,7 @@ LINE_PARSER
 
 DB_LOOKUP (hostton, 1 + strlen (name), (".%s", name),
 	   {
-	     if (strcmp (result->e_name, name) == 0)
+	     if (__strcasecmp (result->e_name, name) == 0)
 	       break;
 	   }, const char *name)
 
