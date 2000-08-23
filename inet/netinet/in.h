24c7f4c14bc1fa62e9599c3acc98cf76c2bef8be
Ulrich Drepper <drepper@redhat.com>
2000-08-23 07:15:54 +0000

    Update.
    2000-08-23  Jakub Jelinek  <jakub@redhat.com>
    
    	* inet/netinet/in.h (struct in6_addr): Don't enforce 64bit alignment
    	on 64bit arches.

--- inet/netinet/in.h
+++ inet/netinet/in.h
@@ -171,14 +171,10 @@ struct in6_addr
 	uint8_t	u6_addr8[16];
 	uint16_t u6_addr16[8];
 	uint32_t u6_addr32[4];
-#if ULONG_MAX > 0xffffffff
-	uint64_t u6_addr64[2];
-#endif
       } in6_u;
 #define s6_addr			in6_u.u6_addr8
 #define s6_addr16		in6_u.u6_addr16
 #define s6_addr32		in6_u.u6_addr32
-#define s6_addr64		in6_u.u6_addr64
   };
 
 extern const struct in6_addr in6addr_any;        /* :: */
