8cb9c6509bde1537467b3432eb38ac26cb7f5e11
Ulrich Drepper <drepper@redhat.com>
2000-09-06 21:04:46 +0000

    Update.
    	* stdlib/Makefile (tests): Add tst-bsearch.
    	* stdlib/tst-bsearch.c: New file.

---
+++ stdlib/tst-bsearch.c
@@ -0,0 +1,84 @@
+/* Copyright (C) 2000 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Contributed by Ulrich Drepper <drepper@redhat.com>, 2000.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct entry
+{
+  int val;
+  const char *str;
+} arr[] =
+{
+  { 0, "zero" },
+  { 1, "one" },
+  { 2, "two" },
+  { 3, "three" },
+  { 4, "four" },
+  { 5, "five" },
+  { 6, "six" },
+  { 7, "seven" },
+  { 8, "eight" },
+  { 9, "nine" },
+  { 10, "ten" }
+};
+#define narr (sizeof (arr) / sizeof (arr[0]))
+
+
+static int
+comp (const void *p1, const void *p2)
+{
+  struct entry *e1 = (struct entry *) p1;
+  struct entry *e2 = (struct entry *) p2;
+
+  return e1->val - e2->val;
+}
+
+
+int
+main (void)
+{
+  int cnt;
+  int result = 0;
+
+  for (cnt = 0; cnt < narr; ++cnt)
+    {
+      struct entry key;
+      struct entry *res;
+
+      key.val = arr[cnt].val;
+
+      res = (struct entry *) bsearch (&key, arr, narr, sizeof (arr[0]), comp);
+      if (res == NULL)
+	{
+	  printf ("entry %d not found\n", cnt);
+	  result = 1;
+	}
+      else if (res != &arr[cnt])
+	{
+	  puts ("wrong entry returned");
+	  result = 1;
+	}
+    }
+
+  if (result == 0)
+    puts ("all OK");
+
+  return result;
+}
