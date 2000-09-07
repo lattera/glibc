04ba87901c8be7f38906147854e0fc1e4351f486
Ulrich Drepper <drepper@redhat.com>
2000-09-07 03:49:56 +0000

    Update.
    	* catgets/Depend: New file.  Add intl.
    	* catgets/Makefile (tests): Add tst-catgets.
    	(generated): Remove de.msg.
    	(generated-dirs): Add de.
    	Add dependency if tst-catgets output on de/libc.cat.
    	* catgets/tst-catgets.c: New file.

---
+++ catgets/tst-catgets.c
@@ -0,0 +1,66 @@
+#include <mcheck.h>
+#include <nl_types.h>
+#include <stdio.h>
+#include <string.h>
+
+
+static const char *msgs[] =
+{
+#define INPUT(str)
+#define OUTPUT(str) str,
+#include <intl/msgs.h>
+};
+#define nmsgs (sizeof (msgs) / sizeof (msgs[0]))
+
+#define ROUNDS 5
+
+int
+main (void)
+{
+  int rnd;
+  int result = 0;
+
+  mtrace ();
+
+  /* We do this a few times to stress the memory handling.  */
+  for (rnd = 0; rnd < ROUNDS; ++rnd)
+    {
+      nl_catd cd = catopen ("libc", 0);
+      int cnt;
+
+      if (cd == (nl_catd) -1)
+	{
+	  printf ("cannot load catalog: %m\n");
+	  result = 1;
+	  break;
+	}
+
+      /* Go through all the messages and compare the result.  */
+      for (cnt = 0; cnt < nmsgs; ++cnt)
+	{
+	  char *trans;
+
+	  trans = catgets (cd, 1, 1 + cnt,
+			   "+#+# is this comes backs it's an error");
+
+	  if (trans == NULL)
+	    {
+	      printf ("catgets return NULL for %d\n", cnt);
+	      result = 1;
+	    }
+	  else if (strcmp (trans, msgs[cnt]) != 0)
+	    {
+	      printf ("expected \"%s\", got \"%s\"\n", msgs[cnt], trans);
+	      result = 1;
+	    }
+	}
+
+      if (catclose (cd) != 0)
+	{
+	  printf ("catclose failed: %m\n");
+	  result = 1;
+	}
+    }
+
+  return result;
+}
