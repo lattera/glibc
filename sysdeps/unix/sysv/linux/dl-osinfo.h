b3e6e3e367322be1d1b8290938153fae35d5c72c
Andreas Jaeger <aj@suse.de>
2000-09-09 10:08:49 +0000

    Include <string.h> for strncpy prototype.

--- sysdeps/unix/sysv/linux/dl-osinfo.h
+++ sysdeps/unix/sysv/linux/dl-osinfo.h
@@ -17,6 +17,7 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
+#include <string.h>
 #include <sys/sysctl.h>
 #include <sys/utsname.h>
 #include "kernel-features.h"
