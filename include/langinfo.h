3e2ad31c49b47f085f13a93fd45e48e2a03554be
Andreas Jaeger <aj@suse.de>
2000-09-26 15:09:33 +0000

    (__nl_langinfo_l): Add prototype.

--- include/langinfo.h
+++ include/langinfo.h
@@ -1,1 +1,7 @@
-#include <locale/langinfo.h>
+#ifndef _LANGINFO_H
+# include <locale/langinfo.h>
+
+/* Internal prototypes.  */
+extern char *__nl_langinfo_l (nl_item __item, __locale_t l);
+
+#endif
