ee6c5330273edda1ab102ad780d1984aca055e77
Ulrich Drepper <drepper@redhat.com>
2000-09-29 01:32:14 +0000

    Update.
    2000-09-28  Ulrich Drepper  <drepper@redhat.com>
    
    	* sysdeps/unix/sysv/linux/bits/resource.h: Add RLIMIT_LOCKS.
    	* sysdeps/unix/sysv/linux/arm/bits/resource.h: Likewise.
    	* sysdeps/unix/sysv/linux/i386/bits/resource.h: Likewise.
    	* sysdeps/unix/sysv/linux/mips/bits/resource.h: Likewise.
    	* sysdeps/unix/sysv/linux/powerpc/bits/resource.h: Likewise.
    	* sysdeps/unix/sysv/linux/sh/bits/resource.h: Likewise.
    	* sysdeps/unix/sysv/linux/sparc/bits/resource.h: Likewise.
    
    2000-09-28  Martin Schwidefsksy  <schwidefsky@de.ibm.com>
    
    2000-09-28  Denis Joseph Barrow  <djbarrow@de.ibm.com>

--- sysdeps/unix/sysv/linux/bits/resource.h
+++ sysdeps/unix/sysv/linux/bits/resource.h
@@ -1,5 +1,5 @@
 /* Bit values & structures for resource limits.  Linux version.
-   Copyright (C) 1994, 1996, 1997, 1998, 1999 Free Software Foundation, Inc.
+   Copyright (C) 1994,1996,1997,1998,1999,2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -76,7 +76,11 @@ enum __rlimit_resource
   RLIMIT_MEMLOCK = 8,
 #define RLIMIT_MEMLOCK RLIMIT_MEMLOCK
 
-  RLIM_NLIMITS = 10
+  /* Maximum number of file locks.  */
+  RLIMIT_LOCKS = 10,
+#define RLIMIT_LOCKS RLIMIT_LOCKS
+
+  RLIM_NLIMITS = 11
 #define RLIMIT_NLIMITS RLIMIT_NLIMITS
 #define RLIM_NLIMITS RLIM_NLIMITS
 };
