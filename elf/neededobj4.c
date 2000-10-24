ef044fc8bfae2048a578339c15b155bc955805ef
Ulrich Drepper <drepper@redhat.com>
2000-10-24 16:59:10 +0000

    Object for unload testing.

---
+++ elf/neededobj4.c
@@ -0,0 +1,11 @@
+extern void a_function (void);
+extern void b_function (void);
+extern void c_function (void);
+
+void
+d_function (void)
+{
+  a_function ();
+  b_function ();
+  c_function ();
+}
