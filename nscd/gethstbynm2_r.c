9ffec5b234e7fe5d16f65d8655b036e64fe00bad
Ulrich Drepper <drepper@redhat.com>
2000-10-28 06:52:11 +0000

    Update.
    	* nscd/gethstbynm2_r.c (__inet_aton): Define as inet_aton since
    	the former is not exported.

--- nscd/gethstbynm2_r.c
+++ nscd/gethstbynm2_r.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1996, 1997, 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 1997, 1998, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1996.
 
@@ -35,5 +35,7 @@
 #define HANDLE_DIGITS_DOTS	1
 #define HAVE_LOOKUP_BUFFER	1
 #define HAVE_AF			1
+
+#define __inet_aton inet_aton
 
 #include "../nss/getXXbyYY_r.c"
