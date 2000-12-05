b75905aee7f0ed09640f3e43618da7b28e8959f5
Ulrich Drepper <drepper@redhat.com>
2000-12-05 04:03:03 +0000

    Nothing.

--- sysdeps/unix/sysv/aix/write.c
+++ sysdeps/unix/sysv/aix/write.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1999 Free Software Foundation, Inc.
+/* Copyright (C) 1999, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1999.
 
