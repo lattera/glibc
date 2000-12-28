e9ffd39f8c04e9e0300baef5f5c0eb1a46a2050a
Andreas Jaeger <aj@suse.de>
2000-12-28 15:08:51 +0000

    Update.

--- include/argp.h
+++ include/argp.h
@@ -1,9 +1,1 @@
-#ifndef _ARGP_H
 #include <argp/argp.h>
-
-/* This hack to allow programs that know what's going on to call argp
-   recursively.  If someday argp is changed not to use the non-reentrant
-   getopt interface, we can get rid of this shit.  XXX */
-extern void _argp_unlock_xxx (void);
-
-#endif
