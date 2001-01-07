bf2c10ded6bd31d7b923408f099b32b549acb934
Ulrich Drepper <drepper@redhat.com>
2001-01-07 20:02:56 +0000

    Update.
    	* sysdeps/generic/bits/byteswap.h (__bswap_64): Partly revert last
    	patch.  We must use unsigned int for l member of union.

--- sysdeps/generic/bits/byteswap.h
+++ sysdeps/generic/bits/byteswap.h
@@ -1,5 +1,5 @@
 /* Macros to swap the order of bytes in integer values.
-   Copyright (C) 1997, 1998, 2000 Free Software Foundation, Inc.
+   Copyright (C) 1997, 1998, 2000, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -53,7 +53,7 @@ __bswap_32 (unsigned int __bsx)
 
 #if defined __GNUC__ && __GNUC__ >= 2
 /* Swap bytes in 64 bit value.  */
-#define __bswap_constant_64(x) \
+# define __bswap_constant_64(x) \
      ((((x) & 0xff00000000000000ull) >> 56)				      \
       | (((x) & 0x00ff000000000000ull) >> 40)				      \
       | (((x) & 0x0000ff0000000000ull) >> 24)				      \
@@ -66,7 +66,7 @@ __bswap_32 (unsigned int __bsx)
 # define __bswap_64(x) \
      (__extension__							      \
       ({ union { __extension__ unsigned long long int __ll;		      \
-		 unsigned long int __l[2]; } __w, __r;			      \
+		 unsigned int __l[2]; } __w, __r;			      \
          if (__builtin_constant_p (x))					      \
 	   __r.__ll = __bswap_constant_64 (x);				      \
 	 else								      \
