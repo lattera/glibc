1950ebc6cb9f699db2cb62b3a203e95d6e1f5e65
Ulrich Drepper <drepper@redhat.com>
2001-01-08 08:59:36 +0000

    Update.
    	* stdio-common/printf.c: Undefine printf.

--- stdio-common/printf.c
+++ stdio-common/printf.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1995, 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995, 1996, 1997, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -18,6 +18,8 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+
+#undef printf
 
 /* Write formatted output to stdout from the format string FORMAT.  */
 /* VARARGS1 */
