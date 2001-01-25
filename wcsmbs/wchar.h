d9826675fcf2575c5961d6352def915d8b06e4d5
Ulrich Drepper <drepper@redhat.com>
2001-01-25 08:43:53 +0000

    Update.
    	* wcsmbs/wchar.h: Get FILE definition for XPG5.

--- wcsmbs/wchar.h
+++ wcsmbs/wchar.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1995-1999, 2000 Free Software Foundation, Inc.
+/* Copyright (C) 1995-1999, 2000, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -31,6 +31,9 @@
 #ifdef _WCHAR_H
 /* Get FILE definition.  */
 # define __need___FILE
+# ifdef __USE_UNIX98
+#  define __need_FILE
+# endif
 # include <stdio.h>
 /* Get va_list definition.  */
 # define __need___va_list
