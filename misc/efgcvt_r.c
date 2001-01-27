1420df2c2ecabd2037bbf9dddbc0e4f9522f2aa1
Andreas Jaeger <aj@suse.de>
2001-01-27 10:54:50 +0000

    Include <float.h>.

--- misc/efgcvt_r.c
+++ misc/efgcvt_r.c
@@ -1,5 +1,5 @@
 /* Compatibility functions for floating point formatting, reentrant versions.
-   Copyright (C) 1995,1996,1997,1998,1999,2000 Free Software Foundation, Inc.
+   Copyright (C) 1995,1996,1997,1998,1999,2000,2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -18,6 +18,7 @@
    Boston, MA 02111-1307, USA.  */
 
 #include <errno.h>
+#include <float.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
