4fef60b393d72c0f45584a9eacbd197ae7918fc8
Ulrich Drepper <drepper@redhat.com>
2001-01-28 06:41:47 +0000

    Update.
    	* sysdeps/unix/stime.c: Include <stddef.h> for NULL.
    	* sysdeps/unix/time.c: Likewise.

--- sysdeps/unix/time.c
+++ sysdeps/unix/time.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1997, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -16,6 +16,7 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
+#include <stddef.h>		/* For NULL.  */
 #include <time.h>
 #include <sys/time.h>
 
