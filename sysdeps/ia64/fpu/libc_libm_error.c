2d7faa721d72da439dd3b732ff9a41d096438cc8
Ulrich Drepper <drepper@redhat.com>
2001-02-19 22:11:23 +0000

    Update.
    2001-02-19  Ulrich Drepper  <drepper@redhat.com>
    
    	* sysdeps/ia64/fpu/Makefile (libm-sysdep-routines): Add libm_error,
    	libm_frexp4, libm_frexp4f, libm_frexp4l, and all the functions also
    	in libc.
    	(routines): Replace libm_error with libc_libm_error.
    	* sysdeps/ia64/fpu/Dist: Add libc_libm_error.c.
    	* sysdeps/ia64/fpu/libc_libm_error.c: New file.

---
+++ sysdeps/ia64/fpu/libc_libm_error.c
@@ -0,0 +1,13 @@
+/* Error handling in libm-style for libc.  */
+
+#include <errno.h>
+
+#include "libm_support.h"
+
+
+void
+__libm_error_support (void *arg1, void *arg2, void *retval,
+		      error_types input_tag)
+{
+  __set_errno (ERANGE);
+}
