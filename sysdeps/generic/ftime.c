37ada9da6e05ab37eb3031c473b9898d6def4334
Ulrich Drepper <drepper@redhat.com>
2001-02-21 20:17:48 +0000

    Update.
    	* sysdeps/generic/ftime.c: Include <time.h>.

--- sysdeps/generic/ftime.c
+++ sysdeps/generic/ftime.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1994, 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1994, 1996, 1997, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -16,6 +16,7 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
+#include <time.h>
 #include <sys/timeb.h>
 #include <sys/time.h>
 #include <errno.h>
