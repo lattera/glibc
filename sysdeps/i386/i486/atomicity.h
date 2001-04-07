5360538bd8689091c2c00d2f3afd12b536a0b0b0
Ulrich Drepper <drepper@redhat.com>
2001-04-07 00:19:07 +0000

    Update.
    	* sysdeps/i386/i486/atomicity.h: Clean up the asms.

--- sysdeps/i386/i486/atomicity.h
+++ sysdeps/i386/i486/atomicity.h
@@ -1,5 +1,5 @@
 /* Low-level functions for atomic operations.  ix86 version, x >= 4.
-   Copyright (C) 1997, 2000 Free Software Foundation, Inc.
+   Copyright (C) 1997, 2000, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -29,7 +29,7 @@ exchange_and_add (volatile uint32_t *mem, uint32_t val)
 {
   register uint32_t result;
   __asm__ __volatile__ ("lock; xaddl %0,%2"
-			: "=r" (result) : "0" (val), "m" (*mem) : "memory");
+			: "=r" (result), "=m" (*mem) : "0" (val), "1" (*mem));
   return result;
 }
 
@@ -37,8 +37,8 @@ static inline void
 __attribute__ ((unused))
 atomic_add (volatile uint32_t *mem, int val)
 {
-  __asm__ __volatile__ ("lock; addl %0,%1"
-			: : "ir" (val), "m" (*mem) : "memory");
+  __asm__ __volatile__ ("lock; addl %1,%0"
+			: "=m" (*mem) : "ir" (val), "0" (*mem));
 }
 
 static inline char
@@ -50,7 +50,7 @@ compare_and_swap (volatile long int *p, long int oldval, long int newval)
 
   __asm__ __volatile__ ("lock; cmpxchgl %3, %1; sete %0"
                         : "=q" (ret), "=m" (*p), "=a" (readval)
-                        : "r" (newval), "m" (*p), "a" (oldval));
+                        : "r" (newval), "1" (*p), "a" (oldval));
   return ret;
 }
 
