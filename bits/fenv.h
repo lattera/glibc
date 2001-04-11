122dbcec35d2ab75b0aa2d156684135dda153fb8
Ulrich Drepper <drepper@redhat.com>
2001-04-11 04:45:43 +0000

    Update.
    2001-04-11  Hans-Peter Nilsson  <hp@axis.com>
    
    	* sysdeps/generic/bits/fenv.h (fexcept_t): Change default type
    	from struct to unsigned int.

--- bits/fenv.h
+++ bits/fenv.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1997, 1998, 1999, 2000 Free Software Foundation, Inc.
+/* Copyright (C) 1997, 1998, 1999, 2000, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -40,13 +40,8 @@
    We define no macro which signals no rounding mode is selectable.  */
 
 
-/* Type representing exception flags.
-   XXX Probably we should also include the signal handler here.  */
-typedef struct
-  {
-    unsigned int __flags;
-  }
-fexcept_t;
+/* Type representing exception flags.  */
+typedef unsigned int fexcept_t;
 
 
 /* Type representing floating-point environment.  */
