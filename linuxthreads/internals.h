e6dcf35202903067d84f0da7ee997679ccf293c6
Andreas Jaeger <aj@suse.de>
2001-04-21 18:38:24 +0000

    Add __pthread_clock_gettime and __pthread_clock_settime prototypes.
    2001-04-21  Andreas Jaeger  <aj@suse.de>
    
    	* internals.h: Add __pthread_clock_gettime and
    	__pthread_clock_settime prototypes.

--- linuxthreads/internals.h
+++ linuxthreads/internals.h
@@ -510,6 +510,9 @@ extern int __pthread_spin_unlock (pthread_spinlock_t *__lock);
 extern int __pthread_spin_init (pthread_spinlock_t *__lock, int __pshared);
 extern int __pthread_spin_destroy (pthread_spinlock_t *__lock);
 
+extern int __pthread_clock_gettime (unsigned long long int freq, struct timespec *tp);
+extern void __pthread_clock_settime (unsigned long long int offset);
+
 
 /* Global pointers to old or new suspend functions */
 
