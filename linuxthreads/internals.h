18ac1129d1a88a3441a7f955c782c77adb78ab0f
Ulrich Drepper <drepper@redhat.com>
2001-04-23 20:03:56 +0000

    Update.
    	* internals.h: Fix parameter type for __pthread_clock_gettime and
    	__pthread_clock_settime.

--- linuxthreads/internals.h
+++ linuxthreads/internals.h
@@ -510,8 +510,8 @@ extern int __pthread_spin_unlock (pthread_spinlock_t *__lock);
 extern int __pthread_spin_init (pthread_spinlock_t *__lock, int __pshared);
 extern int __pthread_spin_destroy (pthread_spinlock_t *__lock);
 
-extern int __pthread_clock_gettime (unsigned long long int freq, struct timespec *tp);
-extern void __pthread_clock_settime (unsigned long long int offset);
+extern int __pthread_clock_gettime (hp_timing_t freq, struct timespec *tp);
+extern void __pthread_clock_settime (hp_timing_t offset);
 
 
 /* Global pointers to old or new suspend functions */
