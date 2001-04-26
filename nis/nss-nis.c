48a92e9d2da28b7bef8474f1effda68c13ed2fd1
Ulrich Drepper <drepper@redhat.com>
2001-04-26 13:44:29 +0000

    Update.
    2001-04-26  Ulrich Drepper  <drepper@redhat.com>
    
    	* nis/nss-nis.h: Move yperr2nss_tab definition into separate file
    	and rename to __yperr2nss_tab.  Change YPERR_COUNT into variable
    	__yperr2nss_count.
    	* nis/nss-nis.c: New file.
    	* nis/Makefile (libnss_compat-routines): Add nss-nis.
    	(libnss_nis-routines): Likewise.
    
    	* sysdeps/unix/sysv/linux/sparc/sparc64/get_clockfreq.c: Some more
    	optimizations.  Patch by Jakub Jelinek <jakub@redhat.com>.

--- nis/nss-nis.c
+++ nis/nss-nis.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1996 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -16,24 +16,32 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
-#ifndef _NIS_NSS_NIS_H
-#define _NIS_NSS_NIS_H	1
-
 #include <rpcsvc/ypclnt.h>
 
+#include "nss-nis.h"
 #include "nsswitch.h"
 
 
 /* Convert YP error number to NSS error number.  */
-extern const enum nss_status __yperr2nss_tab[];
-extern const unsigned int __yperr2nss_count;
-
-static inline enum nss_status
-yperr2nss (int errval)
+const enum nss_status __yperr2nss_tab[] =
 {
-  if ((unsigned int) errval > __yperr2nss_count)
-    return NSS_STATUS_UNAVAIL;
-  return __yperr2nss_tab[errval];
-}
-
-#endif /* nis/nss-nis.h */
+  [YPERR_SUCCESS] = NSS_STATUS_SUCCESS,
+  [YPERR_BADARGS] = NSS_STATUS_UNAVAIL,
+  [YPERR_RPC]     = NSS_STATUS_UNAVAIL,
+  [YPERR_DOMAIN]  = NSS_STATUS_UNAVAIL,
+  [YPERR_MAP]     = NSS_STATUS_UNAVAIL,
+  [YPERR_KEY]     = NSS_STATUS_NOTFOUND,
+  [YPERR_YPERR]   = NSS_STATUS_UNAVAIL,
+  [YPERR_RESRC]   = NSS_STATUS_TRYAGAIN,
+  [YPERR_NOMORE]  = NSS_STATUS_NOTFOUND,
+  [YPERR_PMAP]    = NSS_STATUS_UNAVAIL,
+  [YPERR_YPBIND]  = NSS_STATUS_UNAVAIL,
+  [YPERR_YPSERV]  = NSS_STATUS_UNAVAIL,
+  [YPERR_NODOM]   = NSS_STATUS_UNAVAIL,
+  [YPERR_BADDB]   = NSS_STATUS_UNAVAIL,
+  [YPERR_VERS]    = NSS_STATUS_UNAVAIL,
+  [YPERR_ACCESS]  = NSS_STATUS_UNAVAIL,
+  [YPERR_BUSY]    = NSS_STATUS_TRYAGAIN
+};
+const unsigned int __yperr2nss_count = (sizeof (__yperr2nss_tab)
+				        / sizeof (__yperr2nss_tab[0]));
