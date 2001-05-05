aa2e3ec3866ea0cde49ceeb9d9d4ef741af2fea9
Mark Kettenis <kettenis@gnu.org>
2001-05-05 14:20:35 +0000

    * sysdeps/mach/hurd/bits/environments.h: Add comment from sysdeps/generic/bits/environments.h.  Define _POSIX_V6_* values.
    2001-05-05  Mark Kettenis  <kettenis@gnu.org>
    
    	* sysdeps/mach/hurd/bits/environments.h: Add comment from
    	sysdeps/generic/bits/environments.h.  Define _POSIX_V6_* values.

--- sysdeps/mach/hurd/bits/environments.h
+++ sysdeps/mach/hurd/bits/environments.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1998, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -20,13 +20,34 @@
 # error "Never include this file directly.  Use <unistd.h> instead"
 #endif
 
-/* By default we have 32-bit wide `int', `long int', pointers and `off_t'.  */
+/* This header should define the following symbols under the described
+   situations.  A value `1' means that the model is always supported,
+   `-1' means it is never supported.  Undefined means it cannot be
+   statically decided.
+
+   _POSIX_V6_ILP32_OFF32   32bit int, long, pointers, and off_t type
+   _POSIX_V6_ILP32_OFFBIG  32bit int, long, and pointers and larger off_t type
+
+   _POSIX_V6_LP64_OFF32	   64bit long and pointers and 32bit off_t type
+   _POSIX_V6_LPBIG_OFFBIG  64bit long and pointers and large off_t type
+
+   The macros _XBS5_ILP32_OFF32, _XBS5_ILP32_OFFBIG, _XBS5_LP64_OFF32, and
+   _XBS5_LPBIG_OFFBIG were used in previous versions of the Unix standard
+   and are available only for compatibility.
+*/
+
+/* By default we have 32-bit wide `int', `long int', pointers and
+   `off_t'.  */
+#define _POSIX_V6_ILP32_OFF32	1
 #define _XBS5_ILP32_OFF32	1
 
 /* We can never (yet) provide an environment with the above size but
    a 64-bit size `off_t'.  */
-#define	_XBS5_ILP32_OFFBIG	-1
+#define _POSIX_V6_ILP32_OFFBIG	-1
+#define _XBS5_ILP32_OFFBIG	-1
 
 /* We can never provide environments with 64-bit wide pointers.  */
+#define _POSIX_V6_LP64_OFF64	-1
+#define _POSIX_V6_LPBIG_OFFBIG	-1
 #define _XBS5_LP64_OFF64	-1
 #define _XBS5_LPBIG_OFFBIG	-1
