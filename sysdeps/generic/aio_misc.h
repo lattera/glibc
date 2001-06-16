5ec73bb91f231a0d7a3c54183a9282cafd5511cb
Roland McGrath <roland@gnu.org>
2001-06-16 04:47:37 +0000

    2001-06-15  Roland McGrath  <roland@frob.com>
    	* rt/Makefile (aio-routines, clock-routines, timer-routines): New
    	variables, broken out of librt-routines.
    	(librt-routines): Set using those.
    	(extra-libs, extra-libc-others, tests): Set these unconditionally.
    	(distribute): No need for $(librt-map) or $(librt-routines:=.c) here.
    	* sysdeps/generic/aio_fsync.c: New file.
    	* sysdeps/generic/aio_misc.c: New file.
    	* sysdeps/generic/aio_write.c: New file.
    	* sysdeps/generic/aio_write64.c: New file.
    	* sysdeps/generic/aio_read64.c: New file.
    	* sysdeps/generic/aio_read.c: New file.
    	* sysdeps/generic/aio_notify.c: New file.
    	* sysdeps/generic/lio_listio64.c: New file.
    	* sysdeps/generic/lio_listio.c: New file.
    	* sysdeps/generic/aio_cancel.c: New file.
    	* sysdeps/generic/aio_misc.h: New file.
    	* sysdeps/generic/aio_suspend.c: New file.
    	* rt/aio_cancel.c: Moved to ...
    	* sysdeps/pthread/aio_cancel.c: ... here.
    	* rt/aio_fsync.c: Moved to ...
    	* sysdeps/pthread/aio_fsync.c: ... here.
    	* rt/aio_misc.c: Moved to ...
    	* sysdeps/pthread/aio_misc.c: ... here.
    	* rt/aio_misc.h: Moved to ...
    	* sysdeps/pthread/aio_misc.h: ... here.
    	* rt/aio_notify.c: Moved to ...
    	* sysdeps/pthread/aio_notify.c: ... here.
    	* rt/aio_read.c: Moved to ...
    	* sysdeps/pthread/aio_read.c: ... here.
    	* rt/aio_read64.c: Moved to ...
    	* sysdeps/pthread/aio_read64.c: ... here.
    	* rt/aio_suspend.c: Moved to ...
    	* sysdeps/pthread/aio_suspend.c: ... here.
    	* rt/aio_write.c: Moved to ...
    	* sysdeps/pthread/aio_write.c: ... here.
    	* rt/aio_write64.c: Moved to ...
    	* sysdeps/pthread/aio_write64.c: ... here.
    	* rt/lio_listio.c: Moved to ...
    	* sysdeps/pthread/lio_listio.c: ... here.
    	* rt/lio_listio64.c: Moved to ...
    	* sysdeps/pthread/lio_listio64.c: ... here.

---
+++ sysdeps/generic/aio_misc.h
@@ -0,0 +1,48 @@
+/* Internal declarations for <aio.h> functions implementation.  Stub version.
+   Copyright (C) 2001 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#ifndef _AIO_MISC_H
+#define _AIO_MISC_H	1
+
+#include <aio.h>
+
+
+/* Extend the operation enum.  */
+enum
+{
+  LIO_DSYNC = LIO_READ + 1,
+  LIO_SYNC,
+  LIO_READ64 = LIO_READ | 128,
+  LIO_WRITE64 = LIO_WRITE | 128
+};
+
+/* Union of the two request types.  */
+typedef union
+  {
+    struct aiocb aiocb;
+    struct aiocb64 aiocb64;
+  } aiocb_union;
+
+
+/* Send the signal.  */
+extern int __aio_sigqueue (int sig, const union sigval val, pid_t caller_pid)
+     internal_function;
+
+
+#endif /* aio_misc.h */
---
+++ sysdeps/generic/lio_listio.c
@@ -0,0 +1,42 @@
+/* Enqueue a list of read or write requests.  Stub version.
+   Copyright (C) 2001 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <aio.h>
+#include <errno.h>
+
+#ifdef BE_AIO64
+#define lio_listio	lio_listio64
+#define aiocb		aiocb64
+#define aio_read	aio_read64
+#define aio_write	aio_write64
+#define aio_suspend	aio_suspend64
+#endif
+
+
+int
+lio_listio (int mode,
+	    struct aiocb *const list[], int nent,
+	    struct sigevent *sig)
+{
+  __set_errno (ENOSYS);
+  return -1;
+}
+
+stub_warning (lio_listio)
+#include <stub-tag.h>
