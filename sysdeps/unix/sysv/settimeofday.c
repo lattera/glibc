fec3cf8165d6523e66f99fb923eed0f966823f81
Ulrich Drepper <drepper@redhat.com>
2001-06-21 16:46:17 +0000

    Update.
    2001-06-20  Alexandre Oliva  <aoliva@redhat.com>
    
    	* sysdeps/unix/sysv/settimeofday.c: Include time.h.

--- sysdeps/unix/sysv/settimeofday.c
+++ sysdeps/unix/sysv/settimeofday.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992, 1995, 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1995, 1996, 1997, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -17,6 +17,7 @@
    Boston, MA 02111-1307, USA.  */
 
 #include <errno.h>
+#include <time.h>
 #include <sys/time.h>
 
 /* Set the current time of day and timezone information.
