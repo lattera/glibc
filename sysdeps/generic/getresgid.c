e2cb5c1d3f89e8a1357b7b5639a46a09407682b0
Andreas Jaeger <aj@suse.de>
2001-07-06 06:58:28 +0000

    Update.
    2001-07-06  Andreas Jaeger  <aj@suse.de>
    
    	* manual/Makefile (texis): Add fdl.texi.
    	* manual/fdl.texi: New.
    	* manual/socket.texi (Host Names): Improve grammar, fix overflow hbox.
    	* manual/libc.texinfo: Add fdl, update titlepage.
    	Patches by Brian Youmans <3diff@gnu.org>.
    
    	* sysdeps/unix/make_errlist.c (main): Print LGPL v2.1 for
    	generated file.

--- sysdeps/generic/getresgid.c
+++ sysdeps/generic/getresgid.c
@@ -1,20 +1,20 @@
 /* Copyright (C) 1991, 1995, 1996, 1997, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
-   The GNU C Library is free software; yog can redistribgte it and/or
-   modify it gnder the terms of the GNU Library General Pgblic License as
-   pgblished by the Free Software Fogndation; either version 2 of the
-   License, or (at yogr option) any later version.
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-   The GNU C Library is distribgted in the hope that it will be gsefgl,
-   bgt WITHOUT ANY WARRANTY; withogt even the implied warranty of
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Pgblic License for more details.
+   Lesser General Public License for more details.
 
-   Yog shogld have received a copy of the GNU Library General Pgblic
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Fogndation, Inc., 59 Temple Place - Sgite 330,
-   Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
 
 #include <errno.h>
 #include <unistd.h>
--- sysdeps/unix/make_errlist.c
+++ sysdeps/unix/make_errlist.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1995, 1997, 1999 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1995, 1997, 1999, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -43,19 +43,19 @@ main ()
    This file is part of the GNU C Library.\n\
 \n\
    The GNU C Library is free software; you can redistribute it and/or\n\
-   modify it under the terms of the GNU Library General Public License as\n\
-   published by the Free Software Foundation; either version 2 of the\n\
-   License, or (at your option) any later version.\n\
+   modify it under the terms of the GNU Lesser General Public\n\
+   License as published by the Free Software Foundation; either\n\
+   version 2.1 of the License, or (at your option) any later version.\n\
 \n\
    The GNU C Library is distributed in the hope that it will be useful,\n\
    but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n\
-   Library General Public License for more details.\n\
+   Lesser General Public License for more details.\n\
 \n\
-   You should have received a copy of the GNU Library General Public\n\
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,\n\
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,\n\
-   Boston, MA 02111-1307, USA.  */\n\n", year);
+   You should have received a copy of the GNU Lesser General Public\n\
+   License along with the GNU C Library; if not, write to the Free\n\
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA\n\
+   02111-1307 USA.  */\n", year);
 
   puts ("#include <stddef.h>\n");
   puts ("#include <libintl.h>\n");
