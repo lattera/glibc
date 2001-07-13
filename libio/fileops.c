c9f10034f712b38aabb02b73b59d8b267594bab0
Andreas Jaeger <aj@suse.de>
2001-07-13 13:18:44 +0000

    Include unistd.h for lseek64 prototype.  Closes PR libc/2372.

--- libio/fileops.c
+++ libio/fileops.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993, 1995, 1997-1999, 2000 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1995, 1997-2000, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Written by Per Bothner <bothner@cygnus.com>.
 
@@ -36,6 +36,7 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 #ifdef __STDC__
 #include <stdlib.h>
 #endif
