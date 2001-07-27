277f8cdf9849ec2d701b17dab05199e8fe044b89
Ulrich Drepper <drepper@redhat.com>
2001-07-27 18:09:56 +0000

    Update.
    	* libio/Makefile: Add rules to check output of tst-fopenloc and
    	for memory leaks.
    	* libio/tst-fopenloc.c: New file.

---
+++ libio/tst-fopenloc.c
@@ -0,0 +1,62 @@
+/* Test for ,ccs= handling in fopen.
+   Copyright (C) 2001 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Contributed by Ulrich Drepper <drepper@cygnus.com>, 2001.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#include <errno.h>
+#include <locale.h>
+#include <mcheck.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+
+
+static const char inputfile[] = "../iconvdata/testdata/ISO-8859-1";
+
+
+int
+main (void)
+{
+  FILE *fp;
+
+  mtrace ();
+
+  setlocale (LC_ALL, "de_DE.UTF-8");
+
+  fp = fopen (inputfile, "r,ccs=ISO-8859-1");
+  if (fp == NULL)
+    {
+      printf ("cannot open \"%s\": %s\n", inputfile, strerror (errno));
+      exit (1);
+    }
+
+  while (! feof_unlocked (fp))
+    {
+      wchar_t buf[200];
+
+      if (fgetws_unlocked (buf, sizeof (buf) / sizeof (buf[0]), fp) == NULL)
+	break;
+
+      fputws (buf, stdout);
+    }
+
+  fclose (fp);
+
+  return 0;
+}
