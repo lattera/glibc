7c69dc8bc888762333f5ccf1c8c7fb5883ffc6ed
Ulrich Drepper <drepper@redhat.com>
2001-08-26 08:15:36 +0000

    Update.
    	* sysdeps/s390/s390-32/dl-machine.h (elf_machine_rela_relative):
    	Likewise.
    	* sysdeps/s390/s390-64/dl-machine.h (elf_machine_rela_relative):
    	Likewise.

--- sysdeps/s390/s390-64/dl-machine.h
+++ sysdeps/s390/s390-64/dl-machine.h
@@ -420,8 +420,8 @@ elf_machine_rela (struct link_map *map, const Elf64_Rela *reloc,
 }
 
 static inline void
-elf_machine_rel_relative (Elf64_Addr l_addr, const Elf64_Rel *reloc,
-			  Elf64_Addr *const reloc_addr)
+elf_machine_rela_relative (Elf64_Addr l_addr, const Elf64_Rela *reloc,
+			   Elf64_Addr *const reloc_addr)
 {
   *reloc_addr = l_addr + reloc->r_addend;
 }
