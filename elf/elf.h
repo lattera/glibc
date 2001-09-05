9cd9ea1068e85dc5a211543b5ba4637ffc6df8be
Ulrich Drepper <drepper@redhat.com>
2001-09-05 04:54:25 +0000

    Update.
    2001-09-04  Richard Henderson  <rth@redhat.com>
    
    	* elf/elf.h (R_ALPHA_OP_*, R_ALPHA_IMMED_*): Remove.
    	(R_ALPHA_GPREL16): Rename from R_ALPHA_IMMED_GP_16.

--- elf/elf.h
+++ elf/elf.h
@@ -1706,18 +1706,9 @@ typedef Elf32_Addr Elf32_Conflict;
 #define R_ALPHA_SREL16		9	/* PC relative 16 bit */
 #define R_ALPHA_SREL32		10	/* PC relative 32 bit */
 #define R_ALPHA_SREL64		11	/* PC relative 64 bit */
-#define R_ALPHA_OP_PUSH		12	/* OP stack push */
-#define R_ALPHA_OP_STORE	13	/* OP stack pop and store */
-#define R_ALPHA_OP_PSUB		14	/* OP stack subtract */
-#define R_ALPHA_OP_PRSHIFT	15	/* OP stack right shift */
-#define R_ALPHA_GPVALUE		16
-#define R_ALPHA_GPRELHIGH	17
-#define R_ALPHA_GPRELLOW	18
-#define R_ALPHA_IMMED_GP_16	19
-#define R_ALPHA_IMMED_GP_HI32	20
-#define R_ALPHA_IMMED_SCN_HI32	21
-#define R_ALPHA_IMMED_BR_HI32	22
-#define R_ALPHA_IMMED_LO32	23
+#define R_ALPHA_GPRELHIGH	17	/* GP relative 32 bit, high 16 bits */
+#define R_ALPHA_GPRELLOW	18	/* GP relative 32 bit, low 16 bits */
+#define R_ALPHA_GPREL16		19	/* GP relative 16 bit */
 #define R_ALPHA_COPY		24	/* Copy symbol at runtime */
 #define R_ALPHA_GLOB_DAT	25	/* Create GOT entry */
 #define R_ALPHA_JMP_SLOT	26	/* Create PLT entry */
