de447e48ce0f6caa89599429ba0e27d7154a9732
Ulrich Drepper <drepper@redhat.com>
2001-09-08 17:40:41 +0000

    Alias __libc_fcntl to __fcntl.

--- sysdeps/unix/sysv/aix/fcntl.c
+++ sysdeps/unix/sysv/aix/fcntl.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1999, 2000 Free Software Foundation, Inc.
+/* Copyright (C) 1999, 2000, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -38,3 +38,4 @@ __fcntl (int fdes, int cmd, ...)
   return res;
 }
 strong_alias (__fcntl, fcntl)
+strong_alias (__fcntl, __libc_fcntl)
