c831ae5490d1251a4588b8f11822b12033d8100b
Andreas Jaeger <aj@suse.de>
2001-10-16 11:02:24 +0000

    Fix parentheses.

--- sysdeps/ieee754/ldbl-96/s_nextafterl.c
+++ sysdeps/ieee754/ldbl-96/s_nextafterl.c
@@ -43,8 +43,8 @@ static char rcsid[] = "$NetBSD: $";
 	ix = esx&0x7fff;		/* |x| */
 	iy = esy&0x7fff;		/* |y| */
 
-	if(((ix==0x7fff)&&((hx|lx)!=0) ||   /* x is nan */
-	   ((iy==0x7fff)&&((hy|ly)!=0))     /* y is nan */
+	if (((ix==0x7fff)&&((hx|lx)!=0)) ||   /* x is nan */
+	    ((iy==0x7fff)&&((hy|ly)!=0)))     /* y is nan */
 	   return x+y;
 	if(x==y) return y;		/* x=y, return y */
 	if((ix|hx|lx)==0) {			/* x == 0 */
