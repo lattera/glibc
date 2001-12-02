5ca3ec06e0d1dc5eb0def5697f914fcce3f1d774
Roland McGrath <roland@gnu.org>
2001-12-02 19:43:55 +0000

    * hurd/hurd.h (_hurd_umask): Remove volatile qualifier from decl.

--- hurd/hurd.h
+++ hurd/hurd.h
@@ -79,7 +79,7 @@ __hurd_fail (error_t err)
 extern int _hurd_exec_flags;	/* Flags word passed in exec_startup.  */
 extern struct hurd_port *_hurd_ports;
 extern unsigned int _hurd_nports;
-extern volatile mode_t _hurd_umask;
+extern mode_t _hurd_umask;
 extern sigset_t _hurdsig_traced;
 
 /* Shorthand macro for internal library code referencing _hurd_ports (see
