8bd56b60c47a3664e5179e523bcb6c0547d4fd26
Ulrich Drepper <drepper@redhat.com>
2002-02-01 07:48:43 +0000

    Move _dl_hp_timing_overhead and procinfo-related variables in rtld_global struct.

--- sysdeps/unix/sysv/linux/arm/dl-procinfo.c
+++ sysdeps/unix/sysv/linux/arm/dl-procinfo.c
@@ -1,5 +1,5 @@
 /* Data for Linux/ARM version of processor capability information.
-   Copyright (C) 2001 Free Software Foundation, Inc.
+   Copyright (C) 2001, 2002 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Philip Blundell <philb@gnu.org>, 2001.
 
@@ -19,13 +19,44 @@
    02111-1307 USA.  */
 
 /* This information must be kept in sync with the _DL_HWCAP_COUNT and
-   _DL_PLATFORM_COUNT definitions in procinfo.h.  */
+   _DL_PLATFORM_COUNT definitions in procinfo.h.
 
+   If anything should be added here check whether the size of each string
+   is still ok with the given array size.
 
-/* If anything should be added here check whether the size of each string
-   is still ok with the given array size.  */
-const char _dl_arm_cap_flags[][10] =
-  {
+   All the #ifdefs in the definitions ar equite irritating but
+   necessary if we want to avoid duplicating the information.  There
+   are three different modes:
+
+   - PROCINFO_DECL is defined.  This means we are only interested in
+     declarations.
+
+   - PROCINFO_DECL is not defined:
+
+     + if SHARED is defined the file is included in an array
+       initializer.  The .element = { ... } syntax is needed.
+
+     + if SHARED is not defined a normal array initialization is
+       needed.
+  */
+
+#ifdef PROCINFO_DECL
+EXTERN
+#endif
+#if !defined PROCINFO_DECL && defined SHARED
+  ._dl_arm_cap_flags
+#else
+const char _dl_arm_cap_flags[][10]
+#endif
+#ifndef PROCINFO_DECL
+= {
     "swp", "half", "thumb", "26bit", "fast-mult", "fpa", "vfp", "edsp",
-  };
+  }
+#endif
+#if !defined SHARED || defined PROCINFO_DECL
+;
+#else
+,
+#endif
 
+#undef PROCINFO_DECL
