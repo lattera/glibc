8342ceb49827fa8facb23c6adf3c3bb47b278034
Andreas Jaeger <aj@suse.de>
2002-03-06 10:08:29 +0000

    Add compatibility for 32-bit.

--- sysdeps/unix/sysv/linux/x86_64/sys/reg.h
+++ sysdeps/unix/sysv/linux/x86_64/sys/reg.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001 Free Software Foundation, Inc.
+/* Copyright (C) 2001, 2002 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -18,30 +18,56 @@
 
 #ifndef _SYS_REG_H
 #define _SYS_REG_H	1
+#include <bits/wordsize.h>
 
+
+#if __WORDSIZE == 64
 /* Index into an array of 8 byte longs returned from ptrace for
    location of the users' stored general purpose registers.  */
 
-#define R15	0
-#define R14	1
-#define R13	2
-#define R12	3
-#define RBP	4
-#define RBX	5
-#define R11	6
-#define R10	7
-#define R9	8
-#define R8	9
-#define RAX	10
-#define RCX	11
-#define RDX	12
-#define RSI	13
-#define RDI	14
-#define ORIG_RAX 15
-#define RIP	16
-#define CS	17
-#define EFLAGS	18
-#define RSP	19
-#define SS	20
+# define R15	0
+# define R14	1
+# define R13	2
+# define R12	3
+# define RBP	4
+# define RBX	5
+# define R11	6
+# define R10	7
+# define R9	8
+# define R8	9
+# define RAX	10
+# define RCX	11
+# define RDX	12
+# define RSI	13
+# define RDI	14
+# define ORIG_RAX 15
+# define RIP	16
+# define CS	17
+# define EFLAGS	18
+# define RSP	19
+# define SS	20
+#else
+
+/* Index into an array of 4 byte integers returned from ptrace for
+ * location of the users' stored general purpose registers. */
+
+# define EBX 0
+# define ECX 1
+# define EDX 2
+# define ESI 3
+# define EDI 4
+# define EBP 5
+# define EAX 6
+# define DS 7
+# define ES 8
+# define FS 9
+# define GS 10
+# define ORIG_EAX 11
+# define EIP 12
+# define CS  13
+# define EFL 14
+# define UESP 15
+# define SS   16
+endif
 
 #endif
