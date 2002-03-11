1396cb052d732254a453dae00c29f3a075a90cb3
Ulrich Drepper <drepper@redhat.com>
2002-03-11 21:20:39 +0000

    Round milli seconds to nearest.  Don't return 1000 in millitm.

--- sysdeps/unix/bsd/ftime.c
+++ sysdeps/unix/bsd/ftime.c
@@ -30,7 +30,12 @@ ftime (timebuf)
     return -1;
 
   timebuf->time = tv.tv_sec;
-  timebuf->millitm = (tv.tv_usec + 999) / 1000;
+  timebuf->millitm = (tv.tv_usec + 500) / 1000;
+  if (timebuf->millitm == 1000)
+    {
+      ++timebuf->time;
+      timebuf->millitm = 0;
+    }
   timebuf->timezone = tz.tz_minuteswest;
   timebuf->dstflag = tz.tz_dsttime;
   return 0;
