a4007d1f0ad6380f934f6a07aabbc718ab63ff32
Andreas Jaeger <aj@suse.de>
2002-04-04 08:53:30 +0000

    Remove, the archs are not supported anymore.

--- sysdeps/mips/dec/bits/endian.h
+++
@@ -1,8 +0,0 @@
-/* The MIPS architecture has selectable endianness.
-   The DECstation uses little-endian mode.  */
-
-#ifndef _ENDIAN_H
-# error "Never use <bits/endian.h> directly; include <endian.h> instead."
-#endif
-
-#define __BYTE_ORDER __LITTLE_ENDIAN
--- sysdeps/mips/p40/bits/endian.h
+++
@@ -1,8 +0,0 @@
-/* The MIPS has selectable endianness.
-   The Japanese homebrew P40 architecture uses big-endian mode.  */
-
-#ifndef _ENDIAN_H
-# error "Never use <bits/endian.h> directly; include <endian.h> instead."
-#endif
-
-#define __BYTE_ORDER __BIG_ENDIAN
