3e992afaaf284d24dab4dd136cc3aee5a2b92d6e
Ulrich Drepper <drepper@redhat.com>
2002-05-01 16:41:24 +0000

    (__ngettext, __dngettext, __dcngettext): New declarations. (__gettext, __dgettext, __dcgettext, __dcgettext_internal): Add attribute declarations.

--- include/libintl.h
+++ include/libintl.h
@@ -3,19 +3,43 @@
 #include <locale.h>
 
 /* Now define the internal interfaces.  */
-extern char *__gettext (__const char *__msgid);
+extern char *__gettext (__const char *__msgid)
+     __attribute_format_arg__ (1);
 extern char *__dgettext (__const char *__domainname,
-			 __const char *__msgid);
+			 __const char *__msgid)
+     __attribute_format_arg__ (2);
 extern char *__dcgettext (__const char *__domainname,
-			  __const char *__msgid, int __category);
+			  __const char *__msgid, int __category)
+     __attribute_format_arg__ (2);
 extern char *__dcgettext_internal (__const char *__domainname,
 				   __const char *__msgid, int __category)
+     __attribute_format_arg__ (2)
      attribute_hidden;
+
+extern char *__ngettext (__const char *__msgid1, __const char *__msgid2,
+			 unsigned long int __n)
+     __attribute_format_arg__ (1) __attribute_format_arg__ (2);
+extern char *__dngettext (__const char *__domainname,
+			  __const char *__msgid1, __const char *__msgid2,
+			  unsigned long int __n)
+     __attribute_format_arg__ (2) __attribute_format_arg__ (3);
+extern char *__dcngettext (__const char *__domainname,
+			   __const char *__msgid1, __const char *__msgid2,
+			   unsigned long int __n, int __category)
+     __attribute_format_arg__ (2) __attribute_format_arg__ (3);
+extern char *__dcngettext_internal (__const char *__domainname,
+				    __const char *__msgid1,
+				    __const char *__msgid2,
+				    unsigned long int __n, int __category)
+     __attribute_format_arg__ (2) __attribute_format_arg__ (3)
+     attribute_hidden;
+
 extern char *__textdomain (__const char *__domainname);
 extern char *__bindtextdomain (__const char *__domainname,
 			       __const char *__dirname);
 extern char *__bind_textdomain_codeset (__const char *__domainname,
 					__const char *__codeset);
+
 extern const char _libc_intl_domainname[];
 extern const char _libc_intl_domainname_internal[] attribute_hidden;
 
