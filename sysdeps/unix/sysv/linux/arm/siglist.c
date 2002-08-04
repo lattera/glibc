9b4c30e300c1531aa76bb52739ea0982d0e6cb11
Ulrich Drepper <drepper@redhat.com>
2002-08-04 23:26:50 +0000

    Add _sys_siglist_internal alias.

--- sysdeps/unix/sysv/linux/arm/siglist.c
+++ sysdeps/unix/sysv/linux/arm/siglist.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1997, 1998, 2000 Free Software Foundation, Inc.
+/* Copyright (C) 1997, 1998, 2000, 2002 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -32,6 +32,7 @@ const char *const __new_sys_siglist[NSIG] =
 #include "siglist.h"
 #undef init_sig
 };
+strong_alias (__new_sys_siglist, _sys_siglist_internal)
 
 #if SHLIB_COMPAT (libc, GLIBC_2_0, GLIBC_2_1)
 asm (".type __old_sys_siglist,%object;.size __old_sys_siglist,"
