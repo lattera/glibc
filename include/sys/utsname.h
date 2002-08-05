4f6da0f5d827a462e30634e36161f80026223ba8
Roland McGrath <roland@gnu.org>
2002-08-05 22:47:19 +0000

    * include/sys/utsname.h: Use libc_hidden_proto for uname, __uname.
    	* sysdeps/generic/uname.c: Add libc_hidden_def.
    	* sysdeps/mach/hurd/uname.c: Likewise.

--- include/sys/utsname.h
+++ include/sys/utsname.h
@@ -2,4 +2,7 @@
 #include <posix/sys/utsname.h>
 
 extern int __uname (struct utsname *__name);
+
+libc_hidden_proto (uname)
+libc_hidden_proto (__uname)
 #endif
--- sysdeps/mach/hurd/uname.c
+++ sysdeps/mach/hurd/uname.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992,1993,1994,1996,1997,2000 Free Software Foundation, Inc.
+/* Copyright (C) 1992,93,94,96,97,2000,02 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -44,3 +44,5 @@ __uname (struct utsname *uname)
   return 0;
 }
 weak_alias (__uname, uname)
+libc_hidden_def (__uname)
+libc_hidden_def (uname)
