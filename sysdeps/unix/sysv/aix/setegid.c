e0acc021ce7a0f42d455fac3f8874b2e827156b1
Roland McGrath <roland@gnu.org>
2002-08-13 00:22:48 +0000

    2002-08-13  Jakub Jelinek  <jakub@redhat.com>
    	* include/unistd.h (seteuid, setegid): Add libc_hidden_proto.
    	* sysdeps/generic/seteuid.c (seteuid): Add libc_hidden_def.
    	* sysdeps/generic/setegid.c (setegid): Likewise.
    	* sysdeps/mach/hurd/seteuid.c (seteuid): Likewise.
    	* sysdeps/mach/hurd/setegid.c (setegid): Likewise.
    	* sysdeps/unix/bsd/seteuid.c (seteuid): Likewise.
    	* sysdeps/unix/bsd/setegid.c (setegid): Likewise.
    	* sysdeps/unix/sysv/aix/seteuid.c (seteuid): Likewise.
    	* sysdeps/unix/sysv/aix/setegid.c (setegid): Likewise.
    	* sysdeps/unix/sysv/linux/i386/setegid.c (setegid): Likewise.
    	* sysdeps/unix/sysv/linux/sparc/sparc32/seteuid.c (seteuid): Likewise.
    	* sysdeps/unix/sysv/linux/sparc/sparc32/setegid.c (setegid): Likewise.
    	* sysdeps/unix/sysv/linux/seteuid.c (seteuid): Likewise.
    	* sysdeps/unix/sysv/linux/setegid.c (setegid): Likewise.
    	* sysdeps/unix/sysv/linux/i386/seteuid.c (seteuid): Likewise.
    	Remove fallback if __ASSUME_SETRESUID_SYSCALL.

--- sysdeps/unix/sysv/aix/setegid.c
+++ sysdeps/unix/sysv/aix/setegid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2000 Free Software Foundation, Inc.
+/* Copyright (C) 2000, 2002 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -29,3 +29,4 @@ __setegid (gid_t gid)
   return setgidx (ID_EFFECTIVE, gid);
 }
 strong_alias (__setegid, setegid)
+libc_hidden_def (setegid)
