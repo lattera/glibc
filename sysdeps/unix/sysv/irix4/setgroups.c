94a376cc2cb4d93d0a76d16d6aa296cd18bbab1b
Roland McGrath <roland@gnu.org>
2002-08-13 01:07:58 +0000

    2002-08-12  Roland McGrath  <roland@redhat.com>
    	* include/grp.h (setgroups): Add libc_hidden_proto.
    	* sysdeps/generic/setgroups.c: Add libc_hidden_def.
    	* sysdeps/mach/hurd/setgroups.c: Likewise.
    	* sysdeps/unix/sysv/irix4/setgroups.c: Likewise.
    	* sysdeps/unix/sysv/linux/i386/setgroups.c: Likewise.

--- sysdeps/unix/sysv/irix4/setgroups.c
+++ sysdeps/unix/sysv/irix4/setgroups.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1994, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1994,97,2002 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -30,3 +30,4 @@ setgroups (n, groups)
 {
   return __syssgi (SGI_SETGROUPS, n, groups);
 }
+libc_hidden_def (setgroups)
