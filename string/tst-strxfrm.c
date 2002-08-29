86ad5f0cf575cec046aa6e39184f9a49a7c823f3
Roland McGrath <roland@gnu.org>
2002-08-29 09:26:30 +0000

    2002-08-29  Roland McGrath  <roland@redhat.com>
    	* string/tst-strxfrm.c: Remove __ from function names.

--- string/tst-strxfrm.c
+++ string/tst-strxfrm.c
@@ -15,7 +15,7 @@ test (const char *locale)
   size_t r;
   size_t l;
   char *buf;
-  __locale_t loc;
+  locale_t loc;
   int result = 0;
 
   if (setlocale (LC_COLLATE, locale) == NULL)
@@ -39,9 +39,9 @@ test (const char *locale)
        result = 1;
     }
 
-  loc = __newlocale (1 << LC_ALL, locale, NULL);
+  loc = newlocale (1 << LC_ALL, locale, NULL);
 
-  r = __strxfrm_l (buf, string, bufsize, loc);
+  r = strxfrm_l (buf, string, bufsize, loc);
   l = strlen (buf);
   if (r != l)
     {
@@ -50,7 +50,7 @@ test (const char *locale)
        result = 1;
     }
 
-  __freelocale (loc);
+  freelocale (loc);
 
   free (buf);
 
