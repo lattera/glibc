c5947147ec1c3611a51f4db1da1ea0bc2a8da227
Roland McGrath <roland@gnu.org>
2002-09-18 17:45:53 +0000

    2002-09-18  Roland McGrath  <roland@redhat.com>
    	* sysdeps/unix/sysv/linux/sigaction.c (__libc_sigaction):
    	Add libc_hidden_def.
    	* sysdeps/unix/sysv/linux/arm/sigaction.c: Likewise.
    	* sysdeps/unix/sysv/linux/i386/sigaction.c: Likewise.
    	* sysdeps/unix/sysv/linux/ia64/sigaction.c: Likewise.
    	* sysdeps/unix/sysv/linux/mips/sigaction.c: Likewise.
    	* sysdeps/unix/sysv/linux/s390/s390-64/sigaction.c: Likewise.
    	* sysdeps/unix/sysv/linux/sparc/sparc32/sigaction.c: Likewise.
    	* sysdeps/unix/sysv/linux/sparc/sparc64/sigaction.c: Likewise.
    	* sysdeps/unix/sysv/linux/x86_64/sigaction.c: Likewise.

--- sysdeps/unix/sysv/linux/mips/sigaction.c
+++ sysdeps/unix/sysv/linux/mips/sigaction.c
@@ -134,7 +134,7 @@ __libc_sigaction (sig, act, oact)
   return result;
 #endif
 }
-
+libc_hidden_def (__libc_sigaction)
 weak_alias (__libc_sigaction, __sigaction)
 libc_hidden_weak (__sigaction)
 weak_alias (__libc_sigaction, sigaction)
