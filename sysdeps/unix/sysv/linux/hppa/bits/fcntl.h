700494fc4b18dda563b9344ab6de9ffc77d2a934
Roland McGrath <roland@gnu.org>
2002-10-01 19:42:01 +0000

    2002-10-01  Carlos O'Donell  <carlos@baldric.uwo.ca>
    	* sysdeps/unix/sysv/linux/hppa/bits/fcntl.h
    	[__USE_GNU] (F_SETLEASE, F_GETLEASE, F_NOTIFY): New macros.
    	[__USE_GNU] (DN_ACCESS, DN_MODIFY, DN_CREATE, DN_DELETE, DN_RENAME,
    	DN_ATTRIB, DN_MULTISHOT): New macros.

--- sysdeps/unix/sysv/linux/hppa/bits/fcntl.h
+++ sysdeps/unix/sysv/linux/hppa/bits/fcntl.h
@@ -81,6 +81,12 @@
 # define F_GETSIG	14	/* Get number of signal to be sent.  */
 #endif
 
+#ifdef __USE_GNU
+# define F_SETLEASE     1024    /* Set a lease.  */
+# define F_GETLEASE     1025    /* Enquire what lease is active.  */
+# define F_NOTIFY       1026    /* Request notfications on a directory.  */
+#endif
+
 /* for F_[GET|SET]FL */
 #define FD_CLOEXEC	1	/* actually anything with low bit set goes */
 
@@ -100,6 +106,17 @@
 # define LOCK_NB	4	/* or'd with one of the above to prevent
 				   blocking */
 # define LOCK_UN	8	/* remove lock */
+#endif
+
+#ifdef __USE_GNU
+/* Types of directory notifications that may be requested with F_NOTIFY.  */
+# define DN_ACCESS      0x00000001      /* File accessed.  */
+# define DN_MODIFY      0x00000002      /* File modified.  */
+# define DN_CREATE      0x00000004      /* File created.  */
+# define DN_DELETE      0x00000008      /* File removed.  */
+# define DN_RENAME      0x00000010      /* File renamed.  */
+# define DN_ATTRIB      0x00000020      /* File changed attibutes.  */
+# define DN_MULTISHOT   0x80000000      /* Don't remove notifier.  */
 #endif
 
 struct flock
