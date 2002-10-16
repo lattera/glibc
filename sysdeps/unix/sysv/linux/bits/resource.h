f7d1f342cc8000eb937a38362fa5bcf65023464f
Roland McGrath <roland@gnu.org>
2002-10-16 01:02:25 +0000

    2002-10-15  Roland McGrath  <roland@redhat.com>
    	* sysdeps/unix/sysv/linux/bits/resource.h: Replaced with the contents
    	of the sysdeps/unix/sysv/linux/i386/bits/resource.h file.
    	All the following files were identical or equivalent to it.
    	* sysdeps/unix/sysv/linux/i386/bits/resource.h: File removed.
    	* sysdeps/unix/sysv/linux/arm/bits/resource.h: File removed.
    	* sysdeps/unix/sysv/linux/cris/bits/resource.h: File removed.
    	* sysdeps/unix/sysv/linux/hppa/bits/resource.h: File removed.
    	* sysdeps/unix/sysv/linux/ia64/bits/resource.h: File removed.
    	* sysdeps/unix/sysv/linux/m68k/bits/resource.h: File removed.
    	* sysdeps/unix/sysv/linux/powerpc/bits/resource.h: File removed.
    	* sysdeps/unix/sysv/linux/s390/bits/resource.h: File removed.
    	* sysdeps/unix/sysv/linux/sh/bits/resource.h: File removed.
    	* sysdeps/unix/sysv/linux/x86_64/bits/resource.h: File removed.

--- sysdeps/unix/sysv/linux/bits/resource.h
+++ sysdeps/unix/sysv/linux/bits/resource.h
@@ -64,7 +64,7 @@ enum __rlimit_resource
 #define RLIMIT_NOFILE RLIMIT_NOFILE
 #define RLIMIT_OFILE RLIMIT_OFILE
 
-  /* Address space limit (?) */
+  /* Address space limit.  */
   RLIMIT_AS = 9,
 #define RLIMIT_AS RLIMIT_AS
 
@@ -80,20 +80,21 @@ enum __rlimit_resource
   RLIMIT_LOCKS = 10,
 #define RLIMIT_LOCKS RLIMIT_LOCKS
 
-  RLIM_NLIMITS = 11
+  RLIMIT_NLIMITS = 11,
+  RLIM_NLIMITS = RLIMIT_NLIMITS
 #define RLIMIT_NLIMITS RLIMIT_NLIMITS
 #define RLIM_NLIMITS RLIM_NLIMITS
 };
 
 /* Value to indicate that there is no limit.  */
 #ifndef __USE_FILE_OFFSET64
-# define RLIM_INFINITY ((long int)(~0UL >> 1))
+# define RLIM_INFINITY ((unsigned long int)(~0UL))
 #else
-# define RLIM_INFINITY 0x7fffffffffffffffLL
+# define RLIM_INFINITY 0xffffffffffffffffuLL
 #endif
 
 #ifdef __USE_LARGEFILE64
-# define RLIM64_INFINITY 0x7fffffffffffffffLL
+# define RLIM64_INFINITY 0xffffffffffffffffuLL
 #endif
 
 /* We can represent all limits.  */
