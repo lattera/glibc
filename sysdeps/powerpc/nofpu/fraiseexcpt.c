10fdbe1a5b3131878a182c2f1005cea20fee1a04
Roland McGrath <roland@gnu.org>
2002-10-19 20:06:29 +0000

    2002-10-19  Roland McGrath  <roland@redhat.com>
    	* config.make.in: Nix completely-soft nonsense.
    	* configure.in: Likewise.  Under --without-fp, use nofpu subdirectory
    	of machine directories instead of fpu subdirectory.
    	* sysdeps/powerpc/soft-fp/Makefile: Remove cruft added in last change.
    	* sysdeps/powerpc/nofpu/Makefile: Put it in this new file instead.
    	* sysdeps/powerpc/soft-fp/sim-full.c: Moved to ...
    	* sysdeps/powerpc/nofpu/sim-full.c: ... here.
    	* sysdeps/powerpc/soft-fp/fraiseexcpt.c: Moved to ...
    	* sysdeps/powerpc/nofpu/fraiseexcpt.c: ... here.
    	* sysdeps/powerpc/soft-fp/fegetexcept.c: Moved to ...
    	* sysdeps/powerpc/nofpu/fegetexcept.c: ... here.
    	* sysdeps/powerpc/soft-fp/fclrexcpt.c: Moved to ...
    	* sysdeps/powerpc/nofpu/fclrexcpt.c: ... here.
    	* sysdeps/powerpc/soft-fp/ftestexcept.c: Moved to ...
    	* sysdeps/powerpc/nofpu/ftestexcept.c: ... here.
    	* sysdeps/powerpc/soft-fp/fgetexcptflg.c: Moved to ...
    	* sysdeps/powerpc/nofpu/fgetexcptflg.c: ... here.
    	* sysdeps/powerpc/soft-fp/fsetexcptflg.c: Moved to ...
    	* sysdeps/powerpc/nofpu/fsetexcptflg.c: ... here.
    	* sysdeps/powerpc/soft-fp/fedisblxcpt.c: Moved to ...
    	* sysdeps/powerpc/nofpu/fedisblxcpt.c: ... here.
    	* sysdeps/powerpc/soft-fp/feenablxcpt.c: Moved to ...
    	* sysdeps/powerpc/nofpu/feenablxcpt.c: ... here.
    	* sysdeps/powerpc/soft-fp/fegetenv.c: Moved to ...
    	* sysdeps/powerpc/nofpu/fegetenv.c: ... here.
    	* sysdeps/powerpc/soft-fp/fesetenv.c: Moved to ...
    	* sysdeps/powerpc/nofpu/fesetenv.c: ... here.
    	* sysdeps/powerpc/soft-fp/fegetround.c: Moved to ...
    	* sysdeps/powerpc/nofpu/fegetround.c: ... here.
    	* sysdeps/powerpc/soft-fp/fesetround.c: Moved to ...
    	* sysdeps/powerpc/nofpu/fesetround.c: ... here.
    	* sysdeps/powerpc/soft-fp/feupdateenv.c: Moved to ...
    	* sysdeps/powerpc/nofpu/feupdateenv.c: ... here.
    	* sysdeps/powerpc/soft-fp/feholdexcpt.c: Moved to ...
    	* sysdeps/powerpc/nofpu/feholdexcpt.c: ... here.
    	* sysdeps/powerpc/soft-fp/fenv_const.c: Moved to ...
    	* sysdeps/powerpc/nofpu/fenv_const.c: ... here.
    	* sysdeps/powerpc/soft-fp/libm-test-ulps: Moved to ...
    	* sysdeps/powerpc/nofpu/libm-test-ulps: ... here.
    	* sysdeps/powerpc/soft-fp/soft-supp.h: Moved to ...
    	* sysdeps/powerpc/nofpu/soft-supp.h: ... here.
    	* sysdeps/powerpc/soft-fp/Versions (libc: GLIBC_2.3.2): Moved to ...
    	* sysdeps/powerpc/nofpu/Versions: ... here, new file.

---
+++ sysdeps/powerpc/nofpu/fraiseexcpt.c
@@ -0,0 +1,46 @@
+/* Raise given exceptions (soft-float edition).
+   Copyright (C) 2002 Free Software Foundation, Inc.
+   Contributed by Aldy Hernandez <aldyh@redhat.com>, 2002.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#include "soft-fp.h"
+#include "soft-supp.h"
+#include <signal.h>
+#include <bp-sym.h>
+
+#undef feraiseexcept
+int
+__feraiseexcept (int x)
+{
+  __sim_exceptions |= x;
+  if (x == 0 || __sim_disabled_exceptions & x)
+    /* Ignore exception.  */
+    ;
+  else
+    raise (SIGFPE);
+  return 0;
+}
+
+#include <shlib-compat.h>
+#if SHLIB_COMPAT (libm, GLIBC_2_1, GLIBC_2_2)
+strong_alias (__feraiseexcept, __old_feraiseexcept)
+compat_symbol (libm, BP_SYM (__old_feraiseexcept), BP_SYM (feraiseexcept), GLIBC_2_1);
+#endif
+
+libm_hidden_ver (__feraiseexcept, feraiseexcept)
+versioned_symbol (libm, BP_SYM (__feraiseexcept), BP_SYM (feraiseexcept), GLIBC_2_2);
