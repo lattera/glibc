832737d5e2b924f18884c78d253e8a950c07fcc5
Ulrich Drepper <drepper@redhat.com>
2002-11-20 19:42:37 +0000

    (extend_alloca): New define.  Based on stack direction it'll try to append to the previouls allocated buffer.

--- include/alloca.h
+++ include/alloca.h
@@ -1,6 +1,7 @@
 #ifndef _ALLOCA_H
 
 #include <stdlib/alloca.h>
+#include <stackinfo.h>
 
 #undef	__alloca
 
@@ -17,5 +18,32 @@ extern int __libc_alloca_cutoff (size_t size) __attribute__ ((const));
 #define __MAX_ALLOCA_CUTOFF	65536
 
 #include <allocalim.h>
+
+#if _STACK_GROWS_DOWN
+# define extend_alloca(buf, len, newlen) \
+  (__typeof (buf)) ({ size_t __newlen = (newlen);			      \
+		      char *__newbuf = __alloca (__newlen);		      \
+		      if (__newbuf + __newlen == (char *) buf)		      \
+			len += __newlen;				      \
+		      else						      \
+			len = __newlen;					      \
+		      __newbuf; })
+#elif _STACK_GROWS_UP
+# define extend_alloca(buf, len, newlen) \
+  (__typeof (buf)) ({ size_t __newlen = (newlen);			      \
+		      char *__newbuf = __alloca (__newlen);		      \
+		      char *__buf = (buf);				      \
+		      if (__buf + __newlen == __newbuf)			      \
+			{						      \
+			  len += __newlen;				      \
+			  __newbuf = __buf;				      \
+			}						      \
+		      else						      \
+			len = __newlen;					      \
+		      __newbuf; })
+#else
+# define extern_alloca(buf, len, newlen) \
+  __alloca (((len) = (newlen)))
+#endif
 
 #endif
