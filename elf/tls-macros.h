eec62f42bb1e1afeef75c58dcb4fe666d6c0fcb8
Ulrich Drepper <drepper@redhat.com>
2002-11-21 03:06:21 +0000

    Update.
    	* elf/tls-macros.h: Add IA-64 definitions.
    	Patch by Ian Wienand <ianw@gelato.unsw.edu.au>.

--- elf/tls-macros.h
+++ elf/tls-macros.h
@@ -215,28 +215,76 @@
 
 register void *__gp __asm__("$29");
 
-# define TLS_LE(x)							\
-  ({ int *__l;								\
-     asm ("call_pal 158\n\tlda $0," #x "($0)\t\t!tprel" : "=v"(__l));	\
+# define TLS_LE(x) \
+  ({ int *__l;								      \
+     asm ("call_pal 158\n\tlda $0," #x "($0)\t\t!tprel" : "=v"(__l));	      \
      __l; })
 
-# define TLS_IE(x)							\
-  ({ char *__tp; unsigned long __o;					\
-     asm ("call_pal 158\n\tldq %1," #x "($gp)\t\t!gottprel"		\
-	  : "=v"(__tp), "=r"(__o) : "r"(__gp));				\
+# define TLS_IE(x) \
+  ({ char *__tp; unsigned long __o;					      \
+     asm ("call_pal 158\n\tldq %1," #x "($gp)\t\t!gottprel"		      \
+	  : "=v"(__tp), "=r"(__o) : "r"(__gp));				      \
      (int *)(__tp + __o); })
 
-# define TLS_LD(x)							\
-  ({ extern void *__tls_get_addr(void *); int *__l; void *__i;		\
-     asm ("lda %0," #x "($gp)\t\t!tlsldm" : "=r" (__i) : "r"(__gp));	\
-     __i = __tls_get_addr(__i);						\
-     asm ("lda %0, " #x "(%1)\t\t!dtprel" : "=r"(__l) : "r"(__i));	\
+# define TLS_LD(x) \
+  ({ extern void *__tls_get_addr(void *); int *__l; void *__i;		      \
+     asm ("lda %0," #x "($gp)\t\t!tlsldm" : "=r" (__i) : "r"(__gp));	      \
+     __i = __tls_get_addr(__i);						      \
+     asm ("lda %0, " #x "(%1)\t\t!dtprel" : "=r"(__l) : "r"(__i));	      \
      __l; })
-	  
-# define TLS_GD(x)							\
-  ({ extern void *__tls_get_addr(void *); void *__i;			\
-     asm ("lda %0," #x "($gp)\t\t!tlsgd" : "=r" (__i) : "r"(__gp));	\
+
+# define TLS_GD(x) \
+  ({ extern void *__tls_get_addr(void *); void *__i;			      \
+     asm ("lda %0," #x "($gp)\t\t!tlsgd" : "=r" (__i) : "r"(__gp));	      \
      (int *) __tls_get_addr(__i); })
+
+
+#elif defined __ia64__
+
+# define TLS_LE(x) \
+  ({ void *__l;								      \
+     asm ("ld8 r2=tp\n\t"						      \
+         ";;\n\t"							      \
+         "addl %0=@tpre1(" #x "),r2\n\t"				      \
+         : "=r" (__l) : : "r2"  ); __l; })
+
+# define TLS_IE(x) \
+  ({ void *__l;								      \
+     asm ("addl r16=@ltoff(@tprel(" #x ")),gp\n\t"			      \
+         ";;\n\t"							      \
+         "ld8 r17=[r16]\n\t"						      \
+         ";;\n\t"							      \
+         "add %0=tp,r17\n\t"						      \
+         : "=r" (__l) : : "r16", "r17" ); __l; })
+
+# define TLS_LD(x) \
+  ({ void *__l;								      \
+     asm ("mov loc0=gp\n\t"						      \
+         "addl r16=@ltoff(@dtpmod(" #x ")),gp\n\t"			      \
+         "addl out1=@dtprel(" #x "),r0\n\t"				      \
+         ";;\n\t"							      \
+         "ld8 out0=[r16]\n\t"						      \
+         "br.callrp=__tls_get_addr"					      \
+         ";;\n\t"							      \
+         "mov gp=loc0\n\t"						      \
+         "mov %0=r8\n\t"						      \
+         : "=r" (__l) : : "r16" , "loc0" , "out0" , "out1" , "r8" );	      \
+     __l; })
+
+# define TLS_GD(x) \
+  ({ void *__l;								      \
+     asm ("mov loc0=gp\n\t"						      \
+         "addl r16=@ltoff(@dtpmod(" #x ")),gp\n\t"			      \
+         "addl r17=@ltoff(@dtprel(" #x ")),gp\n\t"			      \
+         ";;\n\t"							      \
+         "ld8 out0=[r16]\n\t"						      \
+         "ld8 out1=[r17]\n\t"						      \
+         "br.callrp=__tls_get_addr"					      \
+         ";;\n\t"							      \
+         "mov gp=loc0\n\t"						      \
+         "mov %0=r8\n\t"						      \
+          : "=r" (__l) : : "r16", "r17" , "loc0" , "out0", "out1" , "r8");    \
+     __l; })
 
 #else
 # error "No support for this architecture so far."
