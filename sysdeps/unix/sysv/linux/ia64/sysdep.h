1c120cb8e7eddf810ae3e997eebd689506c98307
Roland McGrath <roland@gnu.org>
2002-11-22 01:27:01 +0000

    * sysdeps/unix/sysv/linux/ia64/syscalls.list (s_exit_group): Add it.
    	From Ian Wienand <ianw@gelato.unsw.edu.au>.
    
    	* sysdeps/unix/sysv/linux/ia64/sysdep.h (__NR_pread): Define to
    	__NR_pread64 if not defined.
    	(__NR_pwrite): Define to __NR_pwrite64 if not defined.
    	From Ian Wienand <ianw@gelato.unsw.edu.au>.

--- sysdeps/unix/sysv/linux/ia64/sysdep.h
+++ sysdeps/unix/sysv/linux/ia64/sysdep.h
@@ -32,6 +32,16 @@
 # define SYS_ify(syscall_name)	__NR_/**/syscall_name
 #endif
 
+/* This is a kludge to make syscalls.list find these under the names
+   pread and pwrite, since some kernel headers define those names
+   and some define the *64 names for the same system calls.  */
+#if !defined __NR_pread && defined __NR_pread64
+# define __NR_pread __NR_pread64
+#endif
+#if !defined __NR_pwrite && defined __NR_pwrite64
+# define __NR_pwrite __NR_pwrite64
+#endif
+
 #ifdef __ASSEMBLER__
 
 #undef CALL_MCOUNT
