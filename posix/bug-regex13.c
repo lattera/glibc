6d3423184e540742d013aaff0ca1e497264641ae
Roland McGrath <roland@gnu.org>
2002-12-01 23:17:11 +0000

    * posix/bug-regex13.c: Include <string.h>.

--- posix/bug-regex13.c
+++ posix/bug-regex13.c
@@ -23,6 +23,7 @@
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 static struct
 {
--- sysdeps/mach/hurd/bits/posix_opt.h
+++ sysdeps/mach/hurd/bits/posix_opt.h
@@ -1,5 +1,5 @@
 /* Define POSIX options for GNU/Hurd.
-   Copyright (C) 1998,2000,01 Free Software Foundation, Inc.
+   Copyright (C) 1998,2000,2001,2002 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -42,25 +42,19 @@
 #undef _POSIX_SYNCHRONIZED_IO
 
 /* The fsync function is present.  */
-#define	_POSIX_FSYNC	1
+#define	_POSIX_FSYNC	200112L
 
 /* Mapping of files to memory is supported.  */
-#define	_POSIX_MAPPED_FILES	1
+#define	_POSIX_MAPPED_FILES	200112L
 
 /* Locking of ranges of memory is supported.  */
-#define	_POSIX_MEMLOCK_RANGE	1
+#define	_POSIX_MEMLOCK_RANGE	200112L
 
 /* Setting of memory protections is supported.  */
-#define	_POSIX_MEMORY_PROTECTION	1
+#define	_POSIX_MEMORY_PROTECTION	200112L
 
 /* POSIX.4 shared memory objects are supported (using regular files).  */
 #define _POSIX_SHARED_MEMORY_OBJECTS	_POSIX_MAPPED_FILES
-
-/* Implementation supports `poll' function.  */
-#define	_POSIX_POLL	1
-
-/* Implementation supports `select' and `pselect' functions.  */
-#define	_POSIX_SELECT	1
 
 /* Elements of the `c_cc' member of `struct termios' structure
    can be disabled by using the value _POSIX_VDISABLE.  */
@@ -80,6 +74,6 @@
 #define _POSIX_SHELL	1
 
 /* The `spawn' function family is supported.  */
-#define _POSIX_SPAWN	200912L
+#define _POSIX_SPAWN	200112L
 
 #endif /* bits/posix_opt.h */
