688e7bfe2b069ade0353cc41ba1582d12d897714
Ulrich Drepper <drepper@redhat.com>
2002-12-12 09:25:42 +0000

    Update.
    2002-12-11  Jakub Jelinek  <jakub@redhat.com>
    
    	* io/test-utime (main): If stnow.st[am]time is wrong, print it and
    	not st.st[am]time.

--- io/test-utime.c
+++ io/test-utime.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1994, 1996, 1997, 2000 Free Software Foundation, Inc.
+/* Copyright (C) 1994, 1996, 1997, 2000, 2002 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -114,13 +114,13 @@ main (int argc, char *argv[])
 
   if (stnow.st_mtime < now1 || stnow.st_mtime > now2)
     {
-      printf ("modtime %ld <%ld >%ld\n", st.st_mtime, now1, now2);
+      printf ("modtime %ld <%ld >%ld\n", stnow.st_mtime, now1, now2);
       return 1;
     }
 
   if (stnow.st_atime < now1 || stnow.st_atime > now2)
     {
-      printf ("actime %ld <%ld >%ld\n", st.st_atime, now1, now2);
+      printf ("actime %ld <%ld >%ld\n", stnow.st_atime, now1, now2);
       return 1;
     }
 
