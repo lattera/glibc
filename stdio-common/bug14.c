a1a2fec161c0fafca0f8e5df454e9ec9fa15ed99
Ulrich Drepper <drepper@redhat.com>
2003-01-16 00:09:24 +0000

    Update.
    	* stdio-common/Makefile (tests): Add bug14.
    	* stdio-common/bug14.c: New file.

---
+++ stdio-common/bug14.c
@@ -0,0 +1,28 @@
+/* Test case by Al Viro <aviro@redhat.com>.  */
+#include <locale.h>
+#include <wchar.h>
+#include <stdio.h>
+
+/* MB_CUR_MAX multibyte ones (6 UTF+0080, in this case) */
+static const char string[] = "\
+\xc2\x80\xc2\x80\xc2\x80\xc2\x80\xc2\x80\xc2\x80";
+
+int
+main (void)
+{
+  if (setlocale (LC_ALL, "de_DE.utf8") == NULL)
+    {
+      puts ("cannot set locale");
+      exit (1);
+    }
+
+  wchar_t s[7];
+  int n = sscanf (string, "%l[\x80\xc2]", s);
+  if (n != 1)
+    {
+      printf ("return values %d != 1\n", n);
+      exit (1);
+    }
+
+  return 0;
+}
