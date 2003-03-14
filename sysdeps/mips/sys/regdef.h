5ace1393645876e733698cc05dac51a8bfbec965
Alexandre Oliva <aoliva@redhat.com>
2003-03-14 05:31:28 +0000

    * sysdeps/mips/memcpy.S: Map t0-3 to a4-7 on new abis. * sysdeps/mips/memset.S: Likewise. * sysdeps/mips/sys/regdef.h: Alias a4-7 or t0-3 to $8-11 depending on the ABI.
    	* sysdeps/mips/memcpy.S: Map t0-3 to a4-7 on new abis.
    	* sysdeps/mips/memset.S: Likewise.
    	* sysdeps/mips/sys/regdef.h: Alias a4-7 or t0-3 to $8-11
    	depending on the ABI.
    
    2003-03-14  Alexandre Oliva  <aoliva@redhat.com>

--- sysdeps/mips/sys/regdef.h
+++ sysdeps/mips/sys/regdef.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1997, 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1997, 1998, 2002, 2003 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ralf Baechle <ralf@gnu.org>.
 
@@ -31,10 +31,17 @@
 #define a1      $5
 #define a2      $6
 #define a3      $7
+#if _MIPS_SIM != _MIPS_SIM_ABI32
+#define a4	$8
+#define a5	$9
+#define a6	$10
+#define a7	$11
+#else /* if _MIPS_SIM == _MIPS_SIM_ABI32 */
 #define t0      $8      /* caller saved */
 #define t1      $9
 #define t2      $10
 #define t3      $11
+#endif /* _MIPS_SIM == _MIPS_SIM_ABI32 */
 #define t4      $12
 #define t5      $13
 #define t6      $14
