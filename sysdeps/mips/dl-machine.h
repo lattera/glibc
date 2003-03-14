365eccdb099f7cff2fdc589bef7be7806a4bc9c7
Alexandre Oliva <aoliva@redhat.com>
2003-03-14 08:43:13 +0000

    * sysdeps/mips/dl-machine.h: Include sys/asm.h. (elf_machine_matches_host): Prevent linking of o32 and n32 together. (elf_machine_dynamic): Document assumption on $gp. (STRINGXP, STRINGXV, STRINGV_): New macros. (elf_machine_load_address): Use them to stringize PTR_LA and PTR_SUBU. (ELF_DL_FRAME_SIZE, ELF_DL_SAVE_ARG_REGS, ELF_DL_RESTORE_ARG_REGS, IFABIO32): New macros used in... (_dl_runtime_resolve): Adjust it for all 3 ABIs. (__dl_runtime_resolve): Cast the symtab initializer to the right type. (RTLD_START): Use it.  Adjust it for all 3 ABIs. (elf_machine_rel): Mark as always_inline in RTLD_BOOTSTRAP. Handle 64-bit R_MIPS_REL composite relocation and accept R_MIPS_64 relocations to shift addend size to 64 bits. Document assumption regarding local GOT entries.  Document backward-compatibility departing from the ABI behavior in applying relocations that reference section symbols, no longer used.  Support relocations to mis-aligned offsets. * sysdeps/mips/mips64/dl-machine.h: Deleted, obsolete.
    	* sysdeps/mips/dl-machine.h: Include sys/asm.h.
    	(elf_machine_matches_host): Prevent linking of o32 and n32
    	together.
    	(elf_machine_dynamic): Document assumption on $gp.
    	(STRINGXP, STRINGXV, STRINGV_): New macros.
    	(elf_machine_load_address): Use them to stringize PTR_LA and
    	PTR_SUBU.
    	(ELF_DL_FRAME_SIZE, ELF_DL_SAVE_ARG_REGS,
    	ELF_DL_RESTORE_ARG_REGS, IFABIO32): New macros used in...
    	(_dl_runtime_resolve): Adjust it for all 3 ABIs.
    	(__dl_runtime_resolve): Cast the symtab initializer to the
    	right type.
    	(RTLD_START): Use it.  Adjust it for all 3 ABIs.
    	(elf_machine_rel): Mark as always_inline in RTLD_BOOTSTRAP.
    	Handle 64-bit R_MIPS_REL composite relocation and accept
    	R_MIPS_64 relocations to shift addend size to 64 bits.
    	Document assumption regarding local GOT entries.  Document
    	backward-compatibility departing from the ABI behavior in
    	applying relocations that reference section symbols, no longer
    	used.  Support relocations to mis-aligned offsets.
    	* sysdeps/mips/mips64/dl-machine.h: Deleted, obsolete.

--- sysdeps/mips/dl-machine.h
+++ sysdeps/mips/dl-machine.h
@@ -32,6 +32,8 @@
 #error ENTRY_POINT needs to be defined for MIPS.
 #endif
 
+#include <sys/asm.h>
+
 /* The offset of gp from GOT might be system-dependent.  It's set by
    ld.  The same value is also */
 #define OFFSET_GP_GOT 0x7ff0
@@ -72,6 +74,13 @@ do { if ((l)->l_info[DT_MIPS (RLD_MAP)]) \
 static inline int __attribute_used__
 elf_machine_matches_host (const ElfW(Ehdr) *ehdr)
 {
+#if _MIPS_SIM == _MIPS_SIM_ABI32 || _MIPS_SIM == _MIPS_SIM_NABI32
+  /* Don't link o32 and n32 together.  */
+  if (((ehdr->e_flags & EF_MIPS_ABI2) != 0)
+      != (_MIPS_SIM != _MIPS_SIM_ABI32))
+    return 0;
+#endif
+
   switch (ehdr->e_machine)
     {
     case EM_MIPS:
@@ -91,7 +100,7 @@ elf_mips_got_from_gpreg (ElfW(Addr) gpreg)
 
 /* Return the link-time address of _DYNAMIC.  Conveniently, this is the
    first element of the GOT.  This must be inlined in a function which
-   uses global data.  */
+   uses global data.  We assume its $gp points to the primary GOT.  */
 static inline ElfW(Addr)
 elf_machine_dynamic (void)
 {
@@ -99,6 +108,9 @@ elf_machine_dynamic (void)
   return *elf_mips_got_from_gpreg (gp);
 }
 
+#define STRINGXP(X) __STRING(X)
+#define STRINGXV(X) STRINGV_(X)
+#define STRINGV_(...) # __VA_ARGS__
 
 /* Return the run-time load address of the shared object.  */
 static inline ElfW(Addr)
@@ -106,10 +118,10 @@ elf_machine_load_address (void)
 {
   ElfW(Addr) addr;
   asm ("	.set noreorder\n"
-       "	la %0, here\n"
-       "	bltzal $0, here\n"
+       "	" STRINGXP (PTR_LA) " %0, 0f\n"
+       "	bltzal $0, 0f\n"
        "	nop\n"
-       "here:	subu %0, $31, %0\n"
+       "0:	" STRINGXP (PTR_SUBU) " %0, $31, %0\n"
        "	.set reorder\n"
        :	"=r" (addr)
        :	/* No inputs */
@@ -242,6 +254,55 @@ elf_machine_runtime_link_map (ElfW(Addr) gpreg, ElfW(Addr) stub_pc)
   return NULL;
 }
 
+#if _MIPS_SIM == _MIPS_SIM_ABI32
+#define ELF_DL_FRAME_SIZE 40
+
+#define ELF_DL_SAVE_ARG_REGS "\
+	sw	$15, 36($29)\n						      \
+	sw	$4, 16($29)\n						      \
+	sw	$5, 20($29)\n						      \
+	sw	$6, 24($29)\n						      \
+	sw	$7, 28($29)\n						      \
+"
+
+#define ELF_DL_RESTORE_ARG_REGS "\
+	lw	$31, 36($29)\n						      \
+	lw	$4, 16($29)\n						      \
+	lw	$5, 20($29)\n						      \
+	lw	$6, 24($29)\n						      \
+	lw	$7, 28($29)\n						      \
+"
+
+#define IFABIO32(X) X
+
+#else /* _MIPS_SIM == _MIPS_SIM_NABI32 || _MIPS_SIM == _MIPS_SIM_ABI64 */
+
+#define ELF_DL_FRAME_SIZE 64
+
+#define ELF_DL_SAVE_ARG_REGS "\
+	sd	$15, 56($29)\n						      \
+	sd	$4, 8($29)\n						      \
+	sd	$5, 16($29)\n						      \
+	sd	$6, 24($29)\n						      \
+	sd	$7, 32($29)\n						      \
+	sd	$8, 40($29)\n						      \
+	sd	$9, 48($29)\n						      \
+"
+
+#define ELF_DL_RESTORE_ARG_REGS "\
+	ld	$31, 56($29)\n						      \
+	ld	$4, 8($29)\n						      \
+	ld	$5, 16($29)\n						      \
+	ld	$6, 24($29)\n						      \
+	ld	$7, 32($29)\n						      \
+	ld	$8, 40($29)\n						      \
+	ld	$9, 48($29)\n						      \
+"
+
+#define IFABIO32(X)
+
+#endif
+
 /* Define mips specific runtime resolver. The function __dl_runtime_resolve
    is called from assembler function _dl_runtime_resolve which converts
    special argument registers t7 ($15) and t8 ($24):
@@ -272,9 +333,8 @@ __dl_runtime_resolve (ElfW(Word) sym_index,				      \
 {									      \
   struct link_map *l = elf_machine_runtime_link_map (old_gpreg, stub_pc);     \
   const ElfW(Sym) *const symtab						      \
-    = (const void *) D_PTR (l, l_info[DT_SYMTAB]);			      \
-  const char *strtab							      \
-    = (const void *) D_PTR (l, l_info[DT_STRTAB]);			      \
+    = (const ElfW(Sym) *) D_PTR (l, l_info[DT_SYMTAB]);			      \
+  const char *strtab = (const void *) D_PTR (l, l_info[DT_STRTAB]);	      \
   ElfW(Addr) *got							      \
     = (ElfW(Addr) *) D_PTR (l, l_info[DT_PLTGOT]);			      \
   const ElfW(Word) local_gotno						      \
@@ -333,36 +393,30 @@ asm ("\n								      \
 	.type	_dl_runtime_resolve,@function\n				      \
 	.ent	_dl_runtime_resolve\n					      \
 _dl_runtime_resolve:\n							      \
-	.frame	$29, 40, $31\n						      \
+	.frame	$29, " STRINGXP(ELF_DL_FRAME_SIZE) ", $31\n		      \
 	.set noreorder\n						      \
 	# Save GP.\n							      \
 	move	$3, $28\n						      \
+	# Save arguments and sp value in stack.\n			      \
+	" STRINGXP(PTR_SUBIU) "  $29, " STRINGXP(ELF_DL_FRAME_SIZE) "\n	      \
 	# Modify t9 ($25) so as to point .cpload instruction.\n		      \
-	addu	$25, 8\n						      \
+	" IFABIO32(STRINGXP(PTR_ADDIU) "	$25, 12\n") "		      \
 	# Compute GP.\n							      \
-	.cpload $25\n							      \
+	" STRINGXP(SETUP_GP) "\n					      \
+	" STRINGXV(SETUP_GP64 (0, _dl_runtime_resolve)) "\n		      \
 	.set reorder\n							      \
 	# Save slot call pc.\n						      \
 	move	$2, $31\n						      \
-	# Save arguments and sp value in stack.\n			      \
-	subu	$29, 40\n						      \
-	.cprestore 32\n							      \
-	sw	$15, 36($29)\n						      \
-	sw	$4, 16($29)\n						      \
-	sw	$5, 20($29)\n						      \
-	sw	$6, 24($29)\n						      \
-	sw	$7, 28($29)\n						      \
+	" IFABIO32(STRINGXP(CPRESTORE(32))) "\n				      \
+	" ELF_DL_SAVE_ARG_REGS "					      \
 	move	$4, $24\n						      \
 	move	$5, $15\n						      \
 	move	$6, $3\n						      \
 	move	$7, $2\n						      \
 	jal	__dl_runtime_resolve\n					      \
-	lw	$31, 36($29)\n						      \
-	lw	$4, 16($29)\n						      \
-	lw	$5, 20($29)\n						      \
-	lw	$6, 24($29)\n						      \
-	lw	$7, 28($29)\n						      \
-	addu	$29, 40\n						      \
+	" ELF_DL_RESTORE_ARG_REGS "					      \
+	" STRINGXP(RESTORE_GP64) "\n					      \
+	" STRINGXP(PTR_ADDIU) "	$29, " STRINGXP(ELF_DL_FRAME_SIZE) "\n	      \
 	move	$25, $2\n						      \
 	jr	$25\n							      \
 	.end	_dl_runtime_resolve\n					      \
@@ -372,7 +426,6 @@ _dl_runtime_resolve:\n							      \
 /* Mask identifying addresses reserved for the user program,
    where the dynamic linker should not map anything.  */
 #define ELF_MACHINE_USER_ADDRESS_MASK	0x80000000UL
-
 
 
 /* Initial entry point code for the dynamic linker.
@@ -392,71 +445,71 @@ _dl_runtime_resolve:\n							      \
 
 #define RTLD_START asm (\
 	".text\n"\
-	_RTLD_PROLOGUE(ENTRY_POINT)\
-	".set noreorder\n\
-	bltzal $0, 0f\n\
-	nop\n\
-0:	.cpload $31\n\
-	.set reorder\n\
+	_RTLD_PROLOGUE(ENTRY_POINT) "\
+	" STRINGXV(SETUP_GPX($25)) "\n\
+	" STRINGXV(SETUP_GPX64($18,$25)) "\n\
 	# i386 ABI book says that the first entry of GOT holds\n\
 	# the address of the dynamic structure. Though MIPS ABI\n\
 	# doesn't say nothing about this, I emulate this here.\n\
-	la $4, _DYNAMIC\n\
+	" STRINGXP(PTR_LA) " $4, _DYNAMIC\n\
 	# Subtract OFFSET_GP_GOT\n\
-	sw $4, -0x7ff0($28)\n\
+	" STRINGXP(PTR_S) " $4, -0x7ff0($28)\n\
 	move $4, $29\n\
-	subu $29, 16\n\
+	" STRINGXP(PTR_SUBIU) " $29, 16\n\
 	\n\
-	la $8, coff\n\
-	bltzal $8, coff\n\
-coff:	subu $8, $31, $8\n\
+	" STRINGXP(PTR_LA) " $8, .Lcoff\n\
+	bltzal $8, .Lcoff\n\
+.Lcoff:	" STRINGXP(PTR_SUBU) " $8, $31, $8\n\
 	\n\
-	la $25, _dl_start\n\
-	addu $25, $8\n\
+	" STRINGXP(PTR_LA) " $25, _dl_start\n\
+	" STRINGXP(PTR_ADDU) " $25, $8\n\
 	jalr $25\n\
 	\n\
-	addiu $29, 16\n\
+	" STRINGXP(PTR_ADDIU) " $29, 16\n\
 	# Get the value of label '_dl_start_user' in t9 ($25).\n\
-	la $25, _dl_start_user\n\
+	" STRINGXP(PTR_LA) " $25, _dl_start_user\n\
 	.globl _dl_start_user\n\
+	.type _dl_start_user,@function\n\
+	.ent _dl_start_user\n\
 _dl_start_user:\n\
-	.set noreorder\n\
-	.cpload $25\n\
-	.set reorder\n\
+	" STRINGXP(SETUP_GP) "\n\
+	" STRINGXV(SETUP_GP64($18,_dl_start_user)) "\n\
 	move $16, $28\n\
 	# Save the user entry point address in a saved register.\n\
 	move $17, $2\n\
 	# Store the highest stack address\n\
-	sw $29, __libc_stack_end\n\
+	" STRINGXP(PTR_S) " $29, __libc_stack_end\n\
 	# See if we were run as a command with the executable file\n\
 	# name as an extra leading argument.\n\
 	lw $2, _dl_skip_args\n\
 	beq $2, $0, 1f\n\
 	# Load the original argument count.\n\
-	lw $4, 0($29)\n\
+	" STRINGXP(PTR_L) " $4, 0($29)\n\
 	# Subtract _dl_skip_args from it.\n\
 	subu $4, $2\n\
 	# Adjust the stack pointer to skip _dl_skip_args words.\n\
-	sll $2, 2\n\
-	addu $29, $2\n\
+	sll $2, " STRINGXP (PTRLOG) "\n\
+	" STRINGXP(PTR_ADDU) " $29, $2\n\
 	# Save back the modified argument count.\n\
-	sw $4, 0($29)\n\
+	" STRINGXP(PTR_S) " $4, 0($29)\n\
 1:	# Call _dl_init (struct link_map *main_map, int argc, char **argv, char **env) \n\
-	lw $4, _rtld_local\n\
-	lw $5, 0($29)\n\
-	la $6, 4($29)\n\
-	sll $7, $5, 2\n\
-	addu $7, $7, $6\n\
-	addu $7, $7, 4\n\
-	subu $29, 16\n\
+	" STRINGXP(PTR_L) " $4, _rtld_local\n\
+	" STRINGXP(PTR_L) /* or lw???  fixme */ " $5, 0($29)\n\
+	" STRINGXP(PTR_LA) " $6, " STRINGXP (PTRSIZE) "($29)\n\
+	sll $7, $5, " STRINGXP (PTRLOG) "\n\
+	" STRINGXP(PTR_ADDU) " $7, $7, $6\n\
+	" STRINGXP(PTR_ADDU) " $7, $7, " STRINGXP (PTRSIZE) " \n\
+	" STRINGXP(PTR_SUBIU) " $29, 32\n\
+	" STRINGXP(SAVE_GP(16)) "\n\
 	# Call the function to run the initializers.\n\
 	jal _dl_init_internal\n\
-	addiu $29, 16\n\
+	" STRINGXP(PTR_ADDIU)  " $29, 32\n\
 	# Pass our finalizer function to the user in $2 as per ELF ABI.\n\
-	la $2, _dl_fini\n\
+	" STRINGXP(PTR_LA) " $2, _dl_fini\n\
 	# Jump to the user entry point.\n\
 	move $25, $17\n\
-	jr $25\n\t"\
+	jr $25\n\
+	.end _dl_start_user\n\t"\
 	_RTLD_EPILOGUE(ENTRY_POINT)\
 	".previous"\
 );
@@ -472,6 +525,9 @@ _dl_start_user:\n\
    MAP is the object containing the reloc.  */
 
 static inline void
+#ifdef RTLD_BOOTSTRAP
+  __attribute__ ((always_inline))
+#endif
 elf_machine_rel (struct link_map *map, const ElfW(Rel) *reloc,
 		 const ElfW(Sym) *sym, const struct r_found_version *version,
 		 ElfW(Addr) *const reloc_addr)
@@ -490,9 +546,19 @@ elf_machine_rel (struct link_map *map, const ElfW(Rel) *reloc,
 
   switch (r_type)
     {
+#if _MIPS_SIM == _MIPS_SIM_ABI64
+    case (R_MIPS_64 << 8) | R_MIPS_REL32:
+#else
     case R_MIPS_REL32:
+#endif
       {
 	int symidx = ELFW(R_SYM) (reloc->r_info);
+	ElfW(Addr) reloc_value;
+
+	/* Support relocations on mis-aligned offsets.  Should we ever
+	   implement RELA, this should be replaced with an assignment
+	   from reloc->r_addend.  */
+	__builtin_memcpy (&reloc_value, reloc_addr, sizeof (reloc_value));
 
 	if (symidx)
 	  {
@@ -501,10 +567,25 @@ elf_machine_rel (struct link_map *map, const ElfW(Rel) *reloc,
 
 	    if (symidx < gotsym)
 	      {
+		/* This wouldn't work for a symbol imported from other
+		   libraries for which there's no GOT entry, but MIPS
+		   requires every symbol referenced in a dynamic
+		   relocation to have a GOT entry in the primary GOT,
+		   so we only get here for locally-defined symbols.
+		   For section symbols, we should *NOT* be adding
+		   sym->st_value (per the definition of the meaning of
+		   S in reloc expressions in the ELF64 MIPS ABI),
+		   since it should have already been added to
+		   reloc_value by the linker, but older versions of
+		   GNU ld didn't add it, and newer versions don't emit
+		   useless relocations to section symbols any more, so
+		   it is safe to keep on adding sym->st_value, even
+		   though it's not ABI compliant.  Some day we should
+		   bite the bullet and stop doing this.  */
 #ifndef RTLD_BOOTSTRAP
 		if (map != &GL(dl_rtld_map))
 #endif
-		  *reloc_addr += sym->st_value + map->l_addr;
+		  reloc_value += sym->st_value + map->l_addr;
 	      }
 	    else
 	      {
@@ -515,7 +596,7 @@ elf_machine_rel (struct link_map *map, const ElfW(Rel) *reloc,
 		  = (const ElfW(Word))
 		    map->l_info[DT_MIPS (LOCAL_GOTNO)]->d_un.d_val;
 
-		*reloc_addr += got[symidx + local_gotno - gotsym];
+		reloc_value += got[symidx + local_gotno - gotsym];
 #endif
 	      }
 	  }
@@ -523,11 +604,27 @@ elf_machine_rel (struct link_map *map, const ElfW(Rel) *reloc,
 #ifndef RTLD_BOOTSTRAP
 	  if (map != &GL(dl_rtld_map))
 #endif
-	    *reloc_addr += map->l_addr;
+	    reloc_value += map->l_addr;
+
+	__builtin_memcpy (reloc_addr, &reloc_value, sizeof (reloc_value));
       }
       break;
     case R_MIPS_NONE:		/* Alright, Wilbur.  */
       break;
+#if _MIPS_SIM == _MIPS_SIM_ABI64
+    case R_MIPS_64:
+      /* For full compliance with the ELF64 ABI, one must precede the
+	 _REL32/_64 pair of relocations with a _64 relocation, such
+	 that the in-place addend is read as a 64-bit value.  IRIX
+	 didn't pick up on this requirement, so we treat the
+	 _REL32/_64 relocation as a 64-bit relocation even if it's by
+	 itself.  For ABI compliance, we ignore such _64 dummy
+	 relocations.  For RELA, this may be simply removed, since
+	 it's totally unnecessary.  */
+      if (ELFW(R_SYM) (reloc->r_info) == 0)
+	break;
+      /* Fall through.  */
+#endif
     default:
       _dl_reloc_bad_type (map, r_type, 0);
       break;
