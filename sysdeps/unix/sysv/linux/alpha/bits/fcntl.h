4a05f4cc0892c56b00b4aa1bfa11a5a2e9962121
Ulrich Drepper <drepper@redhat.com>
2003-04-19 18:31:57 +0000

    Sync with Linux 2.5.67.

--- sysdeps/unix/sysv/linux/alpha/bits/fcntl.h
+++ sysdeps/unix/sysv/linux/alpha/bits/fcntl.h
@@ -168,6 +168,6 @@ struct flock64
 # define POSIX_FADV_RANDOM	1 /* Expect random page references.  */
 # define POSIX_FADV_SEQUENTIAL	2 /* Expect sequential page references.  */
 # define POSIX_FADV_WILLNEED	3 /* Will need these pages.  */
-# define POSIX_FADV_DONTNEED	6 /* Don't need these pages.  */
-# define POSIX_FADV_NOREUSE	7 /* Data will be accessed once.  */
+# define POSIX_FADV_DONTNEED	4 /* Don't need these pages.  */
+# define POSIX_FADV_NOREUSE	5 /* Data will be accessed once.  */
 #endif
