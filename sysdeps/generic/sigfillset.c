6a87ee1943f6eb52b04bf11098415bed614f9253
Ulrich Drepper <drepper@redhat.com>
2003-04-21 07:48:10 +0000

    Update.
    	* signal/sigfillset.c: Moved to...
    	* sysdeps/generic/sigfillset.c: ...here.  If SIGCANCEL is defined
    	do not set the corresponding bit.

--- sysdeps/generic/sigfillset.c
+++ sysdeps/generic/sigfillset.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991,96,97,2002 Free Software Foundation, Inc.
+/* Copyright (C) 1991,96,97,2002, 2003 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -32,6 +32,11 @@ sigfillset (set)
     }
 
   memset (set, 0xff, sizeof (sigset_t));
+
+  /* If the implementation uses a cancellation signal don't set the bit.  */
+#ifdef SIGCANCEL
+  __sigdelset (set, SIGCANCEL);
+#endif
 
   return 0;
 }
