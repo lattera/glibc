f24dca48290e4a1c731fc0bee94bc9a1f891b3fa
Ulrich Drepper <drepper@redhat.com>
2003-04-25 17:54:19 +0000

    Update.
    2003-04-25  Ulrich Drepper  <drepper@redhat.com>
    
    	* sysdeps/posix/getaddrinfo.c: Include <stdbool.h>.

--- sysdeps/posix/getaddrinfo.c
+++ sysdeps/posix/getaddrinfo.c
@@ -40,6 +40,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ifaddrs.h>
 #include <netdb.h>
 #include <resolv.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
