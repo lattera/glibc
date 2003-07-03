1418bf990e16ae15a4f3b7a8b38ac57a79e7f4dd
Ulrich Drepper <drepper@redhat.com>
2003-07-03 08:51:46 +0000

    Update.
    2003-07-03  Ulrich Drepper  <drepper@redhat.com>
    
    	* sysdeps/unix/sysv/linux/kernel-features.h (__ASSUME_AT_CLKTCK):
    	Define.
    	* sysdeps/unix/sysv/linux/prof-freq.h: New file.

--- sysdeps/unix/sysv/linux/kernel-features.h
+++ sysdeps/unix/sysv/linux/kernel-features.h
@@ -175,6 +175,12 @@
 # define __ASSUME_FCNTL64		1
 #endif
 
+/* The AT_CLKTCK auxiliary vector entry was introduction in the 2.4.0
+   series.  */
+#if __LINUX_KERNEL_VERSION >= 132097
+# define __ASSUME_AT_CLKTCK		1
+#endif
+
 /* Arm got fcntl64 in 2.4.4, PowerPC and SH have it also in 2.4.4 (I
    don't know when it got introduced).  But PowerPC64 does not support
    separate FCNTL64 call, FCNTL is already 64-bit */
---
+++ sysdeps/unix/sysv/linux/prof-freq.c
@@ -0,0 +1,51 @@
+/* Determine realtime clock frequency.
+   Copyright (C) 2003 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#include <sys/time.h>
+#include <libc-internal.h>
+#include "kernel-features.h"
+#include <ldsodefs.h>
+
+
+int
+__profile_frequency (void)
+{
+#ifdef __ASSUME_AT_CLKTCK
+  return GL(dl_clktck);
+#else
+  if (GL(dl_clktck) != )
+    return GL(dl_clktck);
+
+  struct itimerval tim;
+
+  tim.it_interval.tv_sec = 0;
+  tim.it_interval.tv_usec = 1;
+  tim.it_value.tv_sec = 0;
+  tim.it_value.tv_usec = 0;
+
+  __setitimer (ITIMER_REAL, &tim, 0);
+  __setitimer (ITIMER_REAL, 0, &tim);
+
+  if (tim.it_interval.tv_usec < 2)
+    return 0;
+
+  return 1000000 / tim.it_interval.tv_usec;
+#endif
+}
+libc_hidden_def (__profile_frequency)
