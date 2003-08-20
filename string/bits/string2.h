87865838da97993a22a4414d29b6b90f5e750970
Ulrich Drepper <drepper@redhat.com>
2003-08-20 00:08:24 +0000

    Update.
    	* string/bits/string2.h (__STRING2_COPY_TYPE): Add attribute to
    	the type, not to name.

--- string/bits/string2.h
+++ string/bits/string2.h
@@ -1,5 +1,5 @@
 /* Machine-independant string function optimizations.
-   Copyright (C) 1997,1998,1999,2000,2001,2002 Free Software Foundation, Inc.
+   Copyright (C) 1997-2002, 2003 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1997.
 
@@ -76,7 +76,7 @@
    use unaligned memory accesses.  */
 # define __STRING2_COPY_TYPE(N) \
   typedef struct { unsigned char __arr[N]; }				      \
-    __STRING2_COPY_ARR##N __attribute__ ((packed))
+    __attribute__ ((__packed__)) __STRING2_COPY_ARR##N
 __STRING2_COPY_TYPE (2);
 __STRING2_COPY_TYPE (3);
 __STRING2_COPY_TYPE (4);
