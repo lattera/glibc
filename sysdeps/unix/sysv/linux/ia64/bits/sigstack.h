d8b41dab551577f894269995f22d9f5a90439f52
Ulrich Drepper <drepper@redhat.com>
2003-09-05 18:04:05 +0000

    (MINSIGSTKSZ): Fix a typo.

--- sysdeps/unix/sysv/linux/ia64/bits/sigstack.h
+++ sysdeps/unix/sysv/linux/ia64/bits/sigstack.h
@@ -1,5 +1,5 @@
 /* sigstack, sigaltstack definitions.
-   Copyright (C) 1998, 2000, 2002 Free Software Foundation, Inc.
+   Copyright (C) 1998, 2000, 2002, 2003 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -42,7 +42,7 @@ enum
 };
 
 /* Minimum stack size for a signal handler.  */
-#define MINSIGSTKSZ	131027
+#define MINSIGSTKSZ	131072
 
 /* System default stack size.  */
 #define SIGSTKSZ	262144
