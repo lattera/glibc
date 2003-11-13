813ec65a4d69dcf9b2ecaf42395a4ba62dd2d33c
Ulrich Drepper <drepper@redhat.com>
2003-11-13 01:04:26 +0000

    Update.
    2003-11-12  Ulrich Drepper  <drepper@redhat.com>
    
    	* posix/PTESTS: Cleanup.  Fix typoes.  Correct bugs in 2003.2.
    	* posix/runptests.c (main): Handle comments.
    	* posix/PTESTS2C.sed: Handle comments.

--- posix/runptests.c
+++ posix/runptests.c
@@ -1,5 +1,5 @@
 /* POSIX regex testsuite from IEEE 2003.2.
-   Copyright (C) 1998 Free Software Foundation, Inc.
+   Copyright (C) 1998, 2003 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1998.
 
@@ -50,6 +50,8 @@ main (int argc, char *argv[])
 		(int) strlen (tests[cnt].reg),
 		"-----------------------------------------------------");
       }
+    else if (tests[cnt].reg == NULL)
+	printf ("!!! %s\n", tests[cnt].str);
     else
       {
 	regex_t re;
