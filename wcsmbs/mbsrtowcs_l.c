b3e09193c5d405ed48ad92a3a96afe0007fcefec
Ulrich Drepper <drepper@redhat.com>
2004-03-15 01:09:34 +0000

    Update.
    	* wcsmbs/mbsrtowcs.c: Just a wrapper around __mbsrtowcs_l.
    	* wcsmbs/mbsrtowcs_l.c (__mbsrtowcs_l): Add real implementation here.

--- wcsmbs/mbsrtowcs_l.c
+++ wcsmbs/mbsrtowcs_l.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2002 Free Software Foundation, Inc.
+/* Copyright (C) 2002, 2004 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@gnu.org>, 2002.
 
@@ -20,6 +20,128 @@
 #include <ctype.h>
 #include <string.h>
 #include "wcsmbsload.h"
+#include <dlfcn.h>
+#include <errno.h>
+#include <gconv.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+#include <wcsmbsload.h>
 
-#define USE_IN_EXTENDED_LOCALE_MODEL	1
-#include "mbsrtowcs.c"
+#include <assert.h>
+
+#ifndef EILSEQ
+# define EILSEQ EINVAL
+#endif
+
+
+size_t
+attribute_hidden
+__mbsrtowcs_l (dst, src, len, ps, l)
+     wchar_t *dst;
+     const char **src;
+     size_t len;
+     mbstate_t *ps;
+     __locale_t l;
+{
+  struct __gconv_step_data data;
+  size_t result;
+  int status;
+  struct __gconv_step *towc;
+  size_t non_reversible;
+  const struct gconv_fcts *fcts;
+
+  /* Tell where we want the result.  */
+  data.__invocation_counter = 0;
+  data.__internal_use = 1;
+  data.__flags = __GCONV_IS_LAST;
+  data.__statep = ps;
+  data.__trans = NULL;
+
+  /* Get the conversion functions.  */
+  fcts = get_gconv_fcts (l->__locales[LC_CTYPE]);
+
+  /* Get the structure with the function pointers.  */
+  towc = fcts->towc;
+
+  /* We have to handle DST == NULL special.  */
+  if (dst == NULL)
+    {
+      mbstate_t temp_state;
+      wchar_t buf[64];		/* Just an arbitrary size.  */
+      const unsigned char *inbuf = (const unsigned char *) *src;
+      const unsigned char *srcend = inbuf + strlen (inbuf) + 1;
+
+      temp_state = *data.__statep;
+      data.__statep = &temp_state;
+
+      result = 0;
+      data.__outbufend = (char *) buf + sizeof (buf);
+      do
+	{
+	  data.__outbuf = (char *) buf;
+
+	  status = DL_CALL_FCT (towc->__fct,
+				(towc, &data, &inbuf, srcend, NULL,
+				 &non_reversible, 0, 1));
+
+	  result += (wchar_t *) data.__outbuf - buf;
+	}
+      while (status == __GCONV_FULL_OUTPUT);
+
+      if (status == __GCONV_OK || status == __GCONV_EMPTY_INPUT)
+	{
+	  /* There better should be a NUL wide char at the end.  */
+	  assert (((wchar_t *) data.__outbuf)[-1] == L'\0');
+	  /* Don't count the NUL character in.  */
+	  --result;
+	}
+    }
+  else
+    {
+      /* This code is based on the safe assumption that all internal
+	 multi-byte encodings use the NUL byte only to mark the end
+	 of the string.  */
+      const unsigned char *srcend;
+
+      srcend = (const unsigned char *) (*src
+					+ __strnlen (*src, len * MB_CUR_MAX)
+					+ 1);
+
+      data.__outbuf = (unsigned char *) dst;
+      data.__outbufend = data.__outbuf + len * sizeof (wchar_t);
+
+      status = DL_CALL_FCT (towc->__fct,
+			    (towc, &data, (const unsigned char **) src, srcend,
+			     NULL, &non_reversible, 0, 1));
+
+      result = (wchar_t *) data.__outbuf - dst;
+
+      /* We have to determine whether the last character converted
+	 is the NUL character.  */
+      if ((status == __GCONV_OK || status == __GCONV_EMPTY_INPUT)
+	  && ((wchar_t *) dst)[result - 1] == L'\0')
+	{
+	  assert (result > 0);
+	  assert (__mbsinit (data.__statep));
+	  *src = NULL;
+	  --result;
+	}
+    }
+
+  /* There must not be any problems with the conversion but illegal input
+     characters.  */
+  assert (status == __GCONV_OK || status == __GCONV_EMPTY_INPUT
+	  || status == __GCONV_ILLEGAL_INPUT
+	  || status == __GCONV_INCOMPLETE_INPUT
+	  || status == __GCONV_FULL_OUTPUT);
+
+  if (status != __GCONV_OK && status != __GCONV_FULL_OUTPUT
+      && status != __GCONV_EMPTY_INPUT && status != __GCONV_INCOMPLETE_INPUT)
+    {
+      result = (size_t) -1;
+      __set_errno (EILSEQ);
+    }
+
+  return result;
+}
