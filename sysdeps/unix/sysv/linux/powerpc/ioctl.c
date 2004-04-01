e58ab813d6693f6acf401a28a9f9b0592f084f7b
Ulrich Drepper <drepper@redhat.com>
2004-04-01 21:25:31 +0000

    Update.
    	* sysdeps/unix/sysv/linux/powerpc/ioctl.c (__ioctl): Use
    	__tcgetattr() instead of tcgetattr().

--- sysdeps/unix/sysv/linux/powerpc/ioctl.c
+++ sysdeps/unix/sysv/linux/powerpc/ioctl.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1998, 2002, 2003 Free Software Foundation, Inc.
+/* Copyright (C) 1998, 2002, 2003, 2004 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -38,7 +38,7 @@ __ioctl (int fd, unsigned long int request, ...)
   switch (request)
     {
     case TCGETS:
-      result = tcgetattr (fd, (struct termios *) arg);
+      result = __tcgetattr (fd, (struct termios *) arg);
       break;
 
     case TCSETS:
