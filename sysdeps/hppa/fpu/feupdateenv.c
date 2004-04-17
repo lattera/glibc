87ffc6ec7972f7abe7174199addf2911c876d24a
Ulrich Drepper <drepper@redhat.com>
2004-04-17 22:51:43 +0000

    (feupdateenv): Use only sw[0] and call feraiseexcept.

--- sysdeps/hppa/fpu/feupdateenv.c
+++ sysdeps/hppa/fpu/feupdateenv.c
@@ -27,11 +27,10 @@ feupdateenv (const fenv_t *envp)
 
   /* Get the current exception status. */
   __asm__ ("fstd %%fr0,0(%1)" : "=m" (*sw) : "r" (sw));
-  sw[0] &= FE_ALL_EXCEPT;
-  envp->__status_word = envp->__status_word | sw[0];
-  
   /* Install new environment.  */
   fesetenv (envp);
+  /* Raise the saved exceptions */
+  feraiseexcept(sw[0] & FE_ALL_EXCEPT);
 
   /* Success.  */
   return 0;
