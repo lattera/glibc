420a2f8b8dfe796e5f38da1e233ee99be6ec028f
Roland McGrath <roland@gnu.org>
2004-04-29 20:09:32 +0000

    [BZ #140]
    2004-04-29  Philip Blundell  <pb@nexus.co.uk>
    	* sysdeps/arm/dl-machine.h (RTLD_START): Avoid unnecessary GOT
    	entries.
    
    	[BZ #140]
    	* sysdeps/unix/sysv/linux/sys/sysctl.h: Remove linux/compiler.h
    	include.
    	(_LINUX_KERNEL_H, _LINUX_TYPES_H, _LINUX_LIST_H): Only define if not
    	yet defined, #undef back after including linux/sysctl.h if defined
    	here.
    	(__LINUX_COMPILER_H, __user): Define if not yet defined, #undef
    	back after including linux/sysctl.h if defined here.

--- sysdeps/unix/sysv/linux/sys/sysctl.h
+++ sysdeps/unix/sysv/linux/sys/sysctl.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1996, 1999, 2002, 2003 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 1999, 2002, 2003, 2004 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -23,13 +23,43 @@
 #define __need_size_t
 #include <stddef.h>
 /* Prevent more kernel headers than necessary to be included.  */
-#define _LINUX_KERNEL_H	1
-#define _LINUX_TYPES_H	1
-#define _LINUX_LIST_H	1
-/* We do need this one for the declarations in <linux/sysctl.h>,
-   since we've elided the inclusion of <linux/kernel.h> that gets them.  */
-#include <linux/compiler.h>
+#ifndef _LINUX_KERNEL_H
+# define _LINUX_KERNEL_H	1
+# define __undef_LINUX_KERNEL_H
+#endif
+#ifndef _LINUX_TYPES_H
+# define _LINUX_TYPES_H		1
+# define __undef_LINUX_TYPES_H
+#endif
+#ifndef _LINUX_LIST_H
+# define _LINUX_LIST_H		1
+# define __undef_LINUX_LIST_H
+#endif
+#ifndef __LINUX_COMPILER_H
+# define __LINUX_COMPILER_H	1
+# define __user
+# define __undef__LINUX_COMPILER_H
+#endif
+
 #include <linux/sysctl.h>
+
+#ifdef __undef_LINUX_KERNEL_H
+# undef _LINUX_KERNEL_H
+# undef __undef_LINUX_KERNEL_H
+#endif
+#ifdef __undef_LINUX_TYPES_H
+# undef _LINUX_TYPES_H
+# undef __undef_LINUX_TYPES_H
+#endif
+#ifdef __undef_LINUX_LIST_H
+# undef _LINUX_LIST_H
+# undef __undef_LINUX_LIST_H
+#endif
+#ifdef __undef__LINUX_COMPILER_H
+# undef __LINUX_COMPILER_H
+# undef __user
+# undef __undef__LINUX_COMPILER_H
+#endif
 
 __BEGIN_DECLS
 
