8f73811bcf9c1ef8044f28f384245b477b69cd31
Roland McGrath <roland@gnu.org>
2004-07-20 22:14:59 +0000

    * configure.in (add_ons): Substitute this.  Move $add_ons handling
    	after AC_CANONICAL_HOST and default setting of $machine et al.
    	Don't set $subdirs from $add_ons.
    	Instead, source add-on/configure fragments early on.
    	(base_machine): If it's already set, don't set it based on $machine.
    	(libc_config_ok): New variable, set to no.  If an add-on fragment sets
    	it to yes, skip the  tuple sanity check as if --enable-hacker-mode.
    	(sysnames): Try appending add-on names after machine as well.
    	* configure: Regenerated.
    	* config.make.in (add-ons): Set from @add_ons@ instead of @subdirs@.
    	* Makeconfig ($(common-objpfx)config.status): Also depend on configure
    	files in $(add_ons) dirs.
    
    	* sysdeps/unix/sysv/linux/bits/in.h (struct ip_mreq): Remove
    	definition, now in netinet/in.h proper.
    
    	long.  Rewrite example code so that it takes into account l64a output

--- sysdeps/unix/sysv/linux/bits/in.h
+++ sysdeps/unix/sysv/linux/bits/in.h
@@ -85,14 +85,7 @@ struct ip_opts
     char ip_opts[40];		/* Actually variable in size.  */
   };
 
-/* Structure used for IP_ADD_MEMBERSHIP and IP_DROP_MEMBERSHIP. */
-struct ip_mreq
-  {
-    struct in_addr imr_multiaddr;	/* IP multicast address of group */
-    struct in_addr imr_interface;	/* local IP address of interface */
-  };
-
-/* As above but including interface specification by index.  */
+/* Like `struct ip_mreq' but including interface specification by index.  */
 struct ip_mreqn
   {
     struct in_addr imr_multiaddr;	/* IP multicast address of group */
