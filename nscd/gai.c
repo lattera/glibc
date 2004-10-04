91a3b2ad2e819a7fa14b8f9773856e4303a8c99a
Ulrich Drepper <drepper@redhat.com>
2004-10-04 09:36:51 +0000

    Update.
    	* nscd/Makefile (nscd-modules): Add gai.
    	* nscd/gai.c: New file.

---
+++ nscd/gai.c
@@ -0,0 +1,21 @@
+/* This file uses the getaddrinfo code but it compiles it without NSCD
+   support.  We just need a few symbol renames.  */
+#define __getservbyname_r getservbyname_r
+#define __inet_aton inet_aton
+#define __getsockname getsockname
+#define __socket socket
+#define __recvmsg recvmsg
+#define __bind bind
+#define __sendto sendto
+#define __strchrnul strchrnul
+
+#include <getaddrinfo.c>
+
+/* Support code.  */
+#include <check_pf.c>
+#ifdef HAVE_LIBIDN
+# include <libidn/idn-stub.c>
+#endif
+
+/* Some variables normally defined in libc.  */
+service_user *__nss_hosts_database;
