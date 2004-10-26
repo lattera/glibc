af5b614c7e4668f1804320f18743aa1a7db78d58
Roland McGrath <roland@gnu.org>
2004-10-26 01:55:51 +0000

    * nss/nss_files/files-parse.c (nss_files_parse_hidden_def): Define to
    	libnss_files_hidden_def, not hidden_def.  This file is also compiled
    	into libnss_hesiod by #include.

--- nss/nss_files/files-parse.c
+++ nss/nss_files/files-parse.c
@@ -73,7 +73,7 @@ struct parser_data
 /* Export the line parser function so it can be used in nss_db.  */
 # define parser_stclass /* Global */
 # define parse_line CONCAT(_nss_files_parse_,ENTNAME)
-# define nss_files_parse_hidden_def(name) hidden_def (name)
+# define nss_files_parse_hidden_def(name) libnss_files_hidden_def (name)
 #endif
 
 
