893e609847a2f372970e349e0cede2e8529bea71
Ulrich Drepper <drepper@redhat.com>
2004-11-19 21:35:00 +0000

    Update.
    2004-11-19  Ulrich Drepper  <drepper@redhat.com>
    
    	* malloc/malloc.c (_int_free): Add a few more cheap tests for
    	corruption.
    
    	* debug/fprintf_chk.c: Adjust all users.

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -4233,6 +4233,14 @@ _int_free(mstate av, Void_t* mem)
 #endif
       ) {
 
+    if (__builtin_expect (chunk_at_offset (p, size)->size <= 2 * SIZE_SZ, 0)
+	|| __builtin_expect (chunksize (chunk_at_offset (p, size))
+			     >= av->system_mem, 0))
+      {
+	errstr = "invalid next size (fast)";
+	goto errout;
+      }
+
     set_fastchunks(av);
     fb = &(av->fastbins[fastbin_index(size)]);
     /* Another simple check: make sure the top of the bin is not the
@@ -4276,7 +4284,12 @@ _int_free(mstate av, Void_t* mem)
       }
 
     nextsize = chunksize(nextchunk);
-    assert(nextsize > 0);
+    if (__builtin_expect (nextchunk->size <= 2 * SIZE_SZ, 0)
+	|| __builtin_expect (nextsize >= av->system_mem, 0))
+      {
+	errstr = "invalid next size (normal)";
+	goto errout;
+      }
 
     /* consolidate backward */
     if (!prev_inuse(p)) {
