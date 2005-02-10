1d3be03848ec2e6852e3afe0363154aeb383fa1c
Roland McGrath <roland@gnu.org>
2005-02-10 10:06:17 +0000

    * sysdeps/powerpc/powerpc32/bits/link.h: Removed.
    	* sysdeps/powerpc/powerpc64/bits/link.h: Moved to...
    	* sysdeps/powerpc/bits/link.h: ... here.  New file.

--- sysdeps/powerpc/bits/link.h
+++ sysdeps/powerpc/bits/link.h
@@ -1,4 +1,5 @@
-/* Copyright (C) 2004, 2005 Free Software Foundation, Inc.
+/* Machine-specific declarations for dynamic linker interface.  PowerPC version
+   Copyright (C) 2004, 2005 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -20,6 +21,8 @@
 # error "Never include <bits/link.h> directly; use <link.h> instead."
 #endif
 
+
+#if __ELF_NATIVE_CLASS == 32
 
 /* Registers for entry into PLT on PPC32.  */
 typedef struct La_ppc32_regs
@@ -60,3 +63,48 @@ extern unsigned int la_ppc32_gnu_pltexit (Elf32_Sym *__sym,
 					  const char *symname);
 
 __END_DECLS
+
+#else
+
+/* Registers for entry into PLT on PPC64.  */
+typedef struct La_ppc64_regs
+{
+  uint64_t lr_reg[8];
+  double lr_fp[13];
+  uint64_t __padding;
+  uint32_t lr_vreg[12][4];
+  uint64_t lr_r1;
+  uint64_t lr_lr;
+} La_ppc64_regs;
+
+/* Return values for calls from PLT on PPC64.  */
+typedef struct La_ppc64_retval
+{
+  uint64_t lrv_r3;
+  uint64_t lrv_r4;
+  double lrv_fp[8];
+  uint32_t lrv_v2[4];
+} La_ppc64_retval;
+
+
+__BEGIN_DECLS
+
+extern Elf64_Addr la_ppc64_gnu_pltenter (Elf64_Sym *__sym,
+					 unsigned int __ndx,
+					 uintptr_t *__refcook,
+					 uintptr_t *__defcook,
+					 La_ppc64_regs *__regs,
+					 unsigned int *__flags,
+					 const char *__symname,
+					 long int *__framesizep);
+extern unsigned int la_ppc64_gnu_pltexit (Elf64_Sym *__sym,
+					  unsigned int __ndx,
+					  uintptr_t *__refcook,
+					  uintptr_t *__defcook,
+					  const La_ppc64_regs *__inregs,
+					  La_ppc64_retval *__outregs,
+					  const char *symname);
+
+__END_DECLS
+
+#endif
