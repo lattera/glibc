04c8bd46a5ad27f9699256245468f8b1e896c45f
Roland McGrath <roland@gnu.org>
2005-03-02 20:11:55 +0000

    2005-02-21  Alan Modra <amodra@bigpond.net.au>
    	* sysdeps/unix/sysv/linux/powerpc/sys/procfs.h (elf_vrreg_t): Don't
    	use __uint128_t.

--- sysdeps/unix/sysv/linux/powerpc/sys/procfs.h
+++ sysdeps/unix/sysv/linux/powerpc/sys/procfs.h
@@ -46,15 +46,10 @@ typedef elf_greg_t elf_gregset_t[ELF_NGREG];
 typedef double elf_fpreg_t;
 typedef elf_fpreg_t elf_fpregset_t[ELF_NFPREG];
 
-/* gcc 3.1 and newer support __uint128_t.  */
-#if !__GNUC_PREREQ(3,1)
-typedef struct {
-  unsigned long u[4];
-} __attribute((aligned(16))) __uint128_t;
-#endif
-
 /* Altivec registers */
-typedef __uint128_t elf_vrreg_t;
+typedef struct {
+  unsigned int u[4];
+} __attribute__ ((aligned (16))) elf_vrreg_t;
 typedef elf_vrreg_t elf_vrregset_t[ELF_NVRREG];
 #endif
 
