ad529081156700dcc9198f3688c4ab63ed09c110
Ulrich Drepper <drepper@redhat.com>
2005-04-27 16:56:50 +0000

    * sysdeps/pthread/getcpuclockid.c: Include spinlock.h.
    	Fix a typo in kernel-posix-cpy-timers.h include name, use <> instead
    	of "" includes.
    	(pthread_getcpuclockid): Fix 2 typos.

--- linuxthreads/sysdeps/pthread/getcpuclockid.c
+++ linuxthreads/sysdeps/pthread/getcpuclockid.c
@@ -1,5 +1,5 @@
 /* pthread_getcpuclockid -- Get POSIX clockid_t for a pthread_t.  Linux version
-   Copyright (C) 2000, 2001, 2004 Free Software Foundation, Inc.
+   Copyright (C) 2000, 2001, 2004, 2005 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -22,8 +22,9 @@
 #include <sys/time.h>
 #include <time.h>
 #include <internals.h>
-#include "kernel-features.h"
-#include "posix-cpu-timers.h"
+#include <spinlock.h>
+#include <kernel-features.h>
+#include <kernel-posix-cpu-timers.h>
 
 
 #if !(__ASSUME_POSIX_CPU_TIMERS > 0)
@@ -58,12 +59,12 @@ pthread_getcpuclockid (pthread_t thread_id, clockid_t *clock_id)
 # if !(__ASSUME_POSIX_CPU_TIMERS > 0)
 #  if !(__ASSUME_POSIX_TIMERS > 0)
   if (__libc_missing_posix_timers && !__libc_missing_posix_cpu_timers)
-    __libc_missing_cpu_posix_timers = 1;
+    __libc_missing_posix_cpu_timers = 1;
 #  endif
   if (!__libc_missing_posix_cpu_timers)
     {
       INTERNAL_SYSCALL_DECL (err);
-      int r = INTERNAL_SYSCALL (clock_getres, err, 2, tidclock, NULL);
+      int r = INTERNAL_SYSCALL (clock_getres, err, 2, pidclock, NULL);
       if (!INTERNAL_SYSCALL_ERROR_P (r, err))
 # endif
 	{
