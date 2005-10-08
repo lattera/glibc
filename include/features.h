faa26ad4cf1603c3b0c8ef09804aa1742fd4776a
Roland McGrath <roland@gnu.org>
2005-10-08 06:27:44 +0000

    [BZ #1438]
    2005-10-07  Roland McGrath  <roland@redhat.com>
    	[BZ #1438]
    	* include/features.h: Make tests on _FORTIFY_SOURCE and __OPTIMIZE__
    	friendly to -Wundef.
    	(__USE_FORTIFY_LEVEL): Always define it, to 0 if nothing else.

--- include/features.h
+++ include/features.h
@@ -262,12 +262,15 @@
 # define __USE_REENTRANT	1
 #endif
 
-#if _FORTIFY_SOURCE > 0 && __GNUC_PREREQ (4, 1) && __OPTIMIZE__ > 0
-# if _FORTIFY_SOURCE == 1
-#  define __USE_FORTIFY_LEVEL 1
-# elif _FORTIFY_SOURCE > 1
+#if defined _FORTIFY_SOURCE && _FORTIFY_SOURCE > 0 \
+    && __GNUC_PREREQ (4, 1) && defined __OPTIMIZE__ && __OPTIMIZE__ > 0
+# if _FORTIFY_SOURCE > 1
 #  define __USE_FORTIFY_LEVEL 2
+# else
+#  define __USE_FORTIFY_LEVEL 1
 # endif
+#else
+# define __USE_FORTIFY_LEVEL 0
 #endif
 
 /* We do support the IEC 559 math functionality, real and complex.  */
