71b8b018ddb05c9f4a31d728c75019772bf79344
Ulrich Drepper <drepper@redhat.com>
2005-10-30 19:06:52 +0000

    * misc/hsearch_r.c (hsearch_r): Remove duplicate test.
    	Patch by Chris Caputo <ccaputo@alt.net>.
    
    2005-10-28  Kaz Kojima  <kkojima@rr.iij4u.or.jp>
    
    	* sysdeps/unix/sh/sysdep.S: Remove unnecessary weak_alias.
    	* sysdeps/unix/sysv/linux/sh/socket.S: Honor NO_WEAK_ALIAS.

--- misc/hsearch_r.c
+++ misc/hsearch_r.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993, 1995, 1996, 1997, 2002 Free Software Foundation, Inc.
+/* Copyright (C) 1993,1995,1996,1997,2002,2005 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@gnu.ai.mit.edu>, 1993.
 
@@ -206,7 +206,7 @@ hsearch_r (item, action, retval, htab)
     {
       /* If table is full and another entry should be entered return
 	 with error.  */
-      if (action == ENTER && htab->filled == htab->size)
+      if (htab->filled == htab->size)
 	{
 	  __set_errno (ENOMEM);
 	  *retval = NULL;
