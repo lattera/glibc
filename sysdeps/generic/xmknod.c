975407d507f2e8ad6cbf366e22e0f41d598ddcee
Ulrich Drepper <drepper@redhat.com>
2005-12-02 21:41:00 +0000

    Add one more parameter check.

--- sysdeps/generic/xmknod.c
+++ sysdeps/generic/xmknod.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991,1993,1995,1996,1997,2002 Free Software Foundation, Inc.
+/* Copyright (C) 1991,1993,1995-1997,2002,2005 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -27,6 +27,12 @@ int
 __xmknod (int vers, const char *path, mode_t mode, dev_t *dev)
 {
   if (vers != _MKNOD_VER)
+    {
+      __set_errno (EINVAL);
+      return -1;
+    }
+
+  if (path == NULL)
     {
       __set_errno (EINVAL);
       return -1;
