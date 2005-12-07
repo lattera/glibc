43bc8ac6eab0f952faa3f3ca7bd0adfcdba13d9a
Ulrich Drepper <drepper@redhat.com>
2005-12-07 05:49:17 +0000

    * catgets/gencat.c: Use GPL, not LGPL.
    	* elf/cache.c: Likewise.
    	* elf/chroot_canon.c: Likewise.
    	* elf/ldconfig.c: Likewise.
    	* elf/readlib.c: Likewise.
    	* iconv/dummy-repertoire.c: Likewise.
    	* iconv/iconv_charmap.c: Likewise.
    	* iconv/iconv_prog.c: Likewise.
    	* iconv/iconvconfig.c: Likewise.
    	* locale/programs/3level.h: Likewise.
    	* locale/programs/charmap-dir.c: Likewise.
    	* locale/programs/charmap-dir.h: Likewise.
    	* locale/programs/charmap-kw.gperf: Likewise.
    	* locale/programs/charmap.c: Likewise.
    	* locale/programs/charmap.h: Likewise.
    	* locale/programs/config.h: Likewise.
    	* locale/programs/ld-address.c: Likewise.
    	* locale/programs/ld-collate.c: Likewise.
    	* locale/programs/ld-ctype.c: Likewise.
    	* locale/programs/ld-identification.c: Likewise.
    	* locale/programs/ld-measurement.c: Likewise.
    	* locale/programs/ld-messages.c: Likewise.
    	* locale/programs/ld-monetary.c: Likewise.
    	* locale/programs/ld-name.c: Likewise.
    	* locale/programs/ld-numeric.c: Likewise.
    	* locale/programs/ld-paper.c: Likewise.
    	* locale/programs/ld-telephone.c: Likewise.
    	* locale/programs/ld-time.c: Likewise.
    	* locale/programs/linereader.c: Likewise.
    	* locale/programs/linereader.h: Likewise.
    	* locale/programs/locale-spec.c: Likewise.
    	* locale/programs/locale.c: Likewise.
    	* locale/programs/localedef.c: Likewise.
    	* locale/programs/localedef.h: Likewise.
    	* locale/programs/locarchive.c: Likewise.
    	* locale/programs/locfile-kw.gperf: Likewise.
    	* locale/programs/locfile-token.h: Likewise.
    	* locale/programs/locfile.c: Likewise.
    	* locale/programs/locfile.h: Likewise.
    	* locale/programs/repertoire.c: Likewise.
    	* locale/programs/repertoire.h: Likewise.
    	* locale/programs/simple-hash.c: Likewise.
    	* locale/programs/simple-hash.h: Likewise.
    	* locale/programs/xmalloc.c: Likewise.
    	* locale/programs/xstrdup.c: Likewise.
    	* malloc/memusagestat.c: Likewise.
    	* nscd/aicache.c: Likewise.
    	* nscd/cache.c: Likewise.
    	* nscd/connections.c: Likewise.
    	* nscd/dbg_log.c: Likewise.
    	* nscd/gai.c: Likewise.
    	* nscd/getgrgid_r.c: Likewise.
    	* nscd/getgrnam_r.c: Likewise.
    	* nscd/gethstbyad_r.c: Likewise.
    	* nscd/gethstbynm2_r.c: Likewise.
    	* nscd/getpwnam_r.c: Likewise.
    	* nscd/getpwuid_r.c: Likewise.
    	* nscd/grpcache.c: Likewise.
    	* nscd/hstcache.c: Likewise.
    	* nscd/initgrcache.c: Likewise.
    	* nscd/mem.c: Likewise.
    	* nscd/nscd.c: Likewise.
    	* nscd/nscd_conf.c: Likewise.
    	* nscd/pwdcache.c: Likewise.
    	* posix/getconf.c: Likewise.
    	* sysdeps/generic/nscd_setup_thread.c: Likewise.
    	* sysdeps/unix/sysv/linux/nscd_setup_thread.c: Likewise.

--- nscd/gai.c
+++ nscd/gai.c
@@ -1,3 +1,20 @@
+/* Copyright (C) 2004, 2005 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Contributed by Ulrich Drepper <drepper@cygnus.com>, 2004.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2 as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
 /* This file uses the getaddrinfo code but it compiles it without NSCD
    support.  We just need a few symbol renames.  */
 #define __getservbyname_r getservbyname_r
