c72565e5f1124c2dc72573e83406fe999e56091f
Roland McGrath <roland@gnu.org>
2005-12-22 03:12:10 +0000

    2005-12-21  Roland McGrath  <roland@redhat.com>
    	* sysdeps/generic/bits: Subdirectory and all files moved to ...
    	* bits: ... here, new subdirectory.
    	* Makeconfig (+includes): Reordered includes to put build and sysdeps
    	dirs first after $(..)include, $(sysincludes) last.
    	* sysdeps/generic/bits: Subdirectory and all files moved to ...
    	* bits: ... here, new subdirectory.
    	* Makeconfig (+includes): Reordered includes to put build and sysdeps
    	dirs first after $(..)include, $(sysincludes) last.

--- sysdeps/generic/bits/ioctls.h
+++
@@ -1,5 +0,0 @@
-#ifndef _SYS_IOCTL_H
-# error "Never use <bits/ioctls.h> directly; include <sys/ioctl.h> instead."
-#endif
-
-/* This space intentionally left blank.  */
--- sysdeps/generic/bits/local_lim.h
+++
@@ -1,3 +0,0 @@
-/* This file should define the implementation-specific limits described
-   in posix[12]_lim.h.  If there are no useful values to give a limit,
-   don't define it.  */
