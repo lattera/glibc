7031b26a6b950ac2aae555dc1cf2ee4befc3caa5
Roland McGrath <roland@gnu.org>
2005-12-28 05:47:03 +0000

    2005-12-27  Roland McGrath  <roland@redhat.com>
    	* sysdeps/unix/sysv/aix/powerpc/memset.c: Don't use sysdeps/generic.

--- sysdeps/unix/sysv/aix/powerpc/memset.c
+++ sysdeps/unix/sysv/aix/powerpc/memset.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001 Free Software Foundation, Inc.
+/* Copyright (C) 2001, 2005 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -18,4 +18,4 @@
 
 
 /* Until the cache line issues are resolved use the generic implementation.  */
-#include <sysdeps/generic/memset.c>
+#include <string/memset.c>
