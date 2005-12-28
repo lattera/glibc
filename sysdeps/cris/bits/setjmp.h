4a35566071ced0925cac62b694cb106fe76d7d87
Roland McGrath <roland@gnu.org>
2005-12-28 05:44:53 +0000

    2005-12-27  Roland McGrath  <roland@redhat.com>
    	* sysdeps/cris/bits/setjmp.h (_JMPBUF_UNWINDS): Take third argument
    	DEMANGLE, and pass SP value through it.

--- sysdeps/cris/bits/setjmp.h
+++ sysdeps/cris/bits/setjmp.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1997, 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1997, 1998, 2005 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -50,5 +50,5 @@ typedef unsigned long int __jmp_buf[18];
 
 /* Test if longjmp to JMPBUF would unwind the frame
    containing a local variable at ADDRESS.  */
-#define _JMPBUF_UNWINDS(jmpbuf, address) \
-  ((unsigned long int) (address) < (jmpbuf)[JB_SP])
+#define _JMPBUF_UNWINDS(jmpbuf, address, demangle) \
+  ((unsigned long int) (address) < demangle ((jmpbuf)[JB_SP]))
