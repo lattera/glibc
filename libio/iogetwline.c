b5352f2d51056bf47eca9ba8378656786eb06baa
Ulrich Drepper <drepper@redhat.com>
2005-12-30 18:09:21 +0000

    [BZ #2080]
    	* libio/iogetwline.c (_IO_getwline_info): Move return to correct
    	location.

--- libio/iogetwline.c
+++ libio/iogetwline.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993,1997,1998,1999,2000,2002 Free Software Foundation, Inc.
+/* Copyright (C) 1993,1997-2000,2002,2005 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -87,9 +87,9 @@ _IO_getwline_info (fp, buf, n, delim, extract_delim, eof)
 		*ptr++ = wc;
 	      else if (extract_delim < 0)
 		INTUSE(_IO_sputbackc) (fp, wc);
-	      return ptr - buf;
 	      if (extract_delim > 0)
 		++len;
+	      return ptr - buf;
 	    }
 	  *ptr++ = wc;
 	  n--;
