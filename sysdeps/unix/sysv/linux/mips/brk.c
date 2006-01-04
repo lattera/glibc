52039e96e0fb04a5e98aa577faa418b20939f3a4
Andreas Jaeger <aj@suse.de>
2006-01-04 06:46:45 +0000

    (__brk): Use __SYSCALL_CLOBBERS.

--- sysdeps/unix/sysv/linux/mips/brk.c
+++ sysdeps/unix/sysv/linux/mips/brk.c
@@ -1,5 +1,5 @@
 /* brk system call for Linux/MIPS.
-   Copyright (C) 2000, 2005 Free Software Foundation, Inc.
+   Copyright (C) 2000, 2005, 2006 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -41,7 +41,7 @@ __brk (void *addr)
 	 "syscall"		/* Perform the system call.  */
 	 : "=r" (res)
 	 : "I" (SYS_ify (brk)), "r" (addr)
-	 : "$4", "$7");
++	 : "$4", "$7", __SYSCALL_CLOBBERS);
     newbrk = (void *) res;
   }
   __curbrk = newbrk;
