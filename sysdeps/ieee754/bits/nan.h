943db9eb6417433fd2dd82521933b21f4bf211b8
Ulrich Drepper <drepper@redhat.com>
2006-01-04 06:26:39 +0000

    * configure.in (HAVE_LIBAUDIT): Check
    	audit_log_user_avc_message instead of audit_log_avc.
    
    2006-01-04  Joseph S. Myers  <joseph@codesourcery.com>
    
    	* sysdeps/ieee754/bits/nan.h: Fix typo.

--- sysdeps/ieee754/bits/nan.h
+++ sysdeps/ieee754/bits/nan.h
@@ -1,5 +1,5 @@
 /* `NAN' constant for IEEE 754 machines.
-   Copyright (C) 1992, 1996, 1997, 1999, 2004 Free Software Foundation, Inc.
+   Copyright (C) 1992,1996,1997,1999,2004,2006 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -26,13 +26,13 @@
 
 #if __GNUC_PREREQ(3,3)
 
-# define NAN	(__builtin_nanf(""))
+# define NAN	(__builtin_nanf (""))
 
-#elif defined__GNUC__
+#elif defined __GNUC__
 
 # define NAN \
-  (__extension__                                                            \
-   ((union { unsigned __l __attribute__((__mode__(__SI__))); float __d; })  \
+  (__extension__							      \
+   ((union { unsigned __l __attribute__ ((__mode__ (__SI__))); float __d; })  \
     { __l: 0x7fc00000UL }).__d)
 
 #else
