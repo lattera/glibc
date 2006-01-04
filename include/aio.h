224aa5ff04c2207c87411875b6e1dc13931e6a4b
Ulrich Drepper <drepper@redhat.com>
2006-01-04 07:35:13 +0000

    typo.

--- include/aio.h
+++ include/aio.h
@@ -4,7 +4,7 @@
 /* Now define the internal interfaces.  */
 extern void __aio_init (__const struct aioinit *__init);
 
-/* Flag to signal we need to be copmatible with glibc < 2.4 in
+/* Flag to signal we need to be compatible with glibc < 2.4 in
    lio_listio and we do not issue events for each individual list
    element.  */
 #define LIO_NO_INDIVIDUAL_EVENT	128
