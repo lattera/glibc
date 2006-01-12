bb10459f2d534f67ebe771bed85e4ebfcd9eacd1
Ulrich Drepper <drepper@redhat.com>
2006-01-12 02:03:42 +0000

    * libio/wmemstream.c: New file.
    	* libio/Makefile (routines): Add wmemstream.
    	(tests): Add tst-memstream1, tst-memstream2, tst-wmemstream2, and
    	tst-wmemstream2.
    	* libio/tst-memstream1.c: New file.
    	* libio/tst-memstream2.c: New file.
    	* libio/tst-wmemstream1.c: New file.
    	* libio/tst-wmemstream2.c: New file.
    
    	* libio/memstream.c (_IO_mem_sync): Remove useless call to
    	_IO_default_sync.

---
+++ libio/tst-wmemstream1.c
@@ -0,0 +1,5 @@
+#define CHAR_T wchar_t
+#define W(o) L##o
+#define OPEN_MEMSTREAM open_wmemstream
+
+#include "tst-memstream1.c"
--- libio/wmemstream.c
+++ libio/wmemstream.c
@@ -22,59 +22,59 @@
 #include <stdlib.h>
 
 
-struct _IO_FILE_memstream
+struct _IO_FILE_wmemstream
 {
   _IO_strfile _sf;
-  char **bufloc;
+  wchar_t **bufloc;
   _IO_size_t *sizeloc;
 };
 
 
-static int _IO_mem_sync (_IO_FILE* fp) __THROW;
-static void _IO_mem_finish (_IO_FILE* fp, int) __THROW;
+static int _IO_wmem_sync (_IO_FILE* fp) __THROW;
+static void _IO_wmem_finish (_IO_FILE* fp, int) __THROW;
 
 
-static const struct _IO_jump_t _IO_mem_jumps =
+static const struct _IO_jump_t _IO_wmem_jumps =
 {
   JUMP_INIT_DUMMY,
-  JUMP_INIT (finish, _IO_mem_finish),
-  JUMP_INIT (overflow, INTUSE(_IO_str_overflow)),
-  JUMP_INIT (underflow, INTUSE(_IO_str_underflow)),
-  JUMP_INIT (uflow, INTUSE(_IO_default_uflow)),
-  JUMP_INIT (pbackfail, INTUSE(_IO_str_pbackfail)),
-  JUMP_INIT (xsputn, INTUSE(_IO_default_xsputn)),
-  JUMP_INIT (xsgetn, INTUSE(_IO_default_xsgetn)),
-  JUMP_INIT (seekoff, INTUSE(_IO_str_seekoff)),
+  JUMP_INIT (finish, _IO_wmem_finish),
+  JUMP_INIT (overflow, (_IO_overflow_t) _IO_wstr_overflow),
+  JUMP_INIT (underflow, (_IO_underflow_t) _IO_wstr_underflow),
+  JUMP_INIT (uflow, (_IO_underflow_t) INTUSE(_IO_wdefault_uflow)),
+  JUMP_INIT (pbackfail, (_IO_pbackfail_t) _IO_wstr_pbackfail),
+  JUMP_INIT (xsputn, INTUSE(_IO_wdefault_xsputn)),
+  JUMP_INIT (xsgetn, INTUSE(_IO_wdefault_xsgetn)),
+  JUMP_INIT (seekoff, _IO_wstr_seekoff),
   JUMP_INIT (seekpos, _IO_default_seekpos),
   JUMP_INIT (setbuf, _IO_default_setbuf),
-  JUMP_INIT (sync, _IO_mem_sync),
-  JUMP_INIT (doallocate, INTUSE(_IO_default_doallocate)),
+  JUMP_INIT (sync, _IO_wmem_sync),
+  JUMP_INIT (doallocate, INTUSE(_IO_wdefault_doallocate)),
   JUMP_INIT (read, _IO_default_read),
   JUMP_INIT (write, _IO_default_write),
   JUMP_INIT (seek, _IO_default_seek),
   JUMP_INIT (close, _IO_default_close),
   JUMP_INIT (stat, _IO_default_stat),
-  JUMP_INIT(showmanyc, _IO_default_showmanyc),
-  JUMP_INIT(imbue, _IO_default_imbue)
+  JUMP_INIT (showmanyc, _IO_default_showmanyc),
+  JUMP_INIT (imbue, _IO_default_imbue)
 };
 
 /* Open a stream that writes into a malloc'd buffer that is expanded as
    necessary.  *BUFLOC and *SIZELOC are updated with the buffer's location
    and the number of characters written on fflush or fclose.  */
 _IO_FILE *
-open_memstream (bufloc, sizeloc)
-     char **bufloc;
+open_wmemstream (bufloc, sizeloc)
+     wchar_t **bufloc;
      _IO_size_t *sizeloc;
 {
   struct locked_FILE
   {
-    struct _IO_FILE_memstream fp;
+    struct _IO_FILE_wmemstream fp;
 #ifdef _IO_MTSAFE_IO
     _IO_lock_t lock;
 #endif
     struct _IO_wide_data wd;
   } *new_f;
-  char *buf;
+  wchar_t *buf;
 
   new_f = (struct locked_FILE *) malloc (sizeof (struct locked_FILE));
   if (new_f == NULL)
@@ -86,9 +86,11 @@ open_memstream (bufloc, sizeloc)
   buf = malloc (_IO_BUFSIZ);
   if (buf == NULL)
     return NULL;
-  INTUSE(_IO_init) (&new_f->fp._sf._sbf._f, 0);
-  _IO_JUMPS ((struct _IO_FILE_plus *) &new_f->fp._sf._sbf) = &_IO_mem_jumps;
-  _IO_str_init_static_internal (&new_f->fp._sf, buf, _IO_BUFSIZ, buf);
+
+  _IO_no_init (&new_f->fp._sf._sbf._f, 0, 0, &new_f->wd, &_IO_wmem_jumps);
+  _IO_fwide (&new_f->fp._sf._sbf._f, 1);
+  _IO_wstr_init_static (&new_f->fp._sf._sbf._f, buf,
+			_IO_BUFSIZ / sizeof (wchar_t), buf);
   new_f->fp._sf._sbf._f._flags &= ~_IO_USER_BUF;
   new_f->fp._sf._s._allocate_buffer = (_IO_alloc_type) malloc;
   new_f->fp._sf._s._free_buffer = (_IO_free_type) free;
@@ -98,46 +100,50 @@ open_memstream (bufloc, sizeloc)
 
   return (_IO_FILE *) &new_f->fp._sf._sbf;
 }
-libc_hidden_def (open_memstream)
 
 
 static int
-_IO_mem_sync (fp)
+_IO_wmem_sync (fp)
      _IO_FILE* fp;
 {
-  struct _IO_FILE_memstream *mp = (struct _IO_FILE_memstream *) fp;
+  struct _IO_FILE_wmemstream *mp = (struct _IO_FILE_wmemstream *) fp;
 
-  if (fp->_IO_write_ptr == fp->_IO_write_end)
+  if (fp->_wide_data->_IO_write_ptr == fp->_wide_data->_IO_write_end)
     {
-      INTUSE(_IO_str_overflow) (fp, '\0');
-      --fp->_IO_write_ptr;
+      _IO_wstr_overflow (fp, '\0');
+      --fp->_wide_data->_IO_write_ptr;
     }
   else
-    *fp->_IO_write_ptr = '\0';
+    *fp->_wide_data->_IO_write_ptr = '\0';
 
-  *mp->bufloc = fp->_IO_write_base;
-  *mp->sizeloc = fp->_IO_write_ptr - fp->_IO_write_base;
+  *mp->bufloc = fp->_wide_data->_IO_write_base;
+  *mp->sizeloc = (fp->_wide_data->_IO_write_ptr
+		  - fp->_wide_data->_IO_write_base);
 
   return 0;
 }
 
 
 static void
-_IO_mem_finish (fp, dummy)
+_IO_wmem_finish (fp, dummy)
      _IO_FILE* fp;
      int dummy;
 {
-  struct _IO_FILE_memstream *mp = (struct _IO_FILE_memstream *) fp;
+  struct _IO_FILE_wmemstream *mp = (struct _IO_FILE_wmemstream *) fp;
 
-  *mp->bufloc = (char *) realloc (fp->_IO_write_base,
-				  fp->_IO_write_ptr - fp->_IO_write_base + 1);
+  *mp->bufloc = (wchar_t *) realloc (fp->_wide_data->_IO_write_base,
+				     (fp->_wide_data->_IO_write_ptr
+				      - fp->_wide_data->_IO_write_base + 1)
+				     * sizeof (wchar_t));
   if (*mp->bufloc != NULL)
     {
-      (*mp->bufloc)[fp->_IO_write_ptr - fp->_IO_write_base] = '\0';
-      *mp->sizeloc = fp->_IO_write_ptr - fp->_IO_write_base;
+      size_t len = (fp->_wide_data->_IO_write_ptr
+		    - fp->_wide_data->_IO_write_base);
+      (*mp->bufloc)[len] = '\0';
+      *mp->sizeloc = len;
 
-      fp->_IO_buf_base = NULL;
+      fp->_wide_data->_IO_buf_base = NULL;
     }
 
-  _IO_str_finish (fp, 0);
+  _IO_wstr_finish (fp, 0);
 }
