638a783c1158aa469c86b70e3cf2b6c443632087
Roland McGrath <roland@gnu.org>
2006-04-04 08:25:52 +0000

    * soft-fp/adddf3.c: Add libgcc exception.  Update FSF address.
    	* soft-fp/addsf3.c: Likewise.
    	* soft-fp/addtf3.c: Likewise.
    	* soft-fp/divdf3.c: Likewise.
    	* soft-fp/divsf3.c: Likewise.
    	* soft-fp/divtf3.c: Likewise.
    	* soft-fp/double.h: Likewise.
    	* soft-fp/eqdf2.c: Likewise.
    	* soft-fp/eqsf2.c: Likewise.
    	* soft-fp/eqtf2.c: Likewise.
    	* soft-fp/extenddftf2.c: Likewise.
    	* soft-fp/extended.h: Likewise.
    	* soft-fp/extendsfdf2.c: Likewise.
    	* soft-fp/extendsftf2.c: Likewise.
    	* soft-fp/fixdfdi.c: Likewise.
    	* soft-fp/fixdfsi.c: Likewise.
    	* soft-fp/fixsfdi.c: Likewise.
    	* soft-fp/fixsfsi.c: Likewise.
    	* soft-fp/fixtfdi.c: Likewise.
    	* soft-fp/fixtfsi.c: Likewise.
    	* soft-fp/fixunsdfdi.c: Likewise.
    	* soft-fp/fixunsdfsi.c: Likewise.
    	* soft-fp/fixunssfdi.c: Likewise.
    	* soft-fp/fixunssfsi.c: Likewise.
    	* soft-fp/fixunstfdi.c: Likewise.
    	* soft-fp/fixunstfsi.c: Likewise.
    	* soft-fp/floatdidf.c: Likewise.
    	* soft-fp/floatdisf.c: Likewise.
    	* soft-fp/floatditf.c: Likewise.
    	* soft-fp/floatsidf.c: Likewise.
    	* soft-fp/floatsisf.c: Likewise.
    	* soft-fp/floatsitf.c: Likewise.
    	* soft-fp/floatundidf.c: Likewise.
    	* soft-fp/floatundisf.c: Likewise.
    	* soft-fp/floatunditf.c: Likewise.
    	* soft-fp/floatunsidf.c: Likewise.
    	* soft-fp/floatunsisf.c: Likewise.
    	* soft-fp/floatunsitf.c: Likewise.
    	* soft-fp/gedf2.c: Likewise.
    	* soft-fp/gesf2.c: Likewise.
    	* soft-fp/getf2.c: Likewise.
    	* soft-fp/ledf2.c: Likewise.
    	* soft-fp/lesf2.c: Likewise.
    	* soft-fp/letf2.c: Likewise.
    	* soft-fp/muldf3.c: Likewise.
    	* soft-fp/mulsf3.c: Likewise.
    	* soft-fp/multf3.c: Likewise.
    	* soft-fp/negdf2.c: Likewise.
    	* soft-fp/negsf2.c: Likewise.
    	* soft-fp/negtf2.c: Likewise.
    	* soft-fp/op-1.h: Likewise.
    	* soft-fp/op-2.h: Likewise.
    	* soft-fp/op-4.h: Likewise.
    	* soft-fp/op-8.h: Likewise.
    	* soft-fp/op-common.h: Likewise.
    	* soft-fp/quad.h: Likewise.
    	* soft-fp/single.h: Likewise.
    	* soft-fp/soft-fp.h: Likewise.
    	* soft-fp/sqrtdf2.c: Likewise.
    	* soft-fp/sqrtsf2.c: Likewise.
    	* soft-fp/sqrttf2.c: Likewise.
    	* soft-fp/subdf3.c: Likewise.
    	* soft-fp/subsf3.c: Likewise.
    	* soft-fp/subtf3.c: Likewise.
    	* soft-fp/truncdfsf2.c: Likewise.
    	* soft-fp/trunctfdf2.c: Likewise.
    	* soft-fp/trunctfsf2.c: Likewise.
    	* soft-fp/unorddf2.c: Likewise.
    	* soft-fp/unordsf2.c: Likewise.
    	* soft-fp/unordtf2.c: Likewise.

--- soft-fp/op-2.h
+++ soft-fp/op-2.h
@@ -12,6 +12,15 @@
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
 
+   In addition to the permissions in the GNU Lesser General Public
+   License, the Free Software Foundation gives you unlimited
+   permission to link the compiled version of this file into
+   combinations with other programs, and to distribute those
+   combinations without any restriction coming from the use of this
+   file.  (The Lesser General Public License restrictions do apply in
+   other respects; for example, they cover modification of the file,
+   and distribution when not linked into a combine executable.)
+
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -19,8 +28,8 @@
 
    You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-   02111-1307 USA.  */
+   Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.  */
 
 #define _FP_FRAC_DECL_2(X)	_FP_W_TYPE X##_f0, X##_f1
 #define _FP_FRAC_COPY_2(D,S)	(D##_f0 = S##_f0, D##_f1 = S##_f1)
