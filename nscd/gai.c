077a30bc0e51391214095bf2a7df4272b9b8e219
Ulrich Drepper <drepper@redhat.com>
2006-05-04 16:02:39 +0000

    2006-05-04  Ulrich Drepper  <drepper@redhat.com>
    	* nscd/gai.c (__getline): Define.

--- nscd/gai.c
+++ nscd/gai.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2004, 2005 Free Software Foundation, Inc.
+/* Copyright (C) 2004, 2005, 2006 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 2004.
 
@@ -25,6 +25,7 @@
 #define __bind bind
 #define __sendto sendto
 #define __strchrnul strchrnul
+#define __getline getline
 
 #include <getaddrinfo.c>
 
