4c3f81d07ae921bb58def1f34c8758a77989b219
Ulrich Drepper <drepper@redhat.com>
2006-08-12 21:23:54 +0000

    [BZ #2841]
    2006-06-23  Paul Eggert  <eggert@cs.ucla.edu>
    	[BZ #2841]
    	* sysdeps/generic/stdint.h (UINT8_C, UINT16_C): Don't append 'U',
    	since C99 requires the result to promote to 'int' when uint_least8_t
    	and uint_least16_t promote to 'int'.

--- sysdeps/generic/stdint.h
+++ sysdeps/generic/stdint.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1997, 1998, 1999, 2000, 2001 Free Software Foundation, Inc.
+/* Copyright (C) 1997,1998,1999,2000,2001,2006 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -297,8 +297,8 @@ typedef unsigned long long int	uintmax_t;
 # endif
 
 /* Unsigned.  */
-# define UINT8_C(c)	c ## U
-# define UINT16_C(c)	c ## U
+# define UINT8_C(c)	c
+# define UINT16_C(c)	c
 # define UINT32_C(c)	c ## U
 # if __WORDSIZE == 64
 #  define UINT64_C(c)	c ## UL
