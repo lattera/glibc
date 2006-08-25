d678ebc16b240a0052e7d76263fd4cf32ae6bb7c
Ulrich Drepper <drepper@redhat.com>
2006-08-25 13:29:19 +0000

    * sysdeps/unix/sysv/linux/libc_pthread_init.c (freeres_libpthread):
    	Only define ifdef SHARED.

--- nptl/sysdeps/unix/sysv/linux/libc_pthread_init.c
+++ nptl/sysdeps/unix/sysv/linux/libc_pthread_init.c
@@ -58,9 +58,10 @@ __libc_pthread_init (ptr, reclaim, functions)
 #endif
 }
 
-
+#ifdef SHARED
 libc_freeres_fn (freeres_libptread)
 {
   if (__libc_pthread_functions.ptr_freeres != NULL)
     __libc_pthread_functions.ptr_freeres ();
 }
+#endif
