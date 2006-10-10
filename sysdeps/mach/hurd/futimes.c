e298a3189dfbdab4a0bdf40de158b6da98717fca
Roland McGrath <roland@gnu.org>
2006-10-10 09:28:40 +0000

    2006-10-10  Roland McGrath  <roland@frob.com>
    	* sysdeps/mach/hurd/utimes.c: Use a union to avoid an improper cast.
    	* sysdeps/mach/hurd/futimes.c: Likewise.
    	* sysdeps/mach/hurd/lutimes.c: Likewise.

--- sysdeps/mach/hurd/futimes.c
+++ sysdeps/mach/hurd/futimes.c
@@ -1,5 +1,5 @@
 /* futimes -- change access and modification times of open file.  Hurd version.
-   Copyright (C) 2002 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2006 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -28,20 +28,24 @@
 int
 __futimes (int fd, const struct timeval tvp[2])
 {
-  struct timeval timevals[2];
+  union tv
+  {
+    struct timeval tv;
+    time_value_t tvt;
+  };
+  const union tv *u = (const union tv *) tvp;
+  union tv nulltv[2];
   error_t err;
 
   if (tvp == NULL)
     {
       /* Setting the number of microseconds to `-1' tells the
          underlying filesystems to use the current time.  */
-      timevals[1].tv_usec = timevals[0].tv_usec = (time_t)-1;
-      tvp = timevals;
+      nulltv[0].tvt.microseconds = nulltv[1].tvt.microseconds = -1;
+      u = nulltv;
     }
 
-  err = HURD_DPORT_USE (fd, __file_utimes (port,
-					   *(time_value_t *) &tvp[0],
-					   *(time_value_t *) &tvp[1]));
+  err = HURD_DPORT_USE (fd, __file_utimes (port, u[0].tvt, u[1].tvt));
   return err ? __hurd_dfail (fd, err) : 0;
 }
 weak_alias (__futimes, futimes)
