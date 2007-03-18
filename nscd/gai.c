addc8335762203938fa24fc2c91b247cb9172674
Ulrich Drepper <drepper@redhat.com>
2007-03-18 17:55:52 +0000

    * nscd/gai.c: Include alloca.h.
    	(__libc_use_alloca): Define.

--- nscd/gai.c
+++ nscd/gai.c
@@ -15,6 +15,7 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
+#include <alloca.h>
 /* This file uses the getaddrinfo code but it compiles it without NSCD
    support.  We just need a few symbol renames.  */
 #define __inet_aton inet_aton
@@ -25,6 +26,8 @@
 #define __sendto sendto
 #define __strchrnul strchrnul
 #define __getline getline
+/* nscd uses 1MB or 2MB thread stacks.  */
+#define __libc_use_alloca(size) (size <= __MAX_ALLOCA_CUTOFF)
 
 #include <getaddrinfo.c>
 
