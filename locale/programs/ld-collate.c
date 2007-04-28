61d5a67f536dc80a8741e8abb67df2c2cfa6fd17
Ulrich Drepper <drepper@redhat.com>
2007-04-28 06:40:56 +0000

    [BZ #3213]
    * locale/programs/ld-collate.c (collate_read): Fix printing of
    	error message.
    
    	[BZ #3213]
    	* locale/C-translit.h.in: Add entry for U2044.

--- locale/programs/ld-collate.c
+++ locale/programs/ld-collate.c
@@ -3154,8 +3154,9 @@ error while adding equivalent collating symbol"));
 	      if (sp == NULL)
 		{
 		  lr_error (ldfile, _("\
-%s: unknown section name `%s'"),
-			    "LC_COLLATE", arg->val.str.startmb);
+%s: unknown section name `%.*s'"),
+			    "LC_COLLATE", (int) arg->val.str.lenmb,
+			    arg->val.str.startmb);
 		  /* We use the error section.  */
 		  collate->current_section = &collate->error_section;
 
