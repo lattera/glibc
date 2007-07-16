2e2efe652a61fc081a0f08ecfe6c46fddead1fd0
Roland McGrath <roland@gnu.org>
2007-07-16 00:56:07 +0000

    * elf/ldconfig.c: Allow GPLv2 or any later version.
    	* elf/readlib.c: Likewise.
    	* elf/chroot_canon.c: Likewise.
    	* elf/cache.c: Likewise.
    	* nscd/mem.c: Likewise.
    	* nscd/getpwuid_r.c: Likewise.
    	* nscd/grpcache.c: Likewise.
    	* nscd/aicache.c: Likewise.
    	* nscd/getsrvbynm_r.c: Likewise.
    	* nscd/nscd.c: Likewise.
    	* nscd/servicescache.c: Likewise.
    	* nscd/getsrvbypt_r.c: Likewise.
    	* nscd/initgrcache.c: Likewise.
    	* nscd/gethstbyad_r.c: Likewise.
    	* nscd/gethstbynm2_r.c: Likewise.
    	* nscd/getgrnam_r.c: Likewise.
    	* nscd/nscd_setup_thread.c: Likewise.
    	* nscd/getpwnam_r.c: Likewise.
    	* nscd/gai.c: Likewise.
    	* nscd/connections.c: Likewise.
    	* nscd/dbg_log.c: Likewise.
    	* nscd/cache.c: Likewise.
    	* nscd/hstcache.c: Likewise.
    	* nscd/nscd_conf.c: Likewise.
    	* nscd/getgrgid_r.c: Likewise.
    	* nscd/pwdcache.c: Likewise.
    	* catgets/gencat.c: Likewise.
    	* locale/programs/linereader.h: Likewise.
    	* locale/programs/locarchive.c: Likewise.
    	* locale/programs/ld-paper.c: Likewise.
    	* locale/programs/locfile-kw.h: Likewise.
    	* locale/programs/ld-address.c: Likewise.
    	* locale/programs/xmalloc.c: Likewise.
    	* locale/programs/ld-time.c: Likewise.
    	* locale/programs/localedef.c: Likewise.
    	* locale/programs/simple-hash.c: Likewise.
    	* locale/programs/xstrdup.c: Likewise.
    	* locale/programs/ld-numeric.c: Likewise.
    	* locale/programs/locfile-kw.gperf: Likewise.
    	* locale/programs/ld-collate.c: Likewise.
    	* locale/programs/charmap-kw.gperf: Likewise.
    	* locale/programs/charmap.h: Likewise.
    	* locale/programs/charmap-kw.h: Likewise.
    	* locale/programs/config.h: Likewise.
    	* locale/programs/locfile.c: Likewise.
    	* locale/programs/ld-ctype.c: Likewise.
    	* locale/programs/charmap.c: Likewise.
    	* locale/programs/ld-messages.c: Likewise.
    	* locale/programs/repertoire.h: Likewise.
    	* locale/programs/locale.c: Likewise.
    	* locale/programs/ld-name.c: Likewise.
    	* locale/programs/linereader.c: Likewise.
    	* locale/programs/locfile.h: Likewise.
    	* locale/programs/3level.h: Likewise.
    	* locale/programs/ld-monetary.c: Likewise.
    	* locale/programs/ld-measurement.c: Likewise.
    	* locale/programs/charmap-dir.c: Likewise.
    	* locale/programs/ld-identification.c: Likewise.
    	* locale/programs/localedef.h: Likewise.
    	* locale/programs/charmap-dir.h: Likewise.
    	* locale/programs/repertoire.c: Likewise.
    	* locale/programs/simple-hash.h: Likewise.
    	* locale/programs/ld-telephone.c: Likewise.
    	* locale/programs/locale-spec.c: Likewise.
    	* locale/programs/locfile-token.h: Likewise.
    	* posix/getconf.c: Likewise.
    	* iconv/dummy-repertoire.c: Likewise.
    	* iconv/iconv_charmap.c: Likewise.
    	* iconv/iconvconfig.c: Likewise.
    	* iconv/iconv_prog.c: Likewise.
    	* malloc/memusagestat.c: Likewise.
    	* sysdeps/unix/sysv/linux/nscd_setup_thread.c: Likewise.

--- locale/programs/localedef.c
+++ locale/programs/localedef.c
@@ -3,8 +3,9 @@
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1995.
 
    This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License version 2 as
-   published by the Free Software Foundation.
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation; version 2 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
