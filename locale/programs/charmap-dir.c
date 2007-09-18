76c0bea7a329ca94035de1801be77e24beecc1fd
Jakub Jelinek <jakub@redhat.com>
2007-09-18 19:31:02 +0000

    * locale/programs/charmap-dir.c (charmap_aliases): Use %ms instead of
    %as in fscanf format strings.
    	* locale/programs/charmap-dir.c (charmap_aliases): Use %ms instead of
    	%as in fscanf format strings.

--- locale/programs/charmap-dir.c
+++ locale/programs/charmap-dir.c
@@ -1,4 +1,5 @@
-/* Copyright (C) 2000, 2001, 2002, 2003, 2005 Free Software Foundation, Inc.
+/* Copyright (C) 2000, 2001, 2002, 2003, 2005, 2007
+   Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    This program is free software; you can redistribute it and/or modify
@@ -264,8 +265,8 @@ charmap_aliases (const char *directory, const char *name)
       char *alias = NULL;
       char junk[BUFSIZ];
 
-      if (fscanf (stream, " <code_set_name> %as", &alias) == 1
-          || fscanf (stream, "%% alias %as", &alias) == 1)
+      if (fscanf (stream, " <code_set_name> %ms", &alias) == 1
+          || fscanf (stream, "%% alias %ms", &alias) == 1)
         {
           aliases = (char **) xrealloc (aliases,
                                         (naliases + 2) * sizeof (char *));
