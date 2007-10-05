34c2c09296bfb369df9973cf8a4098b6ec526059
Ulrich Drepper <drepper@redhat.com>
2007-10-05 16:31:37 +0000

    * sysdeps/unix/sysv/linux/Makefile (sysdep_routines): Add signalfd.
    	* sysdeps/unix/sysv/linux/signalfd.c: New file.
    	* sysdeps/unix/sysv/linux/sys/signalfd.h: New file.
    	* sysdeps/unix/sysv/linux/Versions: Export signalfd for GLIBC_2.7.

---
+++ sysdeps/unix/sysv/linux/signalfd.c
@@ -0,0 +1,34 @@
+/* Copyright (C) 2007 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#include <errno.h>
+#include <signal.h>
+#include <sys/signalfd.h>
+#include <sysdep.h>
+
+
+int
+signalfd (int fd, const sigset_t *mask)
+{
+#ifdef __NR_signalfd
+  return INLINE_SYSCALL (signalfd, 3, fd, mask, _NSIG / 8);
+#else
+  __set_errno (ENOSYS);
+  return -1;
+#endif
+}
