79fb2e026bb76ef2d80a4c75297bae59018e9613
Ulrich Drepper <drepper@redhat.com>
2007-11-26 13:07:53 +0000

    Define REG_ENOSYS also for Unix98.
    	* posix/regex.h (REG_ENOSYS): Likewise.

--- posix/regex.h
+++ posix/regex.h
@@ -302,7 +302,7 @@ extern reg_syntax_t re_syntax_options;
    `re_error_msg' table in regex.c.  */
 typedef enum
 {
-#ifdef _XOPEN_SOURCE
+#if defined _XOPEN_SOURCE || defined __USE_UNIX98
   REG_ENOSYS = -1,	/* This will never happen for this implementation.  */
 #endif
 
