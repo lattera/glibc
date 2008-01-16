6107639e64c25983ebfa93a2f1cb4f5924c18618
Ulrich Drepper <drepper@redhat.com>
2008-01-16 19:47:17 +0000

    * sysdeps/posix/getaddrinfo.c (default_labels): Adjust for change
    	of names of in in6_addr.
    	(default_precedence): Likewise.
    	* sysdeps/unix/sysv/linux/cmsg_nxthdr.c: Include <stddef.h> for
    	NULL definition.

--- sysdeps/unix/sysv/linux/cmsg_nxthdr.c
+++ sysdeps/unix/sysv/linux/cmsg_nxthdr.c
@@ -1,5 +1,5 @@
 /* Return point to next ancillary data entry in message header.
-   Copyright (C) 1997, 1998, 2001 Free Software Foundation, Inc.
+   Copyright (C) 1997, 1998, 2001, 2008 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -17,6 +17,7 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
+#include <stddef.h>
 #include <sys/socket.h>
 
 
