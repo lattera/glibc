3501542f9475d352fd77bef5c8902cc6c14673c2
Ulrich Drepper <drepper@redhat.com>
2008-08-02 15:53:18 +0000

    * sysdeps/posix/clock_getres.c (hp_timing_getres): Remove inline
    	to prevent warning.

--- sysdeps/posix/clock_getres.c
+++ sysdeps/posix/clock_getres.c
@@ -1,5 +1,5 @@
 /* clock_getres -- Get the resolution of a POSIX clockid_t.
-   Copyright (C) 1999, 2000, 2001, 2003, 2004 Free Software Foundation, Inc.
+   Copyright (C) 1999,2000,2001,2003,2004,2008 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -28,7 +28,7 @@
 #if HP_TIMING_AVAIL
 static long int nsec;		/* Clock frequency of the processor.  */
 
-static inline int
+static int
 hp_timing_getres (struct timespec *res)
 {
   if (__builtin_expect (nsec == 0, 0))
