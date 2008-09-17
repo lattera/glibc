0a26fc4d99ed5dfbeefeae17b6d88743107dad7f
Ulrich Drepper <drepper@redhat.com>
2008-09-17 03:12:41 +0000

    * dlfcn/dlopen.c (dlopen_doit): Allow __RTLD_SPROF flag.
    	Patch by Olivier Fourdan <ofourdan@redhat.com>.

--- dlfcn/dlopen.c
+++ dlfcn/dlopen.c
@@ -60,7 +60,8 @@ dlopen_doit (void *a)
   struct dlopen_args *args = (struct dlopen_args *) a;
 
   if (args->mode & ~(RTLD_BINDING_MASK | RTLD_NOLOAD | RTLD_DEEPBIND
-		     | RTLD_GLOBAL | RTLD_LOCAL | RTLD_NODELETE))
+		     | RTLD_GLOBAL | RTLD_LOCAL | RTLD_NODELETE
+		     | __RTLD_SPROF))
     GLRO(dl_signal_error) (0, NULL, NULL, _("invalid mode parameter"));
 
   args->new = GLRO(dl_open) (args->file ?: "", args->mode | __RTLD_DLOPEN,
