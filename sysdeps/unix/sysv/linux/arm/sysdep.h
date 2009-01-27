a68f927f95aeaa729ed920516fc49a9e639f6552
Daniel Jacobowitz <dan@codesourcery.com>
2009-01-27 15:36:22 +0000

    * sysdeps/unix/sysv/linux/arm/sysdep.h: Include <tls.h>.

--- sysdeps/unix/sysv/linux/arm/sysdep.h
+++ sysdeps/unix/sysv/linux/arm/sysdep.h
@@ -28,6 +28,8 @@
 /* Defines RTLD_PRIVATE_ERRNO and USE_DL_SYSINFO.  */
 #include <dl-sysdep.h>
 
+#include <tls.h>
+
 /* For Linux we can use the system call table in the header file
 	/usr/include/asm/unistd.h
    of the kernel.  But these symbols do not follow the SYS_* syntax
