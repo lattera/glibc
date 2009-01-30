a3bd28468c9f309d3f89815476e6ae16c7342d92
Ulrich Drepper <drepper@redhat.com>
2009-01-30 20:23:23 +0000

    * sysdeps/generic/dl-osinfo.h (_dl_setup_stack_chk_guard): Use
    	memcpy instead of memcmp.
    	(_dl_setup_pointer_guard): Likewise.

--- sysdeps/generic/dl-osinfo.h
+++ sysdeps/generic/dl-osinfo.h
@@ -1,3 +1,22 @@
+/* Operating system specific code for generic dynamic loader functions.
+   Copyright (C) 2009 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
 #include <stdint.h>
 
 static inline uintptr_t __attribute__ ((always_inline))
@@ -13,7 +32,7 @@ _dl_setup_stack_chk_guard (void *dl_random)
       p[0] = 0;
     }
   else
-    memcmp (&ret, dl_random, sizeof (ret));
+    memcpy (&ret, dl_random, sizeof (ret));
   return ret;
 }
 
@@ -24,6 +43,6 @@ _dl_setup_pointer_guard (void *dl_random, uintptr_t stack_chk_guard)
   if (dl_random == NULL)
     ret = stack_chk_guard;
   else
-    memcmp (&ret, (char *) dl_random + sizeof (ret), sizeof (ret));
+    memcpy (&ret, (char *) dl_random + sizeof (ret), sizeof (ret));
   return ret;
 }
