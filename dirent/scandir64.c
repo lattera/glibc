8efb2f1ca042578c0c490a8c7677120c402e0455
Ulrich Drepper <drepper@redhat.com>
2009-03-16 22:37:22 +0000

    * dirent/scandir64.c (scandir64): Adjust declaration to type change.

--- dirent/scandir64.c
+++ dirent/scandir64.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2000 Free Software Foundation, Inc.
+/* Copyright (C) 2000, 2009 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -25,6 +25,7 @@
 int scandir64 (__const char *__restrict __dir,
 	       struct dirent64 ***__restrict __namelist,
 	       int (*__selector) (__const struct dirent64 *),
-	       int (*__cmp) (__const void *, __const void *));
+	       int (*__cmp) (__const struct dirent64 **,
+			     __const struct dirent64 **));
 
 #include <dirent/scandir.c>
