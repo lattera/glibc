5ee3019c00ed0905359dcdd96117355a1622e366
Roland McGrath <roland@gnu.org>
2009-04-02 01:20:33 +0000

    Missed copyright year update

--- elf/elf.h
+++ elf/elf.h
@@ -1,5 +1,5 @@
 /* This file defines standard ELF types, structures, and macros.
-   Copyright (C) 1995-2003,2004,2005,2006,2007,2008
+   Copyright (C) 1995-2003,2004,2005,2006,2007,2008,2009
 	Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
