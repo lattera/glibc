1a2ec17ba6dc3826855e3ad0897a87067bcdfcab
Ulrich Drepper <drepper@redhat.com>
2009-04-07 17:38:50 +0000

    * sysdeps/unix/sysv/linux/kernel-features.h: Power also has
    	preadv/pwritev in 2.6.30.

--- sysdeps/unix/sysv/linux/kernel-features.h
+++ sysdeps/unix/sysv/linux/kernel-features.h
@@ -532,7 +532,7 @@
 
 /* Support for preadv and pwritev was added in 2.6.30.  */
 #if __LINUX_KERNEL_VERSION >= 0x02061e \
-    && (defined __i386__ || defined __x86_64__)
+    && (defined __i386__ || defined __x86_64__ || defined __powerpc__)
 # define __ASSUME_PREADV	1
 # define __ASSUME_PWRITEV	1
 #endif
