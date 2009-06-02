3d5243d4160519e56cba6eceae9b1d0e6b642515
Ulrich Drepper <drepper@redhat.com>
2009-06-02 07:03:02 -0700

    Implement execvpe.
    There is some existing practice in other OSes and it's trivial to
    implement giving the existing code.  Fixes BZ #10221.

--- posix/unistd.h
+++ posix/unistd.h
@@ -551,6 +551,14 @@ extern int execvp (__const char *__file, char *__const __argv[])
 extern int execlp (__const char *__file, __const char *__arg, ...)
      __THROW __nonnull ((1));
 
+#ifdef __USE_GNU
+/* Execute FILE, searching in the `PATH' environment variable if it contains
+   no slashes, with arguments ARGV and environment from `environ'.  */
+extern int execvpe (__const char *__file, char *__const __argv[],
+		    char *__const __envp[])
+     __THROW __nonnull ((1));
+#endif
+
 
 #if defined __USE_MISC || defined __USE_XOPEN
 /* Add INC to priority of the current process.  */
