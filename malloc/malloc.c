6cbbaa50aac809ad6e0692247876c82d58e466bf
Ulrich Drepper <drepper@redhat.com>
2009-07-02 03:30:55 -0700

    Fix possible race when freeing object in fast bin list.

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -4822,6 +4822,7 @@ _int_free(mstate av, mchunkptr p)
 	    goto errout;
 	  }
 	p->fd = fd = old;
+	atomic_full_barrier ();
       }
     while ((old = catomic_compare_and_exchange_val_acq (fb, p, fd)) != fd);
 #else
