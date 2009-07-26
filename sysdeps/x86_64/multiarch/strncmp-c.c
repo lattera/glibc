7956a3d27c6552f57c8b1c3893d55e501fe30e14
H.J. Lu <hongjiu.lu@intel.com>
2009-07-26 13:32:28 -0700

    Add SSE2 support to str{,n}cmp for x86-64.

--- sysdeps/x86_64/multiarch/strncmp-c.c
+++
@@ -1,8 +0,0 @@
-#ifdef SHARED
-#define STRNCMP __strncmp_sse2
-#undef libc_hidden_builtin_def
-#define libc_hidden_builtin_def(name) \
-  __hidden_ver1 (__strncmp_sse2, __GI_strncmp, __strncmp_sse2);
-#endif
-
-#include "strncmp.c"
