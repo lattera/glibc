15efafdf07789322219cc8f938ac758f932fe208
Andreas Schwab <schwab@redhat.com>
2009-08-22 02:01:51 -0700

    Add sigstack handling to Linux ____longjmp_chk on powerpc.

--- nptl/pthread_rwlock_init.c
+++ nptl/pthread_rwlock_init.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2002, 2007 Free Software Foundation, Inc.
+/* Copyright (C) 2002, 2007, 2009 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@redhat.com>, 2002.
 
@@ -37,13 +37,7 @@ __pthread_rwlock_init (rwlock, attr)
 
   iattr = ((const struct pthread_rwlockattr *) attr) ?: &default_attr;
 
-  rwlock->__data.__lock = 0;
-  rwlock->__data.__nr_readers = 0;
-  rwlock->__data.__readers_wakeup = 0;
-  rwlock->__data.__writer_wakeup = 0;
-  rwlock->__data.__nr_readers_queued = 0;
-  rwlock->__data.__nr_writers_queued = 0;
-  rwlock->__data.__writer = 0;
+  memset (rwlock, '\0', sizeof (*rwlock));
 
   rwlock->__data.__flags
     = iattr->lockkind == PTHREAD_RWLOCK_PREFER_WRITER_NONRECURSIVE_NP;
@@ -73,9 +67,6 @@ __pthread_rwlock_init (rwlock, attr)
 			     : THREAD_GETMEM (THREAD_SELF,
 					      header.private_futex));
 #endif
-
-  rwlock->__data.__pad1 = 0;
-  rwlock->__data.__pad2 = 0;
 
   return 0;
 }
