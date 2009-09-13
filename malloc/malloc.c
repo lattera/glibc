bb9510dc345f0b24de212604b2308dc6e6cc939a
Dmitry V. Levin <ldv@altlinux.org>
2009-09-13 19:45:27 +0000

    Clarify memory allocation error diagnostics to avoid confusion

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -4899,8 +4899,7 @@ malloc_printerr(int action, const char *str, void *ptr)
       while (cp > buf)
 	*--cp = '0';
 
-      __libc_message (action & 2,
-		      "*** glibc detected *** %s: %s: 0x%s ***\n",
+      __libc_message (action & 2, "*** Error in `%s': %s: 0x%s ***\n",
 		      __libc_argv[0] ?: "<unknown>", str, cp);
     }
   else if (action & 2)
