f9a7bd536e0b1693db32e1330bbd96108ca63c42
Mike Frsyinger <vapier@gentoo.org>
2009-11-14 19:16:01 -0800

    Fix building on x86 with older kernel headers.
    Fix building on x86 when older linux headers lack __NR_fallocate define.

--- sysdeps/unix/sysv/linux/i386/fallocate64.c
+++ sysdeps/unix/sysv/linux/i386/fallocate64.c
@@ -16,6 +16,7 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
+#include <errno.h>
 #include <fcntl.h>
 #include <sysdep.h>
 
@@ -28,5 +29,10 @@ extern int __call_fallocate (int fd, int mode, __off64_t offset, __off64_t len)
 int
 fallocate64 (int fd, int mode, __off64_t offset, __off64_t len)
 {
+#ifdef __NR_fallocate
   return __call_fallocate (fd, mode, offset, len);
+#else
+  __set_errno (ENOSYS);
+  return -1;
+#endif
 }
