18598ff1051f2cbe545822036a615d9a5dbae5c0
Ulrich Drepper <drepper@redhat.com>
2010-01-10 02:08:32 -0800

    Cleanup strings.h.
    Cleanup the strings.h header for XPG7 and update the tests.

--- string/strings.h
+++ string/strings.h
@@ -1,4 +1,5 @@
-/* Copyright (C) 1991,92,96,97,99,2000,2001,2009 Free Software Foundation, Inc.
+/* Copyright (C) 1991,1992,1996,1997,1999,2000,2001,2009,2010
+   Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -35,6 +36,7 @@
 
 __BEGIN_DECLS
 
+# if !defined __USE_XOPEN2K8 || defined __USE_GNU
 /* Compare N bytes of S1 and S2 (same as memcmp).  */
 extern int bcmp (__const void *__s1, __const void *__s2, size_t __n)
      __THROW __attribute_pure__;
@@ -45,12 +47,8 @@ extern void bcopy (__const void *__src, void *__dest, size_t __n) __THROW;
 /* Set N bytes of S to 0.  */
 extern void bzero (void *__s, size_t __n) __THROW;
 
-/* Return the position of the first bit set in I, or 0 if none are set.
-   The least-significant bit is position 1, the most-significant 32.  */
-extern int ffs (int __i) __THROW __attribute__ ((const));
-
 /* Find the first occurrence of C in S (same as strchr).  */
-# ifdef __CORRECT_ISO_CPP_STRINGS_H_PROTO
+#  ifdef __CORRECT_ISO_CPP_STRINGS_H_PROTO
 extern "C++"
 {
 extern char *index (char *__s, int __c)
@@ -58,7 +56,7 @@ extern char *index (char *__s, int __c)
 extern __const char *index (__const char *__s, int __c)
      __THROW __asm ("index") __attribute_pure__ __nonnull ((1));
 
-#  if defined __OPTIMIZE__ && !defined __CORRECT_ISO_CPP_STRING_H_PROTO
+#   if defined __OPTIMIZE__ && !defined __CORRECT_ISO_CPP_STRING_H_PROTO
 __extern_always_inline char *
 index (char *__s, int __c) __THROW
 {
@@ -70,15 +68,15 @@ index (__const char *__s, int __c) __THROW
 {
   return __builtin_index (__s, __c);
 }
-#  endif
+#   endif
 }
-# else
+#  else
 extern char *index (__const char *__s, int __c)
      __THROW __attribute_pure__ __nonnull ((1));
-# endif
+#  endif
 
 /* Find the last occurrence of C in S (same as strrchr).  */
-# ifdef __CORRECT_ISO_CPP_STRINGS_H_PROTO
+#  ifdef __CORRECT_ISO_CPP_STRINGS_H_PROTO
 extern "C++"
 {
 extern char *rindex (char *__s, int __c)
@@ -86,7 +84,7 @@ extern char *rindex (char *__s, int __c)
 extern __const char *rindex (__const char *__s, int __c)
      __THROW __asm ("rindex") __attribute_pure__ __nonnull ((1));
 
-#  if defined __OPTIMIZE__ && !defined __CORRECT_ISO_CPP_STRING_H_PROTO
+#   if defined __OPTIMIZE__ && !defined __CORRECT_ISO_CPP_STRING_H_PROTO
 __extern_always_inline char *
 rindex (char *__s, int __c) __THROW
 {
@@ -98,12 +96,19 @@ rindex (__const char *__s, int __c) __THROW
 {
   return __builtin_rindex (__s, __c);
 }
-#endif
+#   endif
 }
-# else
+#  else
 extern char *rindex (__const char *__s, int __c)
      __THROW __attribute_pure__ __nonnull ((1));
+#  endif
 # endif
+
+#if !defined __USE_XOPEN2K8 || defined __USE_XOPEN2K8XSI
+/* Return the position of the first bit set in I, or 0 if none are set.
+   The least-significant bit is position 1, the most-significant 32.  */
+extern int ffs (int __i) __THROW __attribute__ ((const));
+#endif
 
 /* Compare S1 and S2, ignoring case.  */
 extern int strcasecmp (__const char *__s1, __const char *__s2)
