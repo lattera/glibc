c589e093e17a46c659a79f09d766f301ef8ef64f
Ulrich Drepper <drepper@redhat.com>
2010-01-14 16:07:58 -0800

    elax some conditions in stdlib.h.

--- stdlib/stdlib.h
+++ stdlib/stdlib.h
@@ -497,8 +497,8 @@ extern void cfree (void *__ptr) __THROW;
 # include <alloca.h>
 #endif /* Use GNU, BSD, or misc.  */
 
-#if ((defined __USE_BSD || defined __USE_XOPEN_EXTENDED)	\
-     && !defined __USE_XOPEN2K) || defined __USE_GNU
+#if (defined __USE_XOPEN_EXTENDED && !defined __USE_XOPEN2K) \
+    || defined __USE_BSD
 /* Allocate SIZE bytes on a page boundary.  The storage cannot be freed.  */
 extern void *valloc (size_t __size) __THROW __attribute_malloc__ __wur;
 #endif
@@ -797,8 +797,8 @@ __END_NAMESPACE_C99
 #endif
 
 
-#if ((defined __USE_SVID || defined __USE_XOPEN_EXTENDED)	\
-     && !defined __USE_XOPEN2K) || defined __USE_GNU
+#if (defined __USE_XOPEN_EXTENDED && !defined __USE_XOPEN2K) \
+    || defined __USE_SVID
 /* Convert floating point numbers to strings.  The returned values are
    valid only until another call to the same function.  */
 
