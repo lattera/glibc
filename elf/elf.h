f2899a62cfd037ac59dddb4dc62c7a2d91238fa7
Roland McGrath <roland@redhat.com>
2010-02-28 16:26:27 -0800

    Define NT_X86_XSTATE constant for ELF core files.

--- elf/elf.h
+++ elf/elf.h
@@ -619,6 +619,7 @@ typedef struct
 #define NT_PPC_VSX	0x102		/* PowerPC VSX registers */
 #define NT_386_TLS	0x200		/* i386 TLS slots (struct user_desc) */
 #define NT_386_IOPERM	0x201		/* x86 io permission bitmap (1=deny) */
+#define NT_X86_XSTATE	0x202		/* x86 extended state using xsave */
 
 /* Legal values for the note segment descriptor types for object files.  */
 
