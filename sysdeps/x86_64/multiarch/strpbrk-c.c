7d9335ecd7ddd16bcbcfcc0f9cc1f0b5d392f849
H.J. Lu <hongjiu.lu@intel.com>
2010-03-24 12:16:24 -0700

    Don't define __strpbrk_sse42 in static library

--- sysdeps/x86_64/multiarch/strpbrk-c.c
+++ sysdeps/x86_64/multiarch/strpbrk-c.c
@@ -1,4 +1,8 @@
-#define USE_AS_STRPBRK
-#define STRCSPN_SSE2 __strpbrk_sse2
-#define STRCSPN_SSE42 __strpbrk_sse42
-#include "strcspn-c.c"
+/* Don't define multiple versions for strpbrk in static library since we
+   need strpbrk before the initialization happened.  */
+#ifdef SHARED
+# define USE_AS_STRPBRK
+# define STRCSPN_SSE2 __strpbrk_sse2
+# define STRCSPN_SSE42 __strpbrk_sse42
+# include "strcspn-c.c"
+#endif
