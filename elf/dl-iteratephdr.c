c739ec3d81a34a87d8ae1276eab4f5880afc3476
Ulrich Drepper <drepper@redhat.com>
2010-05-03 06:34:56 -0700

    Code cleanup in __dl_iterate_phdr.

--- elf/dl-iteratephdr.c
+++ elf/dl-iteratephdr.c
@@ -1,5 +1,5 @@
 /* Get loaded objects program headers.
-   Copyright (C) 2001-2004, 2006-2008, 2009 Free Software Foundation, Inc.
+   Copyright (C) 2001-2004, 2006-2009, 2010 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Jakub Jelinek <jakub@redhat.com>, 2001.
 
@@ -68,7 +68,6 @@ __dl_iterate_phdr (int (*callback) (struct dl_phdr_info *info,
       info.dlpi_phnum = l->l_phnum;
       info.dlpi_adds = GL(dl_load_adds);
       info.dlpi_subs = GL(dl_load_adds) - nloaded;
-      info.dlpi_tls_modid = 0;
       info.dlpi_tls_data = NULL;
       info.dlpi_tls_modid = l->l_tls_modid;
       if (info.dlpi_tls_modid != 0)
