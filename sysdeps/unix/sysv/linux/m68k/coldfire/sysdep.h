cf64098fc4aef77a4b708e5d92aedced16964390
Andreas Schwab <schwab@linux-m68k.org>
2010-08-25 22:55:55 +0200

    m68k: fix SYSCALL_ERROR_LOAD_GOT for Coldfire

--- sysdeps/unix/sysv/linux/m68k/coldfire/sysdep.h
+++ sysdeps/unix/sysv/linux/m68k/coldfire/sysdep.h
@@ -24,7 +24,7 @@
 #include <sysdeps/unix/sysv/linux/m68k/sysdep.h>
 
 #define SYSCALL_ERROR_LOAD_GOT(reg)					      \
-    move.l #_GLOBAL_OFFSET_TABLE_@GOTPC, reg;				      \
+    move.l &_GLOBAL_OFFSET_TABLE_@GOTPC, reg;				      \
     lea (-6, %pc, reg), reg
 
 #endif
