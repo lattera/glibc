e53a31fe575dcfd0dceb9f316d673de34d39abad
Mike Frysinger <vapier@gentoo.org>
2010-09-01 12:42:48 -0700

    Fix typo in x86-64 epoll.h.

--- sysdeps/unix/sysv/linux/x86_64/sys/epoll.h
+++ sysdeps/unix/sysv/linux/x86_64/sys/epoll.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2002,2003,2004,2005,2006,2007,2008 Free Software Foundation, Inc.
+/* Copyright (C) 2002-2008, 2010 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -31,7 +31,7 @@ typedef __sigset_t sigset_t;
 #endif
 
 
-/* Flags to be passed to epoll_create2.  */
+/* Flags to be passed to epoll_create1.  */
 enum
   {
     EPOLL_CLOEXEC = 02000000,
