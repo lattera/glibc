323e2ce5e0f3478e3aa1e3193605fbd07723c866
Joseph Myers <joseph@codesourcery.com>
2010-10-11 14:57:33 +0000

    Add hidden feupdateenv and fetestexcept definitions for ARM.

--- sysdeps/arm/eabi/feupdateenv.c
+++ sysdeps/arm/eabi/feupdateenv.c
@@ -1,5 +1,5 @@
 /* Install given floating-point environment and raise exceptions.
-   Copyright (C) 1997, 1999, 2000, 2008 Free Software Foundation, Inc.
+   Copyright (C) 1997, 1999, 2000, 2008, 2010 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1997.
 
@@ -56,4 +56,5 @@ strong_alias (__feupdateenv, __old_feupdateenv)
 compat_symbol (libm, __old_feupdateenv, feupdateenv, GLIBC_2_1);
 #endif
 
+libm_hidden_ver (__feupdateenv, feupdateenv)
 versioned_symbol (libm, __feupdateenv, feupdateenv, GLIBC_2_2);
--- sysdeps/arm/eabi/ftestexcept.c
+++ sysdeps/arm/eabi/ftestexcept.c
@@ -1,5 +1,5 @@
 /* Test exception in current environment.
-   Copyright (C) 1997, 1998, 2005 Free Software Foundation, Inc.
+   Copyright (C) 1997, 1998, 2005, 2010 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -41,3 +41,4 @@ fetestexcept (int excepts)
   /* Unsupported, return 0.  */
   return 0;
 }
+libm_hidden_def (fetestexcept)
