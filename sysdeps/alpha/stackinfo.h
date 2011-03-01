65342356965f356a7fb9b4578856c5ad0861d423
Aurelien Jarno <aurelien@aurel32.net>
2011-03-01 10:00:28 +1000

    alpha: Define DEFAULT_STACK_PERMS

--- sysdeps/alpha/stackinfo.h
+++ sysdeps/alpha/stackinfo.h
@@ -22,7 +22,13 @@
 #ifndef _STACKINFO_H
 #define _STACKINFO_H	1
 
+#include <elf.h>
+
 /* On Alpha the stack grows down.  */
 #define _STACK_GROWS_DOWN	1
+
+/* Default to an executable stack.  PF_X can be overridden if PT_GNU_STACK is
+ * present, but it is presumed absent.  */
+#define DEFAULT_STACK_PERMS (PF_R|PF_W|PF_X)
 
 #endif	/* stackinfo.h */
