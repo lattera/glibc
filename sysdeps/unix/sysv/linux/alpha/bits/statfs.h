79abd1fbfcd082e5e9857d41d0acb4f64e9299de
Aurelien Jarno <aurelien@aurel32.net>
2011-03-01 10:02:04 +1000

    alpha: Add f_flags to struct statfs

--- sysdeps/unix/sysv/linux/alpha/bits/statfs.h
+++ sysdeps/unix/sysv/linux/alpha/bits/statfs.h
@@ -42,7 +42,8 @@ struct statfs
     __fsid_t f_fsid;
     int f_namelen;
     int f_frsize;
-    int f_spare[5];
+    int f_flags;
+    int f_spare[4];
   };
 
 #ifdef __USE_LARGEFILE64
@@ -58,7 +59,8 @@ struct statfs64
     __fsid_t f_fsid;
     int f_namelen;
     int f_frsize;
-    int f_spare[5];
+    int f_flags;
+    int f_spare[4];
   };
 #endif
 
