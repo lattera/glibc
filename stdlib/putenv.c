ea389b12b3b65c4a7fa91fa76f8c99867eb37865
Ulrich Drepper <drepper@gmail.com>
2011-05-16 10:13:54 -0400

    Handle long variable names in putenv

--- stdlib/putenv.c
+++ stdlib/putenv.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 94, 95, 96, 97, 98, 99 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 94, 95, 96, 97, 98, 99, 11 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -57,14 +57,31 @@ putenv (string)
 
   if (name_end != NULL)
     {
+      char *name;
 #ifdef _LIBC
-      char *name = strndupa (string, name_end - string);
+      int use_malloc = !__libc_use_alloca (name_end - string + 1);
+      if (__builtin_expect (use_malloc, 0))
+	{
+	  name = strndup (string, name_end - string);
+	  if (name == NULL)
+	    return -1;
+	}
+      else
+	name = strndupa (string, name_end - string);
 #else
-      char *name = alloca (name_end - string + 1);
+# define use_malloc 1
+      name = malloc (name_end - string + 1);
+      if (name == NULL)
+	return -1;
       memcpy (name, string, name_end - string);
       name[name_end - string] = '\0';
 #endif
-      return __add_to_environ (name, NULL, string, 1);
+      int result = __add_to_environ (name, NULL, string, 1);
+
+      if (__builtin_expect (use_malloc, 0))
+	free (name);
+
+      return result;
     }
 
   __unsetenv (string);
