6e502e19455c6110dd4487d91b7b7d6d8121f9ba
Ulrich Drepper <drepper@gmail.com>
2011-06-22 08:32:55 -0400

    Clean up after kernel sigcontext header mess

--- sysdeps/unix/sysv/linux/bits/sigcontext.h
+++ sysdeps/unix/sysv/linux/bits/sigcontext.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1996, 1997, 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 1997, 1998, 2011 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -26,4 +26,8 @@
 # define sigcontext_struct sigcontext
 
 # include <asm/sigcontext.h>
+
+/* The Linux kernel headers redefine NULL wrongly, so cleanup afterwards.  */
+# define __need_NULL
+# include <stddef.h>
 #endif
