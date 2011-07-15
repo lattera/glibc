661607b3dd9f0b10667d4040843c1a3dc40f0cc9
Roland McGrath <roland@hack.frob.com>
2011-07-14 20:47:54 -0700

    Quash a warning in strstr-c.c built for static.

--- sysdeps/i386/i686/multiarch/strstr-c.c
+++ sysdeps/i386/i686/multiarch/strstr-c.c
@@ -1,9 +1,11 @@
 #include "init-arch.h"
 
 #define STRSTR __strstr_ia32
+#if defined SHARED && defined DO_VERSIONING && !defined NO_HIDDEN
 #undef libc_hidden_builtin_def
 #define libc_hidden_builtin_def(name) \
   __hidden_ver1 (__strstr_ia32, __GI_strstr, __strstr_ia32);
+#endif
 
 #include "string/strstr.c"
 
