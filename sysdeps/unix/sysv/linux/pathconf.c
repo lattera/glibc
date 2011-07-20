bfc9dc9bcfab65903defcd8a3f4d91f16e7e582d
Andreas Schwab <schwab@redhat.com>
2011-07-20 14:12:57 -0400

    Fix missing include

--- sysdeps/unix/sysv/linux/pathconf.c
+++ sysdeps/unix/sysv/linux/pathconf.c
@@ -1,5 +1,6 @@
 /* Get file-specific information about a file.  Linux version.
-   Copyright (C) 1991,1995,1996,1998-2003,2008,2010,2011 Free Software Foundation, Inc.
+   Copyright (C) 1991,1995,1996,1998-2003,2008,2010,2011
+   Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -20,6 +21,7 @@
 #include <errno.h>
 #include <mntent.h>
 #include <stdio_ext.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/sysmacros.h>
 
