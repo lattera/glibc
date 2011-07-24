798be72d127adf18f382323d0eed0a1e394967f8
Ulrich Drepper <drepper@gmail.com>
2011-07-23 21:45:57 -0400

    Define SEEK_DATA and SEEK_HOLE

--- posix/unistd.h
+++ posix/unistd.h
@@ -312,6 +312,10 @@ extern int faccessat (int __fd, __const char *__file, int __type, int __flag)
 # define SEEK_SET	0	/* Seek from beginning of file.  */
 # define SEEK_CUR	1	/* Seek from current position.  */
 # define SEEK_END	2	/* Seek from end of file.  */
+# ifdef __USE_GNU
+#  define SEEK_DATA	3	/* Seek to next data.  */
+#  define SEEK_HOLE	4	/* Seek to next hole.  */
+# endif
 #endif
 
 #if defined __USE_BSD && !defined L_SET
