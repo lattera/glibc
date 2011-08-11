89edf2e9119b57fc404891670d2bc47bdfdb61ef
Ulrich Drepper <drepper@gmail.com>
2011-08-11 14:04:08 -0400

    Locale-independent parsing in libintl

--- intl/l10nflist.c
+++ intl/l10nflist.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1995-2002, 2004, 2005 Free Software Foundation, Inc.
+/* Copyright (C) 1995-2002, 2004, 2005, 2011 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@gnu.ai.mit.edu>, 1995.
 
@@ -134,7 +134,7 @@ argz_next__ (argz, argz_len, entry)
   if (entry)
     {
       if (entry < argz + argz_len)
-        entry = strchr (entry, '\0') + 1;
+	entry = strchr (entry, '\0') + 1;
 
       return entry >= argz + argz_len ? NULL : (char *) entry;
     }
@@ -334,11 +334,11 @@ _nl_normalize_codeset (codeset, name_len)
   size_t cnt;
 
   for (cnt = 0; cnt < name_len; ++cnt)
-    if (isalnum ((unsigned char) codeset[cnt]))
+    if (__isalnum_l ((unsigned char) codeset[cnt], _nl_C_locobj_ptr))
       {
 	++len;
 
-	if (isalpha ((unsigned char) codeset[cnt]))
+	if (__isalpha_l ((unsigned char) codeset[cnt], _nl_C_locobj_ptr))
 	  only_digit = 0;
       }
 
@@ -352,9 +352,9 @@ _nl_normalize_codeset (codeset, name_len)
 	wp = retval;
 
       for (cnt = 0; cnt < name_len; ++cnt)
-	if (isalpha ((unsigned char) codeset[cnt]))
+	if (__isalpha_l ((unsigned char) codeset[cnt], _nl_C_locobj_ptr))
 	  *wp++ = tolower ((unsigned char) codeset[cnt]);
-	else if (isdigit ((unsigned char) codeset[cnt]))
+	else if (__isdigit_l ((unsigned char) codeset[cnt], _nl_C_locobj_ptr))
 	  *wp++ = codeset[cnt];
 
       *wp = '\0';
