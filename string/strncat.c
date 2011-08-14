69f630971184cae39d578709089d175c4b743390
Roland McGrath <roland@hack.frob.com>
2011-08-14 16:42:06 -0700

    More warning patrol.

--- string/strncat.c
+++ string/strncat.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1991,1997,2011 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -29,10 +29,7 @@ typedef char reg_char;
 #endif
 
 char *
-STRNCAT (s1, s2, n)
-     char *s1;
-     const char *s2;
-     size_t n;
+STRNCAT (char *s1, const char *s2, size_t n)
 {
   reg_char c;
   char *s = s1;
