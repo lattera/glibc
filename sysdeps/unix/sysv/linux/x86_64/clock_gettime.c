fc8bffcccf5821bca179486abef83a7f82526715
Ulrich Drepper <drepper@gmail.com>
2011-09-06 23:50:04 -0400

    Fix handling of __vdso_clock_gettime

--- sysdeps/unix/sysv/linux/x86_64/clock_gettime.c
+++ sysdeps/unix/sysv/linux/x86_64/clock_gettime.c
@@ -2,9 +2,13 @@
 
 #ifdef SHARED
 # define SYSCALL_GETTIME(id, tp) \
-  (*__vdso_clock_gettime) (id, tp)
+  ({ long int (*f) (clockid_t, struct timespec *) = __vdso_clock_gettime; \
+  PTR_DEMANGLE (f);							  \
+  f (id, tp); })
 # define INTERNAL_GETTIME(id, tp) \
-  (*__vdso_clock_gettime) (id, tp)
+  ({ long int (*f) (clockid_t, struct timespec *) = __vdso_clock_gettime; \
+  PTR_DEMANGLE (f);							  \
+  f (id, tp); })
 #endif
 
 #include "../clock_gettime.c"
