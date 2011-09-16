88738eb6e9e5a9d7d7977488519aaffb6263310c
Ulrich Drepper <drepper@gmail.com>
2011-09-15 23:01:56 -0400

    finite for 64-bit platforms

---
+++ sysdeps/ieee754/dbl-64/wordsize-64/s_finite.c
@@ -0,0 +1,32 @@
+/*
+ * ====================================================
+ * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
+ *
+ * Developed at SunPro, a Sun Microsystems, Inc. business.
+ * Permission to use, copy, modify, and distribute this
+ * software is freely granted, provided that this notice
+ * is preserved.
+ * ====================================================
+ */
+
+/*
+ * finite(x) returns 1 is x is finite, else 0;
+ * no branching!
+ */
+
+#include "math.h"
+#include "math_private.h"
+
+int
+__finite(double x)
+{
+  int64_t lx;
+  EXTRACT_WORDS64(lx,x);
+  return (int)((uint64_t)((lx&INT64_C(0x7fffffffffffffff))-INT64_C(0x7ff0000000000000))>>63);
+}
+hidden_def (__finite)
+weak_alias (__finite, finite)
+#ifdef NO_LONG_DOUBLE
+strong_alias (__finite, __finitel)
+weak_alias (__finite, finitel)
+#endif
