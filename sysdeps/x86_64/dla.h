a0cf1edd4cf2b3bf1ec2f772b47c8eaafd899383
Ulrich Drepper <drepper@gmail.com>
2011-10-24 21:17:10 -0400

    Use inline asm for DLA_FMS because of broken old compilers

--- sysdeps/x86_64/dla.h
+++ sysdeps/x86_64/dla.h
@@ -1,6 +1,10 @@
-#if defined __FMA4__ || defined __FMA__
+#ifdef __FMA4__
 # define DLA_FMS(x,y,z) \
-  __builtin_fma (x, y, -z)
+  ({ double __z;							      \
+     asm ("vfmsubsd %3, %2, %1, %0"					      \
+	  : "=x" (__z)							      \
+	  : "x" ((double) (x)), "xm" ((double) (y)) , "x" ((double) (z)));    \
+    __z; })
 #endif
 
 #include "sysdeps/ieee754/dbl-64/dla.h"
