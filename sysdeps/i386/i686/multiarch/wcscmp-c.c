0c92d8a87a47ab3276b4591bd084001053c5af2d
Andreas Schwab <schwab@redhat.com>
2011-10-28 11:45:12 +0200

    Fix some warning nits

--- sysdeps/i386/i686/multiarch/wcscmp-c.c
+++ sysdeps/i386/i686/multiarch/wcscmp-c.c
@@ -1,7 +1,5 @@
-#ifndef  NOT_IN_libc
-
-# define WCSCMP __wcscmp_ia32
-
+#define WCSCMP __wcscmp_ia32
+#ifdef SHARED
 # undef libc_hidden_def
 # define libc_hidden_def(name) \
   __hidden_ver1 (__wcscmp_ia32, __GI_wcscmp, __wcscmp_ia32);
