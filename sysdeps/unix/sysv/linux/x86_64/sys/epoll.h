4920765eb417431261367bf65f1a8a5ffb78baf7
Ulrich Drepper <drepper@gmail.com>
2011-12-21 22:14:05 -0500

    Define EPOLLONESHOT and EPOLLET using unsigned values

--- sysdeps/unix/sysv/linux/x86_64/sys/epoll.h
+++ sysdeps/unix/sysv/linux/x86_64/sys/epoll.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2002-2008, 2010 Free Software Foundation, Inc.
+/* Copyright (C) 2002-2008, 2010, 2011 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -65,9 +65,9 @@ enum EPOLL_EVENTS
 #define EPOLLHUP EPOLLHUP
     EPOLLRDHUP = 0x2000,
 #define EPOLLRDHUP EPOLLRDHUP
-    EPOLLONESHOT = (1 << 30),
+    EPOLLONESHOT = 1u << 30,
 #define EPOLLONESHOT EPOLLONESHOT
-    EPOLLET = (1 << 31)
+    EPOLLET = 1u << 31
 #define EPOLLET EPOLLET
   };
 
