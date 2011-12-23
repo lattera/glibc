c0da14cdda1fa552262ce3624156194eef43e973
Ulrich Drepper <drepper@gmail.com>
2011-12-23 14:03:55 -0500

    Preliminaries for 2.15 release

--- include/features.h
+++ include/features.h
@@ -339,7 +339,7 @@
 /* Major and minor version number of the GNU C library package.  Use
    these macros to test for features in specific releases.  */
 #define	__GLIBC__	2
-#define	__GLIBC_MINOR__	14
+#define	__GLIBC_MINOR__	15
 
 #define __GLIBC_PREREQ(maj, min) \
 	((__GLIBC__ << 16) + __GLIBC_MINOR__ >= ((maj) << 16) + (min))
--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 /* This file just defines the current version number of libc.  */
 
-#define RELEASE "development"
-#define VERSION "2.14.90"
+#define RELEASE "stable"
+#define VERSION "2.15"
