8b43a4cc567222ed61f157eccc0a8e1411082994
Ulrich Drepper <drepper@gmail.com>
2011-12-27 07:35:05 -0500

    Fix aligned_alloc prototype

--- stdlib/stdlib.h
+++ stdlib/stdlib.h
@@ -511,7 +511,8 @@ extern int posix_memalign (void **__memptr, size_t __alignment, size_t __size)
 
 #ifdef __USE_ISOC11
 /* ISO C variant of aligned allocation.  */
-extern int aligned_alloc (size_t __alignment, size_t __size) __THROW __wur;
+extern void *aligned_alloc (size_t __alignment, size_t __size)
+     __THROW __wur __attribute__ ((__malloc__, __alloc_size__ (2)));
 #endif
 
 __BEGIN_NAMESPACE_STD
