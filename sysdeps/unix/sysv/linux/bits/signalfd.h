8e475601f62c115115df1639599f573d2e78d7b7
Joseph Myers <joseph@codesourcery.com>
2012-02-15 20:10:22 +0000

    Split architecture-specific parts of sys/signalfd.h into bits/signalfd.h.

--- sysdeps/unix/sysv/linux/bits/signalfd.h
+++ sysdeps/unix/sysv/linux/bits/signalfd.h
@@ -16,43 +16,14 @@
    <http://www.gnu.org/licenses/>.  */
 
 #ifndef	_SYS_SIGNALFD_H
-#define	_SYS_SIGNALFD_H	1
+# error "Never use <bits/signalfd.h> directly; include <sys/signalfd.h> instead."
+#endif
 
-#define __need_sigset_t
-#include <signal.h>
-#include <stdint.h>
-
-/* Get the platform-dependent flags.  */
-#include <bits/signalfd.h>
-
-struct signalfd_siginfo
-{
-  uint32_t ssi_signo;
-  int32_t ssi_errno;
-  int32_t ssi_code;
-  uint32_t ssi_pid;
-  uint32_t ssi_uid;
-  int32_t ssi_fd;
-  uint32_t ssi_tid;
-  uint32_t ssi_band;
-  uint32_t ssi_overrun;
-  uint32_t ssi_trapno;
-  int32_t ssi_status;
-  int32_t ssi_int;
-  uint64_t ssi_ptr;
-  uint64_t ssi_utime;
-  uint64_t ssi_stime;
-  uint64_t ssi_addr;
-  uint8_t __pad[48];
-};
-
-__BEGIN_DECLS
-
-/* Request notification for delivery of signals in MASK to be
-   performed using descriptor FD.*/
-extern int signalfd (int __fd, const sigset_t *__mask, int __flags)
-  __THROW __nonnull ((2));
-
-__END_DECLS
-
-#endif /* sys/signalfd.h */
+/* Flags for signalfd.  */
+enum
+  {
+    SFD_CLOEXEC = 02000000,
+#define SFD_CLOEXEC SFD_CLOEXEC
+    SFD_NONBLOCK = 00004000
+#define SFD_NONBLOCK SFD_NONBLOCK
+  };
--- sysdeps/unix/sysv/linux/sparc/bits/signalfd.h
+++ sysdeps/unix/sysv/linux/sparc/bits/signalfd.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2007, 2008 Free Software Foundation, Inc.
+/* Copyright (C) 2007-2012 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -16,50 +16,14 @@
    <http://www.gnu.org/licenses/>.  */
 
 #ifndef	_SYS_SIGNALFD_H
-#define	_SYS_SIGNALFD_H	1
-
-#define __need_sigset_t
-#include <signal.h>
-#include <stdint.h>
-
-
-struct signalfd_siginfo
-{
-  uint32_t ssi_signo;
-  int32_t ssi_errno;
-  int32_t ssi_code;
-  uint32_t ssi_pid;
-  uint32_t ssi_uid;
-  int32_t ssi_fd;
-  uint32_t ssi_tid;
-  uint32_t ssi_band;
-  uint32_t ssi_overrun;
-  uint32_t ssi_trapno;
-  int32_t ssi_status;
-  int32_t ssi_int;
-  uint64_t ssi_ptr;
-  uint64_t ssi_utime;
-  uint64_t ssi_stime;
-  uint64_t ssi_addr;
-  uint8_t __pad[48];
-};
+# error "Never use <bits/signalfd.h> directly; include <sys/signalfd.h> instead."
+#endif
 
 /* Flags for signalfd.  */
 enum
   {
     SFD_CLOEXEC = 0x400000,
 #define SFD_CLOEXEC SFD_CLOEXEC
-    SFD_NONBLOCK = 0x4000
+    SFD_NONBLOCK = 0x004000
 #define SFD_NONBLOCK SFD_NONBLOCK
   };
-
-__BEGIN_DECLS
-
-/* Request notification for delivery of signals in MASK to be
-   performed using descriptor FD.*/
-extern int signalfd (int __fd, const sigset_t *__mask, int __flags)
-  __THROW __nonnull ((2));
-
-__END_DECLS
-
-#endif /* sys/signalfd.h */
