8dee4833c5b6d3c2ad506b5c7fdc548504386f96
Aurelien Jarno <aurelien@aurel32.net>
2012-02-17 17:46:56 +0100

    Include <time.h> in clock_settime.c.

--- sysdeps/unix/sysv/linux/clock_settime.c
+++ sysdeps/unix/sysv/linux/clock_settime.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2003, 2004, 2006, 2010 Free Software Foundation, Inc.
+/* Copyright (C) 2003, 2004, 2006, 2010, 2012 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -17,6 +17,7 @@
 
 #include <errno.h>
 #include <sysdep.h>
+#include <time.h>
 
 #include "kernel-posix-cpu-timers.h"
 #include <kernel-features.h>
