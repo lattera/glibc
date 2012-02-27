e7c0dc24ea997ace937eaa3df7fb20b01eb95d14
Joseph Myers <joseph@codesourcery.com>
2012-02-27 15:14:04 +0000

    ARM: fix pthread_attr_t definitions.

--- sysdeps/unix/sysv/linux/arm/nptl/bits/pthreadtypes.h
+++ sysdeps/unix/sysv/linux/arm/nptl/bits/pthreadtypes.h
@@ -38,11 +38,15 @@
 typedef unsigned long int pthread_t;
 
 
-typedef union __pthread_attr
+union pthread_attr_t
 {
   char __size[__SIZEOF_PTHREAD_ATTR_T];
   long int __align;
-} pthread_attr_t;
+};
+#ifndef __have_pthread_attr_t
+typedef union pthread_attr_t pthread_attr_t;
+# define __have_pthread_attr_t	1
+#endif
 
 
 typedef struct __pthread_internal_slist
