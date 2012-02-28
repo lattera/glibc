d40c5d54cb551acba4ef1617464760c5b3d41a14
Joseph Myers <joseph@codesourcery.com>
2012-02-28 16:37:58 +0000

    Regenerate sysdeps/gnu/errlist.c for errno.texi changes.

--- sysdeps/gnu/errlist.c
+++ sysdeps/gnu/errlist.c
@@ -386,7 +386,7 @@ TRANS not representable because of overflow or underflow. */
 /*
 TRANS Resource temporarily unavailable; the call might work if you try again
 TRANS later.  The macro @code{EWOULDBLOCK} is another name for @code{EAGAIN};
-TRANS they are always the same in the GNU C library.
+TRANS they are always the same in @theglibc{}.
 TRANS 
 TRANS This error can happen in a few different situations:
 TRANS 
@@ -421,7 +421,7 @@ TRANS @end itemize */
 #endif
 #if defined (EWOULDBLOCK) && EWOULDBLOCK != EAGAIN
 /*
-TRANS In the GNU C library, this is another name for @code{EAGAIN} (above).
+TRANS In @theglibc{}, this is another name for @code{EAGAIN} (above).
 TRANS The values are always the same, on every operating system.
 TRANS 
 TRANS C libraries in many older Unix systems have @code{EWOULDBLOCK} as a
