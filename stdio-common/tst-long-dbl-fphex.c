70bca0a394a2135927210007f1dd6eceafe1aaf5
Marek Polacek <polacek@redhat.com>
2012-03-08 02:46:43 -0500

    Fix up long double fphex test

--- stdio-common/tst-long-dbl-fphex.c
+++ stdio-common/tst-long-dbl-fphex.c
@@ -24,14 +24,29 @@ static int do_test (void);
 static int
 do_test (void)
 {
+#ifndef NO_LONG_DOUBLE
+  int result = 0;
   const long double x = 24.5;
-  wchar_t a[16 * sizeof (wchar_t)];
-  swprintf (a, 16 * sizeof (wchar_t), L"%La\n", x);
-  wchar_t A[16 * sizeof (wchar_t)];
-  swprintf (A, 16 * sizeof (wchar_t), L"%LA\n", x);
+  wchar_t a[16];
+  swprintf (a, sizeof (a), L"%La\n", x);
+  wchar_t A[16];
+  swprintf (A, sizeof (A) / sizeof (A[0]), L"%LA\n", x);
 
-  return (wmemcmp (a, L"0xc.4p+1", 8) != 0
-	  || wmemcmp (A, L"0XC.4P+1", 8) != 0);
+  /* Here wprintf can return four valid variants.  We must accept all
+     of them.  */
+  result |= (wmemcmp (a, L"0xc.4p+1", 8) == 0
+	     && wmemcmp (A, L"0XC.4P+1", 8) == 0);
+  result |= (wmemcmp (a, L"0x3.1p+3", 8) == 0
+	     && wmemcmp (A, L"0X3.1P+3", 8) == 0);
+  result |= (wmemcmp (a, L"0x6.2p+2", 8) == 0
+	     && wmemcmp (A, L"0X6.2P+2", 8) == 0);
+  result |= (wmemcmp (a, L"0x1.88p+4", 8) == 0
+	     && wmemcmp (A, L"0X1.88P+4", 8) == 0);
+
+  return result != 1;
+#else
+  return 0;
+#endif
 }
 
 #define TEST_FUNCTION do_test ()
