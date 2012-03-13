09a3453f637006538b45f9da69d1895d26a15473
David S. Miller <davem@davemloft.net>
2012-03-13 00:33:58 -0700

    Add missing string.h include for strcpy calls.
    	* nss/nss_db/db-init.c: Include string.h

--- nss/nss_db/db-init.c
+++ nss/nss_db/db-init.c
@@ -1,5 +1,5 @@
 /* Initialization in nss_db module.
-   Copyright (C) 2011 Free Software Foundation, Inc.
+   Copyright (C) 2011, 2012 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -18,7 +18,7 @@
 
 #include <paths.h>
 #include <nscd/nscd.h>
-
+#include <string.h>
 
 static union
 {
