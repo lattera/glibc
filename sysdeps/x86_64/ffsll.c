473c3ef32563ec57aeb0e204e2b0662fe80d2c18
H.J. Lu <hjl.tools@gmail.com>
2012-03-16 15:16:27 -0700

    Define x86-64 ffsl alias only if __LP64__ is defined

--- sysdeps/x86_64/ffsll.c
+++ sysdeps/x86_64/ffsll.c
@@ -1,7 +1,7 @@
 /* ffsll -- find first set bit in a word, counted from least significant end.
    For AMD x86-64.
    This file is part of the GNU C Library.
-   Copyright (C) 1991,92,93,94,97,98,2001 Free Software Foundation, Inc.
+   Copyright (C) 1991-2012 Free Software Foundation, Inc.
    Contributed by Ulrich Drepper <drepper@cygnus.com>.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -36,5 +36,7 @@ ffsll (long long int x)
   return cnt + 1;
 }
 
+#ifdef __LP64__
 #undef	ffsl
 weak_alias (ffsll, ffsl)
+#endif
