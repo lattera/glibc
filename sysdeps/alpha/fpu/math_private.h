bdd7898a58bd76ac3b411a4931ff3f9e77eabe6a
Richard Henderson <rth@twiddle.net>
2012-03-18 17:27:13 -0700

    alpha: Tell math_private.h that we have __isnan et al.

---
+++ sysdeps/alpha/fpu/math_private.h
@@ -0,0 +1,21 @@
+#ifndef ALPHA_MATH_PRIVATE_H
+#define ALPHA_MATH_PRIVATE_H 1
+
+/* In bits/mathinline.h we define __isnan et al.
+   In sysdeps/alpha/fpu/s_isnan.c we move the identifier out of the way
+   via macro hackery.  In both cases, tell math/math_private.h that
+   we have a local copy of the function.  */
+
+#ifndef __isnan
+# define __isnan  __isnan
+#endif
+#ifndef __isnanf
+# define __isnanf __isnanf
+#endif
+#ifndef __isnanl
+# define __isnanl __isnanl
+#endif
+
+#include_next <math_private.h>
+
+#endif /* ALPHA_MATH_PRIVATE_H */
