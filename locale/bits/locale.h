02467e1ca1745158c99c9b2576b05850028394f7
Paul Eggert <eggert@cs.ucla.edu>
2012-05-10 12:38:09 +0000

    [PATCH] [BZ #3440] Make LC_ALL etc. useful in #if expressions.

--- locale/bits/locale.h
+++ locale/bits/locale.h
@@ -1,5 +1,5 @@
 /* Definition of locale category symbol values.
-   Copyright (C) 2001 Free Software Foundation, Inc.
+   Copyright (C) 2001-2012 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -23,21 +23,18 @@
 #ifndef _BITS_LOCALE_H
 #define _BITS_LOCALE_H	1
 
-enum
-{
-  __LC_CTYPE = 0,
-  __LC_NUMERIC = 1,
-  __LC_TIME = 2,
-  __LC_COLLATE = 3,
-  __LC_MONETARY = 4,
-  __LC_MESSAGES = 5,
-  __LC_ALL = 6,
-  __LC_PAPER = 7,
-  __LC_NAME = 8,
-  __LC_ADDRESS = 9,
-  __LC_TELEPHONE = 10,
-  __LC_MEASUREMENT = 11,
-  __LC_IDENTIFICATION = 12
-};
+#define __LC_CTYPE		 0
+#define __LC_NUMERIC		 1
+#define __LC_TIME		 2
+#define __LC_COLLATE		 3
+#define __LC_MONETARY		 4
+#define __LC_MESSAGES		 5
+#define __LC_ALL		 6
+#define __LC_PAPER		 7
+#define __LC_NAME		 8
+#define __LC_ADDRESS		 9
+#define __LC_TELEPHONE		10
+#define __LC_MEASUREMENT	11
+#define __LC_IDENTIFICATION	12
 
 #endif	/* bits/locale.h */
