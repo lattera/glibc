bbc49098ff1bbbd5bdbcd69546b906d3844b4771
Samuel Thibault <samuel.thibault@ens-lyon.org>
2012-05-10 13:33:47 -0700

    Hurd: setitimer.c: Fix unlock in error patch

--- sysdeps/mach/hurd/setitimer.c
+++ sysdeps/mach/hurd/setitimer.c
@@ -1,5 +1,4 @@
-/* Copyright (C) 1994,1995,1996,1997,2000,2001,2005
-	Free Software Foundation, Inc.
+/* Copyright (C) 1994-2012 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -219,7 +218,7 @@ setitimer_locked (const struct itimerval *new, struct itimerval *old,
 	  /* Start up the itimer thread running `timer_thread' (below).  */
 	  if (err = __thread_create (__mach_task_self (),
 				     &_hurd_itimer_thread))
-	    return __hurd_fail (err);
+	    goto out;
 	  _hurd_itimer_thread_stack_base = 0; /* Anywhere.  */
 	  _hurd_itimer_thread_stack_size = __vm_page_size; /* Small stack.  */
 	  if (err = __mach_setup_thread (__mach_task_self (),
