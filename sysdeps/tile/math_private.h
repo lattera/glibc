fa12ed1f6126db0f52d4145d29094c4013829ce3
Chris Metcalf <cmetcalf@tilera.com>
2012-05-16 09:38:48 -0400

    tile: avoid compiler warning in feraiseexcept() math_private override

--- sysdeps/tile/math_private.h
+++ sysdeps/tile/math_private.h
@@ -6,7 +6,8 @@
    And since we don't define FE_INVALID, FE_DIVBYZERO, etc., we
    must ignore the argument of feraiseexcept() as well.  we return
    "1" to indicate we failed to raise an exception, though none of
-   the callers in glibc actually care.  */
-#define feraiseexcept(excepts) 1
+   the callers in glibc actually care.  The extra level of statement
+   expression wrapping avoids "statement with no effect" warnings.  */
+#define feraiseexcept(excepts) ({ 1; })
 
 #endif
