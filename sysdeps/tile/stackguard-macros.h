3f83552f2ca9b2f2ec311d91570146ca51f99809
Chris Metcalf <cmetcalf@tilera.com>
2012-05-17 18:51:36 +0000

    tile: support tilegx32 in stackguard-macros.h

--- sysdeps/tile/stackguard-macros.h
+++ sysdeps/tile/stackguard-macros.h
@@ -1,6 +1,13 @@
+#include <bits/wordsize.h>
+
 #ifdef __tilegx__
-# define STACK_CHK_GUARD \
+# if __WORDSIZE == 64
+#  define STACK_CHK_GUARD \
   ({ uintptr_t x; asm ("addi %0, tp, -16; ld %0, %0" : "=r" (x)); x; })
+# else
+#  define STACK_CHK_GUARD \
+  ({ uintptr_t x; asm ("addi %0, tp, -8; ld4s %0, %0" : "=r" (x)); x; })
+# endif
 #else
 # define STACK_CHK_GUARD \
   ({ uintptr_t x; asm ("addi %0, tp, -8; lw %0, %0" : "=r" (x)); x; })
