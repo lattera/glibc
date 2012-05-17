ac9182e7532b7a903afa1b86545dd9f1cc308a74
Chris Metcalf <cmetcalf@tilera.com>
2012-05-17 11:27:36 -0400

    tile: use ieee754/dbl-64 version of fma() and fmaf()
    It turns out that even if you stub out the rounding and exception
    support and use the ieee754 version, it's still much better than
    the generic version that just uses normal multiply and add.
    The resulting functions have only 1 ULP of error according to the tests.

--- sysdeps/tile/s_fmaf.c
+++ sysdeps/tile/s_fmaf.c
@@ -1,3 +1,8 @@
-/* Although tile uses ieee754/dbl-64, it does not support the
-   rounding modes required to use the standard dbl-64 s_fmaf.c.  */
-#include <math/s_fmaf.c>
+/* tile does not support the rounding modes required by the ieee754/dbl-64
+   implementation, but it's still better than the generic implementation.  */
+
+#define libc_feholdexcept_setround(e, x) do { (void) (e); } while (0)
+#define libc_feupdateenv_test(e, x) ((void) (e), 0)
+#define libc_fetestexcept(x) 0
+
+#include <sysdeps/ieee754/dbl-64/s_fmaf.c>
