48970aba309f08d3c94ddc8796630df87b8e093a
H.J. Lu <hjl.tools@gmail.com>
2012-05-16 20:02:44 -0700

    Fold copyright years

--- time/time.h
+++ time/time.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991-2003,2006,2009,2011,2012 Free Software Foundation, Inc.
+/* Copyright (C) 1991-2012 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
