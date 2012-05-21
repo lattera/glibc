d8d1017e5ea0b5478a3c571fe12a233c9450d41b
Andreas Jaeger <jaegerandi@gmail.com>
2012-05-21 22:27:11 +0200

    Fix warnings in wcschr-c.c
    Fixes:
    In file included from ../sysdeps/i386/i686/multiarch/wcschr-c.c:8:0:
    ../wcsmbs/wcschr.c:26:1: warning: function declaration isn’t a prototype [-Wstrict-prototypes]
    ../wcsmbs/wcschr.c:37:1: warning: data definition has no type or storage class [enabled by default]
    ../wcsmbs/wcschr.c:37:1: warning: type defaults to ‘int’ in declaration of ‘__hidden_ver1’ [enabled by default]
    ../wcsmbs/wcschr.c:37:1: warning: parameter names (without types) in function declaration [enabled by default]

--- sysdeps/i386/i686/multiarch/wcschr-c.c
+++ sysdeps/i386/i686/multiarch/wcschr-c.c
@@ -1,8 +1,14 @@
+#include <wchar.h>
+
 #ifndef NOT_IN_libc
-# undef libc_hidden_def
-# define libc_hidden_def(name) \
-  __hidden_ver1 (__wcschr_ia32, __GI_wcschr, __wcschr_ia32);
+# ifdef SHARED
+#  undef libc_hidden_def
+#  define libc_hidden_def(name) \
+   __hidden_ver1 (__wcschr_ia32, __GI_wcschr, __wcschr_ia32);
+# endif
 # define WCSCHR  __wcschr_ia32
 #endif
+
+extern __typeof (wcschr) __wcschr_ia32;
 
 #include "wcsmbs/wcschr.c"
