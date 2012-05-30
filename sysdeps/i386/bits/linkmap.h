6bd976963f65990e1099eb4e08946bdb5191dc9c
H.J. Lu <hjl.tools@gmail.com>
2012-05-30 15:21:55 -0700

    Use x86_64 bits/{link.h,linkmap.h} for i386/x86_64

--- sysdeps/i386/bits/linkmap.h
+++
@@ -1,6 +0,0 @@
-struct link_map_machine
-  {
-    Elf32_Addr plt; /* Address of .plt + 0x16 */
-    Elf32_Addr gotplt; /* Address of .got + 0x0c */
-    void *tlsdesc_table; /* Address of TLS descriptor hash table.  */
-  };
