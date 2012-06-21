116a106aaf21065d3a9c62b38ad84fc60133a135
Jeff Law <law@redhat.com>
2012-06-21 17:16:15 -0600

    Update copyright years

--- intl/dcigettext.c
+++ intl/dcigettext.c
@@ -1,5 +1,5 @@
 /* Implementation of the internal dcigettext function.
-   Copyright (C) 1995-2005, 2006, 2007, 2008, 2009, 2011
+   Copyright (C) 1995-2005, 2006, 2007, 2008, 2009, 2011, 2012
    Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
