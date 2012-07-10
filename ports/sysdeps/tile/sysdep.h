5f5bcf71f753089c82741578853c5c866d1839e1
Chris Metcalf <cmetcalf@tilera.com>
2012-07-10 10:33:04 -0400

    tile: Convert ASM_GLOBAL_DIRECTIVE to .globl

--- ports/sysdeps/tile/sysdep.h
+++ ports/sysdeps/tile/sysdep.h
@@ -30,7 +30,7 @@
 
 /* Define an entry point visible from C.  */
 #define	ENTRY(name)							      \
-  ASM_GLOBAL_DIRECTIVE C_SYMBOL_NAME(name);				      \
+  .globl C_SYMBOL_NAME(name);						      \
   ASM_TYPE_DIRECTIVE (C_SYMBOL_NAME(name),@function)			      \
   .align 8;								      \
   C_LABEL(name)								      \
