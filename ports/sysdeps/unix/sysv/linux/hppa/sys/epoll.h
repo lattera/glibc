f8d44fdc72253347dae69fd2c4a231efba4fec98
Andreas Jaeger <aj@suse.de>
2012-07-26 13:11:33 +0200

    Merge Linux 3.5 epoll changes
    Add EPOLLWAKEUP.

--- ports/sysdeps/unix/sysv/linux/hppa/sys/epoll.h
+++ ports/sysdeps/unix/sysv/linux/hppa/sys/epoll.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2002-2009, 2012 Free Software Foundation, Inc.
+/* Copyright (C) 2002-2012 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -64,6 +64,8 @@ enum EPOLL_EVENTS
 #define EPOLLHUP EPOLLHUP
     EPOLLRDHUP = 0x2000,
 #define EPOLLRDHUP EPOLLRDHUP
+    EPOLLWAKEUP = 1u << 29,
+#define EPOLLWAKEUP EPOLLWAKEUP
     EPOLLONESHOT = 1u << 30,
 #define EPOLLONESHOT EPOLLONESHOT
     EPOLLET = 1u << 31
