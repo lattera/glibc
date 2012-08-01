b8625cfc6f629f146aa3944b5cfc65025884bd44
Roland McGrath <roland@hack.frob.com>
2012-08-01 11:13:37 -0700

    Clean up mkostemps64 definition for O_LARGEFILE==0.

--- misc/mkostemps.c
+++ misc/mkostemps.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2009 Free Software Foundation, Inc.
+/* Copyright (C) 2009-2012 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -41,3 +41,7 @@ mkostemps (template, suffixlen, flags)
 
   return __gen_tempname (template, suffixlen, flags, __GT_FILE);
 }
+
+#if !defined O_LARGEFILE || O_LARGEFILE == 0
+weak_alias (mkostemps, mkostemps64)
+#endif
