9b5281b65e2ce654610c0809aa829b4b18213b27
Roland McGrath <roland@hack.frob.com>
2012-08-08 11:03:47 -0700

    Fix __dl_start declaration to avoid warning.

--- ports/sysdeps/arm/dl-machine.h
+++ ports/sysdeps/arm/dl-machine.h
@@ -70,7 +70,7 @@ elf_machine_dynamic (void)
 static inline Elf32_Addr __attribute__ ((unused))
 elf_machine_load_address (void)
 {
-  extern void __dl_start asm ("_dl_start");
+  extern Elf32_Addr internal_function __dl_start (void *) asm ("_dl_start");
   Elf32_Addr got_addr = (Elf32_Addr) &__dl_start;
   Elf32_Addr pcrel_addr;
 #ifdef __thumb__
