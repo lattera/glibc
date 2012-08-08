a2433aac7e3524f5c65c557e8ddba760b196f856
Roland McGrath <roland@hack.frob.com>
2012-08-08 10:39:28 -0700

    Add a missing #include.

--- sysdeps/posix/fdopendir.c
+++ sysdeps/posix/fdopendir.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2005, 2006, 2011 Free Software Foundation, Inc.
+/* Copyright (C) 2005-2012 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -18,6 +18,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stddef.h>
 #include <sys/stat.h>
 
 #include <not-cancel.h>
