176790a7341221ae1681ca3aa44ba1e928f789e3
Roland McGrath <roland@hack.frob.com>
2012-08-15 17:02:22 -0700

    Add a missing #include to dl-sym.c.

--- elf/dl-sym.c
+++ elf/dl-sym.c
@@ -1,5 +1,5 @@
 /* Look up a symbol in a shared object loaded by `dlopen'.
-   Copyright (C) 1999-2002,2004,2006,2007,2009 Free Software Foundation, Inc.
+   Copyright (C) 1999-2012 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -19,6 +19,7 @@
 #include <assert.h>
 #include <stddef.h>
 #include <setjmp.h>
+#include <stdlib.h>
 #include <libintl.h>
 
 #include <dlfcn.h>
