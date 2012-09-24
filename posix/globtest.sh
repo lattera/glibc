9a9028b1fe1519262fcbf0b7ede708d4ebbffbd7
Dmitry V. Levin <ldv@altlinux.org>
2012-09-24 20:58:00 +0000

    Add copyright notices to testsuite shell scripts

--- posix/globtest.sh
+++ posix/globtest.sh
@@ -1,4 +1,21 @@
 #! /bin/bash
+# Test for glob(3).
+# Copyright (C) 1997-2011 Free Software Foundation, Inc.
+# This file is part of the GNU C Library.
+
+# The GNU C Library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# The GNU C Library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with the GNU C Library; if not, see
+# <http://www.gnu.org/licenses/>.
 
 common_objpfx=$1; shift
 elf_objpfx=$1; shift
--- posix/wordexp-tst.sh
+++ posix/wordexp-tst.sh
@@ -1,4 +1,21 @@
-#!/bin/sh
+#! /bin/sh
+# Test for wordexp(3).
+# Copyright (C) 1998-2000 Free Software Foundation, Inc.
+# This file is part of the GNU C Library.
+
+# The GNU C Library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# The GNU C Library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with the GNU C Library; if not, see
+# <http://www.gnu.org/licenses/>.
 
 # Some of these tests may look a little weird.
 # The first parameter to wordexp-test is what it gives to wordexp.
