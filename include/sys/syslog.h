dcdae19a3f8eafb7aa597b1d5cb770db76509ec4
Joseph Myers <joseph@codesourcery.com>
2012-11-02 15:41:37 +0000

    Test _ISOMAC in include/sys/syslog.h.

--- include/sys/syslog.h
+++ include/sys/syslog.h
@@ -1,5 +1,6 @@
 #include <misc/sys/syslog.h>
 
+#ifndef _ISOMAC
 libc_hidden_proto (syslog)
 libc_hidden_proto (vsyslog)
 
@@ -7,3 +8,4 @@ extern void __vsyslog_chk (int __pri, int __flag, const char *__fmt,
 			   __gnuc_va_list __ap)
      __attribute__ ((__format__ (__printf__, 3, 0)));
 libc_hidden_proto (__vsyslog_chk)
+#endif
