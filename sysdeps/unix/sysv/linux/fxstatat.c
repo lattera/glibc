12df29e2d24e96421b0ac7842acef79edb19c4af
Joseph Myers <joseph@codesourcery.com>
2012-11-17 03:43:53 +0000

    Include string.h in sysdeps/unix/sysv/linux/fxstatat.c.

--- sysdeps/unix/sysv/linux/fxstatat.c
+++ sysdeps/unix/sysv/linux/fxstatat.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2005, 2006, 2007, 2009 Free Software Foundation, Inc.
+/* Copyright (C) 2005-2012 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -23,6 +23,7 @@
 #include <fcntl.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <kernel_stat.h>
 
