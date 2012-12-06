82123268ac12b7e37c04ea6347a555f679f95704
Chris Metcalf <cmetcalf@tilera.com>
2012-12-06 10:42:06 -0500

    Fix warnings from generic _FPU_GETCW

--- sysdeps/generic/fpu_control.h
+++ sysdeps/generic/fpu_control.h
@@ -1,5 +1,5 @@
 /* FPU control word definitions.  Stub version.
-   Copyright (C) 1996, 1997, 1998 Free Software Foundation, Inc.
+   Copyright (C) 1996-2012 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -29,8 +29,8 @@
 typedef unsigned int fpu_control_t;
 
 /* Macros for accessing the hardware control word.  */
-#define _FPU_GETCW(cw) 0
-#define _FPU_SETCW(cw) do { } while (0)
+#define _FPU_GETCW(cw) (cw) = 0
+#define _FPU_SETCW(cw) (void) (cw)
 
 /* Default control word set at startup.  */
 extern fpu_control_t __fpu_control;
