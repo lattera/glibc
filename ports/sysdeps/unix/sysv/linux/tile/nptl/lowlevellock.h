fdfc65d21f331c0f276a075e17a704183f701555
Chris Metcalf <cmetcalf@tilera.com>
2012-12-28 11:28:08 -0500

    tile: support lll_futex_timed_wait_bitset

--- ports/sysdeps/unix/sysv/linux/tile/nptl/lowlevellock.h
+++ ports/sysdeps/unix/sysv/linux/tile/nptl/lowlevellock.h
@@ -86,6 +86,19 @@
 			      (val), (timespec));			      \
   })
 
+#define lll_futex_timed_wait_bitset(futexp, val, timespec, clockbit, private) \
+  ({									      \
+    INTERNAL_SYSCALL_DECL (__err);					      \
+    long int __ret;							      \
+    int __op = FUTEX_WAIT_BITSET | clockbit;				      \
+									      \
+    __ret = INTERNAL_SYSCALL (futex, __err, 6, (futexp),		      \
+			      __lll_private_flag (__op, private),	      \
+			      (val), (timespec), NULL /* Unused.  */, 	      \
+			      FUTEX_BITSET_MATCH_ANY);			      \
+    __ret;		      						      \
+  })
+
 #define lll_futex_wake(futexp, nr, private) \
   ({									      \
     INTERNAL_SYSCALL_DECL (__err);					      \
