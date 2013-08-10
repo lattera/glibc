eefa3be8e4c2c721a9f277d8ea2e11180231829f
David S. Miller <davem@davemloft.net>
2013-08-10 15:52:55 -0700

    Update version.h and include/features.h for 2.18 release.

--- include/features.h
+++ include/features.h
@@ -353,7 +353,7 @@
 /* Major and minor version number of the GNU C library package.  Use
    these macros to test for features in specific releases.  */
 #define	__GLIBC__	2
-#define	__GLIBC_MINOR__	17
+#define	__GLIBC_MINOR__	18
 
 #define __GLIBC_PREREQ(maj, min) \
 	((__GLIBC__ << 16) + __GLIBC_MINOR__ >= ((maj) << 16) + (min))
--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 /* This file just defines the current version number of libc.  */
 
-#define RELEASE "development"
-#define VERSION "2.17.90"
+#define RELEASE "stable"
+#define VERSION "2.18"
