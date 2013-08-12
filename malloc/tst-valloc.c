7815420bc9127926485e287d233fe638228ab53d
Will Newton <will.newton@linaro.org>
2013-08-12 14:44:36 +0100

    malloc/tst-valloc.c: Improve test coverage and use test-skeleton.c.
    ChangeLog:
    
    2013-10-02  Will Newton  <will.newton@linaro.org>
    
    	* malloc/tst-valloc.c: Rewrite to use test-skeleton.c and
    	improve test coverage.

--- malloc/tst-valloc.c
+++ malloc/tst-valloc.c
@@ -1,23 +1,91 @@
-/* Test case by Stephen Tweedie <sct@redhat.com>.  */
-#include <unistd.h>
-#include <stdio.h>
+/* Copyright (C) 2013 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <errno.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-int
-main (void)
+static int errors = 0;
+
+static void
+merror (const char *msg)
 {
-  char *p;
-  int pagesize = getpagesize ();
-  int i;
-
-  p = valloc (pagesize);
-  i = (long int) p;
-
-  if ((i & (pagesize-1)) != 0)
-    {
-      fprintf (stderr, "Alignment problem: valloc returns %p\n", p);
-      exit (1);
-    }
-
-  return 0;
+  ++errors;
+  printf ("Error: %s\n", msg);
 }
+
+static int
+do_test (void)
+{
+  void *p;
+  unsigned long pagesize = getpagesize();
+  unsigned long ptrval;
+  int save;
+
+  errno = 0;
+
+  p = valloc (-1);
+
+  save = errno;
+
+  if (p != NULL)
+    merror ("valloc (-1) succeeded.");
+
+  if (p == NULL && save != ENOMEM)
+    merror ("valloc (-1) errno is not set correctly");
+
+  errno = 0;
+
+  p = valloc (-pagesize);
+
+  save = errno;
+
+  if (p != NULL)
+    merror ("valloc (-pagesize) succeeded.");
+
+  if (p == NULL && save != ENOMEM)
+    merror ("valloc (-pagesize) errno is not set correctly");
+
+  p = valloc (0);
+
+  if (p == NULL)
+    merror ("valloc (0) failed.");
+
+  free (p);
+
+  p = valloc (32);
+
+  if (p == NULL)
+    merror ("valloc (32) failed.");
+
+  ptrval = (unsigned long)p;
+
+  if (p == NULL)
+    merror ("valloc (32) failed.");
+
+  if ((ptrval & (pagesize - 1)) != 0)
+    merror ("returned pointer is not page aligned.");
+
+  free (p);
+
+  return errors != 0;
+}
+
+#define TEST_FUNCTION do_test ()
+#include "../test-skeleton.c"
