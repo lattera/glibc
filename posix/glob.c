e833b53fe618af287a0b818ac3592d842b76dec8
Eric Blake <eblake@redhat.com>
2013-09-04 17:02:47 -0600

    glob: silence -Wattribute warnings
    Colin Watson reported that some versions of gcc warn about
    attribute leaf used on a static function, since it has no
    effect on anything but external functions.
    
    * posix/glob.c (next_brace_sub, prefix_array, collated_compare):
    Use __THROWNL rather than __THROW on static functions.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

--- posix/glob.c
+++ posix/glob.c
@@ -193,7 +193,7 @@
 # define GET_LOGIN_NAME_MAX()	(-1)
 #endif
 
-static const char *next_brace_sub (const char *begin, int flags) __THROW;
+static const char *next_brace_sub (const char *begin, int flags) __THROWNL;
 
 #endif /* !defined _LIBC || !defined GLOB_ONLY_P */
 
@@ -208,8 +208,8 @@ extern int __glob_pattern_type (const char *pattern, int quote)
     attribute_hidden;
 
 #if !defined _LIBC || !defined GLOB_ONLY_P
-static int prefix_array (const char *prefix, char **array, size_t n) __THROW;
-static int collated_compare (const void *, const void *) __THROW;
+static int prefix_array (const char *prefix, char **array, size_t n) __THROWNL;
+static int collated_compare (const void *, const void *) __THROWNL;
 
 
 /* Find the end of the sub-pattern in a brace expression.  */
