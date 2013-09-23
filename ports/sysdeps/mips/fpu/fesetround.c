6b9a5fe450d21c5852e81f2c357d2af6d20f873c
Steve Ellcey <sellcey@mips.com>
2013-09-23 09:39:32 -0700

    2013-09-23  Steve Ellcey  <sellcey@mips.com>
    	* sysdeps/mips/fpu/fegetround.c (fegetround): Use _FPU_RC_MASK.
    	* sysdeps/mips/fpu/fesetround.c (fesetround): Use _FPU_RC_MASK.

--- ports/sysdeps/mips/fpu/fesetround.c
+++ ports/sysdeps/mips/fpu/fesetround.c
@@ -25,7 +25,7 @@ fesetround (int round)
 {
   fpu_control_t cw;
 
-  if ((round & ~0x3) != 0)
+  if ((round & ~_FPU_RC_MASK) != 0)
     /* ROUND is no valid rounding mode.  */
     return 1;
 
@@ -33,7 +33,7 @@ fesetround (int round)
   _FPU_GETCW (cw);
 
   /* Set rounding bits.  */
-  cw &= ~0x3;
+  cw &= ~_FPU_RC_MASK;
   cw |= round;
   /* Set new state.  */
   _FPU_SETCW (cw);
