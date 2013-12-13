cd05ba913576ce8258533793c2e1bce011980e83
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2013-12-13 14:36:50 -0500

    PowerPC: multiarch memrchr for PowerPC64

--- sysdeps/powerpc/powerpc64/multiarch/ifunc-impl-list.c
+++ sysdeps/powerpc/powerpc64/multiarch/ifunc-impl-list.c
@@ -106,5 +106,13 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
 	      IFUNC_IMPL_ADD (array, i, memchr, 1,
 			      __memchr_ppc))
 
+  /* Support sysdeps/powerpc/powerpc64/multiarch/memrchr.c.  */
+  IFUNC_IMPL (i, name, memrchr,
+	      IFUNC_IMPL_ADD (array, i, memrchr,
+			      hwcap & PPC_FEATURE_HAS_VSX,
+			      __memrchr_power7)
+	      IFUNC_IMPL_ADD (array, i, memrchr, 1,
+			      __memrchr_ppc))
+
   return i;
 }
