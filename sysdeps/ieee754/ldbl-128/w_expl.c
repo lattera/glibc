7beb48cbb77ef32135a07b429838521a0c181377
Andreas Krebbel <krebbel@linux.vnet.ibm.com>
2014-01-15 09:50:31 +0100

    [BZ #16427] Fix ldbl-128 exp overflows.
    Invoke the non-IEEE handling only for numbers special also in the IEEE
    case.  This aligns the exp handling with the other ldbl variants.

--- sysdeps/ieee754/ldbl-128/w_expl.c
+++ sysdeps/ieee754/ldbl-128/w_expl.c
@@ -25,24 +25,16 @@ static char rcsid[] = "$NetBSD: $";
 #include <math.h>
 #include <math_private.h>
 
-static const long double
-o_threshold= 1.1356523406294143949491931077970763428449E4L,
-u_threshold= -1.1433462743336297878837243843452621503410E4;
-
 long double __expl(long double x)	/* wrapper exp */
 {
 #ifdef _IEEE_LIBM
 	return __ieee754_expl(x);
 #else
-	long double z;
-	z = __ieee754_expl(x);
-	if(_LIB_VERSION == _IEEE_) return z;
-	if(__finitel(x)) {
-	    if(x>o_threshold)
-	        return __kernel_standard_l(x,x,206); /* exp overflow */
-	    else if(x<u_threshold)
-	        return __kernel_standard_l(x,x,207); /* exp underflow */
-	}
+	long double z = __ieee754_expl (x);
+	if (__glibc_unlikely (!__finitel (z) || z == 0)
+	    && __finitel (x) && _LIB_VERSION != _IEEE_)
+	  return __kernel_standard_l (x, x, 206 + !!__signbitl (x));
+
 	return z;
 #endif
 }
