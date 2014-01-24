4ab6acaebd0047dc37c6493946484be9f1b4920b
Richard Henderson <rth@twiddle.net>
2014-01-24 09:07:04 -0800

    alpha: Fix tls-macros.h
    Reported to the gcc bugzilla, there was a missing dependency on $gp
    that let the compiler generated $gp reload be scheduled across the
    macros here.

--- ports/sysdeps/alpha/tls-macros.h
+++ ports/sysdeps/alpha/tls-macros.h
@@ -2,21 +2,21 @@
 
 extern void *__tls_get_addr (void *);
 
-# define TLS_GD(x)						\
-  ({ void *__result;						\
-     asm ("lda %0, " #x "($gp) !tlsgd" : "=r" (__result));	\
+# define TLS_GD(x)							\
+  ({ register void *__gp asm ("$29"); void *__result;			\
+     asm ("lda %0, " #x "($gp) !tlsgd" : "=r" (__result) : "r"(__gp));	\
      __tls_get_addr (__result); })
 
-# define TLS_LD(x)						\
-  ({ void *__result;						\
-     asm ("lda %0, " #x "($gp) !tlsldm" : "=r" (__result));	\
-     __result = __tls_get_addr (__result);			\
-     asm ("lda %0, " #x "(%0) !dtprel" : "+r" (__result));	\
+# define TLS_LD(x)							\
+  ({ register void *__gp asm ("$29"); void *__result;			\
+     asm ("lda %0, " #x "($gp) !tlsldm" : "=r" (__result) : "r"(__gp));	\
+     __result = __tls_get_addr (__result);				\
+     asm ("lda %0, " #x "(%0) !dtprel" : "+r" (__result));		\
      __result; })
 
-# define TLS_IE(x)						\
-  ({ long ofs;							\
-     asm ("ldq %0, " #x "($gp) !gottprel" : "=r"(ofs));		\
+# define TLS_IE(x)							\
+  ({ register void *__gp asm ("$29"); long ofs;				\
+     asm ("ldq %0, " #x "($gp) !gottprel" : "=r"(ofs) : "r"(__gp));	\
      __builtin_thread_pointer () + ofs; })
 
 # define TLS_LE(x)						\
