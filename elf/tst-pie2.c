b2264769eec0416d38f6b3dbe33335408154431f
Paul Pluzhnikov <ppluzhnikov@google.com>
2014-03-13 08:19:18 -0700

    Add missing elf/tst-pie2.c -- should have been in commit 798212a01311491d5e14fcda687460b75f8ca286

---
+++ elf/tst-pie2.c
@@ -0,0 +1,38 @@
+/* Test case for BZ #16381
+
+   Copyright (C) 2014 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+
+#include <assert.h>
+
+static int g;
+
+void init_g (void) __attribute__((constructor));
+
+void
+init_g (void)
+{
+  assert (g == 0);
+  g += 1;
+}
+
+int
+main (int argc, char *argv[])
+{
+  return 0;
+}
