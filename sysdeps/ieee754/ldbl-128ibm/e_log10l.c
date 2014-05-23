b72592e75fdecb34e2ac5f931730409a1fb68e8a
Joseph Myers <joseph@codesourcery.com>
2014-05-23 12:07:50 +0000

    Fix log10 (1) in round-downward mode (bug 16977).
    As with various other issues of this kind, bug 16977 is log10 (1)
    wrongly returning -0 rather than +0 in round-downward mode because of
    an implementation effectively in terms of log1p (x - 1).  This patch
    fixes the issue in the same way used for log.
    
    Tested x86_64 and x86 and ulps updated accordingly.  Also tested for
    mips64 to confirm a fix was needed for ldbl-128 and to validate that
    fix (also applied to ldbl-128ibm since that version of logl is
    essentially the same as the ldbl-128 one).
    
    	[BZ #16977]
    	* sysdeps/i386/fpu/e_log10.S (__ieee754_log10): Take absolute
    	value when x - 1 is zero.
    	* sysdeps/i386/fpu/e_log10f.S (__ieee754_log10f): Likewise.
    	* sysdeps/i386/fpu/e_log10l.S (__ieee754_log10l): Likewise.
    	* sysdeps/ieee754/ldbl-128/e_log10l.c (__ieee754_log10l): Return
    	0.0L for an argument of 1.0L.
    	* sysdeps/ieee754/ldbl-128ibm/e_log10l.c (__ieee754_log10l):
    	Likewise.
    	* sysdeps/x86_64/fpu/e_log10l.S (__ieee754_log10l): Take absolute
    	value when x - 1 is zero.
    	* math/libm-test.inc (log10_test): Use ALL_RM_TEST.
    	* sysdeps/i386/fpu/libm-test-ulps: Update.
    	* sysdeps/x86_64/fpu/libm-test-ulps: Likewise.

--- sysdeps/ieee754/ldbl-128ibm/e_log10l.c
+++ sysdeps/ieee754/ldbl-128ibm/e_log10l.c
@@ -195,6 +195,9 @@ __ieee754_log10l (long double x)
   if (hx >= 0x7ff0000000000000LL)
     return (x + x);
 
+  if (x == 1.0L)
+    return 0.0L;
+
 /* separate mantissa from exponent */
 
 /* Note, frexp is used so that denormal numbers
