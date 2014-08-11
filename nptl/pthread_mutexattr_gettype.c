2868e0703d5b8c8e60c6f60de13e876c4d85daa0
Andreas Schwab <schwab@suse.de>
2014-08-11 11:18:26 +0200

    Filter out PTHREAD_MUTEX_NO_ELISION_NP bit in pthread_mutexattr_gettype (BZ #15790)
    pthread_mutexattr_settype adds PTHREAD_MUTEX_NO_ELISION_NP to kind,
    which is an internal flag that pthread_mutexattr_gettype shouldn't
    expose, since pthread_mutexattr_settype wouldn't accept it.

--- nptl/pthread_mutexattr_gettype.c
+++ nptl/pthread_mutexattr_gettype.c
@@ -28,7 +28,8 @@ pthread_mutexattr_gettype (attr, kind)
 
   iattr = (const struct pthread_mutexattr *) attr;
 
-  *kind = iattr->mutexkind & ~PTHREAD_MUTEXATTR_FLAG_BITS;
+  *kind = (iattr->mutexkind & ~PTHREAD_MUTEXATTR_FLAG_BITS
+	   & ~PTHREAD_MUTEX_NO_ELISION_NP);
 
   return 0;
 }
