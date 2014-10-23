167da422b30b35c9eb9fc819ce5d3b3b0d65c6f4
J. Brown <jb999@gmx.de>
2014-10-23 15:28:06 +0200

    Recognize recent x86 CPUs in string.h

--- sysdeps/x86/bits/string.h
+++ sysdeps/x86/bits/string.h
@@ -29,9 +29,14 @@
 			    || defined __pentiumpro__ || defined __pentium4__ \
 			    || defined __nocona__ || defined __atom__ 	      \
 			    || defined __core2__ || defined __corei7__	      \
+			    || defined __sandybridge__ || defined __haswell__ \
+			    || defined __bonnell__ || defined __silvermont__  \
 			    || defined __k6__ || defined __geode__	      \
 			    || defined __k8__ || defined __athlon__	      \
-			    || defined __amdfam10__)
+			    || defined __amdfam10__ || defined __bdver1__     \
+			    || defined __bdver2__ || defined __bdver3__	      \
+			    || defined __bdver4__ || defined __btver1__	      \
+			    || defined __btver2__)
 
 /* We only provide optimizations if the user selects them and if
    GNU CC is used.  */
