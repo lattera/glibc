6e46de42fe1695818a410a7b86d26be8b1527524
Wilco Dijkstra <wdijkstr@arm.com>
2014-10-24 16:08:42 +0000

    This patch improves strcat performance by using strlen and strcpy. Strlen has a fast C implementation, so this improves performance even on targets which don't have an optimized strlen and strcpy - it is 25% faster in bench-strcat. On targets which don't provide an optimized strcat but which do have an optimized strlen and strcpy, performance gain is > 2x.

--- string/strcat.c
+++ string/strcat.c
@@ -23,26 +23,7 @@
 char *
 strcat (char *dest, const char *src)
 {
-  char *s1 = dest;
-  const char *s2 = src;
-  char c;
-
-  /* Find the end of the string.  */
-  do
-    c = *s1++;
-  while (c != '\0');
-
-  /* Make S1 point before the next character, so we can increment
-     it while memory is read (wins on pipelined cpus).  */
-  s1 -= 2;
-
-  do
-    {
-      c = *s2++;
-      *++s1 = c;
-    }
-  while (c != '\0');
-
+  strcpy (dest + strlen (dest), src);
   return dest;
 }
 libc_hidden_builtin_def (strcat)
