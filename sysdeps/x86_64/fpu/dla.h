5ae4fe60e68a6698afadbec00481be96096e3eb4
Joseph Myers <joseph@codesourcery.com>
2014-11-14 18:53:07 +0000

    Remove x86_64 __GNUC_PREREQ (4, 6) conditional.
    This patch removes a conditional on __GNUC_PREREQ (4, 6) in x86_64
    code.
    
    Tested for x86_64 that installed shared libraries are unchanged by
    this patch.  Committed (I think this file reasonably comes under math
    maintainership).
    
    	* sysdeps/x86_64/fpu/dla.h [__FMA4__ && __GNUC_PREREQ (4, 6)]
    	(DLA_FMS): Make definition conditional only on [__FMA4__].
    	[__FMA4__ && !__GNUC_PREREQ (4, 6)] (DLA_FMS): Remove conditional
    	definition.

--- sysdeps/x86_64/fpu/dla.h
+++ sysdeps/x86_64/fpu/dla.h
@@ -1,17 +1,8 @@
 #include <features.h>
 
 #ifdef __FMA4__
-# if __GNUC_PREREQ (4, 6)
-#  define DLA_FMS(x,y,z) \
+# define DLA_FMS(x,y,z) \
   __builtin_fma (x, y, -(z))
-# else
-#  define DLA_FMS(x,y,z) \
-  ({ double __z;							      \
-     asm ("vfmsubsd %3, %2, %1, %0"					      \
-	  : "=x" (__z)							      \
-	  : "x" ((double) (x)), "xm" ((double) (y)) , "x" ((double) (z)));    \
-    __z; })
-# endif
 #endif
 
 #include "sysdeps/ieee754/dbl-64/dla.h"
