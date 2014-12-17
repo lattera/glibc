6b814909b41476ad594547b5f2289889a39cc0dd
Torvald Riegel <triegel@redhat.com>
2014-12-17 23:28:38 +0100

    sh: Remove custom lowlevellock, barrier, condvar, and rwlock implementations.

--- sysdeps/unix/sysv/linux/sh/sh4/lowlevellock.h
+++
@@ -1,4 +0,0 @@
-/*  4 instruction cycles not accessing cache and TLB are needed after
-    trapa instruction to avoid an SH-4 silicon bug.  */
-#define NEED_SYSCALL_INST_PAD
-#include_next <lowlevellock.h>
