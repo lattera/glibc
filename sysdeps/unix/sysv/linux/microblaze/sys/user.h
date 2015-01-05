aee6626c30dd0043990d9554c943fdfdbf9fce83
Matthew Fortune <matthew.fortune@imgtec.com>
2015-01-05 15:12:45 +0000

    MicroBlaze: Fix BZ17791 - Remove fixed page size macros and others
    Microblaze apparently has a variable page size (see thread below) and
    should not hard-code any page-size related macros.
    Also remove macros that are only used for BFD's trad-core support
    which is not relavant for microblaze also according to the thread
    starting here:
    
    https://sourceware.org/ml/libc-ports/2013-11/msg00028.html
    
    This patch is neither built nor tested but mirrors a MIPS patch that
    fixes the same issue.
    
    Thanks,
    Matthew
    
         * sysdepsysdeps/unix/sysv/linux/microblaze/sys/user.h
         (PAGE_SHIFT, PAGE_SIZE, PAGE_MASK, NBPG, UPAGES): Remove.
         (HOST_TEXT_START_ADDR, HOST_STACK_END_ADDR): Remove.
    
    Signed-off-by: David Holsgrove <david.holsgrove@xilinx.com>

--- sysdeps/unix/sysv/linux/microblaze/sys/user.h
+++ sysdeps/unix/sysv/linux/microblaze/sys/user.h
@@ -66,12 +66,4 @@ struct user
   int				u_debugreg [8];
 };
 
-# define PAGE_SHIFT		12
-# define PAGE_SIZE		(1UL << PAGE_SHIFT)
-# define PAGE_MASK		(~(PAGE_SIZE-1))
-# define NBPG			PAGE_SIZE
-# define UPAGES		1
-# define HOST_TEXT_START_ADDR	(u.start_code)
-# define HOST_STACK_END_ADDR	(u.start_stack + u.u_ssize * NBPG)
-
 #endif	/* _SYS_USER_H */
