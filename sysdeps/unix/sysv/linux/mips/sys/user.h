b1efe3bbcfd94941556ffd5b60539207023c8224
Matthew Fortune <matthew.fortune@imgtec.com>
2015-01-05 13:34:41 +0000

    Fix MIPS variable PAGE_SIZE bug (16191)
    MIPS supports a variable page size but glibc defines a constant.
    This causes at least two glibc tests to fail when the page size
    does not match the hard-coded size:
    
    inet/test-ifaddrs
    inet/test_ifindex
    
    	[BZ #16191]
    	* NEWS: Mention bug fix.
    	* sysdeps/unix/sysv/linux/mips/sys/user.h (PAGE_SHIFT): Remove.
    	(PAGE_SIZE, PAGE_MASK, NBPG, UPAGES): Likewise.
    	(HOST_TEXT_START_ADDR, HOST_DATA_START_ADDR): Likewise.
    	(HOST_STACK_END_ADDR): Likewise.

--- sysdeps/unix/sysv/linux/mips/sys/user.h
+++ sysdeps/unix/sysv/linux/mips/sys/user.h
@@ -206,13 +206,4 @@ struct user {
 
 #endif
 
-#define PAGE_SHIFT		12
-#define PAGE_SIZE		(1UL << PAGE_SHIFT)
-#define PAGE_MASK		(~(PAGE_SIZE-1))
-#define NBPG			PAGE_SIZE
-#define UPAGES			1
-#define HOST_TEXT_START_ADDR	(u.start_code)
-#define HOST_DATA_START_ADDR	(u.start_data)
-#define HOST_STACK_END_ADDR	(u.start_stack + u.u_ssize * NBPG)
-
 #endif	/* _SYS_USER_H */
