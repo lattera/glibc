f8d1bb4c25849b528b9771cb5834b0cd17450ae3
Roland McGrath <roland@hack.frob.com>
2015-02-06 14:42:18 -0800

    ARM: Add missing sfi_breg in LDR_GLOBAL macro.

--- sysdeps/arm/sysdep.h
+++ sysdeps/arm/sysdep.h
@@ -198,7 +198,7 @@
 #  define LDR_GLOBAL(R, T, SYMBOL, CONSTANT)				\
 	movw	T, #:lower16:SYMBOL;					\
 	movt	T, #:upper16:SYMBOL;					\
-	ldr	R, [T, $CONSTANT]
+	sfi_breg T, ldr R, [\B, $CONSTANT]
 # elif defined (ARCH_HAS_T2) && defined (PIC) && ARM_PCREL_MOVW_OK
 #  define LDR_GLOBAL(R, T, SYMBOL, CONSTANT)				\
 	movw	R, #:lower16:_GLOBAL_OFFSET_TABLE_ - 97f - PC_OFS;	\
@@ -212,7 +212,7 @@
 97:	add	R, R, pc;						\
 98:	LDST_PC_INDEXED (ldr, T, T, T);					\
 	LDST_INDEXED (ldr, R, T, R, T);					\
-	ldr	R, [R, $CONSTANT]
+	sfi_breg R, ldr	R, [\B, $CONSTANT]
 # else
 #  define LDR_GLOBAL(R, T, SYMBOL, CONSTANT)		\
 	ldr	T, 99f;					\
