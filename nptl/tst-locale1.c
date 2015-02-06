1ac074e66e8ee0e548742fcc79a705284448aea6
Roland McGrath <roland@hack.frob.com>
2015-02-06 12:30:58 -0800

    Conditionalize use of SIGRTMIN in nptl/tst-locale1.c.

--- nptl/tst-locale1.c
+++ nptl/tst-locale1.c
@@ -12,7 +12,11 @@ useless (void)
 {
   pthread_t th;
   pthread_create (&th, 0, (void *(*) (void *)) useless, 0);
+  int result = 0;
+#ifdef SIGRTMIN
   /* This is to check __libc_current_sigrt* can be used in statically
      linked apps.  */
-  return SIGRTMIN;
+  result = SIGRTMIN;
+#endif
+  return result;
 }
