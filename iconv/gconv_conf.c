d19df6ac5ace81ca8412f8bf160ae63f44f6f0cf
Roland McGrath <roland@hack.frob.com>
2015-02-25 15:41:31 -0800

    Don't crash in iconv setup when getcwd fails.

--- iconv/gconv_conf.c
+++ iconv/gconv_conf.c
@@ -468,7 +468,7 @@ __gconv_get_path (void)
 				":", 1),
 		     default_gconv_path, sizeof (default_gconv_path));
 	  cwd = __getcwd (NULL, 0);
-	  cwdlen = strlen (cwd);
+	  cwdlen = __glibc_unlikely (cwd == NULL) ? 0 : strlen (cwd);
 	}
       assert (default_gconv_path[0] == '/');
 
