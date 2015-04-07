d5856d06c3ef62907f2865c502ada4baf3c9efa1
James Cowgill <james410@cowgill.org.uk>
2015-04-07 17:23:54 +0000

    [BZ #17930] MIPS: Define SHM_NORESERVE.
    	[BZ #17930]
    	* sysdeps/unix/sysv/linux/mips/bits/shm.h (SHM_NORESERVE): Define.

--- sysdeps/unix/sysv/linux/mips/bits/shm.h
+++ sysdeps/unix/sysv/linux/mips/bits/shm.h
@@ -67,6 +67,7 @@ struct shmid_ds
 # define SHM_DEST	01000	/* segment will be destroyed on last detach */
 # define SHM_LOCKED	02000   /* segment will not be swapped */
 # define SHM_HUGETLB	04000	/* segment is mapped via hugetlb */
+# define SHM_NORESERVE	010000  /* don't check for reservations */
 
 struct shminfo
   {
