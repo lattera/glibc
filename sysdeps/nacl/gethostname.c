c4c977c6a294d8e0bcaf31059469aa683e0f00fe
Roland McGrath <roland@hack.frob.com>
2015-05-08 13:06:41 -0700

    NaCl: Implement gethostname.

---
+++ sysdeps/nacl/gethostname.c
@@ -0,0 +1,45 @@
+/* Get current host's name.  NaCl version.
+   Copyright (C) 2015 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+/* Put the name of the current host in no more than LEN bytes of NAME.
+   The result is null-terminated if LEN is large enough for the full
+   name and the terminator.  */
+int
+__gethostname (char *name, size_t len)
+{
+  static const char hostname[] = "naclhost";
+
+  int result = 0;
+  size_t copy = sizeof hostname;
+
+  if (len < copy)
+    {
+      errno = ENAMETOOLONG;
+      result = -1;
+      copy = len;
+    }
+
+  memcpy (name, hostname, copy);
+
+  return result;
+}
+weak_alias (__gethostname, gethostname)
