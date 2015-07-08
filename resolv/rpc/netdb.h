b8a91836171f5722bd5bef6dc2e9ce263c897974
Roland McGrath <roland@hack.frob.com>
2015-07-08 13:38:50 -0700

    Install a dummy <rpc/netdb.h> when not building sunrpc/.

---
+++ resolv/rpc/netdb.h
@@ -0,0 +1,3 @@
+/* This is a dummy file for <rpc/netdb.h>, which is included by <netdb.h>.
+   This file is installed when the C library does not support the SunRPC
+   interfaces (including 'struct rpcent' et al) at all.  */
