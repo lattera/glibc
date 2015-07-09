e505234f6e59c0d1a2b6f2e7d80acaf8a7a05d1f
Arjun Shankar <arjun.is@lostca.se>
2015-07-09 14:51:30 +0000

    Modify elf/tst-audit9.c to use test-skeleton.c
    This test was skipped by the use-test-skeleton conversion script
    [29955b5d] because the definition of `main' did not begin according to
    the GNU formatting style that the script assumed.
    
    ChangeLog:
    
    2015-07-09  Arjun Shankar  <arjun.is@lostca.se>
    
    	* elf/tst-audit9.c (main): Converted to ...
    	(do_test): ... this.
    	(TEST_FUNCTION): New macro.
    	Include test-skeleton.c.

--- elf/tst-audit9.c
+++ elf/tst-audit9.c
@@ -1,8 +1,12 @@
 #include <dlfcn.h>
 
-int main(void)
+static int
+do_test (void)
 {
   void *h = dlopen("$ORIGIN/tst-auditmod9b.so", RTLD_LAZY);
   int (*fp)(void) = dlsym(h, "f");
   return fp() - 1;
 }
+
+#define TEST_FUNCTION do_test ()
+#include "../test-skeleton.c"
