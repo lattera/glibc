82641e16aa83c693974297951e23ba08a3e89652
Wilco Dijkstra <wdijkstr@arm.com>
2015-07-13 12:48:33 +0100

    Add AArch64 versions of math_opt_barrier and math_force_eval that avoid going via memory.

--- sysdeps/aarch64/fpu/math_private.h
+++ sysdeps/aarch64/fpu/math_private.h
@@ -22,6 +22,11 @@
 #include <fenv.h>
 #include <fpu_control.h>
 
+#define math_opt_barrier(x) \
+({ __typeof (x) __x = (x); __asm ("" : "+w" (__x)); __x; })
+#define math_force_eval(x) \
+({ __typeof (x) __x = (x); __asm __volatile__ ("" : : "w" (__x)); })
+
 extern __always_inline double
 __ieee754_sqrt (double d)
 {
