9c9184b4491461e39008e7d18d5c472570cd0755
Mike Frysinger <vapier@gentoo.org>
2015-07-21 22:44:48 -0400

    ia64: msg.h: fix msg_qnum/msg_qbytes types
    These two fields have dedicated types, so change the ia64 header to match
    all the other arches.  This fixes the conform test for msg.h.

--- sysdeps/unix/sysv/linux/ia64/bits/msq.h
+++ sysdeps/unix/sysv/linux/ia64/bits/msq.h
@@ -20,7 +20,7 @@
 #error "Never use <bits/msq.h> directly; include <sys/msg.h> instead."
 #endif
 
-#include <sys/types.h>
+#include <bits/types.h>
 
 /* Define options for message queue functions.  */
 #define MSG_NOERROR	010000	/* no error if message is too big */
@@ -29,8 +29,13 @@
 # define MSG_COPY	040000	/* copy (not remove) all queue messages */
 #endif
 
+/* Types used in the structure definition.  */
+typedef unsigned long int msgqnum_t;
+typedef unsigned long int msglen_t;
+
+
 /* Structure of record for one message inside the kernel.
-   The type `struct __msg' is opaque.  */
+   The type `struct msg' is opaque.  */
 struct msqid_ds
 {
   struct ipc_perm msg_perm;	/* structure describing operation permission */
@@ -38,8 +43,8 @@ struct msqid_ds
   __time_t msg_rtime;		/* time of last msgrcv command */
   __time_t msg_ctime;		/* time of last change */
   unsigned long int __msg_cbytes;	/* current number of bytes on queue */
-  unsigned long int msg_qnum;	/* number of messages currently on queue */
-  unsigned long int msg_qbytes;	/* max number of bytes allowed on queue */
+  msgqnum_t msg_qnum;		/* number of messages currently on queue */
+  msglen_t msg_qbytes;		/* max number of bytes allowed on queue */
   __pid_t msg_lspid;		/* pid of last msgsnd() */
   __pid_t msg_lrpid;		/* pid of last msgrcv() */
   unsigned long int __glibc_reserved1;
