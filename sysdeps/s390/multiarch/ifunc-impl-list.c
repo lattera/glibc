798f5b4b5d24e5687dd55acc535954ed1d59c10c
Stefan Liebler <stli@linux.vnet.ibm.com>
2015-08-26 10:26:26 +0200

    S390: Optimize memrchr.
    This patch provides optimized version of memrchr with the z13 vector
    instructions.
    
    ChangeLog:
    
    	* sysdeps/s390/multiarch/memrchr-c.c: New File.
    	* sysdeps/s390/multiarch/memrchr-vx.S: Likewise.
    	* sysdeps/s390/multiarch/memrchr.c: Likewise.
    	* sysdeps/s390/multiarch/Makefile
    	(sysdep_routines): Add memrchr functions.
    	* sysdeps/s390/multiarch/ifunc-impl-list-common.c
    	(__libc_ifunc_impl_list_common): Add ifunc test for memrchr.

--- sysdeps/s390/multiarch/ifunc-impl-list.c
+++ sysdeps/s390/multiarch/ifunc-impl-list.c
@@ -137,6 +137,8 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
 
   IFUNC_VX_IMPL (wmemcmp);
 
+  IFUNC_VX_IMPL (memrchr);
+
 #endif /* HAVE_S390_VX_ASM_SUPPORT */
 
   return i;
