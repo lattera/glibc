6f0f237bf5a3c8d29a6342cbf8b4c17b10aa1d4c
Joseph Myers <joseph@codesourcery.com>
2015-09-18 21:53:22 +0000

    Avoid excess range in results from i386 exp, hypot, pow functions (bug 18980).
    i386 exp, hypot and pow functions can return overflowing and
    underflowing values with excess range and precision; ; Wilco
    Dijkstra's patches to make isfinite etc. expand inline cause this
    pre-existing issue to result in test failures.
    
    This patch fixes those functions to avoid excess range and precision
    in their return values.  Appropriate macros are added for the repeated
    code sequences; in future I'll add more such macros and refactor
    existing code forcing underflow (with or without also eliminating
    excess range and precision from the return value) to use such macros.
    
    Tested for x86.  If, after this patch, you still see x86 libm test
    failures with excess range or precision, please file bugs in Bugzilla.
    
    	[BZ #18980]
    	* sysdeps/i386/fpu/i386-math-asm.h (DEFINE_FLT_MIN): New macro.
    	(DEFINE_DBL_MIN): Likewise.
    	(FLT_NARROW_EVAL_UFLOW_NONNEG_NAN): Likewise.
    	(DBL_NARROW_EVAL_UFLOW_NONNEG_NAN): Likewise.
    	(FLT_NARROW_EVAL_UFLOW_NONNEG): Likewise.
    	(DBL_NARROW_EVAL_UFLOW_NONNEG): Likewise.
    	* sysdeps/i386/fpu/e_exp.S: Include <i386-math-asm.h>.
    	(dbl_min): Replace with use of DEFINE_DBL_MIN.
    	(__ieee754_exp): Use DBL_NARROW_EVAL_UFLOW_NONNEG_NAN.
    	(__exp_finite): Use DBL_NARROW_EVAL_UFLOW_NONNEG.
    	* sysdeps/i386/fpu/e_exp10.S: Include <i386-math-asm.h>.
    	(dbl_min): Replace with use of DEFINE_DBL_MIN.
    	(__ieee754_exp10): Use DBL_NARROW_EVAL_UFLOW_NONNEG_NAN.
    	* sysdeps/i386/fpu/e_exp10f.S: Include <i386-math-asm.h>.
    	(flt_min): Replace with use of DEFINE_FLT_MIN.
    	(__ieee754_exp10f): Use FLT_NARROW_EVAL_UFLOW_NONNEG_NAN.
    	* sysdeps/i386/fpu/e_exp2.S: Include <i386-math-asm.h>.
    	(dbl_min): Replace with use of DEFINE_DBL_MIN.
    	(__ieee754_exp2): Use DBL_NARROW_EVAL_UFLOW_NONNEG_NAN.
    	* sysdeps/i386/fpu/e_exp2f.S: Include <i386-math-asm.h>.
    	(flt_min): Replace with use of DEFINE_FLT_MIN.
    	(__ieee754_exp2f): Use FLT_NARROW_EVAL_UFLOW_NONNEG_NAN.
    	* sysdeps/i386/fpu/e_expf.S: Include <i386-math-asm.h>.
    	(flt_min): Replace with use of DEFINE_FLT_MIN.
    	(__ieee754_expf): Use FLT_NARROW_EVAL_UFLOW_NONNEG_NAN.
    	(__expf_finite): Use FLT_NARROW_EVAL_UFLOW_NONNEG.
    	* sysdeps/i386/fpu/e_hypot.S: Include <i386-math-asm.h>.
    	(__ieee754_hypot): Use DBL_NARROW_EVAL.
    	* sysdeps/i386/fpu/e_hypotf.S: Include <i386-math-asm.h>.
    	(__ieee754_hypotf): Use FLT_NARROW_EVAL.
    	* sysdeps/i386/fpu/e_pow.S: Include <i386-math-asm.h>.
    	(__ieee754_pow): Use DBL_NARROW_EVAL.
    	* sysdeps/i386/fpu/e_powf.S: Include <i386-math-asm.h>.
    	(__ieee754_powf): Use FLT_NARROW_EVAL.
    	* sysdeps/i386/i686/fpu/multiarch/e_expf-sse2.S
    	(__ieee754_expf_sse2): Convert double-precision result to single
    	precision.
    	* sysdeps/i386/fpu/libm-test-ulps: Update.

--- sysdeps/i386/fpu/i386-math-asm.h
+++ sysdeps/i386/fpu/i386-math-asm.h
@@ -36,4 +36,91 @@
 	addl	$8, %esp;			\
 	cfi_adjust_cfa_offset (-8);
 
+/* Define constants for the minimum value of a floating-point
+   type.  */
+#define DEFINE_FLT_MIN				\
+	.section .rodata.cst4,"aM",@progbits,4;	\
+	.p2align 2;				\
+	.type flt_min,@object;			\
+flt_min:					\
+	.byte 0, 0, 0x80, 0;			\
+	.size flt_min, .-flt_min;
+#define DEFINE_DBL_MIN				\
+	.section .rodata.cst8,"aM",@progbits,8;	\
+	.p2align 3;				\
+	.type dbl_min,@object;			\
+dbl_min:					\
+	.byte 0, 0, 0, 0, 0, 0, 0x10, 0;	\
+	.size dbl_min, .-dbl_min;
+
+/* Remove excess range and precision by storing a value on the stack
+   and loading it back.  The value is given to be nonnegative or NaN;
+   if it is subnormal, also force an underflow exception.  The
+   relevant constant for the minimum of the type must have been
+   defined, the MO macro must have been defined for access to memory
+   operands, and, if PIC, the PIC register must have been loaded.  */
+#define FLT_NARROW_EVAL_UFLOW_NONNEG_NAN	\
+	subl	$4, %esp;			\
+	cfi_adjust_cfa_offset (4);		\
+	flds	MO(flt_min);			\
+	fld	%st(1);				\
+	fucompp;				\
+	fnstsw;					\
+	sahf;					\
+	jnc 6424f;				\
+	fld	%st(0);				\
+	fmul	%st(0);				\
+	fstps	(%esp);				\
+6424:	fstps	(%esp);				\
+	flds	(%esp);				\
+	addl	$4, %esp;			\
+	cfi_adjust_cfa_offset (-4);
+#define DBL_NARROW_EVAL_UFLOW_NONNEG_NAN	\
+	subl	$8, %esp;			\
+	cfi_adjust_cfa_offset (8);		\
+	fldl	MO(dbl_min);			\
+	fld	%st(1);				\
+	fucompp;				\
+	fnstsw;					\
+	sahf;					\
+	jnc 6453f;				\
+	fld	%st(0);				\
+	fmul	%st(0);				\
+	fstpl	(%esp);				\
+6453:	fstpl	(%esp);				\
+	fldl	(%esp);				\
+	addl	$8, %esp;			\
+	cfi_adjust_cfa_offset (-8);
+
+/* Likewise, but the argument is not a NaN (so fcom instructions,
+   which support memory operands, can be used).  */
+#define FLT_NARROW_EVAL_UFLOW_NONNEG		\
+	subl	$4, %esp;			\
+	cfi_adjust_cfa_offset (4);		\
+	fcoms	MO(flt_min);			\
+	fnstsw;					\
+	sahf;					\
+	jnc 6424f;				\
+	fld	%st(0);				\
+	fmul	%st(0);				\
+	fstps	(%esp);				\
+6424:	fstps	(%esp);				\
+	flds	(%esp);				\
+	addl	$4, %esp;			\
+	cfi_adjust_cfa_offset (-4);
+#define DBL_NARROW_EVAL_UFLOW_NONNEG		\
+	subl	$8, %esp;			\
+	cfi_adjust_cfa_offset (8);		\
+	fcoml	MO(dbl_min);			\
+	fnstsw;					\
+	sahf;					\
+	jnc 6453f;				\
+	fld	%st(0);				\
+	fmul	%st(0);				\
+	fstpl	(%esp);				\
+6453:	fstpl	(%esp);				\
+	fldl	(%esp);				\
+	addl	$8, %esp;			\
+	cfi_adjust_cfa_offset (-8);
+
 #endif /* i386-math-asm.h.  */
