facdd9ea29ab94aac2b188ec3cc41f8733d769e0
Carlos O'Donell <carlos@systemhalted.org>
2015-10-09 16:35:53 -0400

    Fix typo in bug-strcoll2 (Bug 18589)
    Fix the copyright year and remove contributed by in the
    bug-strcoll2 test. In addition add the correct dependency
    on $(gen-locales) to ensure all the test locales are generated.

--- string/bug-strcoll2.c
+++ string/bug-strcoll2.c
@@ -1,7 +1,6 @@
 /* Bug 18589: sort-test.sh fails at random.
-   Copyright (C) 1998-2015 Free Software Foundation, Inc.
+   Copyright (C) 2015 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
-   Contributed by Ulrich Drepper <drepper@cygnus.com>, 1998.
 
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
