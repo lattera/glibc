3c82bb5042330441a18fd8e8d5b74d25d5540a66
H.J. Lu <hjl.tools@gmail.com>
2015-10-15 14:33:26 -0700

    Mark ld.so internel __uname hidden
    Since ld.so internel __uname is only used internally in ld.so, it can
    be made hidden.
    
    	[BZ #19122]
    	* include/sys/utsname.h [IS_IN (rtld)] (__uname): Add
    	attribute_hidden.

--- include/sys/utsname.h
+++ include/sys/utsname.h
@@ -6,5 +6,9 @@ extern int __uname (struct utsname *__name);
 
 libc_hidden_proto (uname)
 libc_hidden_proto (__uname)
+
+# if IS_IN (rtld)
+extern __typeof (__uname) __uname attribute_hidden;
+# endif
 #endif
 #endif
