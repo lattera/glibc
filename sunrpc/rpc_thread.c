4e1665024a48da9c865e991aff03a5ad03c771aa
Florian Weimer <fweimer@redhat.com>
2015-10-17 12:07:04 +0200

    sunrpc: Rewrite with explicit TLS access using __thread

--- sunrpc/rpc_thread.c
+++ sunrpc/rpc_thread.c
@@ -10,7 +10,8 @@
 
 /* Variable used in non-threaded applications or for the first thread.  */
 static struct rpc_thread_variables __libc_tsd_RPC_VARS_mem;
-__libc_tsd_define (, struct rpc_thread_variables *, RPC_VARS)
+static __thread struct rpc_thread_variables *thread_rpc_vars
+        attribute_tls_model_ie;
 
 /*
  * Task-variable destructor
@@ -18,8 +19,7 @@ __libc_tsd_define (, struct rpc_thread_variables *, RPC_VARS)
 void __attribute__ ((section ("__libc_thread_freeres_fn")))
 __rpc_thread_destroy (void)
 {
-	struct rpc_thread_variables *tvp
-	  = __libc_tsd_get (struct rpc_thread_variables *, RPC_VARS);
+	struct rpc_thread_variables *tvp = thread_rpc_vars;
 
 	if (tvp != NULL) {
 		__rpc_thread_svc_cleanup ();
@@ -34,7 +34,7 @@ __rpc_thread_destroy (void)
 		free (tvp->svc_pollfd_s);
 		if (tvp != &__libc_tsd_RPC_VARS_mem)
 			free (tvp);
-		__libc_tsd_set (struct rpc_thread_variables *, RPC_VARS, NULL);
+		thread_rpc_vars = NULL;
 	}
 }
 #ifdef _LIBC_REENTRANT
@@ -49,8 +49,7 @@ text_set_element (__libc_subfreeres, __rpc_thread_destroy);
 static void
 rpc_thread_multi (void)
 {
-  __libc_tsd_set (struct rpc_thread_variables *, RPC_VARS,
-		  &__libc_tsd_RPC_VARS_mem);
+  thread_rpc_vars = &__libc_tsd_RPC_VARS_mem;
 }
 
 
@@ -58,20 +57,15 @@ struct rpc_thread_variables *
 __rpc_thread_variables (void)
 {
 	__libc_once_define (static, once);
-	struct rpc_thread_variables *tvp;
+	struct rpc_thread_variables *tvp = thread_rpc_vars;
 
-	tvp = __libc_tsd_get (struct rpc_thread_variables *, RPC_VARS);
 	if (tvp == NULL) {
 		__libc_once (once, rpc_thread_multi);
-		tvp = __libc_tsd_get (struct rpc_thread_variables *, RPC_VARS);
+		tvp = thread_rpc_vars;
 		if (tvp == NULL) {
 			tvp = calloc (1, sizeof *tvp);
 			if (tvp != NULL)
-				__libc_tsd_set (struct rpc_thread_variables *,
-						RPC_VARS, tvp);
-			else
-				tvp = __libc_tsd_get (struct rpc_thread_variables *,
-						      RPC_VARS);
+				thread_rpc_vars = tvp;
 		}
 	}
 	return tvp;
