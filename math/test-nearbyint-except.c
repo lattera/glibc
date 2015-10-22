846d9a4a3acdb4939ca7bf6aed48f9f6f26911be
Joseph Myers <joseph@codesourcery.com>
2015-10-22 23:14:55 +0000

    Fix i386 / x86_64 nearbyint exception clearing (bug 15491).
    The implementations of nearbyint functions using x87 floating point
    (i386 all versions, x86_64 long double only) use the fclex
    instruction, which clears any exceptions that were raised before the
    function was called.  These functions must not clear exceptions that
    were raised before they were called.
    
    This patch fixes these functions to save and restore the whole
    floating-point environment (fnstenv / fldenv) as the way of avoiding
    raising "inexact" (recall that there isn't an x87 instruction for
    loading just the status word, so the whole environment has to be saved
    and loaded instead - the code already saved and loaded the control
    word, which is now obtained from the saved environment after this
    patch, to disable traps on "inexact").  In the case of the long double
    functions, any "invalid" exception from frndint (applied to a
    signaling NaN) needs merging into the saved state; this issue doesn't
    apply to the float and double functions because that exception would
    have been raised when the argument is loaded, before the environment
    is saved.
    
    	[BZ #15491]
    	* sysdeps/i386/fpu/s_nearbyint.S (__nearbyint): Save and restore
    	floating-point environment instead of clearing all exceptions.
    	* sysdeps/i386/fpu/s_nearbyintf.S (__nearbyintf): Likewise.
    	* sysdeps/i386/fpu/s_nearbyintl.S (__nearbyintl): Likewise,
    	merging in "invalid" exceptions from frndint.
    	* sysdeps/x86_64/fpu/s_nearbyintl.S (__nearbyintl): Likewise.
    	* math/test-nearbyint-except.c: New file.
    	* math/Makefile (tests): Add test-nearbyint-except.

---
+++ math/test-nearbyint-except.c
@@ -0,0 +1,82 @@
+/* Test nearbyint functions do not clear exceptions (bug 15491).
+   Copyright (C) 2015 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <fenv.h>
+#include <math.h>
+#include <stdio.h>
+
+#include <math-tests.h>
+
+#ifndef FE_INVALID
+# define FE_INVALID 0
+#endif
+
+#define TEST_FUNC(NAME, FLOAT, SUFFIX)					\
+static int								\
+NAME (void)								\
+{									\
+  int result = 0;							\
+  volatile FLOAT a, b __attribute__ ((unused));				\
+  a = 1.0;								\
+  /* nearbyint must not clear already-raised exceptions.  */		\
+  feraiseexcept (FE_ALL_EXCEPT);					\
+  b = nearbyint ## SUFFIX (a);						\
+  if (fetestexcept (FE_ALL_EXCEPT) == FE_ALL_EXCEPT)			\
+    puts ("PASS: " #FLOAT);						\
+  else									\
+    {									\
+      puts ("FAIL: " #FLOAT);						\
+      result = 1;							\
+    }									\
+  /* But it mustn't lose exceptions from sNaN arguments.  */		\
+  if (SNAN_TESTS (FLOAT) && EXCEPTION_TESTS (FLOAT))			\
+    {									\
+      static volatile FLOAT snan = __builtin_nans ## SUFFIX ("");	\
+      volatile FLOAT c __attribute__ ((unused));			\
+      feclearexcept (FE_ALL_EXCEPT);					\
+      c = nearbyint ## SUFFIX (snan);					\
+      if (fetestexcept (FE_INVALID) == FE_INVALID)			\
+	puts ("PASS: " #FLOAT " sNaN");					\
+      else								\
+	{								\
+	  puts ("FAIL: " #FLOAT " sNaN");				\
+	  result = 1;							\
+	}								\
+    }									\
+  return result;							\
+}
+
+TEST_FUNC (float_test, float, f)
+TEST_FUNC (double_test, double, )
+#ifndef NO_LONG_DOUBLE
+TEST_FUNC (ldouble_test, long double, l)
+#endif
+
+static int
+do_test (void)
+{
+  int result = float_test ();
+  result |= double_test ();
+#ifndef NO_LONG_DOUBLE
+  result |= ldouble_test ();
+#endif
+  return result;
+}
+
+#define TEST_FUNCTION do_test ()
+#include "../test-skeleton.c"
