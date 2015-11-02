a5a34d2eaf717fb95a33553170000fb147ded444
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2015-11-02 11:42:09 -0200

    linux: Consolidate Linux open_by_handle_at syscall
    This patch consolidates the open_by_handle_at Linux syscall generation on
    sysdeps/unix/sysv/linux/open_by_handle_at.c.  It basically removes it from
    architectures auto-generation list.
    
    Checked on i686-linux-gnu, x86_64-linux-gnu, x86_64-linux-gnux32,
    arch64-linux-gnu, arm-linux-gnueabihf, powerpc64le-linux-gnu,
    sparc64-linux-gnu, and sparcv9-linux-gnu.
    
    	* sysdeps/unix/sysv/linux/Makefile (sysdep_routines): Add
    	open_by_handle_at.
    	(CFLAGS-open_by_handle_at.c): New flag.
    	* sysdeps/unix/sysv/linux/open_by_handle_at.c: New file.
    	* sysdeps/unix/sysv/linux/syscalls.list (open_by_handle_at): New
    	file.
    	* misc/Makefile (CFLAGS-open_by_handle_at.c): New rule.

---
+++ sysdeps/unix/sysv/linux/open_by_handle_at.c
@@ -0,0 +1,37 @@
+/* Obtain handle for an open file via a handle.  Linux implementation.
+   Copyright (C) 2017 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sysdep-cancel.h>
+
+int
+open_by_handle_at (int mount_fd, struct file_handle *handle, int flags)
+{
+#ifdef __NR_open_by_handle_at
+  return SYSCALL_CANCEL (open_by_handle_at, mount_fd, handle, flags);
+#else
+  __set_errno (ENOSYS);
+  return -1;
+#endif
+}
+
+#ifndef __NR_open_by_handle_at
+stub_warning (open_by_handle_at)
+#endif
