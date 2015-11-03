3d0ad5a8dfb7b8dd862108970a62c2c3ce9ddd72
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2015-11-03 16:03:56 -0200

    posix: Consolidate Linux nanosleep syscall
    This patch consolidates the nanosleep Linux syscall generation on
    sysdeps/unix/sysv/linux/nanosleep.c.  It basically removes it from
    architectures auto-generation list.
    
    Checked on i686-linux-gnu, x86_64-linux-gnu, x86_64-linux-gnux32,
    arch64-linux-gnu, arm-linux-gnueabihf, powerpc64le-linux-gnu,
    sparc64-linux-gnu, and sparcv9-linux-gnu.
    
    	* nptl/Makefile (CFLAGS-nanosleep.c): New rule.
    	* posix/Makefile (CFLAGS-nanosleep.c): Likewise.
    	* sysdeps/unix/sysv/linux/nanosleep.c: New file.
    	* sysdeps/unix/sysv/linux/syscalls.list: Remove nanosleep from
    	auto-generated list.

---
+++ sysdeps/unix/sysv/linux/nanosleep.c
@@ -0,0 +1,30 @@
+/* Linux high resolution nanosleep implementation.
+   Copyright (C) 2017 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <time.h>
+#include <sysdep-cancel.h>
+
+/* Pause execution for a number of nanoseconds.  */
+int
+__nanosleep (const struct timespec *requested_time,
+	     struct timespec *remaining)
+{
+  return SYSCALL_CANCEL (nanosleep, requested_time, remaining);
+}
+libc_hidden_def (__nanosleep)
+weak_alias (__nanosleep, nanosleep)
