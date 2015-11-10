2fee269248c6ef303569d9ac8fec3a27676520e0
Wilco Dijkstra <wdijkstr@arm.com>
2015-11-10 11:13:23 +0000

    Enable _STRING_ARCH_unaligned on AArch64.
            * sysdeps/aarch64/bits/string.h: New file.
            (_STRING_ARCH_unaligned): Define.

---
+++ sysdeps/aarch64/bits/string.h
@@ -0,0 +1,24 @@
+/* Optimized, inlined string functions.  AArch64 version.
+   Copyright (C) 2015 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#ifndef _STRING_H
+# error "Never use <bits/string.h> directly; include <string.h> instead."
+#endif
+
+/* AArch64 implementations support efficient unaligned access.  */
+#define _STRING_ARCH_unaligned 1
