71d1b0166b4ace0d804af2993b3815758b852efc
Joseph Myers <joseph@codesourcery.com>
2015-11-11 00:06:09 +0000

    Fix powerpc nearbyint wrongly clearing "inexact" and leaving traps disabled (bug 19228).
    Similar to bug 15491 recently fixed for x86_64 / x86, the powerpc
    (both powerpc32 and powerpc64) hard-float implementations of
    nearbyintf and nearbyint wrongly clear an "inexact" exception that was
    raised before the function was called; this shows up as failure of the
    test math/test-nearbyint-except added when that bug was fixed.  They
    also wrongly leave traps on "inexact" disabled if they were enabled
    before the function was called.
    
    This patch fixes the bugs similar to how the x86 bug was fixed: saving
    and restoring the whole floating-point state, both to restore the
    original "inexact" flag state and to restore the original state of
    whether traps on "inexact" were enabled.  Because there's a convenient
    point in the powerpc implementations to save state after any sNaN
    arguments will have raised "invalid" but before "inexact" traps need
    to be disabled, no special handling for "invalid" is needed as in the
    x86 version.
    
    Tested for powerpc64 and powerpc32, where it fixes the
    math/test-nearbyint-except failure as well as fixing the new test
    math/test-nearbyint-except-2 added by this patch.  Also tested for
    x86_64 and x86 that the new test passes.
    
    If powerpc experts see a more efficient way of doing this
    (e.g. instruction positioning that's better for pipelines on typical
    processors) then of course followups optimizing the fix are welcome.
    
    	[BZ #19228]
    	* sysdeps/powerpc/powerpc32/fpu/s_nearbyint.S (__nearbyint): Save
    	and restore full floating-point state.
    	* sysdeps/powerpc/powerpc32/fpu/s_nearbyintf.S (__nearbyintf):
    	Likewise.
    	* sysdeps/powerpc/powerpc64/fpu/s_nearbyint.S (__nearbyint):
    	Likewise.
    	* sysdeps/powerpc/powerpc64/fpu/s_nearbyintf.S (__nearbyintf):
    	Likewise.
    	* math/test-nearbyint-except-2.c: New file.
    	* math/Makefile (tests): Add test-nearbyint-except-2.

---
+++ math/test-nearbyint-except-2.c
@@ -0,0 +1,72 @@
+/* Test nearbyint functions do not disable exception traps (bug 19228).
+   Copyright (C) 2015 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <fenv.h>
+#include <math.h>
+#include <stdio.h>
+
+#ifndef FE_INEXACT
+# define FE_INEXACT 0
+#endif
+
+#define TEST_FUNC(NAME, FLOAT, SUFFIX)					\
+static int								\
+NAME (void)								\
+{									\
+  int result = 0;							\
+  volatile FLOAT a, b __attribute__ ((unused));				\
+  a = 1.5;								\
+  /* nearbyint must work when traps on "inexact" are enabled.  */	\
+  b = nearbyint ## SUFFIX (a);						\
+  /* And it must have left those traps enabled.  */			\
+  if (fegetexcept () == FE_INEXACT)					\
+    puts ("PASS: " #FLOAT);						\
+  else									\
+    {									\
+      puts ("FAIL: " #FLOAT);						\
+      result = 1;							\
+    }									\
+  return result;							\
+}
+
+TEST_FUNC (float_test, float, f)
+TEST_FUNC (double_test, double, )
+#ifndef NO_LONG_DOUBLE
+TEST_FUNC (ldouble_test, long double, l)
+#endif
+
+static int
+do_test (void)
+{
+  if (feenableexcept (FE_INEXACT) == -1)
+    {
+      puts ("enabling FE_INEXACT traps failed, cannot test");
+      return 77;
+    }
+  int result = float_test ();
+  feenableexcept (FE_INEXACT);
+  result |= double_test ();
+#ifndef NO_LONG_DOUBLE
+  feenableexcept (FE_INEXACT);
+  result |= ldouble_test ();
+#endif
+  return result;
+}
+
+#define TEST_FUNCTION do_test ()
+#include "../test-skeleton.c"
