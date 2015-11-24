6a4448afe3806fcf607ab9920d45072eafb2e61f
Chris Metcalf <cmetcalf@ezchip.com>
2015-11-24 10:09:16 -0500

    tile: define __NO_LONG_DOUBLE_MATH
    This avoids build failures in the tests, and matches what is in
    bits/mathdef.h.
    
    Update the libc and libm abilist files to include __finitel,
    __isinfl, and __isnanl.

--- sysdeps/tile/bits/mathdef.h
+++ sysdeps/tile/bits/mathdef.h
@@ -46,3 +46,9 @@ typedef double double_t;
 # endif
 
 #endif /* ISO C99 */
+
+#ifndef __NO_LONG_DOUBLE_MATH
+/* Signal that we do not really have a `long double'.  This disables the
+   declaration of all the `long double' function variants.  */
+# define __NO_LONG_DOUBLE_MATH	1
+#endif
