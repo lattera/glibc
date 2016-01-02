lattera/glibc

commit d709042a6e5ab3c360280faad6f9538a34dc8eea

Fix lgamma setting signgam for ISO C (bug 15421).
The lgamma (and likewise lgammaf, lgammal) function wrongly sets the
signgam variable even when building for strict ISO C conformance
(-std=c99 / -std=c11), although the user may define such a variable
and it's only in the implementation namespace for POSIX with XSI
extensions enabled.

Following discussions starting at
<https://sourceware.org/ml/libc-alpha/2013-04/msg00767.html> and
<https://sourceware.org/ml/libc-alpha/2015-10/msg00844.html>, it seems
that the safest approach for fixing this particular issue is for
signgam to become a weak alias for a newly exported symbol __signgam,
with the library functions only setting __signgam, at which point
static linker magic will preserve the alias for newly linked binaries
that refer to the library's signgam rather than defining their own,
while breaking the alias for programs that define their own signgam,
with new symbol versions for lgamma functions and with compat symbols
for existing binaries that set both signgam and __signgam.

This patch implements that approach for the fix.  signgam is made into
a weak alias.  The four symbols __signgam, lgamma, lgammaf, lgammal
get new symbol versions at version GLIBC_2.23, with the existing
versions of lgamma, lgammaf and lgammal becoming compat symbols.

When the compat versions are built, gamma, gammaf and gammal are
aliases for the compat versions (i.e. always set signgam); this is OK
as they are not ISO C functions, and avoids adding new symbol versions
for them unnecessarily.  When the compat versions are not built
(i.e. for static linking and for future glibc ports), gamma, gammaf
and gammal are aliases for the new versions that set __signgam.  The
ldbl-opt versions are updated accordingly.

The lgamma wrappers are adjusted so that the same source files,
included from different files with different definitions of
USE_AS_COMPAT, can build either the new versions or the compat

sysdeps/ia64/fpu/w_lgamma_main.c
@@ -1,4 +1,4 @@
-/* file: lgammaf.c */
+/* file: lgamma.c */
 
 
 // Copyright (c) 2002 Intel Corporation
@@ -43,38 +43,31 @@
 // History
 //==============================================================
 // 02/04/02: Initial version
-// 02/22/02: Removed lgamma/gamma
+// 02/22/02: Removed lgammaf/gammaf
 //
 /*
-//   FUNCTIONS:    float   lgammaf(float x)
-//                 float   gammaf(float x)
+//   FUNCTIONS:    double  lgamma(double x)
+//                 double  gamma(double x)
 //                 Natural logarithm of GAMMA function
 */
 
 #include "libm_support.h"
 
+#include <lgamma-compat.h>
 
-extern float  __libm_lgammaf(float /*x*/, int* /*signgam*/, int /*signgamsz*/);
+extern double __libm_lgamma(double /*x*/, int* /*signgam*/, int /*signgamsz*/);
 
 
-float __ieee754_lgammaf(float x)
+double LGFUNC (lgamma) (double x)
 {
-#ifdef __POSIX__
-    extern int    signgam;
-#else
-    int    signgam;
-#endif
-    return __libm_lgammaf(x, &signgam, sizeof(signgam));
+    return CALL_LGAMMA (double, __libm_lgamma, x);
 }
-weak_alias (__ieee754_lgammaf, lgammaf)
-
-float __ieee754_gammaf(float x)
-{
-#ifdef __POSIX__
-    extern int    signgam;
+#if USE_AS_COMPAT
+compat_symbol (libm, __lgammaf_compat, lgammaf, LGAMMA_OLD_VER);
 #else
-    int    signgam;
+versioned_symbol (libm, __ieee754_lgammaf, lgammaf, LGAMMA_NEW_VER);
 #endif
-    return __libm_lgammaf(x, &signgam, sizeof(signgam));
-}
+#if GAMMA_ALIAS
+strong_alias (LGFUNC (lgammaf), __ieee754_gammaf)
 weak_alias (__ieee754_gammaf, gammaf)
+#endif

sysdeps/ia64/fpu/w_lgammaf_main.c
@@ -1,4 +1,4 @@
-/* file: lgammal.c */
+/* file: lgammaf.c */
 
 
 // Copyright (c) 2002 Intel Corporation
@@ -42,38 +42,32 @@
 
 // History
 //==============================================================
-// 08/15/02: Initial version
+// 02/04/02: Initial version
+// 02/22/02: Removed lgamma/gamma
 //
 /*
-//   FUNCTIONS:    long double  lgammal(long double x)
-//                 long double  gammal(long double x)
+//   FUNCTIONS:    float   lgammaf(float x)
+//                 float   gammaf(float x)
 //                 Natural logarithm of GAMMA function
 */
 
 #include "libm_support.h"
 
+#include <lgamma-compat.h>
 
-extern double __libm_lgammal(long double /*x*/, int* /*signgam*/, int /*signgamsz*/);
+extern float  __libm_lgammaf(float /*x*/, int* /*signgam*/, int /*signgamsz*/);
 
 
-long double __ieee754_lgammal(long double x)
+float LGFUNC (lgammaf) (float x)
 {
-#ifdef __POSIX__
-    extern int    signgam;
-#else
-    int    signgam;
-#endif
-    return __libm_lgammal(x, &signgam, sizeof(signgam));
+    return CALL_LGAMMA (float, __libm_lgammaf, x);
 }
-weak_alias (__ieee754_lgammal, lgammal)
-
-long double __ieee754_gammal(long double x)
-{
-#ifdef __POSIX__
-    extern int    signgam;
+#if USE_AS_COMPAT
+compat_symbol (libm, __lgammaf_compat, lgammaf, LGAMMA_OLD_VER);
 #else
-    int    signgam;
+versioned_symbol (libm, __ieee754_lgammaf, lgammaf, LGAMMA_NEW_VER);
 #endif
-    return __libm_lgammal(x, &signgam, sizeof(signgam));
-}
-weak_alias (__ieee754_gammal, gammal)
+#if GAMMA_ALIAS
+strong_alias (LGFUNC (lgammaf), __ieee754_gammaf)
+weak_alias (__ieee754_gammaf, gammaf)
+#endif

sysdeps/ia64/fpu/w_lgammal_main.c
@@ -1,4 +1,4 @@
-/* file: lgamma.c */
+/* file: lgammal.c */
 
 
 // Copyright (c) 2002 Intel Corporation
@@ -42,39 +42,31 @@
 
 // History
 //==============================================================
-// 02/04/02: Initial version
-// 02/22/02: Removed lgammaf/gammaf
+// 08/15/02: Initial version
 //
 /*
-//   FUNCTIONS:    double  lgamma(double x)
-//                 double  gamma(double x)
+//   FUNCTIONS:    long double  lgammal(long double x)
+//                 long double  gammal(long double x)
 //                 Natural logarithm of GAMMA function
 */
 
 #include "libm_support.h"
 
 
-extern double __libm_lgamma(double /*x*/, int* /*signgam*/, int /*signgamsz*/);
+extern double __libm_lgammal(long double /*x*/, int* /*signgam*/, int /*signgamsz*/);
 
+#include <lgamma-compat.h>
 
-double __ieee754_lgamma(double x)
+long double LGFUNC (lgammal) (long double x)
 {
-#ifdef __POSIX__
-    extern int    signgam;
-#else
-    int    signgam;
-#endif
-    return __libm_lgamma(x, &signgam, sizeof(signgam));
+    return CALL_LGAMMA (long double, __libm_lgammal, x);
 }
-weak_alias (__ieee754_lgamma, lgamma)
-
-double __ieee754_gamma(double x)
-{
-#ifdef __POSIX__
-    extern int    signgam;
+#if USE_AS_COMPAT
+compat_symbol (libm, __lgammal_compat, lgammal, LGAMMA_OLD_VER);
 #else
-    int    signgam;
+versioned_symbol (libm, __ieee754_lgammal, lgammal, LGAMMA_NEW_VER);
 #endif
-    return __libm_lgamma(x, &signgam, sizeof(signgam));
-}
-weak_alias (__ieee754_gamma, gamma)
+#if GAMMA_ALIAS
+strong_alias (LGFUNC (lgammal), __ieee754_gammal)
+weak_alias (__ieee754_gammal, gammal)
+#endif

math/w_lgamma_main.c
@@ -1,0 +1,59 @@
+/* @(#)w_lgamma.c 5.1 93/09/24 */
+/*
+ * ====================================================
+ * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
+ *
+ * Developed at SunPro, a Sun Microsystems, Inc. business.
+ * Permission to use, copy, modify, and distribute this
+ * software is freely granted, provided that this notice
+ * is preserved.
+ * ====================================================
+ */
+
+/* double lgamma(double x)
+ * Return the logarithm of the Gamma function of x.
+ *
+ * Method: call __ieee754_lgamma_r
+ */
+
+#include <math.h>
+#include <math_private.h>
+
+#include <lgamma-compat.h>
+
+#if BUILD_LGAMMA
+double
+LGFUNC (__lgamma) (double x)
+{
+	double y = CALL_LGAMMA (double, __ieee754_lgamma_r, x);
+	if(__builtin_expect(!isfinite(y), 0)
+	   && isfinite(x) && _LIB_VERSION != _IEEE_)
+		return __kernel_standard(x, x,
+					 __floor(x)==x&&x<=0.0
+					 ? 15 /* lgamma pole */
+					 : 14); /* lgamma overflow */
+
+	return y;
+}
+# if USE_AS_COMPAT
+compat_symbol (libm, __lgamma_compat, lgamma, LGAMMA_OLD_VER);
+#  ifdef NO_LONG_DOUBLE
+strong_alias (__lgamma_compat, __lgammal_compat)
+compat_symbol (libm, __lgammal_compat, lgammal, LGAMMA_OLD_VER);
+#  endif
+# else
+versioned_symbol (libm, __lgamma, lgamma, LGAMMA_NEW_VER);
+#  ifdef NO_LONG_DOUBLE
+strong_alias (__lgamma, __lgammal)
+versioned_symbol (libm, __lgammal, lgammal, LGAMMA_NEW_VER);
+#  endif
+# endif
+# if GAMMA_ALIAS
+strong_alias (LGFUNC (__lgamma), __gamma)
+weak_alias (__gamma, gamma)
+#  ifdef NO_LONG_DOUBLE
+strong_alias (__gamma, __gammal)
+weak_alias (__gamma, gammal)
+#  endif
+# endif
+#endif

math/w_lgammaf_main.c
@@ -1,0 +1,44 @@
+/* w_lgammaf.c -- float version of w_lgamma.c.
+ * Conversion to float by Ian Lance Taylor, Cygnus Support, ian@cygnus.com.
+ */
+
+/*
+ * ====================================================
+ * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
+ *
+ * Developed at SunPro, a Sun Microsystems, Inc. business.
+ * Permission to use, copy, modify, and distribute this
+ * software is freely granted, provided that this notice
+ * is preserved.
+ * ====================================================
+ */
+
+#include <math.h>
+#include <math_private.h>
+
+#include <lgamma-compat.h>
+
+#if BUILD_LGAMMA
+float
+LGFUNC (__lgammaf) (float x)
+{
+	float y = CALL_LGAMMA (float, __ieee754_lgammaf_r, x);
+	if(__builtin_expect(!isfinite(y), 0)
+	   && isfinite(x) && _LIB_VERSION != _IEEE_)
+		return __kernel_standard_f(x, x,
+					   __floorf(x)==x&&x<=0.0f
+					   ? 115 /* lgamma pole */
+					   : 114); /* lgamma overflow */
+
+	return y;
+}
+# if USE_AS_COMPAT
+compat_symbol (libm, __lgammaf_compat, lgammaf, LGAMMA_OLD_VER);
+# else
+versioned_symbol (libm, __lgammaf, lgammaf, LGAMMA_NEW_VER);
+# endif
+# if GAMMA_ALIAS
+strong_alias (LGFUNC (__lgammaf), __gammaf)
+weak_alias (__gammaf, gammaf)
+# endif
+#endif

math/w_lgammal_main.c
@@ -1,0 +1,51 @@
+/* w_lgammal.c -- long double version of w_lgamma.c.
+ * Conversion to long double by Ulrich Drepper,
+ * Cygnus Support, drepper@cygnus.com.
+ */
+
+/*
+ * ====================================================
+ * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
+ *
+ * Developed at SunPro, a Sun Microsystems, Inc. business.
+ * Permission to use, copy, modify, and distribute this
+ * software is freely granted, provided that this notice
+ * is preserved.
+ * ====================================================
+ */
+
+/* long double lgammal(long double x)
+ * Return the logarithm of the Gamma function of x.
+ *
+ * Method: call __ieee754_lgammal_r
+ */
+
+#include <math.h>
+#include <math_private.h>
+
+#include <lgamma-compat.h>
+
+#if BUILD_LGAMMA
+long double
+LGFUNC (__lgammal) (long double x)
+{
+	long double y = CALL_LGAMMA (long double, __ieee754_lgammal_r, x);
+	if(__builtin_expect(!isfinite(y), 0)
+	   && isfinite(x) && _LIB_VERSION != _IEEE_)
+		return __kernel_standard_l(x, x,
+					   __floorl(x)==x&&x<=0.0L
+					   ? 215 /* lgamma pole */
+					   : 214); /* lgamma overflow */
+
+	return y;
+}
+# if USE_AS_COMPAT
+compat_symbol (libm, __lgammal_compat, lgammal, LGAMMA_OLD_VER);
+# else
+versioned_symbol (libm, __lgammal, lgammal, LGAMMA_NEW_VER);
+# endif
+# if GAMMA_ALIAS
+strong_alias (LGFUNC (__lgammal), __gammal)
+weak_alias (__gammal, gammal)
+# endif
+#endif

commit 19e075101493016130f8b9321d14084478bd9827

ia64: fpu: fix gamma definition handling [BZ #15421]
The rework in commit d709042a6e5ab3c360280faad6f9538a34dc8eea broke
buiding on ia64 due to compat_symbol expanding into ... in some cases.
The common files were wrapped in a BUILD_LGAMMA check, but the ia64
ones were not.  Add that logic to the ia64 files too.

sysdeps/ia64/fpu/w_lgamma_main.c
@@ -53,21 +53,25 @@
 
 #include "libm_support.h"
 
+#include <math.h>
+#include <math_private.h>
+
 #include <lgamma-compat.h>
 
 extern double __libm_lgamma(double /*x*/, int* /*signgam*/, int /*signgamsz*/);
 
-
+#if BUILD_LGAMMA
 double LGFUNC (lgamma) (double x)
 {
     return CALL_LGAMMA (double, __libm_lgamma, x);
 }
-#if USE_AS_COMPAT
+# if USE_AS_COMPAT
 compat_symbol (libm, __lgamma_compat, lgamma, LGAMMA_OLD_VER);
-#else
+# else
 versioned_symbol (libm, __ieee754_lgamma, lgamma, LGAMMA_NEW_VER);
-#endif
-#if GAMMA_ALIAS
+# endif
+# if GAMMA_ALIAS
 strong_alias (LGFUNC (lgamma), __ieee754_gamma)
 weak_alias (__ieee754_gamma, gamma)
+# endif
 #endif

sysdeps/ia64/fpu/w_lgammaf_main.c
@@ -53,21 +53,25 @@
 
 #include "libm_support.h"
 
+#include <math.h>
+#include <math_private.h>
+
 #include <lgamma-compat.h>
 
 extern float  __libm_lgammaf(float /*x*/, int* /*signgam*/, int /*signgamsz*/);
 
-
+#if BUILD_LGAMMA
 float LGFUNC (lgammaf) (float x)
 {
     return CALL_LGAMMA (float, __libm_lgammaf, x);
 }
-#if USE_AS_COMPAT
+# if USE_AS_COMPAT
 compat_symbol (libm, __lgammaf_compat, lgammaf, LGAMMA_OLD_VER);
-#else
+# else
 versioned_symbol (libm, __ieee754_lgammaf, lgammaf, LGAMMA_NEW_VER);
-#endif
-#if GAMMA_ALIAS
+# endif
+# if GAMMA_ALIAS
 strong_alias (LGFUNC (lgammaf), __ieee754_gammaf)
 weak_alias (__ieee754_gammaf, gammaf)
+# endif
 #endif

sysdeps/ia64/fpu/w_lgammal_main.c
@@ -52,21 +52,25 @@
 
 #include "libm_support.h"
 
-
-extern double __libm_lgammal(long double /*x*/, int* /*signgam*/, int /*signgamsz*/);
+#include <math.h>
+#include <math_private.h>
 
 #include <lgamma-compat.h>
 
+extern double __libm_lgammal(long double /*x*/, int* /*signgam*/, int /*signgamsz*/);
+
+#if BUILD_LGAMMA
 long double LGFUNC (lgammal) (long double x)
 {
     return CALL_LGAMMA (long double, __libm_lgammal, x);
 }
-#if USE_AS_COMPAT
+# if USE_AS_COMPAT
 compat_symbol (libm, __lgammal_compat, lgammal, LGAMMA_OLD_VER);
-#else
+# else
 versioned_symbol (libm, __ieee754_lgammal, lgammal, LGAMMA_NEW_VER);
-#endif
-#if GAMMA_ALIAS
+# endif
+# if GAMMA_ALIAS
 strong_alias (LGFUNC (lgammal), __ieee754_gammal)
 weak_alias (__ieee754_gammal, gammal)
+# endif
 #endif