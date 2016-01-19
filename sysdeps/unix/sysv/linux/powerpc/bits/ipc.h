9200e581e500b6c8ad52733f9c0ac3efc8d4b4b9
Paul E. Murphy <murphyp@linux.vnet.ibm.com>
2016-01-19 11:05:41 -0600

    Cleanup ppc bits/ipc.h
    Ages ago (commit e9dcb08) the ipc syscalls were inlined and
    eventually abstracted away any need for direct __ipc calls.

--- sysdeps/unix/sysv/linux/powerpc/bits/ipc.h
+++ sysdeps/unix/sysv/linux/powerpc/bits/ipc.h
@@ -52,27 +52,3 @@ struct ipc_perm
     __uint64_t __glibc_reserved1;
     __uint64_t __glibc_reserved2;
   };
-
-
-__BEGIN_DECLS
-
-/* The actual system call: all functions are multiplexed by this.  */
-extern int __ipc (int __call, int __first, int __second, int __third,
-		  void *__ptr) __THROW;
-
-__END_DECLS
-
-#ifdef __USE_GNU
-/* The codes for the functions to use the multiplexer `__ipc'.  */
-# define IPCOP_semop	 1
-# define IPCOP_semget	 2
-# define IPCOP_semctl	 3
-# define IPCOP_msgsnd	11
-# define IPCOP_msgrcv	12
-# define IPCOP_msgget	13
-# define IPCOP_msgctl	14
-# define IPCOP_shmat	21
-# define IPCOP_shmdt	22
-# define IPCOP_shmget	23
-# define IPCOP_shmctl	24
-#endif
