aea44bf61aa3f27acbdb7648b6f5dc8882a8987b
H.J. Lu <hjl.tools@gmail.com>
2016-04-01 09:58:59 -0700

    Test 64-byte alignment in memmove benchtest
    Add 64-byte alignment tests in memmove benchtest for 64-byte vector
    registers.
    
    	* benchtests/bench-memmove.c (test_main): Test 64-byte
    	alignment.

--- benchtests/bench-memmove.c
+++ benchtests/bench-memmove.c
@@ -176,6 +176,14 @@ test_main (void)
       do_test (i, 0, 16 * i);
     }
 
+  for (i = 32; i < 64; ++i)
+    {
+      do_test (0, 0, 32 * i);
+      do_test (i, 0, 32 * i);
+      do_test (0, i, 32 * i);
+      do_test (i, i, 32 * i);
+    }
+
   return ret;
 }
 
