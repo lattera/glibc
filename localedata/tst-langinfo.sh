4964bb4591b16b485eac3c5ecc8a383b528ea1a6
Mike Frysinger <vapier@gentoo.org>
2016-04-23 14:14:55 -0400

    tst-langinfo: update yesexpr/noexpr baselines

--- localedata/tst-langinfo.sh
+++ localedata/tst-langinfo.sh
@@ -121,8 +121,8 @@ en_US.ANSI_X3.4-1968 T_FMT       "%r"
 en_US.ANSI_X3.4-1968 T_FMT_AMPM  "%I:%M:%S %p"
 en_US.ANSI_X3.4-1968 RADIXCHAR   .
 en_US.ANSI_X3.4-1968 THOUSEP     ,
-en_US.ANSI_X3.4-1968 YESEXPR     ^[yY].*
-en_US.ANSI_X3.4-1968 NOEXPR      ^[nN].*
+en_US.ANSI_X3.4-1968 YESEXPR     ^[+1yY]
+en_US.ANSI_X3.4-1968 NOEXPR      ^[-0nN]
 en_US.ISO-8859-1     ABMON_1     Jan
 en_US.ISO-8859-1     ABMON_2     Feb
 en_US.ISO-8859-1     ABMON_3     Mar
@@ -155,8 +155,8 @@ en_US.ISO-8859-1     T_FMT       "%r"
 en_US.ISO-8859-1     T_FMT_AMPM  "%I:%M:%S %p"
 en_US.ISO-8859-1     RADIXCHAR   .
 en_US.ISO-8859-1     THOUSEP     ,
-en_US.ISO-8859-1     YESEXPR     ^[yY].*
-en_US.ISO-8859-1     NOEXPR      ^[nN].*
+en_US.ISO-8859-1     YESEXPR     ^[+1yY]
+en_US.ISO-8859-1     NOEXPR      ^[-0nN]
 de_DE.ISO-8859-1     ABDAY_1     So
 de_DE.ISO-8859-1     ABDAY_2     Mo
 de_DE.ISO-8859-1     ABDAY_3     Di
@@ -200,8 +200,8 @@ de_DE.ISO-8859-1     D_FMT       "%d.%m.%Y"
 de_DE.ISO-8859-1     T_FMT       "%T"
 de_DE.ISO-8859-1     RADIXCHAR   ,
 de_DE.ISO-8859-1     THOUSEP     .
-de_DE.ISO-8859-1     YESEXPR     ^[jJyY].*
-de_DE.ISO-8859-1     NOEXPR      ^[nN].*
+de_DE.ISO-8859-1     YESEXPR     ^[+1jJyY]
+de_DE.ISO-8859-1     NOEXPR      ^[-0nN]
 de_DE.UTF-8          ABDAY_1     So
 de_DE.UTF-8          ABDAY_2     Mo
 de_DE.UTF-8          ABDAY_3     Di
@@ -245,8 +245,8 @@ de_DE.UTF-8          D_FMT       "%d.%m.%Y"
 de_DE.UTF-8          T_FMT       "%T"
 de_DE.UTF-8          RADIXCHAR   ,
 de_DE.UTF-8          THOUSEP     .
-de_DE.UTF-8          YESEXPR     ^[jJyY].*
-de_DE.UTF-8          NOEXPR      ^[nN].*
+de_DE.UTF-8          YESEXPR     ^[+1jJyY]
+de_DE.UTF-8          NOEXPR      ^[-0nN]
 fr_FR.ISO-8859-1     ABDAY_1     dim.
 fr_FR.ISO-8859-1     ABDAY_2     lun.
 fr_FR.ISO-8859-1     ABDAY_3     mar.
@@ -290,8 +290,8 @@ fr_FR.ISO-8859-1     D_FMT       "%d/%m/%Y"
 fr_FR.ISO-8859-1     T_FMT       "%T"
 fr_FR.ISO-8859-1     RADIXCHAR   ,
 fr_FR.ISO-8859-1     THOUSEP     " "
-fr_FR.ISO-8859-1     YESEXPR     ^[oOyY].*
-fr_FR.ISO-8859-1     NOEXPR      ^[nN].*
+fr_FR.ISO-8859-1     YESEXPR     ^[+1oOyY]
+fr_FR.ISO-8859-1     NOEXPR      ^[-0nN]
 ja_JP.EUC-JP         ABDAY_1     日
 ja_JP.EUC-JP         ABDAY_2     月
 ja_JP.EUC-JP         ABDAY_3     火
@@ -335,8 +335,8 @@ ja_JP.EUC-JP         ERA_D_FMT   "%EY%m月%d日"
 ja_JP.EUC-JP         ERA_D_T_FMT "%EY%m月%d日 %H時%M分%S秒"
 ja_JP.EUC-JP         RADIXCHAR   .
 ja_JP.EUC-JP         THOUSEP     ,
-ja_JP.EUC-JP         YESEXPR     ^([yYｙＹ]|はい|ハイ)
-ja_JP.EUC-JP         NOEXPR      ^([nNｎＮ]|いいえ|イイエ)
+ja_JP.EUC-JP         YESEXPR     ^([+1yYｙＹ]|はい|ハイ)
+ja_JP.EUC-JP         NOEXPR      ^([-0nNｎＮ]|いいえ|イイエ)
 # Is CRNCYSTR supposed to be the national or international sign?
 # ja_JP.EUC-JP         CRNCYSTR    JPY
 ja_JP.EUC-JP         CODESET     EUC-JP
