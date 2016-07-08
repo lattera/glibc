75a9e6a78844a27990287be6c73a140ae6d17b69
Chris Metcalf <cmetcalf@mellanox.com>
2016-07-08 10:30:59 -0400

    tile: only define __ASSUME_ALIGNED_REGISTER_PAIRS for 32-bit
    The previous uses of this symbol were all in wordsize-32 code.
    In commit eeddfa91cbb1 ("Consolidate off_t/off64_t syscall
    argument passing") it was expanded to be used in pread/pwrite.
    Accordingly, we only define it in 32-bit compilation modes now.
    Both tilepro and tilegx32 follow this convention for the
    kernel ABI.  tilegx64 follows it for passing 128-bit values,
    but there are no such ABIs in the kernel.

--- sysdeps/unix/sysv/linux/tile/kernel-features.h
+++ sysdeps/unix/sysv/linux/tile/kernel-features.h
@@ -21,4 +21,6 @@
 
 /* Define this if your 32-bit syscall API requires 64-bit register
    pairs to start with an even-number register.  */
-#define __ASSUME_ALIGNED_REGISTER_PAIRS	1
+#ifndef _LP64
+# define __ASSUME_ALIGNED_REGISTER_PAIRS	1
+#endif
