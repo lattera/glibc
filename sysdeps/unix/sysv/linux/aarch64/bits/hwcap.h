26c2910ac6889dd21f128d9071418492d544a2dc
Szabolcs Nagy <szabolcs.nagy@arm.com>
2016-07-11 09:57:02 +0100

    [AArch64] Add bits/hwcap.h for aarch64 linux
    AArch64 uses HWCAP bits but they are not defined in sys/auxv.h.
    This patch adds a copy of the linux v4.6 arm64 uapi asm/hwcap.h
    definitions.
    
    	* sysdeps/unix/sysv/linux/aarch64/bits/hwcap.h: New.

---
+++ sysdeps/unix/sysv/linux/aarch64/bits/hwcap.h
@@ -0,0 +1,34 @@
+/* Defines for bits in AT_HWCAP.  AArch64 Linux version.
+   Copyright (C) 2016 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#if !defined (_SYS_AUXV_H)
+# error "Never include <bits/hwcap.h> directly; use <sys/auxv.h> instead."
+#endif
+
+/* The following must match the kernel's <asm/hwcap.h>.  */
+#define HWCAP_FP		(1 << 0)
+#define HWCAP_ASIMD		(1 << 1)
+#define HWCAP_EVTSTRM		(1 << 2)
+#define HWCAP_AES		(1 << 3)
+#define HWCAP_PMULL		(1 << 4)
+#define HWCAP_SHA1		(1 << 5)
+#define HWCAP_SHA2		(1 << 6)
+#define HWCAP_CRC32		(1 << 7)
+#define HWCAP_ATOMICS		(1 << 8)
+#define HWCAP_FPHP		(1 << 9)
+#define HWCAP_ASIMDHP		(1 << 10)
