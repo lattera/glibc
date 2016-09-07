f2227063111582d025778d61263a5662d075714c
Joseph Myers <joseph@codesourcery.com>
2016-09-07 17:47:11 +0000

    Add e500 version of fetestexceptflag.
    When I added fetestexceptflag, I missed that e500 was another case
    that needed its own version because saved exceptions were not directly
    stored in a form that could be ANDed with exception bits (they were
    stored with exceptions in SPE form, but the FE_* macros always use the
    classic hard-float form).  This patch adds an e500 version with the
    required call to __fexcepts_from_spe to convert from one form to the
    other.
    
    Tested for e500.
    
    	* sysdeps/powerpc/powerpc32/e500/nofpu/fetestexceptflag.c: New
    	file.

---
+++ sysdeps/powerpc/powerpc32/e500/nofpu/fetestexceptflag.c
@@ -0,0 +1,25 @@
+/* Test exception in saved exception state.  e500 version.
+   Copyright (C) 2016 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <fenv_libc.h>
+
+int
+fetestexceptflag (const fexcept_t *flagp, int excepts)
+{
+  return __fexcepts_from_spe (*flagp) & excepts & FE_ALL_EXCEPT;
+}
