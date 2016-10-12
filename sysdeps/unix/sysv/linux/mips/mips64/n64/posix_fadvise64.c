716458049510215451d25ac131858b6bdf30f99f
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2016-10-12 13:06:22 -0300

    Fix posix_fadvise64 build on mips64n64
    Recent binutils versions (at least 2.27) complains about libc.so
    when linking sotruss-lib.so with:
    
      libc.so:(*IND*+0x0): multiple definition of `posix_fadvise64@GLIBC_2.2'
      libc.so::(.text+0xcf940): first defined here
      libc.so:(*IND*+0x0): multiple definition of `posix_fadvise64'
      collect2: error: ld returned 1 exit status
    
    Dynamic symbols for libc.so shows (readelf --dyn-syms):
    
       262: 000000000010b950    28 FUNC    GLOBAL DEFAULT   12 posix_fadvise64@GLIBC_2.2
       417: 000000000010b950    28 FUNC    WEAK   DEFAULT   12 posix_fadvise64@@GLIBC_2.2
      1505: 000000000010b950    28 FUNC    GLOBAL DEFAULT   12 posix_fadvise64@@GLIBC_2.3.3
    
    That is, two separate definitions at version GLIBC_2.2. The issue is
    sysdeps/unix/sysv/linux/posix_fadvise64.c creates posix_fadvise64 weak_alias,
    while sysdeps/unix/sysv/linux/mips/mips64/n64/posix_fadvise64.c then adds
    compat_symbol / versioned_symbol calls.
    
    The patch remove the weak_alias definition on mips64 specific version so
    direct weak_alias is disabled.
    
    Checked on mips64n64 build with binutils 2.27.51.20161012.
    
    	* sysdeps/unix/sysv/linux/mips/mips64/n64/posix_fadvise64.c:
    	Undefine weak_alias.

--- sysdeps/unix/sysv/linux/mips/mips64/n64/posix_fadvise64.c
+++ sysdeps/unix/sysv/linux/mips/mips64/n64/posix_fadvise64.c
@@ -15,11 +15,16 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
+#undef weak_alias
+#define weak_alias(a, b)
 #undef strong_alias
 #define strong_alias(a, b)
 
 #include <sysdeps/unix/sysv/linux/posix_fadvise64.c>
 
+/* Although both posix_fadvise and posix_fadvise64 has the same semantic
+   on mips64, there were were releases with both symbol versions (BZ#14044).
+   So we need to continue export them.  */
 #if SHLIB_COMPAT(libc, GLIBC_2_2, GLIBC_2_3_3)
 _strong_alias (__posix_fadvise64_l64, __posix_fadvise64_l32);
 compat_symbol (libc, __posix_fadvise64_l32, posix_fadvise64, GLIBC_2_2);
