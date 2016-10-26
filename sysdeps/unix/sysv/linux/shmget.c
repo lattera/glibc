50a6b4e0e4eff805b3a2b6df1e8c18d57ac40294
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2016-10-26 18:24:53 -0200

    Use shmget syscall for linux implementation
    this patch add a direct call to shmget syscall if it is supported by
    kernel features.
    
    Checked on x86_64, i686, powerpc64le, aarch64, and armhf.
    
    	* sysdeps/unix/sysv/linux/alpha/syscalls.list (shmget): Remove.
    	* sysdeps/unix/sysv/linux/arm/syscalls.list (shmget): Likewise.
    	* sysdeps/unix/sysv/linux/generic/syscalls.list (shmget): Likewise.
    	* sysdeps/unix/sysv/linux/hppa/syscalls.list (shmget): Likewise.
    	* sysdeps/unix/sysv/linux/ia64/syscalls.list (shmget): Likewise.
    	* sysdeps/unix/sysv/linux/microblaze/syscalls.list (shmget): Likewise.
    	* sysdeps/unix/sysv/linux/mips/mips64/syscalls.list (shmget):
    	Likewise.
    	* sysdeps/unix/sysv/linux/s390/s390-64/syscalls.list (shmget):
    	Likewise.
    	* sysdeps/unix/sysv/linux/x86_64/syscalls.list (shmget): Likewise.
    	* sysdeps/unix/sysv/linux/shmget.c (shmget): Use shmget syscall if it
    	is defined.

--- sysdeps/unix/sysv/linux/shmget.c
+++ sysdeps/unix/sysv/linux/shmget.c
@@ -16,13 +16,10 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
-#include <errno.h>
-#include <sys/shm.h>
+#include <sys/msg.h>
 #include <ipc_priv.h>
-#include <stdlib.h>		/* for definition of NULL */
-
 #include <sysdep.h>
-#include <sys/syscall.h>
+#include <errno.h>
 
 /* Return an identifier for an shared memory segment of at least size SIZE
    which is associated with KEY.  */
@@ -30,5 +27,9 @@
 int
 shmget (key_t key, size_t size, int shmflg)
 {
-  return INLINE_SYSCALL (ipc, 5, IPCOP_shmget, key, size, shmflg, NULL);
+#ifdef __ASSUME_DIRECT_SYSVIPC_SYSCALLS
+  return INLINE_SYSCALL_CALL (shmget, key, size, shmflg, NULL);
+#else
+  return INLINE_SYSCALL_CALL (ipc, IPCOP_shmget, key, size, shmflg, NULL);
+#endif
 }
