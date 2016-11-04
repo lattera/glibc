6d1774146f8e512de0cf3b05cca7c0d9205b4e5b
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2016-11-04 18:10:42 -0200

    Fix sparc build due missing __WORDSIZE_TIME64_COMPAT32 definition
    This patch adds the missing Linux sparc definitions from d060cd0.
    Both value are copied from default sparc value [1] and with this
    fix now both sparc 32 and 64 bits builds on Linux.
    
    	* sysdeps/unix/sysv/linux/sparc/bits/wordsize.h
    	(__WORDSIZE_TIME64_COMPAT32): Define for both 32 and  64 bits.
    
    [1] sysdeps/sparc/sparc{32,64}/bits/wordsize.h

--- sysdeps/unix/sysv/linux/sparc/bits/wordsize.h
+++ sysdeps/unix/sysv/linux/sparc/bits/wordsize.h
@@ -2,10 +2,12 @@
 
 #if defined __arch64__ || defined __sparcv9
 # define __WORDSIZE	64
+# define __WORDSIZE_TIME64_COMPAT32	1
 #else
 # define __WORDSIZE	32
 # define __WORDSIZE32_SIZE_ULONG	0
 # define __WORDSIZE32_PTRDIFF_LONG	0
+# define __WORDSIZE_TIME64_COMPAT32	0
 #endif
 
 #if !defined __NO_LONG_DOUBLE_MATH && !defined __LONG_DOUBLE_MATH_OPTIONAL
