26f28fd73d20df8847d93f88b6e2a7bd3bd9bf51
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2016-11-18 14:27:03 -0200

    Consolidate Linux select implementation
    This patch consolidates the select Linux syscall implementation on
    sysdeps/unix/sysv/linux/select.c.  The changes are:
    
      1. Remove select from auto-generation syscalls.list on the architecture
         that uses __NR_select.
      2. Remove generic implementation add a default one that handle all
         current cases (with the expection of alpha)
         The new default implementation will either use __NR_select if
         available of fallback to __NR_pselect6 otherwise.
      3. Add a alpha outlier implementation which requires old compatibility
         symbols.
    
    Checked on i686-linux-gnu, x86_64-linux-gnu, x86_64-linux-gnux32,
    arch64-linux-gnu, arm-linux-gnueabihf, and powerpc64le-linux-gnu.
    
    	* sysdeps/unix/sysv/linux/alpha/Makefile (sysdep_routines): Add
    	osf_select.
    	* sysdeps/unix/sysv/linux/alpha/select.c: New file.
    	* sysdeps/unix/sysv/linux/alpha/syscalls.list: Remove select and
    	osf_select from auto-generation list.
    	* sysdeps/unix/sysv/linux/syscalls.list: Likewise.
    	* sysdeps/unix/sysv/linux/sparc/sparc64/syscalls.list: Likewise.
    	* sysdeps/unix/sysv/linux/generic/select.c: Remove file.
    	* sysdeps/unix/sysv/linux/select.c: New file.

--- sysdeps/unix/sysv/linux/select.c
+++ sysdeps/unix/sysv/linux/select.c
@@ -1,6 +1,6 @@
-/* Copyright (C) 2011-2017 Free Software Foundation, Inc.
+/* Linux select implementation.
+   Copyright (C) 2017 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
-   Contributed by Chris Metcalf <cmetcalf@tilera.com>, 2011.
 
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -28,11 +28,19 @@
    after waiting the interval specified therein.  Returns the number of ready
    descriptors, or -1 for errors.  */
 
+#ifdef __NR__newselect
+# undef __NR_select
+# define __NR_select __NR__newselect
+#endif
+
 int
-__select(int nfds, fd_set *readfds,
-         fd_set *writefds, fd_set *exceptfds,
-         struct timeval *timeout)
+__select (int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
+	  struct timeval *timeout)
 {
+#ifdef __NR_select
+  return SYSCALL_CANCEL (select, nfds, readfds, writefds, exceptfds,
+			 timeout);
+#else
   int result;
   struct timespec ts, *tsp = NULL;
 
@@ -55,6 +63,7 @@ __select(int nfds, fd_set *readfds,
     }
 
   return result;
+#endif
 }
 libc_hidden_def (__select)
 
