ea814db27ae450f9ce1b233dfeeea608d485adde
Gabriel F. T. Gomes <gftg@linux.vnet.ibm.com>
2016-12-21 10:20:08 -0200

    Move w_lgamma_r to libm-compat-calls-auto
    This patch adds the suffix "_compat" to lgamma_r wrappers and make
    some adjustments to #includes and Makefiles.  This is a step towards
    deprecation of wrappers that use _LIB_VERSION / matherr /
    __kernel_standard functionality.
    
    Tested for powerpc64le, s390, and x86_64.
    
    	* math/Makefile (libm-calls): Move w_lgammaF_r...
    	(libm-compat-calls-auto): Here.
    
    	* math/w_lgamma_r.c: Add suffix "_compat" to filename.
    	* math/w_lgammaf_r.c: Likewise.
    	* math/w_lgammal_r.c: Likewise.
    	* sysdeps/ia64/fpu/w_lgammal_r.c: Likewise.
    	* sysdeps/ia64/fpu/w_lgammaf_r.c: Likewise.
    	* sysdeps/ia64/fpu/w_lgamma_r.c: Likewise.
    
    	* math/w_lgamma_r_compat.c: New file, copied from above.
    	* math/w_lgammaf_r_compat.c: Likewise.
    	* math/w_lgammal_r_compat.c: Likewise.
    	* sysdeps/ia64/fpu/w_lgamma_r_compat.c: Likewise.
    	* sysdeps/ia64/fpu/w_lgammaf_r_compat.c: Likewise.
    	* sysdeps/ia64/fpu/w_lgammal_r_compat.c: Likewise.
    
    	* sysdeps/ieee754/ldbl-opt/w_lgamma_r.c: Add suffix "_compat"
    	to filename.
    	* sysdeps/ieee754/ldbl-opt/w_lgammal_r.c: Likewise.
    
    	* sysdeps/ieee754/ldbl-opt/w_lgamma_r_compat.c: New file
    	copied from above and adjusted for the new filenames.
    	* sysdeps/ieee754/ldbl-opt/w_lgammal_r_compat.c: Likewise.

--- sysdeps/ieee754/ldbl-opt/w_lgammal_r_compat.c
+++ sysdeps/ieee754/ldbl-opt/w_lgammal_r_compat.c
@@ -1,5 +1,5 @@
 #include <math_ldbl_opt.h>
 #undef weak_alias
 #define weak_alias(n,a)
-#include <math/w_lgammal_r.c>
+#include <math/w_lgammal_r_compat.c>
 long_double_symbol (libm, __lgammal_r, lgammal_r);
