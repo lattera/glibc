811de1a68863f7903341288a113b8c0ac7aa9c0e
Joseph Myers <joseph@codesourcery.com>
2017-01-04 18:12:14 +0000

    Fix MicroBlaze bits/setjmp.h for C++.
    For MicroBlaze, setjmp/check-installed-headers-cxx fails with:
    
    ../setjmp/setjmp.h:34:8: error: '__jmp_buf_tag' has a field '__jmp_buf_tag::__jmpbuf' whose type depends on the type '<unnamed struct>' which has no linkage [-Werror=subobject-linkage]
    
    This patch fixes this in the same way as for some other architectures:
    the struct used for the internal __jmp_buf type is given the tag
    __jmp_buf_internal_tag.
    
    Tested (compilation tests) with build-many-glibcs.py.
    
    	* sysdeps/microblaze/bits/setjmp.h (__jmp_buf): Give struct tag
    	__jmp_buf_internal_tag.

--- sysdeps/microblaze/bits/setjmp.h
+++ sysdeps/microblaze/bits/setjmp.h
@@ -25,7 +25,7 @@
 # error "Never include <bits/setjmp.h> directly; use <setjmp.h> instead."
 #endif
 
-typedef struct
+typedef struct __jmp_buf_internal_tag
   {
     /* There are 21 4-byte registers that should be saved:
        r1, r2, r13-r31. Actually, there seems no need to save
