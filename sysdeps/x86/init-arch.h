9cd30491dd6d9d4c5e9372d7a5c75f78f3a11260
H.J. Lu <hjl.tools@gmail.com>
2017-06-05 08:28:01 -0700

    x86: Add macros to implement ifunce selection in C
    These macros are used to implement ifunc selection in C.  To implement
    an ifunc function, foo, which returns the address of __foo_sse2 or
    __foo_avx2:
    
       __foo_avx2:
    
       #define foo __redirect_foo
       #define __foo __redirect___foo
       #include <foo.h>
       #undef foo
       #undef __foo
       #define SYMBOL_NAME foo
       #include <init-arch.h>
    
       extern __typeof (REDIRECT_NAME) OPTIMIZE (sse2) attribute_hidden;
       extern __typeof (REDIRECT_NAME) OPTIMIZE (avx2) attribute_hidden;
    
       static inline void *
       foo_selector (void)
       {
         if (use AVX2)
          return OPTIMIZE (avx2);
    
         return OPTIMIZE (sse2);
       }
    
       libc_ifunc_redirected (__redirect_foo, foo, foo_selector ());
    
    	* sysdeps/x86/init-arch.h (PASTER1): New.
    	(EVALUATOR1): Likewise.
    	(PASTER2): Likewise.
    	(EVALUATOR2): Likewise.
    	(REDIRECT_NAME): Likewise.
    	(OPTIMIZE): Likewise.
    	(IFUNC_SELECTOR): Likewise.

--- sysdeps/x86/init-arch.h
+++ sysdeps/x86/init-arch.h
@@ -21,6 +21,46 @@
 # include <ldsodefs.h>
 #endif
 
+/* These macros are used to implement ifunc selection in C.  To implement
+   an ifunc function, foo, which returns the address of __foo_sse2 or
+   __foo_avx2:
+
+   #define foo __redirect_foo
+   #define __foo __redirect___foo
+   #include <foo.h>
+   #undef foo
+   #undef __foo
+   #define SYMBOL_NAME foo
+   #include <init-arch.h>
+
+   extern __typeof (REDIRECT_NAME) OPTIMIZE (sse2) attribute_hidden;
+   extern __typeof (REDIRECT_NAME) OPTIMIZE (avx2) attribute_hidden;
+
+   static inline void *
+   foo_selector (void)
+   {
+     if (use AVX2)
+      return OPTIMIZE (avx2);
+
+     return OPTIMIZE (sse2);
+   }
+
+   libc_ifunc_redirected (__redirect_foo, foo, foo_selector ());
+
+*/
+
+#define PASTER1(x,y)	x##_##y
+#define EVALUATOR1(x,y)	PASTER1 (x,y)
+#define PASTER2(x,y)	__##x##_##y
+#define EVALUATOR2(x,y)	PASTER2 (x,y)
+
+/* Basically set '__redirect_<symbol>' to use as type definition,
+   '__<symbol>_<variant>' as the optimized implementation and
+   '<symbol>_ifunc_selector' as the IFUNC selector.  */
+#define REDIRECT_NAME	EVALUATOR1 (__redirect, SYMBOL_NAME)
+#define OPTIMIZE(name)	EVALUATOR2 (SYMBOL_NAME, name)
+#define IFUNC_SELECTOR	EVALUATOR1 (SYMBOL_NAME, ifunc_selector)
+
 #ifndef __x86_64__
 /* Due to the reordering and the other nifty extensions in i686, it is
    not really good to use heavily i586 optimized code on an i686.  It's
