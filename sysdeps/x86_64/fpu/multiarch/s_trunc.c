ae8372d7e4c44f6839aa3d851d4d0cb486b81cd5
Joseph Myers <joseph@codesourcery.com>
2017-09-20 16:54:05 +0000

    Add SSE4.1 trunc, truncf (bug 20142).
    This patch adds SSE4.1 versions of trunc and truncf, using the roundsd
    / roundss instructions, similar to the versions of ceil, floor, rint
    and nearbyint functions we already have.  In my testing with the glibc
    benchtests these are about 30% faster than the C versions for double,
    20% faster for float.
    
    Tested for x86_64.
    
    	[BZ #20142]
    	* sysdeps/x86_64/fpu/multiarch/Makefile (libm-sysdep_routines):
    	Add s_trunc-c, s_truncf-c, s_trunc-sse4_1 and s_truncf-sse4_1.
    	* sysdeps/x86_64/fpu/multiarch/s_trunc-c.c: New file.
    	* sysdeps/x86_64/fpu/multiarch/s_trunc-sse4_1.S: Likewise.
    	* sysdeps/x86_64/fpu/multiarch/s_trunc.c: Likewise.
    	* sysdeps/x86_64/fpu/multiarch/s_truncf-c.c: Likewise.
    	* sysdeps/x86_64/fpu/multiarch/s_truncf-sse4_1.S: Likewise.
    	* sysdeps/x86_64/fpu/multiarch/s_truncf.c: Likewise.

---
+++ sysdeps/x86_64/fpu/multiarch/s_trunc.c
@@ -0,0 +1,29 @@
+/* Multiple versions of __trunc.
+   Copyright (C) 2017 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#define trunc __redirect_trunc
+#define __trunc __redirect___trunc
+#include <math.h>
+#undef trunc
+#undef __trunc
+
+#define SYMBOL_NAME trunc
+#include "ifunc-sse4_1.h"
+
+libc_ifunc_redirected (__redirect_trunc, __trunc, IFUNC_SELECTOR ());
+weak_alias (__trunc, trunc)
