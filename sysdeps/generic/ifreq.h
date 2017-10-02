b4744101f36021bf2dde713682abbd0cc6935168
H.J. Lu <hjl.tools@gmail.com>
2017-10-01 17:35:26 -0700

    Hide internal __ifreq function [BZ #18822]
    Hide internal __ifreq function to allow direct access within libc.so and
    libc.a without using GOT nor PLT.
    
    	[BZ #18822]
    	* include/ifreq.h: New file.
    	* sysdeps/generic/ifreq.h (__if_nextreq): Removed.
    	(__ifreq): Likewise.
    	* sysdeps/mach/hurd/ifreq.h (__if_nextreq): Removed.
    	(__ifreq): Likewise.

--- sysdeps/generic/ifreq.h
+++ sysdeps/generic/ifreq.h
@@ -23,18 +23,6 @@
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 
-static inline struct ifreq *
-__if_nextreq (struct ifreq *ifr)
-{
-#ifdef _HAVE_SA_LEN
-  if (ifr->ifr_addr.sa_len > sizeof ifr->ifr_addr)
-    return (struct ifreq *) ((char *) &ifr->ifr_addr + ifr->ifr_addr.sa_len);
-#endif
-  return ifr + 1;
-}
-
-extern void __ifreq (struct ifreq **ifreqs, int *num_ifs, int sockfd);
-
 
 static inline void
 __if_freereq (struct ifreq *ifreqs, int num_ifs)
--- sysdeps/mach/hurd/ifreq.h
+++ sysdeps/mach/hurd/ifreq.h
@@ -24,18 +24,6 @@
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 
-static inline struct ifreq *
-__if_nextreq (struct ifreq *ifr)
-{
-#ifdef _HAVE_SA_LEN
-  if (ifr->ifr_addr.sa_len > sizeof ifr->ifr_addr)
-    return (struct ifreq *) ((char *) &ifr->ifr_addr + ifr->ifr_addr.sa_len);
-#endif
-  return ifr + 1;
-}
-
-extern void __ifreq (struct ifreq **ifreqs, int *num_ifs, int sockfd);
-
 
 static inline void
 __if_freereq (struct ifreq *ifreqs, int num_ifs)
