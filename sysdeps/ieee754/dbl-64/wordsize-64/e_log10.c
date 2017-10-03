8f8f8ef7aba40ef883291e4c4d95a419c3327d70
Szabolcs Nagy <szabolcs.nagy@arm.com>
2017-10-03 17:13:18 +0100

    [BZ #22243] fix log2(0) and log(10) in downward rounding
    On 64bit targets if the SVID compat wrapper is suppressed (e.g. static linking)
    then log2(0) and log10(0) returned inf instead of -inf.
    
    	[BZ #22243]
    	* sysdeps/ieee754/dbl-64/wordsize-64/e_log10.c (__ieee754_log10): Use fabs.
    	* sysdeps/ieee754/dbl-64/wordsize-64/e_log2.c (__ieee754_log2): Likewise.

--- sysdeps/ieee754/dbl-64/wordsize-64/e_log10.c
+++ sysdeps/ieee754/dbl-64/wordsize-64/e_log10.c
@@ -65,7 +65,7 @@ __ieee754_log10 (double x)
   if (hx < INT64_C(0x0010000000000000))
     {				/* x < 2**-1022  */
       if (__glibc_unlikely ((hx & UINT64_C(0x7fffffffffffffff)) == 0))
-	return -two54 / (x - x);	/* log(+-0)=-inf */
+	return -two54 / fabs (x);	/* log(+-0)=-inf */
       if (__glibc_unlikely (hx < 0))
 	return (x - x) / (x - x);	/* log(-#) = NaN */
       k -= 54;
