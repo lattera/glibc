6dff1983695fe0a5dc80e41edfa4b0dac9fecac2
Joseph Myers <joseph@codesourcery.com>
2017-10-06 15:02:12 +0000

    Remove redundant ldbl-64-128 files.
    Various source files in ldbl-64-128 are redundant, because they wrap
    files that no longer provide public symbols that need special
    versioning (those symbols having moved to separate errno-setting
    wrappers), or, in the case of w_scalblnl.c, because the type-generic
    template now does everything required (it deals with symbol versioning
    for use in libm, and this file is never built for libc anyway - the
    compat scalbln* symbols in libc, as opposed to scalbn*, are only for
    i386 and m68k and are aliases to the corresponding scalbn* symbols).
    This patch removes those redundant files.
    
    Tested with build-many-glibcs.py (for all ldbl-64-128 configurations)
    that installed stripped shared libraries are unchanged by this patch.
    
    	* sysdeps/ieee754/ldbl-64-128/e_ilogbl.c: Remove file.
    	* sysdeps/ieee754/ldbl-64-128/s_log1pl.c: Likewise.
    	* sysdeps/ieee754/ldbl-64-128/s_scalblnl.c: Likewise.
    	* sysdeps/ieee754/ldbl-64-128/s_scalbnl.c: Likewise.
    	* sysdeps/ieee754/ldbl-64-128/w_scalblnl.c: Likewise.

--- sysdeps/ieee754/ldbl-64-128/s_log1pl.c
+++
@@ -1,2 +0,0 @@
-#include <math_ldbl_opt.h>
-#include <sysdeps/ieee754/ldbl-128/s_log1pl.c>
--- sysdeps/ieee754/ldbl-64-128/s_scalbnl.c
+++
@@ -1,4 +0,0 @@
-#include <math_ldbl_opt.h>
-#undef weak_alias
-#define weak_alias(n,a)
-#include <sysdeps/ieee754/ldbl-128/s_scalbnl.c>
