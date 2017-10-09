c7509db215198d3cc460f9291d976d9d9b3102cb
Joseph Myers <joseph@codesourcery.com>
2017-10-09 16:52:56 +0000

    Remove ldbl-opt w_scalbln.c.
    The ldbl-opt version of w_scalbln.c is not in fact needed; it handles
    compat symbol versions for libc, but this file isn't built for libc,
    only for libm.  This patch removes this file.
    
    Tested with build-many-glibcs.py that installed stripped shared
    libraries are unchanged by this patch.
    
    	* sysdeps/ieee754/ldbl-opt/w_scalbln.c: Remove file.

--- sysdeps/ieee754/ldbl-opt/w_scalbln.c
+++
@@ -1,5 +0,0 @@
-#include <math-type-macros-double.h>
-#include <w_scalbln_template.c>
-#if IS_IN (libc) && LONG_DOUBLE_COMPAT (libc, GLIBC_2_1)
-compat_symbol (libc, __w_scalbln, scalblnl, GLIBC_2_1);
-#endif
