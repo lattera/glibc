0ccc7153ccd8fbecefb0793a65cd5a85cdb099fc
H.J. Lu <hjl.tools@gmail.com>
2017-10-22 08:03:46 -0700

    x86-64: Add logf with FMA
    For workload-spec2017.wrf, on Skylake, it improves performance by:
    
                               Before            After     Improvement
    reciprocal-throughput      16.1534          13.8874       16%
    latency                    41.9642          34.3072       22%
    
    	* sysdeps/x86_64/fpu/multiarch/Makefile (libm-sysdep_routines):
    	Add e_logf-fma.
    	(CFLAGS-e_logf-fma.c): New.
    	* sysdeps/x86_64/fpu/multiarch/e_logf-fma.c: New file.
    	* sysdeps/x86_64/fpu/multiarch/e_logf.c: Likewise.

---
+++ sysdeps/x86_64/fpu/multiarch/e_logf.c
@@ -0,0 +1,40 @@
+/* Multiple versions of logf.
+   Copyright (C) 2017 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+extern float __redirect_logf (float);
+
+#define SYMBOL_NAME logf
+#include "ifunc-fma.h"
+
+libc_ifunc_redirected (__redirect_logf, __logf, IFUNC_SELECTOR ());
+
+#ifdef SHARED
+__hidden_ver1 (__logf, __GI___logf, __redirect_logf)
+  __attribute__ ((visibility ("hidden")));
+
+# include <shlib-compat.h>
+versioned_symbol (libm, __logf, logf, GLIBC_2_27);
+#else
+weak_alias (__logf, logf)
+#endif
+
+strong_alias (__logf, __ieee754_logf)
+strong_alias (__logf, __logf_finite)
+
+#define __logf __logf_sse2
+#include <sysdeps/ieee754/flt-32/e_logf.c>
