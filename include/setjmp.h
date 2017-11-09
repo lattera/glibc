8cb5295d3f642af43694ba39bc56c8575380aeb3
H.J. Lu <hjl.tools@gmail.com>
2017-11-09 05:09:49 -0800

    Add jmp_buf-macros.h
    Verify that sizes, alignments and field offsets of jmp_buf as well as
    sigjmp_buf are unchanged regardless how struct __jmp_buf_tag is defined.
    Since jmp_buf is target specific, jmp_buf-macros.h is added for each
    Linux target.  A new target must provides its own jmp_buf-macros.h.
    
    TODO: Hurd needs to provide a jmp_buf-macros.h.
    
    Tested with build-many-glibcs.py.
    
    	* include/setjmp.h [!_ISOMAC]: Include <stddef.h> and
    	<jmp_buf-macros.h>.
    	[!_ISOMAC] (STR_HELPER): New.
    	[!_ISOMAC] (STR): Likewise.
    	[!_ISOMAC] (TEST_SIZE): Likewise.
    	[!_ISOMAC] (TEST_ALIGN): Likewise.
    	[!_ISOMAC] (TEST_OFFSET): Likewise.
    	[!_ISOMAC] Add _Static_assert to check sizes, alignments and
    	field offsets of jmp_buf as well as sigjmp_buf.
    	* sysdeps/unix/sysv/linux/aarch64/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/alpha/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/arm/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/hppa/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/i386/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/ia64/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/m68k/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/microblaze/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/mips/mips32/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/mips/mips64/n32/jmp_buf-macros.h:
    	Likewise.
    	* sysdeps/unix/sysv/linux/mips/mips64/n64/jmp_buf-macros.h:
    	Likewise.
    	* sysdeps/unix/sysv/linux/nios2/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/powerpc/powerpc32/jmp_buf-macros.h:
    	Likewise.
    	* sysdeps/unix/sysv/linux/powerpc/powerpc64/jmp_buf-macros.h:
    	Likewise.
    	* sysdeps/unix/sysv/linux/s390/s390-32/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/s390/s390-64/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/sh/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/sparc/sparc32/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/sparc/sparc64/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/tile/tilegx/tilegx32/jmp_buf-macros.h:
    	Likewise.
    	* sysdeps/unix/sysv/linux/tile/tilegx/tilegx64/jmp_buf-macros.h:
    	Likewise.
    	* sysdeps/unix/sysv/linux/tile/tilepro/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/x86_64/64/jmp_buf-macros.h: Likewise.
    	* sysdeps/unix/sysv/linux/x86_64/x32/jmp_buf-macros.h: Likewise.

--- include/setjmp.h
+++ include/setjmp.h
@@ -31,6 +31,40 @@ libc_hidden_proto (__sigsetjmp)
 # if IS_IN (rtld) && !defined NO_RTLD_HIDDEN
 extern __typeof (__sigsetjmp) __sigsetjmp attribute_hidden;
 # endif
+
+/* Check jmp_buf sizes, alignments and offsets.  */
+# include <stddef.h>
+# include <jmp_buf-macros.h>
+
+# define STR_HELPER(x) #x
+# define STR(x) STR_HELPER(x)
+
+# define TEST_SIZE(type, size) \
+  _Static_assert (sizeof (type) == size, \
+		  "size of " #type " != " \
+		  STR (size))
+# define TEST_ALIGN(type, align) \
+  _Static_assert (__alignof__ (type) == align , \
+		  "align of " #type " != " \
+		  STR (align))
+# define TEST_OFFSET(type, member, offset) \
+  _Static_assert (offsetof (type, member) == offset, \
+		  "offset of " #member " field of " #type " != " \
+		  STR (offset))
+
+/* Check if jmp_buf have the expected sizes.  */
+TEST_SIZE (jmp_buf, JMP_BUF_SIZE);
+TEST_SIZE (sigjmp_buf, SIGJMP_BUF_SIZE);
+
+/* Check if jmp_buf have the expected alignments.  */
+TEST_ALIGN (jmp_buf, JMP_BUF_ALIGN);
+TEST_ALIGN (sigjmp_buf, SIGJMP_BUF_ALIGN);
+
+/* Check if internal fields in jmp_buf have the expected offsets.  */
+TEST_OFFSET (struct __jmp_buf_tag, __mask_was_saved,
+	     MASK_WAS_SAVED_OFFSET);
+TEST_OFFSET (struct __jmp_buf_tag, __saved_mask,
+	     SAVED_MASK_OFFSET);
 #endif
 
 #endif
