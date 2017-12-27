7e21972bde2b92de5862dcf373dcc5efe2a66e0a
Dmitry V. Levin <ldv@altlinux.org>
2017-12-27 00:54:38 +0000

    linux: move a shared part of sys/ptrace.h to bits/ptrace-shared.h
    Move a shared part of sys/ptrace.h which is the same on all
    architectures to a separate file.
    
    * sysdeps/unix/sysv/linux/sys/ptrace.h: Include <bits/ptrace-shared.h>.
    (__ptrace_setoptions, __ptrace_eventcodes, __ptrace_peeksiginfo_args,
    __ptrace_peeksiginfo_flags, ptrace): Move to ...
    * sysdeps/unix/sysv/linux/bits/ptrace-shared.h: ... new file.
    * sysdeps/unix/sysv/linux/Makefile (sysdep_headers): Add
    bits/ptrace-shared.h.
    * sysdeps/unix/sysv/linux/aarch64/sys/ptrace.h: Include
    <bits/ptrace-shared.h>.
    (__ptrace_setoptions, __ptrace_eventcodes, __ptrace_peeksiginfo_args,
    __ptrace_peeksiginfo_flags, ptrace): Remove.
    * sysdeps/unix/sysv/linux/ia64/sys/ptrace.h: Likewise.
    * sysdeps/unix/sysv/linux/powerpc/sys/ptrace.h: Likewise.
    * sysdeps/unix/sysv/linux/s390/sys/ptrace.h: Likewise.
    * sysdeps/unix/sysv/linux/sparc/sys/ptrace.h: Likewise.
    * sysdeps/unix/sysv/linux/tile/sys/ptrace.h: Likewise.

--- sysdeps/unix/sysv/linux/tile/sys/ptrace.h
+++ sysdeps/unix/sysv/linux/tile/sys/ptrace.h
@@ -1,4 +1,5 @@
-/* Copyright (C) 2011-2017 Free Software Foundation, Inc.
+/* `ptrace' debugger support interface.  Linux/Tile version.
+   Copyright (C) 2011-2017 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Chris Metcalf <cmetcalf@tilera.com>, 2011.
 
@@ -136,60 +137,7 @@ enum __ptrace_request
 };
 
 
-/* Options set using PTRACE_SETOPTIONS.  */
-enum __ptrace_setoptions
-{
-  PTRACE_O_TRACESYSGOOD	= 0x00000001,
-  PTRACE_O_TRACEFORK	= 0x00000002,
-  PTRACE_O_TRACEVFORK   = 0x00000004,
-  PTRACE_O_TRACECLONE	= 0x00000008,
-  PTRACE_O_TRACEEXEC	= 0x00000010,
-  PTRACE_O_TRACEVFORKDONE = 0x00000020,
-  PTRACE_O_TRACEEXIT	= 0x00000040,
-  PTRACE_O_TRACESECCOMP = 0x00000080,
-  PTRACE_O_EXITKILL	= 0x00100000,
-  PTRACE_O_SUSPEND_SECCOMP	= 0x00200000,
-  PTRACE_O_MASK		= 0x003000ff
-};
-
-enum __ptrace_eventcodes
-{
-/* Wait extended result codes for the above trace options.  */
-  PTRACE_EVENT_FORK	= 1,
-  PTRACE_EVENT_VFORK	= 2,
-  PTRACE_EVENT_CLONE	= 3,
-  PTRACE_EVENT_EXEC	= 4,
-  PTRACE_EVENT_VFORK_DONE = 5,
-  PTRACE_EVENT_EXIT	= 6,
-  PTRACE_EVENT_SECCOMP  = 7,
-/* Extended result codes enabled by means other than options.  */
-  PTRACE_EVENT_STOP	= 128
-};
-
-/* Arguments for PTRACE_PEEKSIGINFO.  */
-struct __ptrace_peeksiginfo_args
-{
-  __uint64_t off;	/* From which siginfo to start.  */
-  __uint32_t flags;	/* Flags for peeksiginfo.  */
-  __int32_t nr;		/* How many siginfos to take.  */
-};
-
-enum __ptrace_peeksiginfo_flags
-{
-  /* Read signals from a shared (process wide) queue.  */
-  PTRACE_PEEKSIGINFO_SHARED = (1 << 0)
-};
-
-/* Perform process tracing functions.  REQUEST is one of the values
-   above, and determines the action to be taken.
-   For all requests except PTRACE_TRACEME, PID specifies the process to be
-   traced.
-
-   PID and the other arguments described above for the various requests should
-   appear (those that are used for the particular request) as:
-     pid_t PID, void *ADDR, int DATA, void *ADDR2
-   after REQUEST.  */
-extern long int ptrace (enum __ptrace_request __request, ...) __THROW;
+#include <bits/ptrace-shared.h>
 
 __END_DECLS
 
