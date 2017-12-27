31a98837be41b03853fcc607e3a7be6b0525a910
Palmer Dabbelt <palmer@dabbelt.com>
2017-12-26 20:52:36 -0800

    Add RISC-V dynamic relocations to elf.h
    These relocations can appear in shared objects on RISC-V ELF systems.
    
    2018-01-06  Palmer Dabbelt  <palmer@sifive.com>
    
           * elf/elf.h (R_RISCV_NONE): New define.
           (R_RISCV_32): Likewise.
           (R_RISCV_64): Likewise.
           (R_RISCV_RELATIVE): Likewise.
           (R_RISCV_COPY): Likewise.
           (R_RISCV_JUMP_SLOT): Likewise.
           (R_RISCV_TLS_DTPMOD32): Likewise.
           (R_RISCV_TLS_DTPMOD64): Likewise.
           (R_RISCV_TLS_DTPREL32): Likewise.
           (R_RISCV_TLS_DTPREL64): Likewise.
           (R_RISCV_TLS_TPREL32): Likewise.
           (R_RISCV_TLS_TPREL64): Likewise.

--- elf/elf.h
+++ elf/elf.h
@@ -3762,6 +3762,20 @@ enum
 
 #define R_TILEGX_NUM		130
 
+/* RISC-V relocations.  */
+#define R_RISCV_NONE          0
+#define R_RISCV_32            1
+#define R_RISCV_64            2
+#define R_RISCV_RELATIVE      3
+#define R_RISCV_COPY          4
+#define R_RISCV_JUMP_SLOT     5
+#define R_RISCV_TLS_DTPMOD32  6
+#define R_RISCV_TLS_DTPMOD64  7
+#define R_RISCV_TLS_DTPREL32  8
+#define R_RISCV_TLS_DTPREL64  9
+#define R_RISCV_TLS_TPREL32  10
+#define R_RISCV_TLS_TPREL64  11
+
 /* BPF specific declarations.  */
 
 #define R_BPF_NONE		0	/* No reloc */
