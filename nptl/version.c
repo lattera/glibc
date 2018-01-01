09533208febe923479261a27b7691abef297d604
Joseph Myers <joseph@codesourcery.com>
2018-01-01 00:41:16 +0000

    Update copyright dates not handled by scripts/update-copyrights.
    I've updated copyright dates in glibc for 2018.  This is the patch for
    the changes not generated by scripts/update-copyrights and subsequent
    build / regeneration of generated files.
    
    Please remember to include 2018 in the dates for any new files added
    in future (which means updating any existing uncommitted patches you
    have that add new files to use the new copyright dates in them).
    
    	* NEWS: Update copyright dates.
    	* catgets/gencat.c (print_version): Likewise.
    	* csu/version.c (banner): Likewise.
    	* debug/catchsegv.sh: Likewise.
    	* debug/pcprofiledump.c (print_version): Likewise.
    	* debug/xtrace.sh (do_version): Likewise.
    	* elf/ldconfig.c (print_version): Likewise.
    	* elf/ldd.bash.in: Likewise.
    	* elf/pldd.c (print_version): Likewise.
    	* elf/sotruss.sh: Likewise.
    	* elf/sprof.c (print_version): Likewise.
    	* iconv/iconv_prog.c (print_version): Likewise.
    	* iconv/iconvconfig.c (print_version): Likewise.
    	* locale/programs/locale.c (print_version): Likewise.
    	* locale/programs/localedef.c (print_version): Likewise.
    	* login/programs/pt_chown.c (print_version): Likewise.
    	* malloc/memusage.sh (do_version): Likewise.
    	* malloc/memusagestat.c (print_version): Likewise.
    	* malloc/mtrace.pl: Likewise.
    	* manual/libc.texinfo: Likewise.
    	* nptl/version.c (banner): Likewise.
    	* nscd/nscd.c (print_version): Likewise.
    	* nss/getent.c (print_version): Likewise.
    	* nss/makedb.c (print_version): Likewise.
    	* posix/getconf.c (main): Likewise.
    	* scripts/test-installation.pl: Likewise.
    	* sysdeps/unix/sysv/linux/lddlibc4.c (main): Likewise.

--- nptl/version.c
+++ nptl/version.c
@@ -23,7 +23,7 @@
 
 static const char banner[] =
 "Native POSIX Threads Library\n\
-Copyright (C) 2017 Free Software Foundation, Inc.\n\
+Copyright (C) 2018 Free Software Foundation, Inc.\n\
 This is free software; see the source for copying conditions.\n\
 There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A\n\
 PARTICULAR PURPOSE.\n"
