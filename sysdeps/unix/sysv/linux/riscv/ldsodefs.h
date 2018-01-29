ee53fed9d8aee94d5980a36aab28889d93a4c49d
Palmer Dabbelt <palmer@dabbelt.com>
2018-01-29 10:29:13 -0800

    RISC-V: Linux Startup and Dynamic Loading Code
    This contains the Linux-specific code for loading programs on RISC-V.
    
    2018-01-29  Palmer Dabbelt  <palmer@sifive.com>
    
            * sysdeps/unix/sysv/linux/riscv/dl-static.c: New file.
            * sysdeps/unix/sysv/linux/riscv/ldconfig.h: Likewise.
            * sysdeps/unix/sysv/linux/riscv/ldsodefs.h: Likewise.

---
+++ sysdeps/unix/sysv/linux/riscv/ldsodefs.h
@@ -0,0 +1,32 @@
+/* RISC-V dynamic linker data structures for loaded ELF shared objects.
+   Copyright (C) 2001-2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#ifndef	_LDSODEFS_H
+
+/* Get the real definitions.  */
+#include_next <ldsodefs.h>
+
+/* Now define our stuff.  */
+
+/* We need special support to initialize DSO loaded for statically linked
+   binaries.  */
+extern void _dl_static_init (struct link_map *map);
+#undef DL_STATIC_INIT
+#define DL_STATIC_INIT(map) _dl_static_init (map)
+
+#endif /* ldsodefs.h */
