9a123ff05d624f429aa31fce10a8276a52a11f0d
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-01-30 01:17:51 +0100

    hurd: include generic's hp-timing.h instead of copying it
    * sysdeps/mach/hurd/hp-timing.h: include <sysdeps/generic/hp-timing.h>
    instead of copying it.

--- sysdeps/mach/hurd/hp-timing.h
+++ sysdeps/mach/hurd/hp-timing.h
@@ -17,22 +17,5 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
-#ifndef _HP_TIMING_H
-#define _HP_TIMING_H	1
-
 /* We don't have support for high precision timing for now. */
-
-/* Provide dummy definitions.  */
-#define HP_TIMING_AVAIL		(0)
-#define HP_SMALL_TIMING_AVAIL	(0)
-#define HP_TIMING_INLINE	(0)
-typedef int hp_timing_t;
-#define HP_TIMING_NOW(var)
-#define HP_TIMING_DIFF(Diff, Start, End)
-#define HP_TIMING_ACCUM_NT(Sum, Diff)
-#define HP_TIMING_PRINT(Buf, Len, Val)
-
-/* Since this implementation is not available we tell the user about it.  */
-#define HP_TIMING_NONAVAIL	1
-
-#endif	/* hp-timing.h */
+#include <sysdeps/generic/hp-timing.h>
