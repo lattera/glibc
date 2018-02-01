23158b08a0908f381459f273a984c6fd328363cb
Dmitry V. Levin <ldv@altlinux.org>
2018-02-01 16:17:18 +0000

    Update for 2.27 release

--- include/features.h
+++ include/features.h
@@ -417,7 +417,7 @@
 /* Major and minor version number of the GNU C library package.  Use
    these macros to test for features in specific releases.  */
 #define	__GLIBC__	2
-#define	__GLIBC_MINOR__	26
+#define	__GLIBC_MINOR__	27
 
 #define __GLIBC_PREREQ(maj, min) \
 	((__GLIBC__ << 16) + __GLIBC_MINOR__ >= ((maj) << 16) + (min))
