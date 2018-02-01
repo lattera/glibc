eb65a3d545f9820853b454ce342385799dd479e9
Joseph Myers <joseph@codesourcery.com>
2018-02-01 20:58:57 +0000

    Remove some math_private.h libc_feholdexcept_setround overrides.
    math_private.h headers for configurations lacking support for
    floating-point exceptions and rounding modes define
    libc_feholdexcept_setround to override the default version with one
    that discards its rounding mode argument.
    
    Unlike other such libc_fe* macros that I removed, this one is actually
    used for such configurations (in dbl-64/e_sqrt.c).  However, this does
    not make the macro required.  It's only used for such configurations
    with FE_TONEAREST as the rounding mode (anything needing another mode
    should not be used when that mode is unavailable), and the default
    definition just calls __feholdexcept and __fesetround.  Since we now
    have suitable inline do-nothing definitions of __feholdexcept and
    __fesetround for the cases of no exceptions and rounding modes, we can
    just rely on those inlines to achieve the same optimization as this
    macro definition.  Thus, this patch removes those macro definitions
    (and the math_private.h headers containing them, when no longer needed
    after that removal).
    
    Tested with build-many-glibcs.py that installed stripped shared
    libraries are unchanged by the patch.
    
    	* sysdeps/m68k/coldfire/fpu/math_private.h: Move to ....
    	* sysdeps/m68k/coldfire/math_private.h: ... here.
    	* sysdeps/m68k/coldfire/nofpu/math_private.h: Remove file.
    	* sysdeps/tile/math_private.h: Likewise.
    	* sysdeps/microblaze/math_private.h (libc_feholdexcept_setround):
    	Remove macro.
    	* sysdeps/nios2/math_private.h (libc_feholdexcept_setround):
    	Likewise.

--- sysdeps/nios2/math_private.h
+++ sysdeps/nios2/math_private.h
@@ -1,25 +1,6 @@
 #ifndef NIO2_MATH_PRIVATE_H
 #define NIO2_MATH_PRIVATE_H 1
 
-/* Suppress use of exceptions here to avoid build errors if the FE_*
-   macros aren't defined. Only allow rounding modes implemented for Nios II.
-
-   This does mean that some code will silently fail to report exceptions,
-   set rounding mode as expected, etc., but it allows math code to compile
-   that otherwise wouldn't (such as math/s_fma.c) and so is valuable.
-
-   We intentionally ignore the "exception" arguments of functions that
-   take an exception, since we can't even evaluate the argument
-   without causing a build failure.  The extra level of statement
-   expression wrapping avoids "statement with no effect" warnings.
-   Since the callers don't check for errors anyway, we just claim
-   success in every case.
-
-   The overrides for libc_ functions must happen before we include
-   the generic math_private.h.  */
-
-#define libc_feholdexcept_setround(env, exc)   ({ (void) (env); 0; })
-
 /* Enable __finitel, __isinfl, and __isnanl for binary compatibility
    when built without long double support. */
 #define LDBL_CLASSIFY_COMPAT 1
