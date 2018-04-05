08e92e2443a6ca4cfd45b87e22bef3454f369752
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-04-05 09:38:58 +0200

    Revert "hurd: Avoid PLTs for longjmp & siglongjmp"
    This reverts commit e5f9508a32426b7c3948dc741d84355071ed9539.

--- setjmp/longjmp.c
+++ setjmp/longjmp.c
@@ -45,7 +45,5 @@ __libc_siglongjmp (sigjmp_buf env, int val)
 strong_alias (__libc_siglongjmp, __libc_longjmp)
 weak_alias (__libc_siglongjmp, _longjmp)
 weak_alias (__libc_siglongjmp, longjmp)
-libc_hidden_def (longjmp)
 weak_alias (__libc_siglongjmp, siglongjmp)
-libc_hidden_def (siglongjmp)
 #endif
