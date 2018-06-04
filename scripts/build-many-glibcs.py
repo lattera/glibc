ab3a0da0b59d4a2c5be70c8d17437c0bad1a6230
Joseph Myers <joseph@codesourcery.com>
2018-06-04 17:11:11 +0000

    Use Linux 4.17 in build-many-glibcs.py.
    	* scripts/build-many-glibcs.py (Context.checkout): Default Linux
    	version to 4.17

--- scripts/build-many-glibcs.py
+++ scripts/build-many-glibcs.py
@@ -709,7 +709,7 @@ class Context(object):
                             'gcc': 'vcs-8',
                             'glibc': 'vcs-mainline',
                             'gmp': '6.1.2',
-                            'linux': '4.16',
+                            'linux': '4.17',
                             'mpc': '1.1.0',
                             'mpfr': '4.0.1',
                             'mig': 'vcs-mainline',
