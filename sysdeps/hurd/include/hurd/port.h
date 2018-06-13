c8c6e6d6d0859e5e9b3c6f8b3daa4e6b6e709873
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-06-14 00:28:03 +0200

    hurd: Avoid PLTs for _hurd_port_locked_get/set
    	* sysdeps/hurd/include/hurd/port.h: New file.

---
+++ sysdeps/hurd/include/hurd/port.h
@@ -0,0 +1,9 @@
+#ifndef	_HURD_PORT_H
+#include_next <hurd/port.h>
+libc_hidden_proto (_hurd_port_locked_get)
+libc_hidden_proto (_hurd_port_locked_set)
+#ifdef _HURD_PORT_H_HIDDEN_DEF
+libc_hidden_def (_hurd_port_locked_get)
+libc_hidden_def (_hurd_port_locked_set)
+#endif
+#endif
