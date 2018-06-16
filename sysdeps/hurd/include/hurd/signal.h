4d0ac0375ba54da7d36aae6b7932e8813b2c89af
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-06-16 14:07:23 +0000

    hurd: Fix reference to _hurd_self_sigstate
    	* sysdeps/hurd/include/hurd/signal.h (_hurd_self_sigstate): Add hidden
    	prototype and definition.
    	* sysdeps/mach/hurd/i386/____longjmp_chk.S (____longjmp_chk): Use
    	hidden target for _hurd_self_sigstate.

--- sysdeps/hurd/include/hurd/signal.h
+++ sysdeps/hurd/include/hurd/signal.h
@@ -1,9 +1,18 @@
 #ifndef	_HURD_SIGNAL_H
+extern struct hurd_sigstate *_hurd_self_sigstate (void) __attribute__ ((__const__));
+#ifndef _ISOMAC
+libc_hidden_proto (_hurd_self_sigstate)
+#endif
+
 #include_next <hurd/signal.h>
+
 #ifndef _ISOMAC
 libc_hidden_proto (_hurd_exception2signal)
 libc_hidden_proto (_hurd_intr_rpc_mach_msg)
 libc_hidden_proto (_hurd_thread_sigstate)
 libc_hidden_proto (_hurd_raise_signal)
+#endif
+#ifdef _HURD_SIGNAL_H_HIDDEN_DEF
+libc_hidden_def (_hurd_self_sigstate)
 #endif
 #endif
