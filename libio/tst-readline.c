4fa34da6793c442255fc003cf659c85b197ab29a
Florian Weimer <fweimer@redhat.com>
2018-07-10 11:03:08 +0200

    Fix copyright years in recent commits

--- libio/tst-readline.c
+++ libio/tst-readline.c
@@ -1,5 +1,5 @@
 /* Test the __libc_readline_unlocked function.
-   Copyright (C) 2017 Free Software Foundation, Inc.
+   Copyright (C) 2018 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
--- nss/tst-nss-files-hosts-getent.c
+++ nss/tst-nss-files-hosts-getent.c
@@ -1,5 +1,5 @@
 /* Enumerate /etc/hosts with a long line (bug 18991).
-   Copyright (C) 2017 Free Software Foundation, Inc.
+   Copyright (C) 2018 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
