ea5c662c623125ad39fc43d5b9224150256407a9
Wilco Dijkstra <wdijkstr@arm.com>
2018-08-10 17:31:30 +0100

    Improve performance of sincosf
    This patch is a complete rewrite of sincosf.  The new version is
    significantly faster, as well as simple and accurate.
    The worst-case ULP is 0.5607, maximum relative error is 0.5303 * 2^-23 over
    all 4 billion inputs.  In non-nearest rounding modes the error is 1ULP.
    
    The algorithm uses 3 main cases: small inputs which don't need argument
    reduction, small inputs which need a simple range reduction and large inputs
    requiring complex range reduction.  The code uses approximate integer
    comparisons to quickly decide between these cases.
    
    The small range reducer uses a single reduction step to handle values up to
    120.0.  It is fastest on targets which support inlined round instructions.
    
    The large range reducer uses integer arithmetic for simplicity.  It does a
    32x96 bit multiply to compute a 64-bit modulo result.  This is more than
    accurate enough to handle the worst-case cancellation for values close to
    an integer multiple of PI/4.  It could be further optimized, however it is
    already much faster than necessary.
    
    sincosf throughput gains on Cortex-A72:
    * |x| < 0x1p-12 : 1.6x
    * |x| < M_PI_4  : 1.7x
    * |x| < 2 * M_PI: 1.5x
    * |x| < 120.0   : 1.8x
    * |x| < Inf     : 2.3x
    
    	* math/Makefile: Add s_sincosf_data.c.
    	* sysdeps/ia64/fpu/s_sincosf_data.c: New file.
    	* sysdeps/ieee754/flt-32/s_sincosf.h (abstop12): Add new function.
    	(sincosf_poly): Likewise.
    	(reduce_small): Likewise.
    	(reduce_large): Likewise.
    	* sysdeps/ieee754/flt-32/s_sincosf.c (sincosf): Rewrite.
    	* sysdeps/ieee754/flt-32/s_sincosf_data.c: New file with sincosf data.
    	* sysdeps/m68k/m680x0/fpu/s_sincosf_data.c: New file.
    	* sysdeps/x86_64/fpu/s_sincosf_data.c: New file.

--- sysdeps/ieee754/flt-32/s_sincosf.c
+++ sysdeps/ieee754/flt-32/s_sincosf.c
@@ -1,5 +1,5 @@
 /* Compute sine and cosine of argument.
-   Copyright (C) 2017-2018 Free Software Foundation, Inc.
+   Copyright (C) 2018 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -17,9 +17,11 @@
    <http://www.gnu.org/licenses/>.  */
 
 #include <errno.h>
+#include <stdint.h>
 #include <math.h>
-#include <math_private.h>
+#include <math-barriers.h>
 #include <libm-alias-float.h>
+#include "math_config.h"
 #include "s_sincosf.h"
 
 #ifndef SINCOSF
@@ -28,141 +30,71 @@
 # define SINCOSF_FUNC SINCOSF
 #endif
 
+/* Fast sincosf implementation.  Worst-case ULP is 0.5607, maximum relative
+   error is 0.5303 * 2^-23.  A single-step range reduction is used for
+   small values.  Large inputs have their range reduced using fast integer
+   arithmetic.  */
 void
-SINCOSF_FUNC (float x, float *sinx, float *cosx)
+SINCOSF_FUNC (float y, float *sinp, float *cosp)
 {
-  double cx;
-  double theta = x;
-  double abstheta = fabs (theta);
-  /* If |x|< Pi/4.  */
-  if (isless (abstheta, M_PI_4))
+  double x = y;
+  double s;
+  int n;
+  const sincos_t *p = &__sincosf_table[0];
+
+  if (abstop12 (y) < abstop12 (pio4))
     {
-      if (abstheta >= 0x1p-5) /* |x| >= 2^-5.  */
-	{
-	  const double theta2 = theta * theta;
-	  /* Chebyshev polynomial of the form for sin and cos.  */
-	  cx = C3 + theta2 * C4;
-	  cx = C2 + theta2 * cx;
-	  cx = C1 + theta2 * cx;
-	  cx = C0 + theta2 * cx;
-	  cx = 1.0 + theta2 * cx;
-	  *cosx = cx;
-	  cx = S3 + theta2 * S4;
-	  cx = S2 + theta2 * cx;
-	  cx = S1 + theta2 * cx;
-	  cx = S0 + theta2 * cx;
-	  cx = theta + theta * theta2 * cx;
-	  *sinx = cx;
-	}
-      else if (abstheta >= 0x1p-27)     /* |x| >= 2^-27.  */
-	{
-	  /* A simpler Chebyshev approximation is close enough for this range:
-	     for sin: x+x^3*(SS0+x^2*SS1)
-	     for cos: 1.0+x^2*(CC0+x^3*CC1).  */
-	  const double theta2 = theta * theta;
-	  cx = CC0 + theta * theta2 * CC1;
-	  cx = 1.0 + theta2 * cx;
-	  *cosx = cx;
-	  cx = SS0 + theta2 * SS1;
-	  cx = theta + theta * theta2 * cx;
-	  *sinx = cx;
-	}
-      else
-	{
-	  /* Handle some special cases.  */
-	  if (theta)
-	    *sinx = theta - (theta * SMALL);
-	  else
-	    *sinx = theta;
-	  *cosx = 1.0 - abstheta;
-	}
+      double x2 = x * x;
+
+      if (__glibc_unlikely (abstop12 (y) < abstop12 (0x1p-12f)))
+      {
+	/* Force underflow for tiny y.  */
+	if (__glibc_unlikely (abstop12 (y) < abstop12 (0x1p-126f)))
+	  math_force_eval ((float)x2);
+	*sinp = y;
+	*cosp = 1.0f;
+	return;
+      }
+
+      sincosf_poly (x, x2, p, 0, sinp, cosp);
     }
-  else                          /* |x| >= Pi/4.  */
+  else if (abstop12 (y) < abstop12 (120.0f))
     {
-      unsigned int signbit = isless (x, 0);
-      if (isless (abstheta, 9 * M_PI_4))        /* |x| < 9*Pi/4.  */
-	{
-	  /* There are cases where FE_UPWARD rounding mode can
-	     produce a result of abstheta * inv_PI_4 == 9,
-	     where abstheta < 9pi/4, so the domain for
-	     pio2_table must go to 5 (9 / 2 + 1).  */
-	  unsigned int n = (abstheta * inv_PI_4) + 1;
-	  theta = abstheta - pio2_table[n / 2];
-	  *sinx = reduced_sin (theta, n, signbit);
-	  *cosx = reduced_cos (theta, n);
-	}
-      else if (isless (abstheta, INFINITY))
-	{
-	  if (abstheta < 0x1p+23)     /* |x| < 2^23.  */
-	    {
-	      unsigned int n = ((unsigned int) (abstheta * inv_PI_4)) + 1;
-	      double x = n / 2;
-	      theta = (abstheta - x * PI_2_hi) - x * PI_2_lo;
-	      /* Argument reduction needed.  */
-	      *sinx = reduced_sin (theta, n, signbit);
-	      *cosx = reduced_cos (theta, n);
-	    }
-	  else                  /* |x| >= 2^23.  */
-	    {
-	      x = fabsf (x);
-	      int exponent;
-	      GET_FLOAT_WORD (exponent, x);
-	      exponent
-	        = (exponent >> FLOAT_EXPONENT_SHIFT) - FLOAT_EXPONENT_BIAS;
-	      exponent += 3;
-	      exponent /= 28;
-	      double a = invpio4_table[exponent] * x;
-	      double b = invpio4_table[exponent + 1] * x;
-	      double c = invpio4_table[exponent + 2] * x;
-	      double d = invpio4_table[exponent + 3] * x;
-	      uint64_t l = a;
-	      l &= ~0x7;
-	      a -= l;
-	      double e = a + b;
-	      l = e;
-	      e = a - l;
-	      if (l & 1)
-	        {
-	          e -= 1.0;
-	          e += b;
-	          e += c;
-	          e += d;
-	          e *= M_PI_4;
-		  *sinx = reduced_sin (e, l + 1, signbit);
-		  *cosx = reduced_cos (e, l + 1);
-	        }
-	      else
-		{
-		  e += b;
-		  e += c;
-		  e += d;
-		  if (e <= 1.0)
-		    {
-		      e *= M_PI_4;
-		      *sinx = reduced_sin (e, l + 1, signbit);
-		      *cosx = reduced_cos (e, l + 1);
-		    }
-		  else
-		    {
-		      l++;
-		      e -= 2.0;
-		      e *= M_PI_4;
-		      *sinx = reduced_sin (e, l + 1, signbit);
-		      *cosx = reduced_cos (e, l + 1);
-		    }
-		}
-	    }
-	}
-      else
-	{
-	  int32_t ix;
-	  /* High word of x.  */
-	  GET_FLOAT_WORD (ix, abstheta);
-	  /* sin/cos(Inf or NaN) is NaN.  */
-	  *sinx = *cosx = x - x;
-	  if (ix == 0x7f800000)
-	    __set_errno (EDOM);
-	}
+      x = reduce_fast (x, p, &n);
+
+      /* Setup the signs for sin and cos.  */
+      s = p->sign[n & 3];
+
+      if (n & 2)
+	p = &__sincosf_table[1];
+
+      sincosf_poly (x * s, x * x, p, n, sinp, cosp);
+    }
+  else if (__glibc_likely (abstop12 (y) < abstop12 (INFINITY)))
+    {
+      uint32_t xi = asuint (y);
+      int sign = xi >> 31;
+
+      x = reduce_large (xi, &n);
+
+      /* Setup signs for sin and cos - include original sign.  */
+      s = p->sign[(n + sign) & 3];
+
+      if ((n + sign) & 2)
+	p = &__sincosf_table[1];
+
+      sincosf_poly (x * s, x * x, p, n, sinp, cosp);
+    }
+  else
+    {
+      /* Return NaN if Inf or NaN for both sin and cos.  */
+      *sinp = *cosp = y - y;
+#if WANT_ERRNO
+      /* Needed to set errno for +-Inf, the add is a hack to work
+	 around a gcc register allocation issue: just passing y
+	 affects code generation in the fast path (PR86901).  */
+      __math_invalidf (y + y);
+#endif
     }
 }
 
