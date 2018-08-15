436e4d5b965abe592d26150cb518accf9ded8fe4
Siddhesh Poyarekar <siddhesh@sourceware.org>
2018-08-15 23:01:33 +0530

    [aarch64] Add an ASIMD variant of strlen for falkor
    This variant of strlen uses vector loads and operations to reduce the
    size of the code and also eliminate the non-ascii fallback.  This
    works very well for falkor because of its two vector units and
    efficient vector ops.  In the best case it reduces latency of cases in
    bench-strlen by 48%, with gains throughout the benchmark.
    strlen-walk also sees uniform gains in the 5%-15% range.
    
    Overall the routine appears to work better than the stock one for falkor
    regardless of the benchmark, length of string or cache state.
    
    The same cannot be said of a53 and a72 though.  a53 performance was
    greatly reduced and for a72 it was a bit of a mixed bag, slightly on the
    negative side but I reckon it might be fast in some situations.
    
    	* sysdeps/aarch64/strlen.S (__strlen): Rename to STRLEN.
    	[!STRLEN](STRLEN): Set to __strlen.
    	* sysdeps/aarch64/multiarch/strlen.c: New file.
    	* sysdeps/aarch64/multiarch/strlen_generic.S: Likewise.
    	* sysdeps/aarch64/multiarch/strlen_asimd.S: Likewise.
    	* sysdeps/aarch64/multiarch/ifunc-impl-list.c
    	(__libc_ifunc_impl_list): Add strlen.
    	* sysdeps/aarch64/multiarch/Makefile (sysdep_routines): Add
    	strlen_generic and strlen_asimd.
    
    Reviewed-By: szabolcs.nagy@arm.com
    CC: pinskia@gmail.com

--- sysdeps/aarch64/multiarch/ifunc-impl-list.c
+++ sysdeps/aarch64/multiarch/ifunc-impl-list.c
@@ -53,5 +53,9 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
 	      IFUNC_IMPL_ADD (array, i, memset, (zva_size == 64), __memset_falkor)
 	      IFUNC_IMPL_ADD (array, i, memset, 1, __memset_generic))
 
+  IFUNC_IMPL (i, name, strlen,
+	      IFUNC_IMPL_ADD (array, i, strlen, 1, __strlen_asimd)
+	      IFUNC_IMPL_ADD (array, i, strlen, 1, __strlen_generic))
+
   return i;
 }
---
+++ sysdeps/aarch64/multiarch/strlen.c
@@ -0,0 +1,41 @@
+/* Multiple versions of strlen. AARCH64 version.
+   Copyright (C) 2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+/* Define multiple versions only for the definition in libc.  */
+
+#if IS_IN (libc)
+/* Redefine strlen so that the compiler won't complain about the type
+   mismatch with the IFUNC selector in strong_alias, below.  */
+# undef strlen
+# define strlen __redirect_strlen
+# include <string.h>
+# include <init-arch.h>
+
+#define USE_ASIMD_STRLEN() IS_FALKOR (midr)
+
+extern __typeof (__redirect_strlen) __strlen;
+
+extern __typeof (__redirect_strlen) __strlen_generic attribute_hidden;
+extern __typeof (__redirect_strlen) __strlen_asimd attribute_hidden;
+
+libc_ifunc (__strlen,
+	    (USE_ASIMD_STRLEN () ? __strlen_asimd : __strlen_generic));
+
+# undef strlen
+strong_alias (__strlen, strlen);
+#endif
